bcaaaa3604c57bf3b6aa2a68f86cb2d486bf3ffc
marine.dumousseau@wanadoo.fr <marine.dumousseau@wanadoo.fr@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2010-11-26 10:25:34 +0000

    FeatureRangeRule : updated the description of the rule.
    FeatureUtils : updated the Mi numbers for 'c-terminal ranges' and 'n-terminal' ranges

--- trunk/validator/psimi-schema-validator/src/main/java/psidev/psi/mi/validator/extension/rules/FeatureRangeRule.java
+++ trunk/validator/psimi-schema-validator/src/main/java/psidev/psi/mi/validator/extension/rules/FeatureRangeRule.java
@@ -26,14 +26,16 @@ public class FeatureRangeRule extends Mi25InteractionRule {
 
         // describe the rule.
         setName( "Feature range Check" );
-        setDescription( "Checks that the each interactor's feature range is valid : not out of bound (inferior to 1 or superior to the sequence length), not overlapping and compliant with the feature range status." );
-        addTip( "When the feature range start is 'undetermined', the range position values should always be 0 or null." );
-        addTip( "When the feature range start is 'n-terminal', the range position values should always be 1 because 'n-terminal' means the first amino acid of the participant." );
-        addTip( "When the feature range start is 'c-terminal', the range position values should always be the length of the participant sequence because 'c-terminal' means the last amino " +
+        setDescription( "Checks that the each interactor's feature range is valid : not out of bound (inferior to 1 or superior to the sequence length), not overlapping and compliant with the feature range status. " +
+                "WARNING : the status 'c-terminal' and 'n-terminal' cannot be used anymore for n-terminal/c-terminal features where the exact positions are not known. It is recommended to use " +
+                "the new terms 'c-terminal range' (MI:1039) and 'n-terminal range' (MI:1040) for such features" );
+        addTip( "When the feature range status is 'undetermined', 'n-terminal range' or 'c-terminal range', the range position values should always be 0 or null." );
+        addTip( "When the feature range status is 'n-terminal', the range position values should always be 1 because 'n-terminal' means the first amino acid of the participant." );
+        addTip( "When the feature range status is 'c-terminal', the range position values should always be the length of the participant sequence because 'c-terminal' means the last amino " +
                 "acid of the participant. A 'c-terminal' position equal to null or 0 can be accepted if the sequence of the interactor is not known." );
-        addTip( "When the feature range start is 'greater-than', the range position values must be strictly inferior to the sequence length as 'greater-than' is exclusive." );
-        addTip( "When the feature range start is 'less-than', the range position values must be strictly superior to 1 as 'less-than' is exclusive." );
-        addTip( "When the feature range start is 'certain', 'range', 'greater than' or 'less than', the range values should never be inferior or equal to 0." );
+        addTip( "When the feature range status is 'greater-than', the range position values must be strictly inferior to the sequence length as 'greater-than' is exclusive." );
+        addTip( "When the feature range status is 'less-than', the range position values must be strictly superior to 1 as 'less-than' is exclusive." );
+        addTip( "When the feature range status is 'certain', 'range', 'greater than' or 'less than', the range values should never be inferior or equal to 0." );
         addTip( "The ranges should not be out of the total protein sequence." );
         addTip( "If the start position is not a range, we should not have a start interval and vice versa for the end position." );
         addTip( "The start and end intervals of a range should not overlap." );
