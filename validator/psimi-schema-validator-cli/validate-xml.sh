40601e1c31fa7342034648c085b74291c975425c
marine.dumousseau@wanadoo.fr <marine.dumousseau@wanadoo.fr@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-01-10 15:55:55 +0000

    updated the scripts for running validator in command line

--- validator/psimi-schema-validator-cli/validate-xml.sh
+++ validator/psimi-schema-validator-cli/validate-xml.sh
@@ -2,4 +2,4 @@
 
 FILE=$1
 
-mvn clean install -Pvalidate -Dfile=$FILE -Dscope=xml -Dlevel=WARN -Dmaven.test.skip
+mvn -U clean install -Pvalidate -Dfile=$FILE -Dscope=xml -Dlevel=WARN -Dmaven.test.skip
