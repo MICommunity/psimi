da17aaced2dfde7ba5468a7352898374f4c65b40
christine.jandrasits <christine.jandrasits@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-03-23 14:08:39 +0000

    An abstract test case for the framework

---
+++ psimitab26/src/test/java/org/hupo/psi/calimocho/AbstractCalimochoTest.java
@@ -0,0 +1,43 @@
+package org.hupo.psi.calimocho;
+
+import org.hupo.psi.calimocho.io.formatter.LiteralFieldFormatter;
+import org.hupo.psi.calimocho.io.parser.LiteralFieldParser;
+import org.hupo.psi.calimocho.model.ColumnDefinitionBuilder;
+import org.hupo.psi.calimocho.model.DocumentDefinition;
+import org.hupo.psi.calimocho.model.DocumentDefinitionBuilder;
+
+/**
+ * TODO document this !
+ *
+ * @author Christine Jandrasits (cjandras@ebi.ac.uk)
+ * @version $Id$
+ * @since TODO add POM version
+ */
+public abstract class AbstractCalimochoTest {
+
+    public DocumentDefinition buildGeneListDefinition() {
+        return new DocumentDefinitionBuilder()
+                .addColumnDefinition( new ColumnDefinitionBuilder()
+                                              .setKey( "gene" )
+                                              .setPosition( 1 )
+                                              .setEmptyValue( "" )
+                                              .setIsAllowsEmpty( false )
+                                              .setFieldSeparator( "," )
+                                              .setFieldDelimiter( "" )
+                                              .setFieldParser( new LiteralFieldParser() )
+                                              .setFieldFormatter( new LiteralFieldFormatter() )
+                                              .build() )
+                .addColumnDefinition( new ColumnDefinitionBuilder()
+                                              .setKey( "taxid" )
+                                              .setPosition( 2 )
+                                              .setEmptyValue( "" )
+                                              .setIsAllowsEmpty( false )
+                                              .setFieldSeparator( "," )
+                                              .setFieldDelimiter( "" )
+                                              .setFieldParser( new LiteralFieldParser() )
+                                              .setFieldFormatter( new LiteralFieldFormatter() )
+                                              .build() )
+                .setColumnSeparator( "|" )
+                .build();
+    }
+}
