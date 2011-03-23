ed8f473f9533efaadc507cc71376901182f38c89
brunoaranda <brunoaranda@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-03-23 11:22:17 +0000

    Added stub for LiteralFieldParser

---
+++ trunk/psimitab26/src/main/java/org/hupo/psi/calimocho/parser/LiteralFieldFormatter.java
@@ -0,0 +1,24 @@
+package org.hupo.psi.calimocho.parser;
+
+import org.hupo.psi.calimocho.io.FieldFormatter;
+import org.hupo.psi.calimocho.model.Field;
+import org.hupo.psi.calimocho.model.Row;
+
+/**
+ * TODO document this !
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since TODO add POM version
+ */
+public class LiteralFieldFormatter implements FieldFormatter {
+
+
+    public String format( Field field ) {
+        return null;
+    }
+
+    public String format( Field field, Row row ) {
+        return null;
+    }
+}
