2ab081253b2ae198a1ab1071c48c99410256bd06
brunoaranda <brunoaranda@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-03-23 13:52:33 +0000

    Added tests for calimocho's field parsers

---
+++ trunk/psimitab26/src/test/java/org/hupo/psi/calimocho/io/parser/LiteralFieldParserTest.java
@@ -0,0 +1,38 @@
+package org.hupo.psi.calimocho.io.parser;
+
+import org.hupo.psi.calimocho.model.CalimochoKeys;
+import org.hupo.psi.calimocho.model.Field;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * TODO document this !
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since TODO add POM version
+ */
+public class LiteralFieldParserTest {
+
+    @Test
+    public void parse1() throws Exception {
+        String value = "lala";
+
+        LiteralFieldParser parser = new LiteralFieldParser();
+        final Field field = parser.parse( value, null );
+
+        Assert.assertEquals( 1, field.getEntries().size() );
+        Assert.assertEquals( "lala", field.get( CalimochoKeys.VALUE ) );
+    }
+
+    @Test
+    public void parse2() throws Exception {
+        String value = "";
+
+        LiteralFieldParser parser = new LiteralFieldParser();
+        final Field field = parser.parse( value, null );
+
+        Assert.assertEquals( 1, field.getEntries().size() );
+        Assert.assertEquals( "", field.get( CalimochoKeys.VALUE ) );
+    }
+}
