df80058e3a969a26049858c264ea5e413458cb31
christine.jandrasits <christine.jandrasits@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-03-23 10:43:32 +0000

    Initial interface and default implementation for the RowWriter.

---
+++ trunk/psimitab26/src/main/java/org/hupo/psi/calimocho/io/RowWriter.java
@@ -0,0 +1,21 @@
+package org.hupo.psi.calimocho.io;
+
+import org.hupo.psi.calimocho.model.Row;
+
+import java.io.*;
+import java.util.List;
+
+/**
+ * TODO document this !
+ *
+ * @author Christine Jandrasits (cjandras@ebi.ac.uk)
+ * @version $Id$
+ * @since TODO add POM version
+ */
+public interface RowWriter {
+    void read( Writer writer, List<Row> rows ) throws IOException, IllegalRowException;
+
+    String writeLine( Row row ) throws IllegalRowException, IllegalColumnException, IllegalFieldException;
+
+    void read( OutputStream os, List<Row> rows ) throws IOException, IllegalRowException;
+}
