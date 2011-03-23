9b6dc7ea9b23a41fd1e83bc4d501926171176ba0
christine.jandrasits <christine.jandrasits@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-03-23 16:14:10 +0000

    Implementation of the XrefFieldFormatter (unit tested)

--- trunk/psimitab26/src/main/java/org/hupo/psi/tab/io/formatter/XrefFieldFormatter.java
+++ trunk/psimitab26/src/main/java/org/hupo/psi/tab/io/formatter/XrefFieldFormatter.java
@@ -1,9 +1,12 @@
 package org.hupo.psi.tab.io.formatter;
 
+import com.sun.org.apache.bcel.internal.generic.CALOAD;
 import org.hupo.psi.calimocho.io.FieldFormatter;
 import org.hupo.psi.calimocho.io.IllegalFieldException;
+import org.hupo.psi.calimocho.model.CalimochoKeys;
 import org.hupo.psi.calimocho.model.Field;
 import org.hupo.psi.calimocho.model.Row;
+import org.hupo.psi.tab.util.MitabEscapeUtils;
 
 /**
  * TODO document this !
@@ -15,10 +18,30 @@ import org.hupo.psi.calimocho.model.Row;
 public class XrefFieldFormatter implements FieldFormatter {
 
     public String format( Field field ) throws IllegalFieldException {
-        throw new UnsupportedOperationException( "Not implemented" );
+        StringBuilder sb = new StringBuilder();
+
+        final String db = field.get( CalimochoKeys.DB );
+        final String value = field.get( CalimochoKeys.VALUE );
+
+        if(db == null){
+            throw new IllegalFieldException( "Missing database in field: " + field.toString() );
+        }
+
+        if(value == null){
+            throw new IllegalFieldException( "Missing value in field: " + field.toString() );
+        }
+
+        sb.append( MitabEscapeUtils.escapeFieldElement( db ) ).append( ':' ).append( MitabEscapeUtils.escapeFieldElement( value ) );
+
+        final String text = field.get( CalimochoKeys.TEXT );
+        if(text != null){
+           sb.append('(').append( MitabEscapeUtils.escapeFieldElement( text )).append( ')' );
+        }
+
+        return sb.toString();
     }
 
     public String format( Field field, Row row ) throws IllegalFieldException {
-        throw new UnsupportedOperationException( "Not implemented" );
+        return format(field);
     }
 }
