8e74d34ba82ad3c18950f8353dc3f56a7c14a568
marine.dumousseau@wanadoo.fr <marine.dumousseau@wanadoo.fr@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-03-23 11:35:41 +0000

    MitabDocumentDefinitionFactory : finished the mitab25() method

--- psimitab26/src/main/java/org/hupo/psi/tab/util/MitabDocumentDefinitionFactory.java
+++ psimitab26/src/main/java/org/hupo/psi/tab/util/MitabDocumentDefinitionFactory.java
@@ -1,8 +1,13 @@
 package org.hupo.psi.tab.util;
 
 import org.hupo.psi.calimocho.model.ColumnDefinition;
+import org.hupo.psi.calimocho.model.ColumnDefinitionBuilder;
 import org.hupo.psi.calimocho.model.DocumentDefinition;
 import org.hupo.psi.calimocho.model.DocumentDefinitionBuilder;
+import org.hupo.psi.calimocho.parser.LiteralFieldFormatter;
+import org.hupo.psi.calimocho.parser.LiteralFieldParser;
+import org.hupo.psi.tab.parser.XrefFieldFormatter;
+import org.hupo.psi.tab.parser.XrefFieldParser;
 
 /**
  * TODO document this !
@@ -16,14 +21,108 @@ public class MitabDocumentDefinitionFactory {
     private MitabDocumentDefinitionFactory() {}
 
     public static DocumentDefinition mitab25() {
+        XrefFieldParser xrefParser = new XrefFieldParser();
+        XrefFieldFormatter xrefFormatter = new XrefFieldFormatter();
+
+        LiteralFieldParser literalParser = new LiteralFieldParser();
+        LiteralFieldFormatter literalFieldFormatter = new LiteralFieldFormatter();
+
         ColumnDefinition idACol = new ColumnDefinitionBuilder()
                 .setKey("idA")
                 .setFieldSeparator("|")
+                .setEmptyValue("-")
+                .setFieldDelimiter("")
+                .setIsAllowsEmpty(false)
+                .setPosition(0)
+                .setFieldFormatter(xrefFormatter)
+                .setFieldParser(xrefParser)
                 .build();
 
         ColumnDefinition idBCol = new ColumnDefinitionBuilder()
-                .extendColumnDefinition(idA)
+                .extendColumnDefinition(idACol)
                 .setKey("idB")
+                .setPosition(1)
+                .build();
+
+        ColumnDefinition altidA = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(idACol)
+                .setKey("altidA")
+                .setPosition(2)
+                .setIsAllowsEmpty(true)
+                .build();
+
+        ColumnDefinition altidB = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("altidB")
+                .setPosition(3)
+                .build();
+
+        ColumnDefinition aliasA = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("aliasA")
+                .setPosition(4)
+                .build();
+
+        ColumnDefinition aliasB = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("aliasB")
+                .setPosition(5)
+                .build();
+
+        ColumnDefinition detmethod_exact = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("detmethod_exact")
+                .setPosition(6)
+                .build();
+
+        ColumnDefinition pubauth = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("pubauth")
+                .setPosition(7)
+                .setFieldFormatter(literalFieldFormatter)
+                .setFieldParser(literalParser)
+                .build();
+
+        ColumnDefinition pubid = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("pubid")
+                .setPosition(8)
+                .build();
+
+        ColumnDefinition taxidA = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("taxidA")
+                .setPosition(9)
+                .build();
+
+        ColumnDefinition taxidB = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("taxidB")
+                .setPosition(10)
+                .build();
+
+        ColumnDefinition type_exact = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("type_exact")
+                .setPosition(11)
+                .build();
+
+        ColumnDefinition source = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("source")
+                .setPosition(12)
+                .build();
+
+        ColumnDefinition interaction_id = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("interaction_id")
+                .setPosition(13)
+                .build();
+
+        ColumnDefinition confidence = new ColumnDefinitionBuilder()
+                .extendColumnDefinition(altidA)
+                .setKey("confidence")
+                .setPosition(14)
                 .build();
 
         DocumentDefinition docDefinition = new DocumentDefinitionBuilder()
