c3514c32823d443fd44bc3ded7a68ec7204fbde7
samuel.kerrien <samuel.kerrien@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-03-23 16:05:33 +0000

    Added FieldBuilder

---
+++ trunk/psimitab26/src/main/java/org/hupo/psi/calimocho/model/FieldBuilder.java
@@ -0,0 +1,36 @@
+package org.hupo.psi.calimocho.model;
+
+import org.hupo.psi.calimocho.io.IllegalFieldException;
+
+/**
+ * TODO document this !
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ * @since TODO add POM version
+ */
+public class FieldBuilder {
+
+    DefaultField field = new DefaultField();
+
+    public FieldBuilder addKey( String key ) {
+        field.set( CalimochoKeys.KEY, key );
+        return this;
+    }
+
+    public FieldBuilder addKeyValue( String key, String value ) {
+        field.set( key, value );
+        return this;
+    }
+
+    public Field build() throws IllegalFieldException {
+        validate( field );
+        return field;
+    }
+
+    private void validate( DefaultField field ) throws IllegalFieldException {
+        if( field.get( CalimochoKeys.KEY ) == null ) {
+            throw new IllegalFieldException( "A field must have a key" );
+        }
+    }
+}
