8025720e53f04d740fe2bd07b9e033e1f6c37646
brunoaranda <brunoaranda@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-03-23 11:21:39 +0000

    Added stub for XrefFieldParser

---
+++ trunk/psimitab26/src/main/java/org/hupo/psi/tab/parser/XrefFieldFormatter.java
@@ -0,0 +1,23 @@
+package org.hupo.psi.tab.parser;
+
+import org.hupo.psi.calimocho.io.FieldFormatter;
+import org.hupo.psi.calimocho.model.Field;
+import org.hupo.psi.calimocho.model.Row;
+
+/**
+ * TODO document this !
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ * @since TODO add POM version
+ */
+public class XrefFieldFormatter implements FieldFormatter {
+
+    public String format( Field field ) {
+        throw new UnsupportedOperationException( "Not implemented" );
+    }
+
+    public String format( Field field, Row row ) {
+        throw new UnsupportedOperationException( "Not implemented" );
+    }
+}
