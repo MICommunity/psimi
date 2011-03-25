2bcb4487706c9d2b71fd7ac414d9779695ea4c94
brunoaranda <brunoaranda@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-03-25 16:05:04 +0000

    Created simple document definition

--- calimocho-tab/src/test/java/org/hupo/psi/tab/Playground.java
+++ calimocho-tab/src/test/java/org/hupo/psi/tab/Playground.java
@@ -1,6 +1,8 @@
 package org.hupo.psi.tab;
 
+import org.hupo.psi.calimocho.io.DocumentConverter;
 import org.hupo.psi.calimocho.model.Row;
+import org.hupo.psi.calimocho.model.definition.PlainDocumentDefinition;
 import org.hupo.psi.tab.io.DefaultRowReader;
 import org.hupo.psi.tab.io.RowReader;
 import org.hupo.psi.tab.model.ColumnBasedDocumentDefinition;
@@ -10,39 +12,17 @@ import org.hupo.psi.tab.util.MitabDocumentDefinitionFactory;
 import java.io.InputStream;
 import java.util.Collection;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Training
- * Date: 24/03/11
- * Time: 10:17
- * To change this template use File | Settings | File Templates.
- */
-public class Playground
-{
+public class Playground {
 
     public static void main(String[] args) throws Exception {
         InputStream stream = Playground.class.getResourceAsStream("/META-INF/mitab26/14726512.txt");
 
+        ColumnBasedDocumentDefinition mitabDefinition = MitabDocumentDefinitionFactory.mitab26();
+        PlainDocumentDefinition plainDefinition = new PlainDocumentDefinition();
 
-        ColumnBasedDocumentDefinition mitabDocDefinition = MitabDocumentDefinitionFactory.mitab26();
+        DocumentConverter converter = new DocumentConverter( mitabDefinition, plainDefinition );
+        converter.convert( stream, System.out );
 
-        RowReader reader = new DefaultRowReader(mitabDocDefinition);
-
-        for (Row row : reader.read(stream)) {
-            System.out.println("--------------------------------\nNEW ROW\n-----------------------------------");
-            Collection<ColumnDefinition> columns = mitabDocDefinition.getColumns();
-
-            for (ColumnDefinition col : columns) {
-                System.out.println(col.getKey());
-
-//                Collection<Field> fields = row.getFields(col);
-//
-//                for (Field field : fields) {
-//                    System.out.println("\t"+field);
-//                }
-
-            }
-        }
     }
 
 }
