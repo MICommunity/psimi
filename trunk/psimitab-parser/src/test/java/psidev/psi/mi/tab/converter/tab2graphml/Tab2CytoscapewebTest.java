83b4b44dabfe0773ba4b4b444184a7855f27a135
christine.jandrasits@gmail.com <christine.jandrasits@gmail.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-07-08 09:38:00 +0000

    Enabled GraphmlBuilder to cope with mitab files with header and without header and added tests for it.

--- trunk/psimitab-parser/src/test/java/psidev/psi/mi/tab/converter/tab2graphml/Tab2CytoscapewebTest.java
+++ trunk/psimitab-parser/src/test/java/psidev/psi/mi/tab/converter/tab2graphml/Tab2CytoscapewebTest.java
@@ -15,17 +15,20 @@ import java.io.InputStream;
  */
 public class Tab2CytoscapewebTest{
     @Test
-    public void convert(){
-        InputStream inputStream = getClass().getResourceAsStream("/mitab-samples/12167173.txt");
-        String ouput = null;
+    public void convertNoHeader(){
+        InputStream inputStream = getClass().getResourceAsStream("/mitab-samples/12167173_noHeader.txt");
+        String output = null;
         Tab2Cytoscapeweb tab2Cytoscapeweb = new Tab2Cytoscapeweb();
         try {
-            ouput = tab2Cytoscapeweb.convert(inputStream);
+            output = tab2Cytoscapeweb.convert(inputStream);
         } catch (ConverterException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
+
+        // make sure some data was transformed
+        Assert.assertTrue((GraphmlBuilder.GRAPHML_FOOTER.length() + GraphmlBuilder.GRAPHML_HEADER.length()) < output.length());
 
         String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
                 "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"  \n" +
@@ -40,10 +43,192 @@ public class Tab2CytoscapewebTest{
                 "    <default>ELLIPSE</default>\n" +
                 "  </key>\n" +
                 "  <graph id=\"G\" edgedefault=\"undirected\">\n" +
+                "     <node id=\"1\">\n" +
+                "        <data key=\"label\">pop1</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#P87060</data>\n" +
+                "        <data key=\"specie\">Schizosaccharomyces pombe 972h-</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <node id=\"2\">\n" +
+                "        <data key=\"label\">pop2</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#O14170</data>\n" +
+                "        <data key=\"specie\">Schizosaccharomyces pombe 972h-</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <edge source=\"1\" target=\"2\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"1\" target=\"2\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"1\" target=\"2\">\n" +
+                "     </edge>\n" +
+                "     <node id=\"3\">\n" +
+                "        <data key=\"label\">pip1</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#O13959</data>\n" +
+                "        <data key=\"specie\">Schizosaccharomyces pombe 972h-</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <node id=\"4\">\n" +
+                "        <data key=\"label\">cul1</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#O13790</data>\n" +
+                "        <data key=\"specie\">Schizosaccharomyces pombe 972h-</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <edge source=\"3\" target=\"4\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"1\">\n" +
+                "     </edge>\n" +
+                "     <node id=\"5\">\n" +
+                "        <data key=\"label\">skp1</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#Q9Y709</data>\n" +
+                "        <data key=\"specie\">Schizosaccharomyces pombe 972h-</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <edge source=\"3\" target=\"5\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"4\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"2\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"5\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"4\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"2\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"5\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"1\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"4\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"5\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"1\" target=\"2\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"2\" target=\"1\">\n" +
+                "     </edge>\n" +
+                "     <node id=\"6\">\n" +
+                "        <data key=\"label\">rum1</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#P40380</data>\n" +
+                "        <data key=\"specie\">Schizosaccharomyces pombe 972h-</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <edge source=\"2\" target=\"6\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"4\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"5\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"2\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"1\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"5\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"1\" target=\"3\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"1\" target=\"4\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"1\" target=\"2\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"2\" target=\"3\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"2\" target=\"4\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"2\" target=\"1\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"4\" target=\"3\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"4\" target=\"2\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"1\" target=\"5\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"2\" target=\"5\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"4\" target=\"5\">\n" +
+                "     </edge>\n" +
                 "  </graph>\n" +
                 "</graphml>";
 
-        Assert.assertEquals(expected,ouput);
-        System.out.println(ouput);
+        Assert.assertEquals(expected, output);
+    }
+
+    @Test
+    public void convertWithHeader(){
+        InputStream inputStream = getClass().getResourceAsStream("/mitab-samples/12167173.txt");
+        String output = null;
+        Tab2Cytoscapeweb tab2Cytoscapeweb = new Tab2Cytoscapeweb();
+        try {
+            output = tab2Cytoscapeweb.convert(inputStream);
+        } catch (ConverterException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        // make sure some data was transformed
+        Assert.assertTrue((GraphmlBuilder.GRAPHML_FOOTER.length() + GraphmlBuilder.GRAPHML_HEADER.length()) < output.length());
+
+        String expected = GraphmlBuilder.GRAPHML_HEADER +
+                "     <node id=\"1\">\n" +
+                "        <data key=\"label\">SPBC2G2.18</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#P87060</data>\n" +
+                "        <data key=\"specie\">schpo</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <node id=\"2\">\n" +
+                "        <data key=\"label\">sud1</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#O14170</data>\n" +
+                "        <data key=\"specie\">schpo</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <edge source=\"1\" target=\"2\">\n" +
+                "     </edge>\n" +
+                "     <node id=\"3\">\n" +
+                "        <data key=\"label\">pcu1</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#O13790</data>\n" +
+                "        <data key=\"specie\">schpo</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <edge source=\"2\" target=\"3\">\n" +
+                "     </edge>\n" +
+                "     <node id=\"4\">\n" +
+                "        <data key=\"label\">SPAC23H4.18c</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#O13959</data>\n" +
+                "        <data key=\"specie\">schpo</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <edge source=\"4\" target=\"3\">\n" +
+                "     </edge>\n" +
+                "     <node id=\"5\">\n" +
+                "        <data key=\"label\">SPBC32F12.09</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#P40380</data>\n" +
+                "        <data key=\"specie\">schpo</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <edge source=\"2\" target=\"5\">\n" +
+                "     </edge>\n" +
+                "     <node id=\"6\">\n" +
+                "        <data key=\"label\">skp1</data>\n" +
+                "        <data key=\"identifier\">uniprotkb#Q9Y709</data>\n" +
+                "        <data key=\"specie\">schpo</data>\n" +
+                "        <data key=\"type\">protein</data>\n" +
+                "     </node>\n" +
+                "     <edge source=\"2\" target=\"6\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"1\" target=\"6\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"4\" target=\"2\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"4\" target=\"1\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"1\" target=\"3\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"4\" target=\"6\">\n" +
+                "     </edge>\n" +
+                "     <edge source=\"3\" target=\"6\">\n" +
+                "     </edge>\n" + GraphmlBuilder.GRAPHML_FOOTER;
+
+        Assert.assertEquals(expected,output);
     }
 }
