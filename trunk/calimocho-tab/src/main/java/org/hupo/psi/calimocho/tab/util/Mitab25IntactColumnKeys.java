fe94e44246a673f4790479ab48346230ff92e86c
brunoaranda <brunoaranda@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-08-26 01:32:27 +0000

    Class was in wrong package

---
+++ trunk/calimocho-tab/src/main/java/org/hupo/psi/calimocho/tab/util/Mitab25IntactColumnKeys.java
@@ -0,0 +1,28 @@
+package org.hupo.psi.calimocho.tab.util;
+
+/**
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public interface Mitab25IntactColumnKeys {
+
+    String KEY_EXPROLE_A = Mitab26ColumnKeys.KEY_EXPROLE_A;
+    String KEY_EXPROLE_B = Mitab26ColumnKeys.KEY_EXPROLE_B;
+    String KEY_BIOROLE_A = Mitab26ColumnKeys.KEY_BIOROLE_A;
+    String KEY_BIOROLE_B = Mitab26ColumnKeys.KEY_BIOROLE_B;
+    String KEY_XREFS_A = Mitab26ColumnKeys.KEY_XREFS_A;
+    String KEY_XREFS_B = Mitab26ColumnKeys.KEY_XREFS_B;
+    String KEY_INTERACTOR_TYPE_A = Mitab26ColumnKeys.KEY_INTERACTOR_TYPE_A;
+    String KEY_INTERACTOR_TYPE_B = Mitab26ColumnKeys.KEY_INTERACTOR_TYPE_B;
+    String KEY_HOST_ORGANISM = Mitab26ColumnKeys.KEY_HOST_ORGANISM;
+    String KEY_EXPANSION = Mitab26ColumnKeys.KEY_EXPANSION;
+    String KEY_DATASET = "dataset";
+    String KEY_ANNOTATIONS_A = Mitab26ColumnKeys.KEY_ANNOTATIONS_A;
+    String KEY_ANNOTATIONS_B = Mitab26ColumnKeys.KEY_ANNOTATIONS_B;
+    String KEY_PARAMETERS_A = Mitab26ColumnKeys.KEY_PARAMETERS_A;
+    String KEY_PARAMETERS_B = Mitab26ColumnKeys.KEY_PARAMETERS_B;
+    String KEY_PARAMETERS_I = Mitab26ColumnKeys.KEY_PARAMETERS_I;
+
+
+}
+
