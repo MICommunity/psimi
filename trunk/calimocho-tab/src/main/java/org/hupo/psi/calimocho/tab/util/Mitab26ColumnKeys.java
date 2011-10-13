8becba31e2a2162f621399d25e40f6cba4754a67
brunoaranda <brunoaranda@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-10-13 14:30:31 +0000

    Using constant keys from the calimocho core

--- trunk/calimocho-tab/src/main/java/org/hupo/psi/calimocho/tab/util/Mitab26ColumnKeys.java
+++ trunk/calimocho-tab/src/main/java/org/hupo/psi/calimocho/tab/util/Mitab26ColumnKeys.java
@@ -15,35 +15,37 @@
  */
 package org.hupo.psi.calimocho.tab.util;
 
+import org.hupo.psi.calimocho.key.InteractionKeys;
+
 /**
  * @author Bruno Aranda (baranda@ebi.ac.uk)
  * @version $Id$
  */
 public interface Mitab26ColumnKeys {
 
-    String KEY_EXPANSION = "expansion";
-    String KEY_BIOROLE_A = "bioRoleA";
-    String KEY_BIOROLE_B = "bioRoleB";
-    String KEY_EXPROLE_A = "expRoleA";
-    String KEY_EXPROLE_B = "expRoleB";
-    String KEY_INTERACTOR_TYPE_A = "typeA";
-    String KEY_INTERACTOR_TYPE_B = "typeB";
-    String KEY_XREFS_A = "xrefsA";
-    String KEY_XREFS_B = "xrefsB";
-    String KEY_XREFS_I = "xrefsI";
-    String KEY_ANNOTATIONS_A = "annotationsA";
-    String KEY_ANNOTATIONS_B = "annotationsB";
-    String KEY_ANNOTATIONS_I = "annotationsI";
-    String KEY_HOST_ORGANISM = "hostOrganism";
-    String KEY_PARAMETERS_A = "parametersA";
-    String KEY_PARAMETERS_B = "parametersB";
-    String KEY_PARAMETERS_I = "parametersI";
-    String KEY_CREATION_DATE = "creationDate";
-    String KEY_UPDATE_DATE = "updateDate";
-    String KEY_CHECKSUM_A = "checksumA";
-    String KEY_CHECKSUM_B = "checksumB";
-    String KEY_CHECKSUM_I = "checksumI";
-    String KEY_NEGATIVE = "negative";
+    String KEY_EXPANSION = InteractionKeys.KEY_EXPANSION;
+    String KEY_BIOROLE_A = InteractionKeys.KEY_BIOROLE_A;
+    String KEY_BIOROLE_B = InteractionKeys.KEY_BIOROLE_B;
+    String KEY_EXPROLE_A = InteractionKeys.KEY_EXPROLE_A;
+    String KEY_EXPROLE_B = InteractionKeys.KEY_EXPROLE_B;
+    String KEY_INTERACTOR_TYPE_A = InteractionKeys.KEY_INTERACTOR_TYPE_A;
+    String KEY_INTERACTOR_TYPE_B = InteractionKeys.KEY_INTERACTOR_TYPE_B;
+    String KEY_XREFS_A = InteractionKeys.KEY_XREFS_A;
+    String KEY_XREFS_B = InteractionKeys.KEY_XREFS_B;
+    String KEY_XREFS_I = InteractionKeys.KEY_XREFS_I;
+    String KEY_ANNOTATIONS_A = InteractionKeys.KEY_ANNOTATIONS_A;
+    String KEY_ANNOTATIONS_B = InteractionKeys.KEY_ANNOTATIONS_B;
+    String KEY_ANNOTATIONS_I = InteractionKeys.KEY_ANNOTATIONS_I;
+    String KEY_HOST_ORGANISM = InteractionKeys.KEY_HOST_ORGANISM;
+    String KEY_PARAMETERS_A = InteractionKeys.KEY_PARAMETERS_A;
+    String KEY_PARAMETERS_B = InteractionKeys.KEY_PARAMETERS_B;
+    String KEY_PARAMETERS_I = InteractionKeys.KEY_PARAMETERS_I;
+    String KEY_CREATION_DATE = InteractionKeys.KEY_CREATION_DATE;
+    String KEY_UPDATE_DATE = InteractionKeys.KEY_UPDATE_DATE;
+    String KEY_CHECKSUM_A = InteractionKeys.KEY_CHECKSUM_A;
+    String KEY_CHECKSUM_B = InteractionKeys.KEY_CHECKSUM_B;
+    String KEY_CHECKSUM_I = InteractionKeys.KEY_CHECKSUM_I;
+    String KEY_NEGATIVE = InteractionKeys.KEY_NEGATIVE;
 
 
 }
