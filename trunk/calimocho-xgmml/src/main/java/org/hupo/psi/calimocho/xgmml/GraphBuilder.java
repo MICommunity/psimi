4d1072c4d41515a6a0ccdff7b2c987e85d75b0c3
brunoaranda <brunoaranda@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2011-10-17 14:31:52 +0000

    Exporting atts for edges too

--- trunk/calimocho-xgmml/src/main/java/org/hupo/psi/calimocho/xgmml/GraphBuilder.java
+++ trunk/calimocho-xgmml/src/main/java/org/hupo/psi/calimocho/xgmml/GraphBuilder.java
@@ -18,7 +18,9 @@ package org.hupo.psi.calimocho.xgmml;
 import calimocho.internal.rdf.*;
 import calimocho.internal.xgmml.*;
 import calimocho.internal.xgmml.ObjectFactory;
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
 import org.hupo.psi.calimocho.key.CalimochoKeys;
 import org.hupo.psi.calimocho.key.InteractionKeys;
 import org.hupo.psi.calimocho.model.CalimochoDocument;
@@ -29,6 +31,7 @@ import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -36,13 +39,13 @@ import java.util.Map;
  * @version $Id$
  */
 public class GraphBuilder {
-    
+
     private String title;
     private String description;
     private String source;
-    
-    private Map<String,Node> nodeMap;
-    
+
+    private Map<String, Node> nodeMap;
+
     private ObjectFactory xgmmlObjectFactory;
     private calimocho.internal.rdf.ObjectFactory rdfObjectFactory;
 
@@ -62,7 +65,7 @@ public class GraphBuilder {
 
     public Graph createGraph(CalimochoDocument calimochoDocument) {
         Graph graph = xgmmlObjectFactory.createGraph();
-        graph.setId("calimocho-"+System.currentTimeMillis());
+        graph.setId("calimocho-" + System.currentTimeMillis());
         graph.setLabel(title);
 //        graph.setDirected("1");
         graph.getAtts().add(createAtt("documentVersion", "1.1"));
@@ -81,8 +84,6 @@ public class GraphBuilder {
             addNodeToGraph(graph, nodeA);
             addNodeToGraph(graph, nodeB);
             addEdgeToMap(graph, edge);
-
-            System.out.println("Edge: "+nodeA.getLabel()+" "+nodeB.getLabel());
         }
 
         // calculate node positions
@@ -114,10 +115,10 @@ public class GraphBuilder {
             x = distance * colIndex;
             y = distance * rowIndex;
         }
-        
-        double viewCenter = ((cols-1) * distance) / 2;
-                
-        graph.getAtts().add(createAtt("GRAPH_VIEW_ZOOM", "0.86823", ObjectType.REAL));        
+
+        double viewCenter = ((cols - 1) * distance) / 2;
+
+        graph.getAtts().add(createAtt("GRAPH_VIEW_ZOOM", "0.86823", ObjectType.REAL));
         graph.getAtts().add(createAtt("GRAPH_VIEW_CENTER_X", String.valueOf(viewCenter), ObjectType.REAL));
         graph.getAtts().add(createAtt("GRAPH_VIEW_CENTER_Y", String.valueOf(viewCenter), ObjectType.REAL));
 
@@ -138,15 +139,15 @@ public class GraphBuilder {
         final Description description = rdfObjectFactory.createDescription();
         description.setContent(this.description);
         rdfDescription.getDcmes().add(rdfObjectFactory.createDescription(description));
-        
+
         final Identifier identifier = rdfObjectFactory.createIdentifier();
         identifier.setContent("N/A");
         rdfDescription.getDcmes().add(rdfObjectFactory.createIdentifier(identifier));
-        
+
         final Date date = rdfObjectFactory.createDate();
         date.setContent(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date()));
         rdfDescription.getDcmes().add(rdfObjectFactory.createDate(date));
-        
+
         final Title title = rdfObjectFactory.createTitle();
         title.setContent(this.title);
         rdfDescription.getDcmes().add(rdfObjectFactory.createTitle(title));
@@ -160,12 +161,12 @@ public class GraphBuilder {
         rdfDescription.getDcmes().add(rdfObjectFactory.createFormat(format));
 
         networkMetadata.getContent().add(rdfRDF);
-        
+
         return networkMetadata;
     }
 
     private void addEdgeToMap(Graph graph, Edge edge) {
-       graph.getNodesAndEdges().add(edge);
+        graph.getNodesAndEdges().add(edge);
     }
 
     private void addNodeToGraph(Graph graph, Node node) {
@@ -208,16 +209,18 @@ public class GraphBuilder {
 
     private Node toNode(Collection<Field> idFields, Collection<Field> altidFields, Collection<Field> aliasFields,
                         Collection<Field> taxidFields, Collection<Field> bioRoleFields, Collection<Field> expRoleFields,
-                        Collection<Field> typeFields, Collection<Field> xrefFields, Collection<Field> annotFields, Collection<Field> paramFields, 
+                        Collection<Field> typeFields, Collection<Field> xrefFields, Collection<Field> annotFields, Collection<Field> paramFields,
                         int nodeIndex) {
         String displayName = null;
 
         Node node = xgmmlObjectFactory.createNode();
-        
-        Collection<Field> altidAndAliasFields = new ArrayList<Field>(altidFields.size()+aliasFields.size());
+
+        Multimap<String, Att> attMultimap = HashMultimap.create();
+
+        Collection<Field> altidAndAliasFields = new ArrayList<Field>(altidFields.size() + aliasFields.size());
         altidAndAliasFields.addAll(altidFields);
         altidAndAliasFields.addAll(aliasFields);
-        
+
         for (Field alField : altidAndAliasFields) {
             if ("gene name".equals(alField.get(CalimochoKeys.TEXT))) {
                 displayName = alField.get(CalimochoKeys.VALUE);
@@ -225,7 +228,7 @@ public class GraphBuilder {
                 break;
             }
         }
-        
+
         if (displayName == null) {
             for (Field idField : idFields) {
                 if ("uniprotkb".equals(idField.get(CalimochoKeys.KEY)) || "chebi".equals(idField.get(CalimochoKeys.KEY))) {
@@ -233,15 +236,15 @@ public class GraphBuilder {
                     break;
                 }
             }
-            
+
             if (displayName == null) {
                 displayName = idFields.iterator().next().get(CalimochoKeys.VALUE);
             }
         }
-        
+
         //species
         String taxid = null;
-        
+
         if (!taxidFields.isEmpty()) {
             taxid = taxidFields.iterator().next().get(CalimochoKeys.VALUE);
         }
@@ -254,39 +257,34 @@ public class GraphBuilder {
 
         node.setId(String.valueOf(++nodeIndex));
         node.setLabel(key);
-        
-        node.getAtts().add(createAtt("canonicalName", displayName, ObjectType.STRING));
 
-        addFieldsAsAtts(idFields, node, "id");
-        addFieldsAsAtts(altidFields, node, "altid");
-        addFieldsAsAtts(aliasFields, node, "alias");
-        addFieldsAsAtts(taxidFields, node, null);
-        addFieldsAsAtts(bioRoleFields, node, "biorole");
-        addFieldsAsAtts(expRoleFields, node, "exprole");
-        addFieldsAsAtts(typeFields, node, "type");
-        addFieldsAsAtts(xrefFields, node, "xref");
-        addFieldsAsAtts(annotFields, node, "annot");
-        addFieldsAsAtts(paramFields, node, "param");
+        addToMultimap(attMultimap, createAtt("canonicalName", displayName, ObjectType.STRING));
+
+        addFieldsAsAtts(idFields, attMultimap, "id");
+        addFieldsAsAtts(altidFields, attMultimap, "altid");
+        addFieldsAsAtts(aliasFields, attMultimap, "alias");
+        addFieldsAsAtts(taxidFields, attMultimap, null);
+        addFieldsAsAtts(bioRoleFields, attMultimap, "biorole");
+        addFieldsAsAtts(expRoleFields, attMultimap, "exprole");
+        addFieldsAsAtts(typeFields, attMultimap, "type");
+        addFieldsAsAtts(xrefFields, attMultimap, "xref");
+        addFieldsAsAtts(annotFields, attMultimap, "annot");
+        addFieldsAsAtts(paramFields, attMultimap, "param");
+
+        // process the multimap. When there is more than one value, create a list att to wrap the atts
+        final List<Att> atts = node.getAtts();
+
+        addAttsFromMap(attMultimap, atts);
+
+        node.getAtts().addAll(atts);
 
         return node;
     }
 
-    private void addFieldsAsAtts(Collection<Field> idFields, Node node, String prefix) {
-        for (Field field : idFields) {
-            
-            String prefixAndDot = "";
-            
-            if (prefix != null && !prefix.isEmpty()) {
-                prefixAndDot = prefix+".";
-            }
-            
-            Att att = createAtt(prefixAndDot+field.get(CalimochoKeys.KEY), field.get(CalimochoKeys.VALUE));
-            att.setType(ObjectType.STRING);
-            node.getAtts().add(att);
-        }
-    }
 
     private Edge toEdge(Row row, Node nodeA, Node nodeB) {
+        Multimap<String, Att> attMultimap = HashMultimap.create();
+
         Edge edge = xgmmlObjectFactory.createEdge();
         edge.setSource(nodeA.getId());
         edge.setTarget(nodeB.getId());
@@ -294,10 +292,33 @@ public class GraphBuilder {
         final Collection<Field> idFields = row.getFields(InteractionKeys.KEY_INTERACTION_ID);
 
         if (!idFields.isEmpty()) {
-            edge.setLabel(idFields.iterator().next().get(CalimochoKeys.VALUE));
+            edge.setLabel(nodeA.getLabel()+" ("+idFields.iterator().next().get(CalimochoKeys.VALUE)+") "+nodeB.getLabel());
         } else {
-            edge.setLabel("Interaction-" + nodeA.getId() + "-" + nodeB.getId());
+            edge.setLabel(nodeA.getLabel() + "(interaction)" + nodeB.getLabel());
         }
+
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_DETMETHOD), attMultimap, "detmethod");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_PUBAUTH), attMultimap, "pubauth");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_PUBID), attMultimap, "pubid");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_INTERACTION_TYPE), attMultimap, "type");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_SOURCE), attMultimap, "source");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_INTERACTION_ID), attMultimap, "id");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_CONFIDENCE), attMultimap, "confidence");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_DATASET), attMultimap, "dataset");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_EXPANSION), attMultimap, "expansion");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_XREFS_I), attMultimap, "xref");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_HOST_ORGANISM), attMultimap, "host");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_ANNOTATIONS_I), attMultimap, "annot");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_PARAMETERS_I), attMultimap, "param");
+        addFieldsAsAtts(row.getFields(InteractionKeys.KEY_NEGATIVE), attMultimap, "negative");
+
+        // process the multimap. When there is more than one value, create a list att to wrap the atts
+        final List<Att> atts = edge.getAtts();
+
+        addAttsFromMap(attMultimap, atts);
+
+        edge.getAtts().addAll(atts);
+
         return edge;
     }
 
@@ -313,9 +334,45 @@ public class GraphBuilder {
         return att;
     }
 
+    private void addToMultimap(Multimap<String, Att> multimap, Att att) {
+        multimap.put(att.getName(), att);
+    }
+
+    private void addFieldsAsAtts(Collection<Field> idFields, Multimap<String, Att> multimap, String prefix) {
+        for (Field field : idFields) {
+
+            String prefixAndDot = "";
+
+            if (prefix != null && !prefix.isEmpty()) {
+                prefixAndDot = prefix + ".";
+            }
+
+            Att att = createAtt(prefixAndDot + field.get(CalimochoKeys.KEY), field.get(CalimochoKeys.VALUE));
+            att.setType(ObjectType.STRING);
+
+            addToMultimap(multimap, att);
+        }
+    }
+
+    private void addAttsFromMap(Multimap<String, Att> attMultimap, List<Att> atts) {
+        for (Map.Entry<String, Collection<Att>> entry : attMultimap.asMap().entrySet()) {
+            if (entry.getValue().size() > 1) {
+                Att list = createAtt(entry.getKey(), null);
+
+                for (Att att : entry.getValue()) {
+                    list.getContent().add(att);
+                }
+
+                atts.add(list);
+            } else {
+                atts.add(entry.getValue().iterator().next());
+            }
+        }
+    }
+
     private Graphics createGraphicsForNode(Node node, double x, double y) {
         Graphics graphics = xgmmlObjectFactory.createGraphics();
-        
+
         graphics.setH(25.0);
         graphics.setW(45.0);
         graphics.setFill("#ffffff");
@@ -324,20 +381,20 @@ public class GraphBuilder {
 
         graphics.setX(x);
         graphics.setY(y);
-        
+
         // shape depends on type
         graphics.setType(TypeGraphicsType.ELLIPSE);
-        
+
         for (Att att : node.getAtts()) {
             if (att.getName().startsWith("type.")) {
-                
+
                 if ("MI:0326".equals(att.getValue())) { // protein
                     graphics.setFill("#00ffcc");
                 } else if ("MI:0328".equals(att.getValue())) { // small molecule
                     graphics.setType(TypeGraphicsType.TRIANGLE);
                     graphics.setFill("#33CCCC");
                 }
-                
+
             }
         }
 
