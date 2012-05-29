e0ffff8cee00a89c460eefaed60a12f8945ba0e6
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-05-29 16:07:00 +0000

    added some converter

---
+++ calimocho-solr/src/main/java/psidev/psi/mi/calimocho/solr/converter/SolrFieldConverter.java
@@ -0,0 +1,17 @@
+package psidev.psi.mi.calimocho.solr.converter;
+
+import org.apache.solr.common.SolrInputDocument;
+import org.hupo.psi.calimocho.model.Field;
+
+/**
+ * TODO comment this
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>29/05/12</pre>
+ */
+
+public interface SolrFieldConverter {
+    
+    public void indexFieldValues(Field field, SolrFieldName name, SolrInputDocument doc);
+}
---
+++ calimocho-solr/src/main/java/psidev/psi/mi/calimocho/solr/converter/XrefFieldCrossReference.java
@@ -0,0 +1,33 @@
+package psidev.psi.mi.calimocho.solr.converter;
+
+import org.apache.solr.common.SolrInputDocument;
+import org.hupo.psi.calimocho.key.CalimochoKeys;
+import org.hupo.psi.calimocho.model.Field;
+
+/**
+ * TODO comment this
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>29/05/12</pre>
+ */
+
+public class XrefFieldCrossReference implements SolrFieldConverter{
+    
+    public void indexFieldValues(Field field, SolrFieldName name, SolrInputDocument doc) {
+        
+        String db = field.get(CalimochoKeys.DB);
+        String value = field.get(CalimochoKeys.VALUE);
+        String nameField = name.toString();
+
+        if (db != null){
+            doc.addField(nameField, db);
+            if (value != null){
+                doc.addField(nameField, db+":"+value);
+            }
+        }
+        if (value != null){
+            doc.addField(nameField, value);
+        }
+    }
+}
