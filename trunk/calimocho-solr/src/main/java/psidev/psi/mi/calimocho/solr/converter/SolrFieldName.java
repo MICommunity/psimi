8dcb48e0280b9aea6a2f72af43a99facc2fdbe8b
maedi.breuer@gmail.com <maedi.breuer@gmail.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-05-29 15:25:28 +0000

    added converter class

---
+++ trunk/calimocho-solr/src/main/java/psidev/psi/mi/calimocho/solr/converter/SolrFieldName.java
@@ -0,0 +1,46 @@
+package psidev.psi.mi.calimocho.solr.converter;
+
+/**
+ * field names defined in psicquic solr schema xml
+ *
+ * @author kbreuer
+ */
+public enum SolrFieldName {
+
+   idA,
+   idB,
+   id,
+   alias,
+   identifier,
+   pubauth,
+   pubid,
+   taxidA,
+   taxidB,
+   species,
+   type,
+   detmethod,
+   interaction_id,
+   pbioroleA,
+   pbioroleB,
+   pbiorole,
+   ptypeA,
+   ptypeB,
+   ptype,
+   pxrefA,
+   pxrefB,
+   pxref,
+   xref,
+   annot,
+   udate,
+   negative,
+   complex,
+   ftypeA,
+   ftypeB,
+   ftype,
+   pmethodA,
+   pmethodB,
+   stc,
+   param,
+   recId;
+
+}
