ff97253a3f790246d2957f529c1d675a659ab7a8
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-05-29 12:45:18 +0000

    added calimocho-solr

--- calimocho-xgmml/src/test/java/org/hupo/psi/calimocho/xgmml/Playground.java
+++ calimocho-xgmml/src/test/java/org/hupo/psi/calimocho/xgmml/Playground.java
@@ -11,12 +11,11 @@ public class Playground {
 
     public static void main(String[] args) throws Exception {
 //        InputStream stream = Playground.class.getResourceAsStream("/META-INF/mitab/brca2.mitab25-ia.txt");
-        final InputStream stream = new FileInputStream("/tmp/mitab-interactions.txt");
-        final String outFile = "/tmp/tmp2/pride.xgmml";
+        final InputStream stream = new FileInputStream("/home/marine/Downloads/P62258_Q13.txt");
+        final String outFile = "/home/marine/Desktop/P62258_Q13.xgmml";
 
         ColumnBasedDocumentDefinition mitabDefinition = MitabDocumentDefinitionFactory.mitab25Intact();
         DocumentDefinition definition = new XGMMLDocumentDefinition();
-
 
         OutputStream os = new FileOutputStream(outFile);
 
@@ -24,14 +23,14 @@ public class Playground {
         converter.convert( stream, os );
 
         os.flush();
+        os.close();
 
-
-        BufferedReader in = new BufferedReader(new FileReader(outFile));
+        /*BufferedReader in = new BufferedReader(new FileReader(outFile));
         String str;
         while ((str = in.readLine()) != null) {
             System.out.println(str);
         }
-        in.close();
+        in.close();*/
 
     }
 
