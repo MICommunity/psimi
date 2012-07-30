5cc1b3cdababb1afd73bc33f4c10e2df75933da3
noedelta@gmail.com <noedelta@gmail.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-07-30 15:08:05 +0000

    Avoid the problem with integers write as decimal values (example 2.0)

--- trunk/calimocho-tab/src/main/java/org/hupo/psi/calimocho/tab/io/parser/PositiveIntegerFieldParser.java
+++ trunk/calimocho-tab/src/main/java/org/hupo/psi/calimocho/tab/io/parser/PositiveIntegerFieldParser.java
@@ -18,9 +18,9 @@ import org.hupo.psi.calimocho.tab.model.ColumnDefinition;
 public class PositiveIntegerFieldParser implements FieldParser{
 
     public Field parse( String fieldStr, ColumnDefinition columnDefinition ) throws IllegalFieldException {
-        int num = 0;
+        int num;
         try {
-            num = Integer.parseInt( fieldStr );
+            num = Integer.parseInt( fieldStr.substring(fieldStr.lastIndexOf(".")) );
 
             if (num < 0){
                 throw new IllegalFieldException( "Positive integer expected, found: "+fieldStr );
