db248b972323adacbc8ed4a337547b92f223b7c8
noedelta@gmail.com <noedelta@gmail.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-10-01 13:58:57 +0000

    AliasByTypeComparator: updated with display_short and display_long.

--- psimitab-parser/src/test/java/psidev/psi/mi/tab/converter/tab2graphml/AliasByTypeComparatorTest.java
+++ psimitab-parser/src/test/java/psidev/psi/mi/tab/converter/tab2graphml/AliasByTypeComparatorTest.java
@@ -25,16 +25,20 @@ public class AliasByTypeComparatorTest {
         aliases.add( new AliasImpl("uniprotkb", "lala", "orf name") );
         aliases.add( new AliasImpl("uniprotkb", "Small nuclear ribonuclear protein D homolog", "gene name synonym") );
         aliases.add( new AliasImpl("uniprotkb", "C6orf28", "gene name synonym") );
-        aliases.add( new AliasImpl("uniprotkb", "LSM2", "gene name") );
+		aliases.add( new AliasImpl("uniprotkb", "C6orf28", "display_short") );
+		aliases.add( new AliasImpl("uniprotkb", "C6orf28", "display_long") );
+		aliases.add( new AliasImpl("uniprotkb", "LSM2", "gene name") );
 
         final AliasComparator comparator = new AliasByTypeComparator();
         Collections.sort(aliases, comparator);
 
         Assert.assertTrue(comparator.hasMatchedAny());
 
-        Assert.assertEquals( "gene name", aliases.get( 0 ).getAliasType() );
-        Assert.assertEquals( "orf name", aliases.get( 1 ).getAliasType() );
-        Assert.assertEquals( "locus name", aliases.get( 2 ).getAliasType() );
-        Assert.assertEquals( "gene name synonym", aliases.get( 3 ).getAliasType() );
+		Assert.assertEquals( "display_short", aliases.get( 0 ).getAliasType() );
+		Assert.assertEquals( "display_long", aliases.get( 1 ).getAliasType() );
+		Assert.assertEquals( "gene name", aliases.get( 2 ).getAliasType() );
+        Assert.assertEquals( "orf name", aliases.get( 3 ).getAliasType() );
+        Assert.assertEquals( "locus name", aliases.get( 4 ).getAliasType() );
+        Assert.assertEquals( "gene name synonym", aliases.get( 5 ).getAliasType() );
     }
 }
