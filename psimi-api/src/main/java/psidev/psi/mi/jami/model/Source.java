0e7ef3f1efe6316134b9eefb4d133fb30aecc82f
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-11-28 08:27:57 +0000

    added more java doc and updated the source whic could have a postal address

--- psimi-api/src/main/java/psidev/psi/mi/jami/model/Source.java
+++ psimi-api/src/main/java/psidev/psi/mi/jami/model/Source.java
@@ -1,7 +1,7 @@
 package psidev.psi.mi.jami.model;
 
 /**
- * Source of the data. Normally an organisation
+ * Source of the data. Can be an institution, organisation, database, etc.
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
@@ -10,9 +10,42 @@ package psidev.psi.mi.jami.model;
 
 public interface Source extends CvTerm{
 
+    /**
+     * Public URL of the data source.
+     * It can be null
+     * @return the url
+     */
     public String getUrl();
+
+    /**
+     * Set the URL
+     * @param url
+     */
     public void setUrl(String url);
 
+    /**
+     * Postal address of the data source.
+     * It can be null
+     * @return the url
+     */
+    public String getPostalAddress();
+
+    /**
+     * Set the postal address
+     * @param address
+     */
+    public void setPostalAddress(String address);
+
+    /**
+     * Bibliographical reference of the data source.
+     * It can be null.
+     * @return the bibref
+     */
     public ExternalIdentifier getBibRef();
+
+    /**
+     * Set the bibliographical reference
+     * @param ref : publication reference
+     */
     public void setBibRef(ExternalIdentifier ref);
 }
