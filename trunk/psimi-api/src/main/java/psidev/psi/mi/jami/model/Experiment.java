d7e6c06f412946ebd0bc1ef528b5bc88291e4046
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-11-30 14:49:17 +0000

    Experiment : added collection of interactions and updated java doc

--- trunk/psimi-api/src/main/java/psidev/psi/mi/jami/model/Experiment.java
+++ trunk/psimi-api/src/main/java/psidev/psi/mi/jami/model/Experiment.java
@@ -1,9 +1,10 @@
 package psidev.psi.mi.jami.model;
 
+import java.util.Collection;
 import java.util.Set;
 
 /**
- * Experimental details about an interaction
+ * Experiment in which some interactions has been determined.
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
@@ -12,22 +13,77 @@ import java.util.Set;
 
 public interface Experiment {
 
+    /**
+     * The publication where the experiment has been described.
+     * It cannot be null.
+     * @return the publication
+     */
     public Publication getPublication();
+
+    /**
+     * Set the publication where the experiment has been described.
+     * @param publication : the publication
+     * @throws IllegalArgumentException if the publication is null
+     */
     public void setPublication(Publication publication);
 
-    public String getShortName();
-    public void setShortName(String name);
+    /**
+     * The short label of experiment.
+     * It can be null.
+     * @return the shortName
+     */
+    public String getShortLabel();
 
-    public String getFullName();
-    public void setFullName(String name);
+    /**
+     * Set the short label of an experiment
+     * @param name : short name
+     */
+    public void setShortLabel(String name);
 
+    /**
+     * Set of cross references for an experiment which can give more information about the experiment.
+     * It cannot be null and if the experiment does not have any xrefs, the method should return an empty set.
+     * @return the xrefs
+     */
     public Set<Xref> getXrefs();
 
+    /**
+     * Set of annotations for an experiment.
+     * It cannot be null. If the experiment does not have any annotations, the method should return an empty set.
+     * @return the annotations
+     */
     public Set<Annotation> getAnnotations();
 
+    /**
+     * The experimental method to determine the interaction. It is a controlled vocabulary term and cannot not be null.
+     * @return the interaction detection method
+     */
     public CvTerm getInteractionDetectionMethod();
+
+    /**
+     * Set the interaction detection method for this experiment
+     * @param term : the detction method
+     * @throws IllegalArgumentException if the interaction detection method is null
+     */
     public void setInteractionDetectionMethod(CvTerm term);
 
+    /**
+     * The host organism where the interaction took place in this experiment.
+     * It can be null.
+     * @return the host organism
+     */
     public Organism getHostOrganism();
+
+    /**
+     * Sets the host organism of an experiment
+     * @param organism : host organism
+     */
     public void setHostOrganism(Organism organism);
+
+    /**
+     * The interactions determined in this experiment.
+     * The collection cannot be null. If the experiment did not show any interactions, the method should return an empty collection
+     * @return the interactions
+     */
+    public Collection<Interaction> getInteractions();
 }
