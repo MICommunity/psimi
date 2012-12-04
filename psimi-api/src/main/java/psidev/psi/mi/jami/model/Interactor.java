4449da88a5e7f9db2dde48503f875b2df3d9a2b9
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-12-04 09:27:03 +0000

    Added examples in java doc

--- psimi-api/src/main/java/psidev/psi/mi/jami/model/Interactor.java
+++ psimi-api/src/main/java/psidev/psi/mi/jami/model/Interactor.java
@@ -3,7 +3,7 @@ package psidev.psi.mi.jami.model;
 import java.util.Set;
 
 /**
- * Molecule that interacts
+ * Molecule or complex of molecules that interacts with other molecules/complexes
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
@@ -12,23 +12,78 @@ import java.util.Set;
 
 public interface Interactor {
 
+    /**
+     * The short name of the interactor.
+     * It cannot be null or empty.
+     * @return the short name
+     */
     public String getShortName();
+
+    /**
+     * Sets the short name of an interactor
+     * @param name : short name
+     * @throws IllegalArgumentException if name is null or empty
+     */
     public void setShortName(String name);
 
+    /**
+     * The full name of the interactor.
+     * It can be null
+     * @return the full name
+     */
     public String getFullName();
+
+    /**
+     * Sets the full name of the interactor
+     * @param name : full name
+     */
     public void setFullName(String name);
 
+    /**
+     * Unique external identifier for the interactor. It should be unambiguous.
+     * The unique identifier can be null if the interactor is not registered in any external databases.
+     * @return the unique identifier
+     */
     public ExternalIdentifier getUniqueIdentifier();
+
+    /**
+     * Sets the unique identifier for this interactor
+     * @param identifier : unique identifier
+     */
     public void setUniqueIdentifier(ExternalIdentifier identifier);
 
+    /**
+     * Alternative identifiers for this interactor from other databases.
+     * The set cannot be null, when an interactor does not have any alternative identifiers, the method should return an empty set.
+     * @return the alternative identifier
+     */
     public Set<ExternalIdentifier> getAlternativeIdentifiers();
 
+    /**
+     * Set of checksums computed for this interactor.
+     * The set cannot be null so when an interactor does not have a chacksum, the method should return an empty set
+     * @return the set of checksums
+     */
     public Set<Checksum> getChecksums();
 
+    /**
+     * Set of other xrefs that give more information about the interactor.
+     * Ex: GO references to gives function/process/location information
+     * @return other xrefs
+     */
     public Set<Xref> getXrefs();
 
+    /**
+     * Set of annotations for an interactor.
+     * The set cannot be null and if the interactor does not have any annotations, it should return an empty set.
+     * @return the annotations
+     */
     public Set<Annotation> getAnnotations();
 
+    /**
+     *
+     * @return
+     */
     public Set<Alias> getAliases();
 
     public Organism getOrganism();
