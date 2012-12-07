c684704b5a28cd7eb26463aeeea4e17b45e7e954
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-12-07 11:03:00 +0000

    Added java doc to interactor interfaces

--- trunk/psimi-api/src/main/java/psidev/psi/mi/jami/model/NucleicAcid.java
+++ trunk/psimi-api/src/main/java/psidev/psi/mi/jami/model/NucleicAcid.java
@@ -1,7 +1,7 @@
 package psidev.psi.mi.jami.model;
 
 /**
- * Polymers of nucleotides
+ * Polymers of nucleotides which can interact with other molecules
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
@@ -10,9 +10,28 @@ package psidev.psi.mi.jami.model;
 
 public interface NucleicAcid extends Interactor {
 
+    /**
+     * The unique DDBJ/EMBL/GemBank identifier which identifies the nucleic acid.
+     * It can be null if it is not known and in such a case, the sequence should be provided.
+     * @return the DDBJ/EMBL/GemBank identifier
+     */
     public String getDdbjEmblGenbank();
+
+    /**
+     * Sets the DDBJ/EMBL/GemBank identifier
+     * @param id : DDBJ/EMBL/GemBank identifier
+     */
     public void setDdbjEmblGenbank(String id);
 
+    /**
+     * The sequence of nucleotides for this nucleic acid
+     * @return the sequence
+     */
     public String getSequence();
+
+    /**
+     * Sets the sequence
+     * @param sequence : the sequence
+     */
     public void setSequence(String sequence);
 }
