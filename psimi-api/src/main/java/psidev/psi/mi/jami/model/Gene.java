e14bb3f416fdc32725f44e8588d74a97bf68d734
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-12-07 11:03:00 +0000

    Added java doc to interactor interfaces

--- psimi-api/src/main/java/psidev/psi/mi/jami/model/Gene.java
+++ psimi-api/src/main/java/psidev/psi/mi/jami/model/Gene.java
@@ -10,15 +10,54 @@ package psidev.psi.mi.jami.model;
 
 public interface Gene extends Interactor{
 
+    /**
+     * The unique Ensembl accession which identifies the gene.
+     * It can be null
+     * Ex:ENSG00000172115
+     * @return the ENSEMBL accession
+     */
     public String getEnsembl();
+
+    /**
+     * Sets the Ensembl accession for this gene
+     * @param ac : Ensembl accession
+     */
     public void setEnsembl(String ac);
 
+    /**
+     * The Ensembl genome accession which identifies the gene.
+     * It can be null.
+     * Ex: ENSG00000139618
+     * @return the Ensembl geneome accessiom
+     */
     public String getEnsembleGenome();
     public void setEnsemblGenome(String ac);
 
+    /**
+     * The Entrez gene id which identifies this gene.
+     * It can be null
+     * Ex: 54205
+     * @return the Gene id
+     */
     public String getEntrezGeneId();
+
+    /**
+     * Sets the Entrez gene id
+     * @param id: Entrez gene id
+     */
     public void setEntrezGeneId(String id);
 
-    public String getRefseqAc();
-    public void setRefseqAc(String ac);
+    /**
+     * The refseq accession which identifies the gene.
+     * It can be null
+     * Ex: NM_001071821.1
+     * @return the Refseq accession
+     */
+    public String getRefseq();
+
+    /**
+     * Sets the refseq accession
+     * @param ac: the refseq accession
+     */
+    public void setRefseq(String ac);
 }
