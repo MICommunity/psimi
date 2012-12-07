f01edcb3b37467c53caef5a799bee328586f40cf
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-12-07 13:29:21 +0000

    Updated feature interface

---
+++ psimi-api/src/main/java/psidev/psi/mi/jami/model/BiologicalFeature.java
@@ -0,0 +1,13 @@
+package psidev.psi.mi.jami.model;
+
+/**
+ * 	Biological property of a participant that may be involved with or interfere with the binding of a molecule.
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>23/11/12</pre>
+ */
+
+public interface BiologicalFeature extends Feature<BiologicalFeature>{
+
+}
