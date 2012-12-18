e94a73b45b20dd3e63cbe9fc6a89e99071a483fd
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-12-18 15:58:30 +0000

    Added Alias comparators

---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/utils/comparator/AliasComparator.java
@@ -0,0 +1,72 @@
+package psidev.psi.mi.jami.utils.comparator;
+
+import psidev.psi.mi.jami.model.Alias;
+import psidev.psi.mi.jami.model.CvTerm;
+
+import java.util.Comparator;
+
+/**
+ * Abstract comparator for aliases.
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>18/12/12</pre>
+ */
+
+public abstract class AliasComparator<T extends CvTermComparator> implements Comparator<Alias>{
+
+    protected T typeComparator;
+
+    public AliasComparator(){
+        instantiateTypeComparator();
+    }
+
+    protected abstract void instantiateTypeComparator();
+
+    public int compare(Alias alias1, Alias alias2) {
+        int EQUAL = 0;
+        int BEFORE = -1;
+        int AFTER = 1;
+
+        if (alias1 == null && alias2 == null){
+            return EQUAL;
+        }
+        else if (alias1 == null){
+            return AFTER;
+        }
+        else if (alias2 == null){
+            return BEFORE;
+        }
+        else {
+            CvTerm type1 = alias1.getType();
+            CvTerm type2 = alias2.getType();
+            // no types so relies on the name only
+            if (type1 == null && type2 == null){
+                // check names which cannot be null
+                String name1 = alias1.getName();
+                String name2 = alias2.getName();
+
+                return name1.compareTo(name2);
+            }
+            // type 1 is null so it comes after
+            else if (type1 == null){
+                return AFTER;
+            }
+            // type 2 is null so it comes after
+            else if (type2 == null){
+                return BEFORE;
+            }
+            else {
+                int comp = typeComparator.compare(type1, type2);
+                if (comp != 0){
+                    return comp;
+                }
+                // check identifiers which cannot be null
+                String name1 = alias1.getName();
+                String name2 = alias2.getName();
+
+                return name1.compareTo(name2);
+            }
+        }
+    }
+}
