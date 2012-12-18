89d4ce7ffade660e775ab66c9eca2119d07e4090
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2012-12-18 16:11:54 +0000

    Added AnnotationConverters

---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/utils/comparator/AnnotationComparator.java
@@ -0,0 +1,67 @@
+package psidev.psi.mi.jami.utils.comparator;
+
+import psidev.psi.mi.jami.model.Annotation;
+import psidev.psi.mi.jami.model.CvTerm;
+
+import java.util.Comparator;
+
+/**
+ * Abstract comparator for annotations.
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>18/12/12</pre>
+ */
+
+public abstract class AnnotationComparator<T extends CvTermComparator> implements Comparator<Annotation> {
+
+    protected T topicComparator;
+
+    public AnnotationComparator(){
+        instantiateAnnotationComparator();
+    }
+
+    protected abstract void instantiateAnnotationComparator();
+
+    public int compare(Annotation annotation1, Annotation annotation2) {
+        int EQUAL = 0;
+        int BEFORE = -1;
+        int AFTER = 1;
+
+        if (annotation1 == null && annotation2 == null){
+            return EQUAL;
+        }
+        else if (annotation1 == null){
+            return AFTER;
+        }
+        else if (annotation2 == null){
+            return BEFORE;
+        }
+        else {
+            CvTerm topic1 = annotation1.getTopic();
+            CvTerm topic2 = annotation2.getTopic();
+
+            int comp = topicComparator.compare(topic1, topic2);
+            if (comp != 0){
+                return comp;
+            }
+            // check annotation text
+            String text1 = annotation1.getValue();
+            String text2 = annotation2.getValue();
+
+            if (text1 == null && text2 == null){
+                return EQUAL;
+            }
+            else if (text1 == null){
+                return AFTER;
+            }
+            else if (text2 == null){
+                return BEFORE;
+            }
+            else {
+
+                return text1.toLowerCase().compareTo(text2.toLowerCase());
+            }
+        }
+    }
+}
---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/utils/comparator/DefaultAnnotationComparator.java
@@ -0,0 +1,21 @@
+package psidev.psi.mi.jami.utils.comparator;
+
+/**
+ * Default annotation comparator
+ *
+ * - Two annotations which are null are equals
+ * - The annotation which is not null is before null.
+ * - use DefaultCvTermComparator to compare the topics. If they are equals, compares the values (case insensitive)
+ * - If both annotations have same topic, the one with a null value is always after the one with a non null value.
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>18/12/12</pre>
+ */
+
+public class DefaultAnnotationComparator extends AnnotationComparator<DefaultCvTermComparator>{
+    @Override
+    protected void instantiateAnnotationComparator() {
+        this.topicComparator = new DefaultCvTermComparator();
+    }
+}
