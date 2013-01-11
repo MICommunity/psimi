97dbcd910cae249694543af545b2cf98732f9e3b
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-01-11 15:21:55 +0000

    Created InteractorCandidates

---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/model/InteractorCandidates.java
@@ -0,0 +1,21 @@
+package psidev.psi.mi.jami.model;
+
+import java.util.Set;
+
+/**
+ * InteractorCandidates represents a list of potential interactors but we cannot determine which one interacts.
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>11/01/13</pre>
+ */
+
+public interface InteractorCandidates extends Interactor{
+
+    /**
+     * The set of potential interactors.
+     * It cannot be null. If we don't have any candidates, the method should return an empty set.
+     * @return
+     */
+    public Set<Interactor> getCandidates();
+}
