1c1be416a2a40e6efdd7e281a5959f0a377b87dd
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-01-15 14:00:06 +0000

    Added GeneComparator

---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/utils/comparator/interactor/ExactGeneComparator.java
@@ -0,0 +1,41 @@
+package psidev.psi.mi.jami.utils.comparator.interactor;
+
+import psidev.psi.mi.jami.model.Gene;
+
+/**
+ * Exact genes comparator.
+ * It will look first at ensembl identifier if both are set. If the ensembl identifiers are not both set, it will look at the
+ * ensemblGenome identifiers. If at least one ensemblGemome identifiers is not set, it will look at the entrez/gene id. If at least one entrez/gene id is not set, it will look at the refseq identifiers.
+ * If the properties of a gene were not enough to compare the genes, it will use ExactInteractorComparator to compare the interactor properties
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>15/01/13</pre>
+ */
+
+public class ExactGeneComparator extends GeneComparator {
+
+    /**
+     * Creates a new ExactGeneComparator. It will uses an ExactInteractorComparator to compares interactor properties
+     * @param interactorComparator : the exactInteractorComparator to compare interactor properties
+     */
+    public ExactGeneComparator(ExactInteractorComparator interactorComparator) {
+        super(interactorComparator);
+    }
+
+    @Override
+    /**
+     * It will look first at ensembl identifier if both are set. If the ensembl identifiers are not both set, it will look at the
+     * ensemblGenome identifiers. If at least one ensemblGemome identifiers is not set, it will look at the entrez/gene id. If at least one entrez/gene id is not set, it will look at the refseq identifiers.
+     * If the properties of a gene were not enough to compare the genes, it will use ExactInteractorComparator to compare the interactor properties
+     *
+     */
+    public int compare(Gene gene1, Gene gene2) {
+        return super.compare(gene1, gene2);
+    }
+
+    @Override
+    public ExactInteractorComparator getInteractorComparator() {
+        return (ExactInteractorComparator) this.interactorComparator;
+    }
+}
---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/utils/comparator/interactor/UnambiguousGeneComparator.java
@@ -0,0 +1,56 @@
+package psidev.psi.mi.jami.utils.comparator.interactor;
+
+import psidev.psi.mi.jami.model.Gene;
+
+/**
+ * Unambiguous gene comparator.
+ * It will look first at ensembl identifier if both are set. If the ensembl identifiers are not both set, it will look at the
+ * ensemblGenome identifiers. If at least one ensemblGemome identifiers is not set, it will look at the entrez/gene id. If at least one entrez/gene id is not set, it will look at the refseq identifiers.
+ * If the properties of a gene were not enough to compare the genes, it will use UnambiguousInteractorComparator to compare the interactor properties
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>15/01/13</pre>
+ */
+
+public class UnambiguousGeneComparator extends GeneComparator {
+
+    private static UnambiguousGeneComparator unambiguousGeneComparator;
+
+    /**
+     * Creates a new UnambiguousGeneComparator. It will uses a UnambiguousInteractorComparator to compare interactor properties
+     */
+    public UnambiguousGeneComparator(){
+        super(new UnambiguousInteractorComparator());
+    }
+
+    @Override
+    /**
+     * It will look first at ensembl identifier if both are set. If the ensembl identifiers are not both set, it will look at the
+     * ensemblGenome identifiers. If at least one ensemblGemome identifiers is not set, it will look at the entrez/gene id. If at least one entrez/gene id is not set, it will look at the refseq identifiers.
+     * If the properties of a gene were not enough to compare the genes, it will use UnambiguousInteractorComparator to compare the interactor properties
+     *
+     */
+    public int compare(Gene gene1, Gene gene2) {
+        return super.compare(gene1, gene2);
+    }
+
+    @Override
+    public UnambiguousInteractorComparator getInteractorComparator() {
+        return (UnambiguousInteractorComparator) this.interactorComparator;
+    }
+
+    /**
+     * Use UnambiguousGeneComparator to know if two genes are equals.
+     * @param gene1
+     * @param gene2
+     * @return true if the two genes are equal
+     */
+    public static boolean areEquals(Gene gene1, Gene gene2){
+        if (unambiguousGeneComparator == null){
+            unambiguousGeneComparator = new UnambiguousGeneComparator();
+        }
+
+        return unambiguousGeneComparator.compare(gene1, gene2) == 0;
+    }
+}
