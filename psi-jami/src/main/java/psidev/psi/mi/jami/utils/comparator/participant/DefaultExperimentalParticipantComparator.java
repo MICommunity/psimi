ed74388b7700c08d55c77aa1979e69fb439a1668
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-01-17 13:59:54 +0000

    Created ExperimentalParticipantComparators

---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/utils/comparator/participant/DefaultExperimentalParticipantComparator.java
@@ -0,0 +1,67 @@
+package psidev.psi.mi.jami.utils.comparator.participant;
+
+import psidev.psi.mi.jami.model.ExperimentalParticipant;
+import psidev.psi.mi.jami.utils.comparator.cv.DefaultCvTermComparator;
+import psidev.psi.mi.jami.utils.comparator.organism.DefaultOrganismComparator;
+
+/**
+ * Default Experimental participant comparator.
+ *
+ * It will first compares experimental roles using DefaultCvTermComparator. If both experimental roles are equals, it
+ * will look at the identification methods using DefaultCvTermComparator. If both identification methods are equals, it will
+ * look at the experimental preparations using DefaultCvTermComparator. If both experimental preparations are equals, it will
+ * look at the expressed in organisms using DefaultOrganismComparator.
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>17/01/13</pre>
+ */
+
+public class DefaultExperimentalParticipantComparator extends ExperimentalParticipantComparator{
+
+    private static DefaultExperimentalParticipantComparator defaultExperimentalParticipantComparator;
+
+    /**
+     * Creates a new DefaultExperimentalParticipantComparator. It will use a DefaultParticipantComparator to compare
+     * the basic properties of a participant, a DefaultCvTermComparator to compare experimental roles, preparations and identification methods
+     * and a DefaultOrganismComparator to compare expressed in Organisms
+     */
+    public DefaultExperimentalParticipantComparator() {
+        super(new DefaultParticipantComparator(), new DefaultCvTermComparator(), new DefaultOrganismComparator());
+    }
+
+    @Override
+    public DefaultParticipantComparator getParticipantComparator() {
+        return (DefaultParticipantComparator) this.participantComparator;
+    }
+
+    @Override
+    public DefaultOrganismComparator getOrganismComparator() {
+        return (DefaultOrganismComparator) this.organismComparator;
+    }
+
+    @Override
+    /**
+     * It will first compares experimental roles using DefaultCvTermComparator. If both experimental roles are equals, it
+     * will look at the identification methods using DefaultCvTermComparator. If both identification methods are equals, it will
+     * look at the experimental preparations using DefaultCvTermComparator. If both experimental preparations are equals, it will
+     * look at the expressed in organisms using DefaultOrganismComparator.
+     */
+    public int compare(ExperimentalParticipant experimentalParticipant1, ExperimentalParticipant experimentalParticipant2) {
+        return super.compare(experimentalParticipant1, experimentalParticipant2);
+    }
+
+    /**
+     * Use DefaultExperimentalParticipantComparator to know if two experimental participants are equals.
+     * @param experimentalParticipant1
+     * @param component2
+     * @return true if the two experimental participants are equal
+     */
+    public static boolean areEquals(ExperimentalParticipant experimentalParticipant1, ExperimentalParticipant component2){
+        if (defaultExperimentalParticipantComparator == null){
+            defaultExperimentalParticipantComparator = new DefaultExperimentalParticipantComparator();
+        }
+
+        return defaultExperimentalParticipantComparator.compare(experimentalParticipant1, component2) == 0;
+    }
+}
---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/utils/comparator/participant/UnambiguousExperimentalParticipantInteractorComparator.java
@@ -0,0 +1,69 @@
+package psidev.psi.mi.jami.utils.comparator.participant;
+
+import psidev.psi.mi.jami.model.ExperimentalParticipant;
+import psidev.psi.mi.jami.utils.comparator.cv.UnambiguousCvTermComparator;
+import psidev.psi.mi.jami.utils.comparator.organism.UnambiguousOrganismComparator;
+
+/**
+ * Unambiguous experimental participant comparator based on the interactor only.
+ * It will compare the basic properties of an experimental participant using UnambiguousParticipantInteractorComparator.
+ *
+ * This comparator will ignore all the other properties of an experimental participant.
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>17/01/13</pre>
+ */
+
+public class UnambiguousExperimentalParticipantInteractorComparator extends ExperimentalParticipantComparator{
+    private static UnambiguousExperimentalParticipantInteractorComparator unambiguousExperimentalParticipantInteractorComparator;
+
+    /**
+     * Creates a new UnambiguousExperimentalParticipantInteractorComparator. It will use a UnambiguousParticipantInteractorComparator to compare
+     * the basic properties of a participant.
+     */
+    public UnambiguousExperimentalParticipantInteractorComparator() {
+        super(new UnambiguousParticipantInteractorComparator(), new UnambiguousCvTermComparator(), new UnambiguousOrganismComparator());
+    }
+
+    @Override
+    /**
+     * It will compare the basic properties of an experimental participant using UnambiguousParticipantInteractorComparator.
+     *
+     * This comparator will ignore all the other properties of an experimental participant.
+     */
+    public int compare(ExperimentalParticipant experimentalParticipant1, ExperimentalParticipant experimentalParticipant2) {
+        int EQUAL = 0;
+        int BEFORE = -1;
+        int AFTER = 1;
+
+        if (experimentalParticipant1 == null && experimentalParticipant1 == null){
+            return EQUAL;
+        }
+        else if (experimentalParticipant1 == null){
+            return AFTER;
+        }
+        else if (experimentalParticipant2 == null){
+            return BEFORE;
+        }
+        else {
+
+            // compares interactor participant properties
+            return participantComparator.compare(experimentalParticipant1, experimentalParticipant2);
+        }
+    }
+
+    /**
+     * Use UnambiguousExperimentalParticipantInteractorComparator to know if two experimental participants are equals.
+     * @param experimentalParticipant1
+     * @param component2
+     * @return true if the two experimental participants are equal
+     */
+    public static boolean areEquals(ExperimentalParticipant experimentalParticipant1, ExperimentalParticipant component2){
+        if (unambiguousExperimentalParticipantInteractorComparator == null){
+            unambiguousExperimentalParticipantInteractorComparator = new UnambiguousExperimentalParticipantInteractorComparator();
+        }
+
+        return unambiguousExperimentalParticipantInteractorComparator.compare(experimentalParticipant1, component2) == 0;
+    }
+}
