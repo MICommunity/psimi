1c57294afe5c3c371bc6fe51b7e94740f3f05fe1
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-01-22 16:55:29 +0000

    Created default implementation for Experiment

---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/model/impl/DefaultExperiment.java
@@ -0,0 +1,130 @@
+package psidev.psi.mi.jami.model.impl;
+
+import psidev.psi.mi.jami.model.*;
+import psidev.psi.mi.jami.utils.comparator.experiment.UnambiguousExperimentComparator;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Default implementation for Experiment
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>22/01/13</pre>
+ */
+
+public class DefaultExperiment implements Experiment {
+
+    private Publication publication;
+    private String shortLabel;
+    private Set<Xref> xrefs;
+    private Set<Annotation> annotations;
+    private CvTerm interactionDetectionMethod;
+    private Organism horstOrganism;
+    private Collection<ExperimentalInteraction> interactions;
+
+    public DefaultExperiment(Publication publication, CvTerm interactionDetectionMethod){
+         if (publication == null){
+             throw new IllegalArgumentException("The publication is required and cannot be null");
+         }
+        this.publication = publication;
+        if (interactionDetectionMethod == null){
+            throw new IllegalArgumentException("The interaction detection method is required and cannot be null");
+        }
+        this.interactionDetectionMethod = interactionDetectionMethod;
+
+        this.xrefs = new HashSet<Xref>();
+        this.annotations = new HashSet<Annotation>();
+        this.interactions = new HashSet<ExperimentalInteraction>();
+    }
+
+    public DefaultExperiment(String shortLabel, Publication publication, CvTerm interactionDetectionMethod){
+        this(publication, interactionDetectionMethod);
+        this.shortLabel = shortLabel;
+    }
+
+    public DefaultExperiment(Publication publication, CvTerm interactionDetectionMethod, Organism organism){
+        this(publication, interactionDetectionMethod);
+        this.horstOrganism = organism;
+    }
+
+    public DefaultExperiment(String shortLabel, Publication publication, CvTerm interactionDetectionMethod, Organism organism){
+        this(shortLabel, publication, interactionDetectionMethod);
+        this.horstOrganism = organism;
+    }
+
+    public Publication getPublication() {
+        return this.publication;
+    }
+
+    public void setPublication(Publication publication) {
+        if (publication == null){
+            throw new IllegalArgumentException("The publication cannot be null");
+        }
+        this.publication = publication;
+    }
+
+    public String getShortLabel() {
+        return this.shortLabel;
+    }
+
+    public void setShortLabel(String name) {
+        this.shortLabel = name;
+    }
+
+    public Set<Xref> getXrefs() {
+        return this.xrefs;
+    }
+
+    public Set<Annotation> getAnnotations() {
+        return this.annotations;
+    }
+
+    public CvTerm getInteractionDetectionMethod() {
+        return this.interactionDetectionMethod;
+    }
+
+    public void setInteractionDetectionMethod(CvTerm term) {
+        if (term == null){
+            throw new IllegalArgumentException("The interaction detection method cannot be null");
+        }
+        this.interactionDetectionMethod = term;
+    }
+
+    public Organism getHostOrganism() {
+        return this.horstOrganism;
+    }
+
+    public void setHostOrganism(Organism organism) {
+        this.horstOrganism = organism;
+    }
+
+    public Collection<ExperimentalInteraction> getInteractions() {
+        return this.interactions;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }
+
+        if (!(o instanceof Experiment)){
+            return false;
+        }
+
+        return UnambiguousExperimentComparator.areEquals(this, (Experiment) o);
+    }
+
+    @Override
+    public int hashCode() {
+        return UnambiguousExperimentComparator.hashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return publication.toString() + "( " + interactionDetectionMethod.toString() + (horstOrganism != null ? ", " + horstOrganism.toString():"") + " )";
+    }
+}
