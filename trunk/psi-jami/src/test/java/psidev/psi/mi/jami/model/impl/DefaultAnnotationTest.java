2804a960722a826a682add6cbbb7e4deabecc32b
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-02-05 15:47:49 +0000

    Added unit tests for Annotation and AnnotationComparator

---
+++ trunk/psi-jami/src/test/java/psidev/psi/mi/jami/model/impl/DefaultAnnotationTest.java
@@ -0,0 +1,38 @@
+package psidev.psi.mi.jami.model.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import psidev.psi.mi.jami.model.Annotation;
+import psidev.psi.mi.jami.utils.factory.CvTermFactory;
+
+/**
+ * Unit tester for DefaultAnnotation
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>05/02/13</pre>
+ */
+
+public class DefaultAnnotationTest {
+
+    @Test
+    public void test_create_annotation_topic_and_value() throws Exception {
+        Annotation annotation = new DefaultAnnotation(CvTermFactory.createComplexPhysicalProperties(), "Molecular mass = 154 kDa");
+
+        Assert.assertEquals(CvTermFactory.createComplexPhysicalProperties(), annotation.getTopic());
+        Assert.assertEquals("Molecular mass = 154 kDa", annotation.getValue());
+    }
+
+    @Test
+    public void test_create_annotation_topic_only() throws Exception {
+        Annotation annotation = new DefaultAnnotation(CvTermFactory.createComplexPhysicalProperties());
+
+        Assert.assertEquals(CvTermFactory.createComplexPhysicalProperties(), annotation.getTopic());
+        Assert.assertNull(annotation.getValue());
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void test_create_annotation_no_topic() throws Exception {
+        Annotation annotation = new DefaultAnnotation(null, "Molecular mass = 154 kDa");
+    }
+}
