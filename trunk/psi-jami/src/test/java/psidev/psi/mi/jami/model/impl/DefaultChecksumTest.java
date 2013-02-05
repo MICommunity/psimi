31c07afe17f87e140026c81c9dcdc0591aa10ab7
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-02-05 16:33:58 +0000

    Added unit tests for Checksum and ChecksumComparator

---
+++ trunk/psi-jami/src/test/java/psidev/psi/mi/jami/model/impl/DefaultChecksumTest.java
@@ -0,0 +1,35 @@
+package psidev.psi.mi.jami.model.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import psidev.psi.mi.jami.model.Checksum;
+import psidev.psi.mi.jami.utils.factory.CvTermFactory;
+
+/**
+ * Unit tester for Checksum
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>05/02/13</pre>
+ */
+
+public class DefaultChecksumTest {
+
+    @Test
+    public void test_create_checksum_method_and_value() throws Exception {
+        Checksum checksum = new DefaultChecksum(CvTermFactory.createRogid(), "xxxxxxxxx");
+
+        Assert.assertEquals(CvTermFactory.createRogid(), checksum.getMethod());
+        Assert.assertEquals("xxxxxxxxx", checksum.getValue());
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void test_create_checksum_no_method() throws Exception {
+        Checksum checksum = new DefaultChecksum(null, "xxxxxxxxx");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void test_create_checksum_no_value() throws Exception {
+        Checksum checksum = new DefaultChecksum(CvTermFactory.createRogid(), null);
+    }
+}
