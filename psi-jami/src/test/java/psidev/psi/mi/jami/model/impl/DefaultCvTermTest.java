8ea3792d7749bf43966a2da85e9b37a264647e86
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-02-08 11:16:17 +0000

    Added more unit tests for DefaultCvTerm

--- psi-jami/src/test/java/psidev/psi/mi/jami/model/impl/DefaultCvTermTest.java
+++ psi-jami/src/test/java/psidev/psi/mi/jami/model/impl/DefaultCvTermTest.java
@@ -81,4 +81,222 @@ public class DefaultCvTermTest {
         Assert.assertNotNull(uniprotkb.getSynonyms());
         Assert.assertNotNull(uniprotkb.getXrefs());
     }
+
+    @Test
+    public void test_create_cvTerm_mi() throws Exception {
+        CvTerm uniprotkb = new DefaultCvTerm("uniprotkb", Xref.UNIPROTKB_MI);
+
+        Assert.assertEquals("uniprotkb", uniprotkb.getShortName());
+        Assert.assertEquals(Xref.UNIPROTKB_MI, uniprotkb.getMIIdentifier());
+        Assert.assertNotNull(uniprotkb.getAnnotations());
+        Assert.assertNotNull(uniprotkb.getParents());
+        Assert.assertNotNull(uniprotkb.getChildren());
+        Assert.assertNotNull(uniprotkb.getSynonyms());
+        Assert.assertNotNull(uniprotkb.getXrefs());
+
+        Assert.assertTrue(uniprotkb.getIdentifiers().size() == 1);
+        Assert.assertEquals(Xref.UNIPROTKB_MI, uniprotkb.getIdentifiers().iterator().next().getId());
+        Assert.assertEquals(CvTerm.PSI_MI, uniprotkb.getIdentifiers().iterator().next().getDatabase().getShortName());
+        Assert.assertEquals(Xref.IDENTITY, uniprotkb.getIdentifiers().iterator().next().getQualifier().getShortName());
+    }
+
+    @Test
+    public void test_create_cvTerm_mod() throws Exception {
+        CvTerm term = new DefaultCvTerm("modTerm", new DefaultXref(CvTermFactory.createPsiModDatabase(), "MOD:xxx"));
+
+        Assert.assertEquals("modTerm", term.getShortName());
+        Assert.assertEquals("MOD:xxx", term.getMODIdentifier());
+        Assert.assertNull(term.getMIIdentifier());
+        Assert.assertNotNull(term.getAnnotations());
+        Assert.assertNotNull(term.getParents());
+        Assert.assertNotNull(term.getChildren());
+        Assert.assertNotNull(term.getSynonyms());
+        Assert.assertNotNull(term.getXrefs());
+
+        Assert.assertTrue(term.getIdentifiers().size() == 1);
+        Assert.assertEquals("MOD:xxx", term.getIdentifiers().iterator().next().getId());
+        Assert.assertEquals(CvTerm.PSI_MOD, term.getIdentifiers().iterator().next().getDatabase().getShortName());
+        Assert.assertNull(term.getIdentifiers().iterator().next().getQualifier());
+    }
+
+    @Test
+    public void test_set_cvTerm_mi() throws Exception {
+        CvTerm uniprotkb = new DefaultCvTerm("uniprotkb");
+        uniprotkb.setMIIdentifier(Xref.UNIPROTKB_MI);
+
+        Assert.assertEquals("uniprotkb", uniprotkb.getShortName());
+        Assert.assertEquals(Xref.UNIPROTKB_MI, uniprotkb.getMIIdentifier());
+        Assert.assertNotNull(uniprotkb.getAnnotations());
+        Assert.assertNotNull(uniprotkb.getParents());
+        Assert.assertNotNull(uniprotkb.getChildren());
+        Assert.assertNotNull(uniprotkb.getSynonyms());
+        Assert.assertNotNull(uniprotkb.getXrefs());
+
+        Assert.assertTrue(uniprotkb.getIdentifiers().size() == 1);
+        Assert.assertEquals(Xref.UNIPROTKB_MI, uniprotkb.getIdentifiers().iterator().next().getId());
+        Assert.assertEquals(CvTerm.PSI_MI, uniprotkb.getIdentifiers().iterator().next().getDatabase().getShortName());
+        Assert.assertEquals(Xref.IDENTITY, uniprotkb.getIdentifiers().iterator().next().getQualifier().getShortName());
+    }
+
+    @Test
+    public void test_set_cvTerm_mod() throws Exception {
+        CvTerm term = new DefaultCvTerm("modTerm");
+        term.setMODIdentifier("MOD:xxx");
+
+        Assert.assertEquals("modTerm", term.getShortName());
+        Assert.assertEquals("MOD:xxx", term.getMODIdentifier());
+        Assert.assertNull(term.getMIIdentifier());
+        Assert.assertNotNull(term.getAnnotations());
+        Assert.assertNotNull(term.getParents());
+        Assert.assertNotNull(term.getChildren());
+        Assert.assertNotNull(term.getSynonyms());
+        Assert.assertNotNull(term.getXrefs());
+
+        Assert.assertTrue(term.getIdentifiers().size() == 1);
+        Assert.assertEquals("MOD:xxx", term.getIdentifiers().iterator().next().getId());
+        Assert.assertEquals(CvTerm.PSI_MOD, term.getIdentifiers().iterator().next().getDatabase().getShortName());
+        Assert.assertEquals(Xref.IDENTITY, term.getIdentifiers().iterator().next().getQualifier().getShortName());
+    }
+
+    @Test
+    public void test_reset_cvTerm_mi() throws Exception {
+        CvTerm uniprotkb = new DefaultCvTerm("uniprotkb", Xref.UNIPROTKB_MI);
+        uniprotkb.setMIIdentifier("MI:xxx");
+
+        Assert.assertEquals("uniprotkb", uniprotkb.getShortName());
+        Assert.assertEquals("MI:xxx", uniprotkb.getMIIdentifier());
+        Assert.assertNotNull(uniprotkb.getAnnotations());
+        Assert.assertNotNull(uniprotkb.getParents());
+        Assert.assertNotNull(uniprotkb.getChildren());
+        Assert.assertNotNull(uniprotkb.getSynonyms());
+        Assert.assertNotNull(uniprotkb.getXrefs());
+
+        Assert.assertTrue(uniprotkb.getIdentifiers().size() == 1);
+        Assert.assertEquals("MI:xxx", uniprotkb.getIdentifiers().iterator().next().getId());
+        Assert.assertEquals(CvTerm.PSI_MI, uniprotkb.getIdentifiers().iterator().next().getDatabase().getShortName());
+        Assert.assertEquals(Xref.IDENTITY, uniprotkb.getIdentifiers().iterator().next().getQualifier().getShortName());
+    }
+
+    @Test
+    public void test_reset_cvTerm_mod() throws Exception {
+        CvTerm term = new DefaultCvTerm("modTerm", new DefaultXref(CvTermFactory.createPsiModDatabase(), "MOD:xxx"));
+        term.setMODIdentifier("MOD:xxx3");
+
+        Assert.assertEquals("modTerm", term.getShortName());
+        Assert.assertEquals("MOD:xxx3", term.getMODIdentifier());
+        Assert.assertNull(term.getMIIdentifier());
+        Assert.assertNotNull(term.getAnnotations());
+        Assert.assertNotNull(term.getParents());
+        Assert.assertNotNull(term.getChildren());
+        Assert.assertNotNull(term.getSynonyms());
+        Assert.assertNotNull(term.getXrefs());
+
+        Assert.assertTrue(term.getIdentifiers().size() == 1);
+        Assert.assertEquals("MOD:xxx3", term.getIdentifiers().iterator().next().getId());
+        Assert.assertEquals(CvTerm.PSI_MOD, term.getIdentifiers().iterator().next().getDatabase().getShortName());
+        Assert.assertEquals(Xref.IDENTITY, term.getIdentifiers().iterator().next().getQualifier().getShortName());
+    }
+
+    @Test
+    public void test_add_cvTerm_mod_identity_replace_mod_without_identity() throws Exception {
+        CvTerm term = new DefaultCvTerm("modTerm", new DefaultXref(CvTermFactory.createPsiModDatabase(), "MOD:xxx"));
+        term.getIdentifiers().add(new DefaultXref(CvTermFactory.createPsiModDatabase(), "MOD:xxx3", CvTermFactory.createIdentityQualifier()));
+
+        Assert.assertEquals("modTerm", term.getShortName());
+        Assert.assertEquals("MOD:xxx3", term.getMODIdentifier());
+        Assert.assertNull(term.getMIIdentifier());
+        Assert.assertNotNull(term.getAnnotations());
+        Assert.assertNotNull(term.getParents());
+        Assert.assertNotNull(term.getChildren());
+        Assert.assertNotNull(term.getSynonyms());
+        Assert.assertNotNull(term.getXrefs());
+
+        Assert.assertTrue(term.getIdentifiers().size() == 2);
+    }
+
+    @Test
+    public void test_add_cvTerm_mi_identity_replace_mi_without_identity() throws Exception {
+        CvTerm term = new DefaultCvTerm("miTerm", new DefaultXref(CvTermFactory.createPsiMiDatabase(), "MI:xxx"));
+        term.getIdentifiers().add(new DefaultXref(CvTermFactory.createPsiMiDatabase(), "MI:xxx3", CvTermFactory.createIdentityQualifier()));
+
+        Assert.assertEquals("miTerm", term.getShortName());
+        Assert.assertEquals("MI:xxx3", term.getMIIdentifier());
+        Assert.assertNull(term.getMODIdentifier());
+        Assert.assertNotNull(term.getAnnotations());
+        Assert.assertNotNull(term.getParents());
+        Assert.assertNotNull(term.getChildren());
+        Assert.assertNotNull(term.getSynonyms());
+        Assert.assertNotNull(term.getXrefs());
+
+        Assert.assertTrue(term.getIdentifiers().size() == 2);
+    }
+
+    @Test
+    public void test_remove_cvTerm_mod_identity() throws Exception {
+        Xref mod = new DefaultXref(CvTermFactory.createPsiModDatabase(), "MOD:xxx");
+        CvTerm term = new DefaultCvTerm("modTerm", mod);
+        term.getIdentifiers().remove(mod);
+
+        Assert.assertEquals("modTerm", term.getShortName());
+        Assert.assertNull(term.getMODIdentifier());
+        Assert.assertNull(term.getMIIdentifier());
+        Assert.assertNotNull(term.getAnnotations());
+        Assert.assertNotNull(term.getParents());
+        Assert.assertNotNull(term.getChildren());
+        Assert.assertNotNull(term.getSynonyms());
+        Assert.assertNotNull(term.getXrefs());
+
+        Assert.assertTrue(term.getIdentifiers().isEmpty());
+    }
+
+    @Test
+    public void test_remove_cvTerm_mi_identity() throws Exception {
+        Xref mi = new DefaultXref(CvTermFactory.createPsiMiDatabase(), "MI:xxx");
+        CvTerm term = new DefaultCvTerm("miTerm", mi);
+        term.getIdentifiers().remove(mi);
+
+        Assert.assertEquals("miTerm", term.getShortName());
+        Assert.assertNull(term.getMIIdentifier());
+        Assert.assertNull(term.getMODIdentifier());
+        Assert.assertNotNull(term.getAnnotations());
+        Assert.assertNotNull(term.getParents());
+        Assert.assertNotNull(term.getChildren());
+        Assert.assertNotNull(term.getSynonyms());
+        Assert.assertNotNull(term.getXrefs());
+
+        Assert.assertTrue(term.getIdentifiers().isEmpty());
+    }
+
+    @Test
+    public void test_create_cvTerm_mi_remove_all_mi() throws Exception {
+        CvTerm uniprotkb = new DefaultCvTerm("uniprotkb", Xref.UNIPROTKB_MI);
+        uniprotkb.setMIIdentifier(null);
+
+        Assert.assertEquals("uniprotkb", uniprotkb.getShortName());
+        Assert.assertNull(uniprotkb.getMIIdentifier());
+        Assert.assertNotNull(uniprotkb.getAnnotations());
+        Assert.assertNotNull(uniprotkb.getParents());
+        Assert.assertNotNull(uniprotkb.getChildren());
+        Assert.assertNotNull(uniprotkb.getSynonyms());
+        Assert.assertNotNull(uniprotkb.getXrefs());
+
+        Assert.assertTrue(uniprotkb.getIdentifiers().isEmpty());
+    }
+
+    @Test
+    public void test_create_cvTerm_mod_remove_all_mod() throws Exception {
+        CvTerm term = new DefaultCvTerm("modTerm", new DefaultXref(CvTermFactory.createPsiModDatabase(), "MOD:xxx"));
+        term.setMODIdentifier(null);
+
+        Assert.assertEquals("modTerm", term.getShortName());
+        Assert.assertNull(term.getMODIdentifier());
+        Assert.assertNull(term.getMIIdentifier());
+        Assert.assertNotNull(term.getAnnotations());
+        Assert.assertNotNull(term.getParents());
+        Assert.assertNotNull(term.getChildren());
+        Assert.assertNotNull(term.getSynonyms());
+        Assert.assertNotNull(term.getXrefs());
+
+        Assert.assertTrue(term.getIdentifiers().isEmpty());
+    }
 }
