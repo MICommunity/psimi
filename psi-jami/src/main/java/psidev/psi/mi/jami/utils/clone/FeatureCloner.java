e2b895c368376455382faecb82db2d5c9d01e893
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-02-12 15:19:17 +0000

    FeatureCloner will copy properties of a feature source to a feature target

---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/utils/clone/FeatureCloner.java
@@ -0,0 +1,41 @@
+package psidev.psi.mi.jami.utils.clone;
+
+import psidev.psi.mi.jami.model.Feature;
+
+/**
+ * This class will clone/copy properties of a feature
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>12/02/13</pre>
+ */
+
+public class FeatureCloner {
+
+    /***
+     * This method will copy properties of Feature source in Feature target and will overrid all the other properties of Target feature.
+     * Only the participant is not copied
+     * @param source
+     * @param target
+     */
+    public static void copyAndOverrideFeatureProperties(Feature source, Feature target){
+        if (source != null && target != null){
+            target.setShortName(source.getShortName());
+            target.setFullName(source.getFullName());
+            target.setType(source.getType());
+            target.setInterpro(source.getInterpro());
+
+            // copy collections
+            target.getAnnotations().clear();
+            target.getAnnotations().addAll(source.getAnnotations());
+            target.getIdentifiers().clear();
+            target.getIdentifiers().addAll(source.getIdentifiers());
+            target.getBindingFeatures().clear();
+            target.getBindingFeatures().addAll(source.getBindingFeatures());
+            target.getXrefs().clear();
+            target.getXrefs().addAll(source.getXrefs());
+            target.getRanges().clear();
+            target.getRanges().addAll(source.getRanges());
+        }
+    }
+}
