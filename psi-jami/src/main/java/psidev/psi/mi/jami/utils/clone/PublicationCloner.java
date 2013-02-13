901f581ac0137068840eb91443cfb02a8e3dd4ea
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-02-13 11:41:24 +0000

    Added a new utility cloner for basic publications

---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/utils/clone/PublicationCloner.java
@@ -0,0 +1,42 @@
+package psidev.psi.mi.jami.utils.clone;
+
+import psidev.psi.mi.jami.model.Publication;
+
+/**
+ * Utility class for cloning publications
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>13/02/13</pre>
+ */
+
+public class PublicationCloner {
+
+    /***
+     * This method will copy properties of publication source in publication target and will override all the other properties of Target publication.
+     * This method will ignore experiments
+     * @param source
+     * @param target
+     */
+    public static void copyAndOverrideInteractorProperties(Publication source, Publication target){
+        if (source != null && target != null){
+            target.setCurationDepth(source.getCurationDepth());
+            target.setDoi(source.getDoi());
+            target.setPubmedId(source.getPubmedId());
+            target.setJournal(source.getJournal());
+            target.setReleasedDate(source.getReleasedDate());
+            target.setTitle(source.getTitle());
+            target.setPublicationDate(source.getPublicationDate());
+
+            // copy collections
+            target.getAnnotations().clear();
+            target.getAnnotations().addAll(source.getAnnotations());
+            target.getIdentifiers().clear();
+            target.getIdentifiers().addAll(source.getIdentifiers());
+            target.getAuthors().clear();
+            target.getAuthors().addAll(source.getAuthors());
+            target.getXrefs().clear();
+            target.getXrefs().addAll(source.getXrefs());
+        }
+    }
+}
