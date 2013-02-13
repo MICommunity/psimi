c52cd0d2dd65bb8bd3e4a52d52c75b442d164df9
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-02-13 11:24:15 +0000

    Added a new utility cloner for basic Interactors

--- trunk/psi-jami/src/main/java/psidev/psi/mi/jami/utils/clone/InteractorCloner.java
+++ trunk/psi-jami/src/main/java/psidev/psi/mi/jami/utils/clone/InteractorCloner.java
@@ -1,41 +1,40 @@
 package psidev.psi.mi.jami.utils.clone;
 
-import psidev.psi.mi.jami.model.Feature;
+import psidev.psi.mi.jami.model.Interactor;
 
 /**
- * This class will clone/copy properties of a feature
+ * Cloner for interactors
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
- * @since <pre>12/02/13</pre>
+ * @since <pre>13/02/13</pre>
  */
 
-public class FeatureCloner {
+public class InteractorCloner {
 
     /***
-     * This method will copy properties of Feature source in Feature target and will override all the other properties of Target feature.
-     * Only the participant is not copied
+     * This method will copy properties of interactor source in interactor target and will override all the other properties of Target interactor.
      * @param source
      * @param target
      */
-    public static void copyAndOverrideFeatureProperties(Feature source, Feature target){
+    public static void copyAndOverrideInteractorProperties(Interactor source, Interactor target){
         if (source != null && target != null){
             target.setShortName(source.getShortName());
             target.setFullName(source.getFullName());
             target.setType(source.getType());
-            target.setInterpro(source.getInterpro());
+            target.setOrganism(source.getOrganism());
 
             // copy collections
             target.getAnnotations().clear();
             target.getAnnotations().addAll(source.getAnnotations());
             target.getIdentifiers().clear();
             target.getIdentifiers().addAll(source.getIdentifiers());
-            target.getBindingFeatures().clear();
-            target.getBindingFeatures().addAll(source.getBindingFeatures());
+            target.getAliases().clear();
+            target.getAliases().addAll(source.getAliases());
             target.getXrefs().clear();
             target.getXrefs().addAll(source.getXrefs());
-            target.getRanges().clear();
-            target.getRanges().addAll(source.getRanges());
+            target.getChecksums().clear();
+            target.getChecksums().addAll(source.getChecksums());
         }
     }
 }
