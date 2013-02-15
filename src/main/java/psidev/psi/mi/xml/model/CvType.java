c92ad05341697fce86b0da8d71af2b8ebb58c520
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-02-15 13:14:19 +0000

    CvType is now implementing CvObject

--- src/main/java/psidev/psi/mi/xml/model/CvType.java
+++ src/main/java/psidev/psi/mi/xml/model/CvType.java
@@ -7,6 +7,15 @@
 package psidev.psi.mi.xml.model;
 
 
+import psidev.psi.mi.jami.model.CvTerm;
+import psidev.psi.mi.jami.model.impl.DefaultCvTerm;
+import psidev.psi.mi.jami.utils.XrefUtils;
+import psidev.psi.mi.jami.utils.collection.AbstractListHavingPoperties;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  * Reference to an external controlled vocabulary.
  * <p/>
@@ -28,21 +37,39 @@ package psidev.psi.mi.xml.model;
  * </pre>
  */
 
-public abstract class CvType implements NamesContainer, XrefContainer {
+public abstract class CvType extends DefaultCvTerm implements NamesContainer, XrefContainer {
 
     private Names names;
 
     private Xref xref;
 
+    private final static String UNSPECIFIED = "unspecified";
+
     ///////////////////////////
     // Getters and Setters
 
     protected CvType() {
+        super(UNSPECIFIED);
     }
 
     protected CvType( Names names, Xref xref ) {
+        super(names != null ? names.getShortLabel() : UNSPECIFIED);
         setNames( names );
-        setXref( xref );
+
+        // set xrefs
+        if (xref != null){
+            this.xref = new CvTermXref(xref.getPrimaryRef(), xref.getSecondaryRef());
+        }
+    }
+
+    @Override
+    protected void initializeXrefs() {
+        this.xrefs = new CvTermXrefList();
+    }
+
+    @Override
+    protected void initializeIdentifiers() {
+        this.identifiers = new CvTermIdentifierList();
     }
 
     /**
@@ -88,7 +115,69 @@ public abstract class CvType implements NamesContainer, XrefContainer {
      * @param value allowed object is {@link Xref }
      */
     public void setXref( Xref value ) {
-        this.xref = value;
+        if (value != null){
+            this.xref = new CvTermXref(value.getPrimaryRef(), value.getSecondaryRef());
+        }
+        else {
+            identifiers.clear();
+            xrefs.clear();
+            this.xref = null;
+        }
+    }
+
+    protected void processAddedIdentifier(psidev.psi.mi.jami.model.Xref added){
+        // the added identifier is psi-mi and it is not the current mi identifier
+        if (miIdentifier != added && XrefUtils.isXrefFromDatabase(added, CvTerm.PSI_MI_MI, CvTerm.PSI_MI)){
+            // the current psi-mi identifier is not identity, we may want to set miIdentifier
+            if (!XrefUtils.doesXrefHaveQualifier(miIdentifier, psidev.psi.mi.jami.model.Xref.IDENTITY_MI, psidev.psi.mi.jami.model.Xref.IDENTITY)){
+                // the miidentifier is not set, we can set the miidentifier
+                if (miIdentifier == null){
+                    miIdentifier = added;
+                }
+                else if (XrefUtils.doesXrefHaveQualifier(added, psidev.psi.mi.jami.model.Xref.IDENTITY_MI, psidev.psi.mi.jami.model.Xref.IDENTITY)){
+                    miIdentifier = added;
+                }
+                // the added xref is secondary object and the current mi is not a secondary object, we reset miidentifier
+                else if (!XrefUtils.doesXrefHaveQualifier(miIdentifier, psidev.psi.mi.jami.model.Xref.SECONDARY_MI, psidev.psi.mi.jami.model.Xref.SECONDARY)
+                        && XrefUtils.doesXrefHaveQualifier(added, psidev.psi.mi.jami.model.Xref.SECONDARY_MI, psidev.psi.mi.jami.model.Xref.SECONDARY)){
+                    miIdentifier = added;
+                }
+            }
+        }
+        // the added identifier is psi-mod and it is not the current mod identifier
+        else if (modIdentifier != added && XrefUtils.isXrefFromDatabase(added, CvTerm.PSI_MOD_MI, CvTerm.PSI_MOD)){
+            // the current psi-mod identifier is not identity, we may want to set modIdentifier
+            if (!XrefUtils.doesXrefHaveQualifier(modIdentifier, psidev.psi.mi.jami.model.Xref.IDENTITY_MI, psidev.psi.mi.jami.model.Xref.IDENTITY)){
+                // the modIdentifier is not set, we can set the modIdentifier
+                if (modIdentifier == null){
+                    modIdentifier = added;
+                }
+                else if (XrefUtils.doesXrefHaveQualifier(added, psidev.psi.mi.jami.model.Xref.IDENTITY_MI, psidev.psi.mi.jami.model.Xref.IDENTITY)){
+                    modIdentifier = added;
+                }
+                // the added xref is secondary object and the current mi is not a secondary object, we reset miidentifier
+                else if (!XrefUtils.doesXrefHaveQualifier(modIdentifier, psidev.psi.mi.jami.model.Xref.SECONDARY_MI, psidev.psi.mi.jami.model.Xref.SECONDARY)
+                        && XrefUtils.doesXrefHaveQualifier(added, psidev.psi.mi.jami.model.Xref.SECONDARY_MI, psidev.psi.mi.jami.model.Xref.SECONDARY)){
+                    modIdentifier = added;
+                }
+            }
+        }
+    }
+
+    protected void processRemovedIdentifier(psidev.psi.mi.jami.model.Xref removed){
+        // the removed identifier is psi-mi
+        if (miIdentifier != null && miIdentifier.equals(removed)){
+            miIdentifier = XrefUtils.collectFirstIdentifierWithDatabase(identifiers, CvTerm.PSI_MI_MI, CvTerm.PSI_MI);
+        }
+        // the removed identifier is psi-mod
+        else if (modIdentifier != null && modIdentifier.equals(removed)){
+            modIdentifier = XrefUtils.collectFirstIdentifierWithDatabase(identifiers, CvTerm.PSI_MOD_MI, CvTerm.PSI_MOD);
+        }
+    }
+
+    protected void clearPropertiesLinkedToIdentifiers(){
+        miIdentifier = null;
+        modIdentifier = null;
     }
 
     //////////////////////////
@@ -132,5 +221,331 @@ public abstract class CvType implements NamesContainer, XrefContainer {
         result = ( names != null ? names.hashCode() : 0 );
         result = 29 * result + ( xref != null ? xref.hashCode() : 0 );
         return result;
+    }
+
+    protected class CvTermXref extends Xref{
+
+        protected boolean isPrimaryAnIdentity = false;
+        protected SecondaryRefList extendedSecondaryRefList = new SecondaryRefList();
+
+        ///////////////////////////
+        // Constructors
+
+        public CvTermXref() {
+            super();
+        }
+
+        public CvTermXref( DbReference primaryRef ) {
+            super(primaryRef);
+        }
+
+        public CvTermXref( DbReference primaryRef, Collection<DbReference> secondaryRef ) {
+            super( primaryRef );
+
+            if (secondaryRef != null && !secondaryRef.isEmpty()){
+                secondaryRef.addAll(secondaryRef);
+            }
+        }
+
+        ///////////////////////////
+        // Getters and Setters
+
+        /**
+         * Sets the value of the primaryRef property.
+         *
+         * @param value allowed object is {@link DbReference }
+         */
+        public void setPrimaryRef( DbReference value ) {
+            if (getPrimaryRef() == null){
+                super.setPrimaryRef(value);
+            }
+            else if (isPrimaryAnIdentity){
+                ((CvTermIdentifierList)identifiers).removeOnly(getPrimaryRef());
+                processRemovedIdentifier(getPrimaryRef());
+                super.setPrimaryRef(value);
+
+                if (value != null){
+                    if (XrefUtils.isXrefAnIdentifier(value)){
+                        ((CvTermIdentifierList)identifiers).addOnly(value);
+                        processAddedIdentifier(value);
+                        isPrimaryAnIdentity = true;
+                    }
+                    else {
+                        ((CvTermXrefList)xrefs).addOnly(value);
+                    }
+                }
+            }
+            else {
+                ((CvTermXrefList)xrefs).removeOnly(getPrimaryRef());
+                super.setPrimaryRef(value);
+
+                if (value != null){
+                    if (XrefUtils.isXrefAnIdentifier(value)){
+                        ((CvTermIdentifierList)identifiers).addOnly(value);
+                        processAddedIdentifier(value);
+                        isPrimaryAnIdentity = true;
+                    }
+                    else {
+                        ((CvTermXrefList)xrefs).addOnly(value);
+                    }
+                }
+            }
+        }
+
+        public void setPrimaryRefOnly( DbReference value ) {
+            if (value == null && !extendedSecondaryRefList.isEmpty()){
+                super.setPrimaryRef(extendedSecondaryRefList.get(0));
+                extendedSecondaryRefList.remove(0);
+            }
+            else {
+                super.setPrimaryRef(value);
+                if (XrefUtils.isXrefAnIdentifier(value)){
+                    isPrimaryAnIdentity = true;
+                }
+            }
+        }
+
+        /**
+         * Check if the optional secondaryRef is defined.
+         *
+         * @return true if defined, false otherwise.
+         */
+        public boolean hasSecondaryRef() {
+            return ( extendedSecondaryRefList != null ) && ( !extendedSecondaryRefList.isEmpty() );
+        }
+
+        /**
+         * Gets the value of the secondaryRef property.
+         * <p/>
+         * <p/>
+         * This accessor method returns a reference to the live list, not a snapshot. Therefore any modification you make to
+         * the returned list will be present inside the JAXB object. This is why there is not a <CODE>set</CODE> method for
+         * the secondaryRef property.
+         * <p/>
+         * <p/>
+         * For example, to add a new item, do as follows:
+         * <pre>
+         *    getSecondaryRef().add(newItem);
+         * </pre>
+         * <p/>
+         * <p/>
+         * <p/>
+         * Objects of the following type(s) are allowed in the list {@link DbReference }
+         */
+        public Collection<DbReference> getSecondaryRef() {
+            return this.extendedSecondaryRefList;
+        }
+
+        public Collection<DbReference> getAllDbReferences() {
+            Collection<DbReference> refs = new ArrayList<DbReference>();
+            if ( getPrimaryRef() != null ) {
+                refs.add( getPrimaryRef() );
+            }
+            if ( extendedSecondaryRefList != null ) {
+                refs.addAll( extendedSecondaryRefList );
+            }
+            return refs;
+        }
+
+        //////////////////////
+        // Object override
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder();
+            sb.append( "Xref" );
+            sb.append( "{primaryRef=" ).append( getPrimaryRef() );
+            sb.append( ", secondaryRef=" ).append( extendedSecondaryRefList );
+            sb.append( '}' );
+            return sb.toString();
+        }
+
+        protected class SecondaryRefList extends AbstractListHavingPoperties<DbReference>{
+
+            @Override
+            protected void processAddedObjectEvent(DbReference added) {
+                if (XrefUtils.isXrefAnIdentifier(added)){
+                    ((CvTermIdentifierList)identifiers).addOnly(added);
+                    processAddedIdentifier(added);
+                }
+                else {
+                    ((CvTermXrefList)xrefs).addOnly(added);
+                }
+            }
+
+            @Override
+            protected void processRemovedObjectEvent(DbReference removed) {
+                if (XrefUtils.isXrefAnIdentifier(removed)){
+                    ((CvTermIdentifierList)identifiers).removeOnly(removed);
+                    processRemovedIdentifier(removed);
+                }
+                else {
+                    ((CvTermXrefList)xrefs).removeOnly(removed);
+                }
+            }
+
+            @Override
+            protected void clearProperties() {
+                Collection<DbReference> primary = Arrays.asList(getPrimaryRef());
+                ((CvTermIdentifierList)identifiers).retainAllOnly(primary);
+                clearPropertiesLinkedToIdentifiers();
+                ((CvTermXrefList)xrefs).retainAllOnly(primary);
+            }
+        }
+    }
+
+    private class CvTermIdentifierList extends AbstractListHavingPoperties<psidev.psi.mi.jami.model.Xref> {
+        public CvTermIdentifierList(){
+            super();
+        }
+
+        @Override
+        protected void processAddedObjectEvent(psidev.psi.mi.jami.model.Xref added) {
+
+            if (xref != null){
+                CvTermXref reference = (CvTermXref) xref;
+
+                if (xref.getPrimaryRef() == null){
+                    if (added instanceof DbReference){
+                        reference.setPrimaryRefOnly((DbReference) added);
+                        processAddedIdentifier(added);
+                    }
+                    else {
+                        DbReference fixedRef = new DbReference(added.getDatabase().getShortName(), added.getDatabase().getMIIdentifier(), added.getId(),
+                                added.getQualifier() != null ? added.getQualifier().getShortName() : null, added.getQualifier() != null ? added.getQualifier().getMIIdentifier() : null);
+
+                        reference.setPrimaryRefOnly(fixedRef);
+                        processAddedIdentifier(fixedRef);
+                    }
+                }
+                else {
+                    if (added instanceof DbReference){
+                        reference.extendedSecondaryRefList.addOnly((DbReference) added);
+                        processAddedIdentifier(added);
+                    }
+                    else {
+                        DbReference fixedRef = new DbReference(added.getDatabase().getShortName(), added.getDatabase().getMIIdentifier(), added.getId(),
+                                added.getQualifier() != null ? added.getQualifier().getShortName() : null, added.getQualifier() != null ? added.getQualifier().getMIIdentifier() : null);
+
+                        reference.extendedSecondaryRefList.addOnly(fixedRef);
+                        processAddedIdentifier(fixedRef);
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void processRemovedObjectEvent(psidev.psi.mi.jami.model.Xref removed) {
+
+            if (xref != null){
+                CvTermXref reference = (CvTermXref) xref;
+
+                if (reference.getPrimaryRef() == removed){
+                    reference.setPrimaryRefOnly(null);
+                    processRemovedIdentifier(removed);
+                }
+                else {
+                    if (removed instanceof DbReference){
+                        reference.extendedSecondaryRefList.removeOnly((DbReference) removed);
+                        processRemovedIdentifier(removed);
+
+                    }
+                    else {
+                        DbReference fixedRef = new DbReference(removed.getDatabase().getShortName(), removed.getDatabase().getMIIdentifier(), removed.getId(),
+                                removed.getQualifier() != null ? removed.getQualifier().getShortName() : null, removed.getQualifier() != null ? removed.getQualifier().getMIIdentifier() : null);
+
+                        reference.extendedSecondaryRefList.removeOnly(fixedRef);
+                        processRemovedIdentifier(removed);
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void clearProperties() {
+            clearPropertiesLinkedToIdentifiers();
+
+            if (xref != null){
+                CvTermXref reference = (CvTermXref) xref;
+                reference.extendedSecondaryRefList.retainAllOnly(xrefs);
+
+                if (reference.isPrimaryAnIdentity){
+                    reference.setPrimaryRefOnly(null);
+                }
+            }
+        }
+    }
+
+    private class CvTermXrefList extends AbstractListHavingPoperties<psidev.psi.mi.jami.model.Xref> {
+        public CvTermXrefList(){
+            super();
+        }
+
+        @Override
+        protected void processAddedObjectEvent(psidev.psi.mi.jami.model.Xref added) {
+
+            if (xref != null){
+                CvTermXref reference = (CvTermXref) xref;
+
+                if (xref.getPrimaryRef() == null){
+                    if (added instanceof DbReference){
+                        reference.setPrimaryRefOnly((DbReference) added);
+                    }
+                    else {
+                        DbReference fixedRef = new DbReference(added.getDatabase().getShortName(), added.getDatabase().getMIIdentifier(), added.getId(),
+                                added.getQualifier() != null ? added.getQualifier().getShortName() : null, added.getQualifier() != null ? added.getQualifier().getMIIdentifier() : null);
+
+                        reference.setPrimaryRefOnly(fixedRef);
+                    }
+                }
+                else {
+                    if (added instanceof DbReference){
+                        reference.extendedSecondaryRefList.addOnly((DbReference) added);
+                    }
+                    else {
+                        DbReference fixedRef = new DbReference(added.getDatabase().getShortName(), added.getDatabase().getMIIdentifier(), added.getId(),
+                                added.getQualifier() != null ? added.getQualifier().getShortName() : null, added.getQualifier() != null ? added.getQualifier().getMIIdentifier() : null);
+
+                        reference.extendedSecondaryRefList.addOnly(fixedRef);
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void processRemovedObjectEvent(psidev.psi.mi.jami.model.Xref removed) {
+            if (xref != null){
+                CvTermXref reference = (CvTermXref) xref;
+
+                if (reference.getPrimaryRef() == removed){
+                    reference.setPrimaryRefOnly(null);
+                }
+                else {
+                    if (removed instanceof DbReference){
+                        reference.extendedSecondaryRefList.removeOnly((DbReference) removed);
+
+                    }
+                    else {
+                        DbReference fixedRef = new DbReference(removed.getDatabase().getShortName(), removed.getDatabase().getMIIdentifier(), removed.getId(),
+                                removed.getQualifier() != null ? removed.getQualifier().getShortName() : null, removed.getQualifier() != null ? removed.getQualifier().getMIIdentifier() : null);
+
+                        reference.extendedSecondaryRefList.removeOnly(fixedRef);
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void clearProperties() {
+
+            if (xref != null){
+                CvTermXref reference = (CvTermXref) xref;
+                reference.extendedSecondaryRefList.retainAllOnly(identifiers);
+
+                if (!reference.isPrimaryAnIdentity){
+                    reference.setPrimaryRefOnly(null);
+                }
+            }
+        }
     }
 }
