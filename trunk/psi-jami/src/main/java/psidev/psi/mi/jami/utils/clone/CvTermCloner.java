bf42b35bc0328796734fb0d40a7832ea7765a011
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-02-19 16:00:46 +0000

    Added CvTermCloner

---
+++ trunk/psi-jami/src/main/java/psidev/psi/mi/jami/utils/clone/CvTermCloner.java
@@ -0,0 +1,34 @@
+package psidev.psi.mi.jami.utils.clone;
+
+import psidev.psi.mi.jami.model.CvTerm;
+
+/**
+ * Utility class for cloning CvTerms
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>19/02/13</pre>
+ */
+
+public class CvTermCloner {
+
+    /***
+     * This method will copy properties of cv term source in cv term target and will override all the other properties of Target cv term.
+     * @param source
+     * @param target
+     */
+    public static void copyAndOverrideCvTermProperties(CvTerm source, CvTerm target){
+        if (source != null && target != null){
+            target.setShortName(source.getShortName());
+            target.setFullName(source.getFullName());
+
+            // copy collections
+            target.getAnnotations().clear();
+            target.getAnnotations().addAll(source.getAnnotations());
+            target.getXrefs().clear();
+            target.getXrefs().addAll(source.getXrefs());
+            target.getSynonyms().clear();
+            target.getSynonyms().addAll(source.getSynonyms());
+        }
+    }
+}
