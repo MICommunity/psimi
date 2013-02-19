9eb391c38c97c3d0e04088b0fca0f36848395a49
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-02-19 11:38:16 +0000

    Updated DefaultSource

--- trunk/psi-jami/src/main/java/psidev/psi/mi/jami/model/impl/DefaultSource.java
+++ trunk/psi-jami/src/main/java/psidev/psi/mi/jami/model/impl/DefaultSource.java
@@ -1,7 +1,12 @@
 package psidev.psi.mi.jami.model.impl;
 
+import psidev.psi.mi.jami.model.Annotation;
+import psidev.psi.mi.jami.model.CvTerm;
 import psidev.psi.mi.jami.model.Source;
 import psidev.psi.mi.jami.model.Xref;
+import psidev.psi.mi.jami.utils.AnnotationUtils;
+import psidev.psi.mi.jami.utils.collection.AbstractListHavingPoperties;
+import psidev.psi.mi.jami.utils.factory.CvTermFactory;
 
 /**
  * Default implementation for Source
@@ -13,7 +18,7 @@ import psidev.psi.mi.jami.model.Xref;
 
 public class DefaultSource extends DefaultCvTerm implements Source {
 
-    private String url;
+    private Annotation url;
     private String postalAddress;
     private Xref bibRef;
 
@@ -31,21 +36,21 @@ public class DefaultSource extends DefaultCvTerm implements Source {
 
     public DefaultSource(String shortName, String url, String address, Xref bibRef) {
         super(shortName);
-        this.url = url;
+        setUrl(url);
         this.postalAddress = address;
         this.bibRef = bibRef;
     }
 
     public DefaultSource(String shortName, Xref ontologyId, String url, String address, Xref bibRef) {
         super(shortName, ontologyId);
-        this.url = url;
+        setUrl(url);
         this.postalAddress = address;
         this.bibRef = bibRef;
     }
 
     public DefaultSource(String shortName, String fullName, Xref ontologyId, String url, String address, Xref bibRef) {
         super(shortName, fullName, ontologyId);
-        this.url = url;
+        setUrl(url);
         this.postalAddress = address;
         this.bibRef = bibRef;
     }
@@ -60,24 +65,43 @@ public class DefaultSource extends DefaultCvTerm implements Source {
 
     public DefaultSource(String shortName, String miId, String url, String address, Xref bibRef) {
         super(shortName, miId);
-        this.url = url;
+        setUrl(url);
         this.postalAddress = address;
         this.bibRef = bibRef;
     }
 
     public DefaultSource(String shortName, String fullName, String miId, String url, String address, Xref bibRef) {
         super(shortName, fullName, miId);
-        this.url = url;
+        setUrl(url);
         this.postalAddress = address;
         this.bibRef = bibRef;
     }
 
+    @Override
+    protected void initializeAnnotations() {
+        this.annotations = new SourceAnnotationList();
+    }
+
     public String getUrl() {
-        return this.url;
+        return this.url != null ? this.url.getValue() : null;
     }
 
     public void setUrl(String url) {
-        this.url = url;
+        // add new url if not null
+        if (url != null){
+            CvTerm complexPhysicalProperties = CvTermFactory.createMICvTerm(Annotation.URL, Annotation.URL_MI);
+            // first remove old url if not null
+            if (this.url != null){
+                annotations.remove(this.url);
+            }
+            this.url = new DefaultAnnotation(complexPhysicalProperties, url);
+            this.annotations.add(this.url);
+        }
+        // remove all url if the collection is not empty
+        else if (!this.annotations.isEmpty()) {
+            AnnotationUtils.removeAllAnnotationsWithTopic(annotations, Annotation.URL_MI, Annotation.URL);
+            this.url = null;
+        }
     }
 
     public String getPostalAddress() {
@@ -94,5 +118,30 @@ public class DefaultSource extends DefaultCvTerm implements Source {
 
     public void setBibRef(Xref ref) {
         this.bibRef = ref;
+    }
+
+    private class SourceAnnotationList extends AbstractListHavingPoperties<Annotation> {
+        public SourceAnnotationList(){
+            super();
+        }
+
+        @Override
+        protected void processAddedObjectEvent(Annotation added) {
+            if (url == null && AnnotationUtils.doesAnnotationHaveTopic(added, Annotation.URL_MI, Annotation.URL)){
+                url = added;
+            }
+        }
+
+        @Override
+        protected void processRemovedObjectEvent(Annotation removed) {
+            if (url != null && url.equals(removed)){
+                url = null;
+            }
+        }
+
+        @Override
+        protected void clearProperties() {
+            url = null;
+        }
     }
 }
