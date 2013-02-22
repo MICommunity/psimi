0c24db14988c097061c915eba900932ddfa7d188
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-02-22 10:22:49 +0000

    RangeCloner : to clone ranges

---
+++ trunk/psi-jami/src/main/java/psidev/psi/mi/jami/utils/clone/RangeCloner.java
@@ -0,0 +1,20 @@
+package psidev.psi.mi.jami.utils.clone;
+
+import psidev.psi.mi.jami.model.Range;
+
+/**
+ * Utility class for cloning ranges
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>22/02/13</pre>
+ */
+
+public class RangeCloner {
+
+    public static void copyAndOverrideRangeProperties(Range source, Range target){
+        if (source != null && target != null){
+            target.setPositions(source.getStart(), source.getEnd());
+        }
+    }
+}
