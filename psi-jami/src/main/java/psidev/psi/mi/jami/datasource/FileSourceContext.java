bc8a00f8fb9d603b9f10962feeee5a4ef23f393b
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-03-01 14:48:00 +0000

    Updated iterators in StreamingDataSources

--- psi-jami/src/main/java/psidev/psi/mi/jami/datasource/FileSourceContext.java
+++ psi-jami/src/main/java/psidev/psi/mi/jami/datasource/FileSourceContext.java
@@ -1,31 +1,12 @@
 package psidev.psi.mi.jami.datasource;
 
 /**
- * A fileSourceContext contains some information
+ * Some context about the datasource
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
  * @since <pre>01/03/13</pre>
  */
 
-public class FileSourceContext implements DataSourceContext{
-
-    private int lineNumber;
-    private int columnNumber;
-
-    public int getLineNumber() {
-        return lineNumber;
-    }
-
-    public void setLineNumber(int lineNumber) {
-        this.lineNumber = lineNumber;
-    }
-
-    public int getColumnNumber() {
-        return columnNumber;
-    }
-
-    public void setColumnNumber(int columnNumber) {
-        this.columnNumber = columnNumber;
-    }
+public interface FileSourceContext {
 }
