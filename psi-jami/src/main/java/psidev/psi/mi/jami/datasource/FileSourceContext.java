918257b25a1ba40818cfd8d0c308c7ff0997d03f
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-03-01 13:48:32 +0000

    Created DataSourceFactory

---
+++ psi-jami/src/main/java/psidev/psi/mi/jami/datasource/FileSourceContext.java
@@ -0,0 +1,12 @@
+package psidev.psi.mi.jami.datasource;
+
+/**
+ * A fileSourceContext contains some information
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>01/03/13</pre>
+ */
+
+public interface FileSourceContext {
+}
--- psi-jami/src/main/java/psidev/psi/mi/jami/datasource/MolecularInteractionDataSource.java
+++ psi-jami/src/main/java/psidev/psi/mi/jami/datasource/MolecularInteractionDataSource.java
@@ -1,5 +1,6 @@
 package psidev.psi.mi.jami.datasource;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,4 +14,5 @@ import java.util.Map;
 public interface MolecularInteractionDataSource {
 
     public void initialiseContext(Map<String, Object> options);
+    public Map<DataSourceError, List<FileSourceContext>>  getDataSourceErrors();
 }
