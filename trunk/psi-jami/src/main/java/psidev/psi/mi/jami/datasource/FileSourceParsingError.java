3a050c99d99fcf07a71ecf2ea3c65217209b33dc
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-03-15 10:11:21 +0000

    Added FileSourceParsingError enum

---
+++ trunk/psi-jami/src/main/java/psidev/psi/mi/jami/datasource/FileSourceParsingError.java
@@ -0,0 +1,14 @@
+package psidev.psi.mi.jami.datasource;
+
+/**
+ * This enum is giving topics for different parsing error types
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>15/03/13</pre>
+ */
+
+public enum FileSourceParsingError {
+
+    invalid_syntax, clustered_content
+}
