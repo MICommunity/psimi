c941d5728e5c78fe073e78480324cd2fd626cf38
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-03-21 10:49:45 +0000

    Updated MICvRuleManager

--- src/main/java/psidev/psi/mi/validator/extension/rules/cvmapping/MICvRule.java
+++ src/main/java/psidev/psi/mi/validator/extension/rules/cvmapping/MICvRule.java
@@ -9,7 +9,9 @@ import psidev.psi.tools.validator.rules.cvmapping.CvRuleImpl;
 import psidev.psi.tools.validator.rules.cvmapping.Recommendation;
 import psidev.psi.tools.validator.xpath.XPathResult;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * CvRule extension for MI validator
@@ -21,8 +23,30 @@ import java.util.List;
 
 public class MICvRule extends CvRuleImpl{
 
+    private Map<String, String> equivalentXmlPath;
+
     public MICvRule(OntologyManager ontologyManager) {
         super(ontologyManager);
+        equivalentXmlPath = new HashMap<String, String>();
+        initialiseMapOfEquivalentXMLPath();
+    }
+
+    private void initialiseMapOfEquivalentXMLPath(){
+
+        equivalentXmlPath.put("interactionEvidence", "entrySet/entry/interactionList/interaction");
+        equivalentXmlPath.put("experiment", "experiments/experimentDescription");
+        equivalentXmlPath.put("participantEvidences", "participants");
+        equivalentXmlPath.put("featureEvidences", "features");
+        equivalentXmlPath.put("@MIIdentifier", "xref/primaryRef/@id");
+        equivalentXmlPath.put("@PARIdentifier", "database-,*-xref/primaryRef/@id");
+        equivalentXmlPath.put("@MODIdentifier", "xref/primaryRef/@id");
+        equivalentXmlPath.put("detectionMethod", "featureDetectionMethod");
+        equivalentXmlPath.put("identificationMethod", "participantIdentificationMethod");
+        equivalentXmlPath.put("experimentalRole", "experimentalRoles");
+        equivalentXmlPath.put("identifiers[1]", "xref/primaryRef");
+        equivalentXmlPath.put("type", "featureEvidences-featureType,interactor-interactorType");
+        equivalentXmlPath.put("publication", "bibref");
+        equivalentXmlPath.put("database", "xref/primaryRef/@dbAc");
     }
 
     @Override
