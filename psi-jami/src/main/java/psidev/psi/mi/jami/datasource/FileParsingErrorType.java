4e8e1c51b84af5605fc4594dfe14eae6f6121e30
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-03-25 12:32:17 +0000

    Added new error types

--- psi-jami/src/main/java/psidev/psi/mi/jami/datasource/FileParsingErrorType.java
+++ psi-jami/src/main/java/psidev/psi/mi/jami/datasource/FileParsingErrorType.java
@@ -12,6 +12,6 @@ public enum FileParsingErrorType {
 
     invalid_syntax, multiple_experimental_roles, multiple_host_organisms, multiple_experiments, multiple_expressed_in,
     multiple_participant_identification_methods, multiple_interaction_types, missing_biological_role, missing_cv, clustered_content,
-    missing_database, missing_database_accession
+    missing_database, missing_database_accession, missing_range_status, missing_range_position, feature_without_ranges
 
 }
