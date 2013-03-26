af25a58feeac339c05574af99fd1ef8ed3d4b797
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-03-26 13:07:23 +0000

    Added a method to save a file temporarily

--- trunk/psi-jami/src/main/java/psidev/psi/mi/jami/utils/MolecularInteractionDataSourceUtils.java
+++ trunk/psi-jami/src/main/java/psidev/psi/mi/jami/utils/MolecularInteractionDataSourceUtils.java
@@ -2,6 +2,7 @@ package psidev.psi.mi.jami.utils;
 
 import psidev.psi.mi.jami.datasource.DataSourceError;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -36,5 +37,52 @@ public class MolecularInteractionDataSourceUtils {
         }
 
         return filteredErrors;
+    }
+
+    /**
+     * Store the content of the given input stream into a temporary file and return its descriptor.
+     *
+     * @param is the input stream to store.
+     * @return a File descriptor describing a temporary file storing the content of the given input stream.
+     * @throws java.io.IOException if an IO error occur.
+     */
+    public static File storeAsTemporaryFile( InputStream is, String name, String extension ) throws IOException {
+
+        if ( is == null ) {
+            throw new IllegalArgumentException( "You must give a non null InputStream" );
+        }
+        // Create a temp file and write URL content in it.
+        File tempDirectory = new File( System.getProperty( "java.io.tmpdir", "tmp" ) );
+        if ( !tempDirectory.exists() ) {
+            if ( !tempDirectory.mkdirs() ) {
+                throw new IOException( "Cannot create temp directory: " + tempDirectory.getAbsolutePath() );
+            }
+        }
+
+        File tempFile = File.createTempFile( name, extension , tempDirectory );
+
+        BufferedReader in = new BufferedReader( new InputStreamReader( is ) );
+        try{
+
+            BufferedWriter out = new BufferedWriter( new FileWriter( tempFile ) );
+
+            try{
+                String line;
+                while ( ( line = in.readLine() ) != null ) {
+                    out.write( line );
+                }
+
+
+                out.flush();
+            }
+            finally {
+                out.close();
+            }
+        }
+        finally {
+            in.close();
+        }
+
+        return tempFile;
     }
 }
