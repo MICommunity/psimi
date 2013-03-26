790e3904ab72616442038011e919380a30ff3284
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-03-26 16:56:02 +0000

    Added missing_alias_name

--- trunk/psi-jami/src/main/java/psidev/psi/mi/jami/datasource/FileParsingErrorType.java
+++ trunk/psi-jami/src/main/java/psidev/psi/mi/jami/datasource/FileParsingErrorType.java
@@ -13,6 +13,6 @@ public enum FileParsingErrorType {
     invalid_syntax, multiple_experimental_roles, multiple_host_organisms, multiple_experiments, multiple_expressed_in,
     multiple_participant_identification_methods, multiple_interaction_types, missing_biological_role, missing_cv, clustered_content,
     missing_database, missing_database_accession, invalid_feature_range, feature_without_ranges, missing_publication, missing_interaction_detection_method,
-    interaction_without_any_participants, missing_interactor_type, missing_interaction_type
+    interaction_without_any_participants, missing_interactor_type, missing_interaction_type, missing_alias_name
 
 }
