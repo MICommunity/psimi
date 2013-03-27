d73cf987390fc10da622effd6ff9bcc7f2f7176a
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-03-27 16:35:19 +0000

    Added more basic rules

---
+++ tags/psimi-schema-validator-2.1.0-SNAPSHOT/src/main/java/psidev/psi/mi/validator/extension/rules/psimi/ClusteredElementRule.java
@@ -0,0 +1,70 @@
+package psidev.psi.mi.validator.extension.rules.psimi;
+
+import psidev.psi.mi.jami.datasource.FileParsingErrorType;
+import psidev.psi.mi.jami.datasource.FileSourceError;
+import psidev.psi.mi.jami.datasource.MolecularInteractionFileDataSource;
+import psidev.psi.mi.jami.utils.MolecularInteractionFileDataSourceUtils;
+import psidev.psi.mi.validator.extension.Mi25Context;
+import psidev.psi.mi.validator.extension.rules.RuleUtils;
+import psidev.psi.tools.ontology_manager.OntologyManager;
+import psidev.psi.tools.validator.MessageLevel;
+import psidev.psi.tools.validator.ValidatorException;
+import psidev.psi.tools.validator.ValidatorMessage;
+import psidev.psi.tools.validator.rules.codedrule.ObjectRule;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * This rule can check if an element is considered clustered
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>27/03/13</pre>
+ */
+
+public class ClusteredElementRule extends ObjectRule<MolecularInteractionFileDataSource> {
+
+
+    public ClusteredElementRule(OntologyManager ontologyManager) {
+        super(ontologyManager);
+        setName( "Clustered element check" );
+
+        setDescription( "Check if an object is considered as a clustered object (example: a clustered column in MITAB)." );
+    }
+
+    @Override
+    public boolean canCheck(Object t) {
+        return ontologyManager instanceof MolecularInteractionFileDataSource;
+    }
+
+    @Override
+    public Collection<ValidatorMessage> check(MolecularInteractionFileDataSource molecularInteractionFileDataSource) throws ValidatorException {
+
+        // list of messages to return
+        List<ValidatorMessage> messages = new ArrayList<ValidatorMessage>();
+
+        Collection<FileSourceError> multipleTypes = MolecularInteractionFileDataSourceUtils.collectAllDataSourceErrorsHavingErrorType(molecularInteractionFileDataSource.getDataSourceErrors(), FileParsingErrorType.clustered_content.toString());
+        for (FileSourceError error : multipleTypes){
+            Mi25Context context = null;
+            if (error.getSourceContext() != null){
+                context = RuleUtils.buildContext(error.getSourceContext());
+            }
+            else {
+                context = new Mi25Context();
+            }
+
+            messages.add( new ValidatorMessage( error.getLabel() + ": " + error.getMessage(),
+                    MessageLevel.WARN,
+                    context,
+                    this ) );
+        }
+
+        return messages;
+    }
+
+    public String getId() {
+        return "R33";
+    }
+}
