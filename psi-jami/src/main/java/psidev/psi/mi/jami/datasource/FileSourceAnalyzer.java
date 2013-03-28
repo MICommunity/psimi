9ecb277b65fcb9ddb61464265cd7fa2d97ed72ef
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-03-28 13:25:07 +0000

    Updated DataSourceFactory

--- psi-jami/src/main/java/psidev/psi/mi/jami/datasource/FileSourceAnalyzer.java
+++ psi-jami/src/main/java/psidev/psi/mi/jami/datasource/FileSourceAnalyzer.java
@@ -1,5 +1,7 @@
 package psidev.psi.mi.jami.datasource;
 
+import psidev.psi.mi.jami.utils.MolecularInteractionFileDataSourceUtils;
+
 import java.io.*;
 
 /**
@@ -139,44 +141,52 @@ public class FileSourceAnalyzer {
             throw new IOException("The input stream cannot be null.");
         }
         else {
+
+            File temp = MolecularInteractionFileDataSourceUtils.storeAsTemporaryFile(stream, "molecular_interaction_stream"+System.currentTimeMillis(), ".txt");
+
             // check first line
             BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
 
-            String line = reader.readLine();
+            try {
+                String line = reader.readLine();
 
-            if (line == null){
-                return new OpenedInputStream(line, reader, MolecularInteractionSource.other);
-            }
-
-            // skip empty lines or break lines or xml encode line
-            while (line != null && (line.trim().length() == 0 || line.trim().equals(System.getProperty("line.separator")))){
-                line = reader.readLine();
-            }
-
-            if (line == null){
-                return new OpenedInputStream(line, reader, MolecularInteractionSource.other);
-            }
-            // we have xml
-            else if (line.trim().startsWith("<?xml")){
-                line = reader.readLine();
-                if (line.startsWith("<entrySet")){
-                    return new OpenedInputStream(line, reader, MolecularInteractionSource.psi25_xml);
+                if (line == null){
+                    return new OpenedInputStream(temp, MolecularInteractionSource.other);
                 }
-                else {
-                    return new OpenedInputStream(line, reader, MolecularInteractionSource.other);
+
+                // skip empty lines or break lines or xml encode line
+                while (line != null && (line.trim().length() == 0 || line.trim().equals(System.getProperty("line.separator")))){
+                    line = reader.readLine();
+                }
+
+                if (line == null){
+                    return new OpenedInputStream(temp, MolecularInteractionSource.other);
+                }
+                // we have xml
+                else if (line.trim().startsWith("<?xml")){
+                    line = reader.readLine();
+                    if (line.startsWith("<entrySet")){
+                        return new OpenedInputStream(temp, MolecularInteractionSource.psi25_xml);
+                    }
+                    else {
+                        return new OpenedInputStream(temp, MolecularInteractionSource.other);
+                    }
+                }
+                else if (line.startsWith("<entrySet")){
+                    return new OpenedInputStream(temp,  MolecularInteractionSource.psi25_xml);
+                }
+                else if (line.toLowerCase().trim().startsWith("#ID(s) interactor A\tID(s) interactor B\tAlt. ID(s) interactor A\tAlt. ID(s) interactor B\tAlias(es) interactor A\tAlias(es) interactor B\tInteraction detection method(s)\tPublication 1st author(s)\tPublication Identifier(s)\tTaxid interactor A\tTaxid interactor B\tInteraction type(s)\tSource database(s)\tInteraction identifier(s)\tConfidence value(s)".toLowerCase())){
+                    return new OpenedInputStream(temp,  MolecularInteractionSource.mitab);
+                }
+                else if (line.contains("\t") && line.split("\t").length >= 15){
+                    return new OpenedInputStream(temp,  MolecularInteractionSource.mitab);
+                }
+                else{
+                    return new OpenedInputStream(temp, MolecularInteractionSource.other);
                 }
             }
-            else if (line.startsWith("<entrySet")){
-                return new OpenedInputStream(line, reader, MolecularInteractionSource.psi25_xml);
-            }
-            else if (line.toLowerCase().trim().startsWith("#ID(s) interactor A\tID(s) interactor B\tAlt. ID(s) interactor A\tAlt. ID(s) interactor B\tAlias(es) interactor A\tAlias(es) interactor B\tInteraction detection method(s)\tPublication 1st author(s)\tPublication Identifier(s)\tTaxid interactor A\tTaxid interactor B\tInteraction type(s)\tSource database(s)\tInteraction identifier(s)\tConfidence value(s)".toLowerCase())){
-                return new OpenedInputStream(line, reader, MolecularInteractionSource.mitab);
-            }
-            else if (line.contains("\t") && line.split("\t").length >= 15){
-                return new OpenedInputStream(line, reader, MolecularInteractionSource.mitab);
-            }
-            else{
-                return new OpenedInputStream(line, reader, MolecularInteractionSource.other);
+            finally {
+                reader.close();
             }
         }
     }
--- psi-jami/src/main/java/psidev/psi/mi/jami/datasource/OpenedInputStream.java
+++ psi-jami/src/main/java/psidev/psi/mi/jami/datasource/OpenedInputStream.java
@@ -1,212 +1,39 @@
 package psidev.psi.mi.jami.datasource;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 /**
- * The openedInputStream is an inputStream that is wrapping an opened inputStream and the first line that was read to identify the molecular interaction source
+ * The openedInputStream contains an inputStream that is wrapping an opened inputStream and the first line that was read to identify the molecular interaction source
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
  * @since <pre>01/03/13</pre>
  */
 
-public class OpenedInputStream extends InputStream {
+public class OpenedInputStream {
 
-    private String firstLine;
-    private String nextLine;
-    private BufferedReader reader;
-    private boolean hasReadFirstLine=false;
-    private InputStream firstLineInputStream;
-    private InputStream nextLineInputStream;
+    private InputStream stream;
+    private File file;
 
     private MolecularInteractionSource source;
 
-    public OpenedInputStream(String firstLine, BufferedReader openedStream, MolecularInteractionSource source){
-        this.firstLine = firstLine;
-        if (firstLine == null){
-            hasReadFirstLine = true;
-        }
-        if (openedStream == null){
-            throw new IllegalArgumentException("The opened reader cannot be null");
-        }
-        this.reader = openedStream;
+    public OpenedInputStream(File file, MolecularInteractionSource source) throws FileNotFoundException {
+        this.file = file;
+        stream = new FileInputStream(file);
         this.source = source;
     }
 
-    @Override
-    public int read() throws IOException {
-
-        if (hasReadFirstLine){
-            if (nextLineInputStream == null){
-                nextLineInputStream = readNextLine();
-            }
-
-            return nextLineInputStream.read();
-        }
-        else {
-            if (firstLineInputStream == null){
-                firstLineInputStream = readLine();
-            }
-
-            int charRead = firstLineInputStream.read();
-            // we reached the end of firstresults
-            if (charRead == -1){
-                hasReadFirstLine = true;
-                if (nextLineInputStream == null){
-                    nextLineInputStream = readNextLine();
-                }
-
-                return nextLineInputStream.read();
-            }
-
-            return charRead;
-        }
-    }
-
-    @Override
-    public int read(byte[] bytes) throws java.io.IOException {
-
-        return read(bytes, 0, bytes.length);
-    }
-
-    @Override
-    public int read(byte[] bytes, int off, int len) throws java.io.IOException {
-        if (bytes == null){
-            throw new NullPointerException("The array of bytes to read for the OpenedInputStream is null.");
-        }
-        if (off < 0){
-            throw new IndexOutOfBoundsException("The off value to read the OpenedInputStream cannot be negative " + off);
-        }
-        if (len < 0){
-            throw new IndexOutOfBoundsException("The len value to read the OpenedInputStream cannot be negative " + len);
-        }
-        if (len < bytes.length - off ){
-            throw new IndexOutOfBoundsException("The len value to read the OpenedInputStream cannot be inferior to bytes.length - off. Length = " + len + ", bytes.length - off = " + Integer.toString(bytes.length - off));
-        }
-        if (len == 0){
-            return 0;
-        }
-
-        if (hasReadFirstLine){
-            if (nextLineInputStream == null){
-                nextLineInputStream = readNextLine();
-            }
-
-            int numberCharRead = Math.max(0, nextLineInputStream.read(bytes, off, len));
-
-            // we need to read the next openedStream line because we reached the end of the current one
-            while (numberCharRead < len && nextLineInputStream != null){
-                nextLineInputStream = readNextLine();
-
-                if (nextLineInputStream != null){
-                    int newNumberCharRead = Math.max(0,nextLineInputStream.read(bytes, off + numberCharRead, len - numberCharRead));
-                    numberCharRead+=newNumberCharRead;
-                }
-            }
-
-            if (numberCharRead == 0){
-                return -1;
-            }
-
-            return numberCharRead;
-        }
-        else {
-            if (firstLineInputStream == null){
-                firstLineInputStream = readLine();
-            }
-
-            int numberCharRead = Math.max(0, firstLineInputStream.read(bytes, off, len));
-
-            // we reached the end of firstresults
-            if (numberCharRead < len){
-                hasReadFirstLine = true;
-                nextLineInputStream = readNextLine();
-            }
-
-            // we need to read the next openedStream line because we reached the end of the current one
-            while (numberCharRead < len && nextLineInputStream != null){
-                nextLineInputStream = readNextLine();
-
-                if (nextLineInputStream != null){
-                    int newNumberCharRead = Math.max(0,nextLineInputStream.read(bytes, off + numberCharRead, len - numberCharRead));
-                    numberCharRead+=newNumberCharRead;
-                }
-            }
-
-            if (numberCharRead == 0){
-                return -1;
-            }
-
-            return numberCharRead;
-        }
-    }
-
-    @Override
-    public int available() throws java.io.IOException {
-
-        if (hasReadFirstLine){
-            if (nextLineInputStream == null){
-                nextLineInputStream = readNextLine();
-            }
-
-            if (nextLineInputStream != null){
-                return nextLineInputStream.available();
-            }
-        }
-        else{
-            if (firstLineInputStream == null){
-                firstLineInputStream = readLine();
-            }
-            int available = firstLineInputStream.available();
-
-            if (available == 0){
-                if (nextLineInputStream == null){
-                    nextLineInputStream = readNextLine();
-                }
-
-                if (nextLineInputStream != null){
-                    return nextLineInputStream.available();
-                }
-            }
-        }
-
-        return 0;
-    }
-
-    private InputStream readLine() throws IOException {
-
-        return new ByteArrayInputStream(firstLine.getBytes());
-    }
-
-    private InputStream readNextLine() throws IOException {
-
-        if (nextLine != null){
-            nextLine = reader.readLine();
-
-            if (nextLine != null){
-                return new ByteArrayInputStream(nextLine.getBytes());
-            }
-        }
-
-        return null;
-    }
-
-    @Override
-    public void close() throws IOException {
-        reader.close();
-
-        if (firstLineInputStream != null){
-            firstLineInputStream.close();
-        }
-        if (nextLineInputStream != null){
-            nextLineInputStream.close();
-        }
+    public InputStream getStream() {
+        return stream;
     }
 
     public MolecularInteractionSource getSource() {
         return source;
+    }
+
+    public void close(){
+        if (file != null){
+            file.delete();
+        }
     }
 }
