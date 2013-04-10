901a7f649532b41946adb551ea34a87fb06f9332
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-04-10 13:18:23 +0000

    created psimi-enricher module

---
+++ psimi-enricher/src/main/java/psidev/psi/mi/App.java
@@ -0,0 +1,13 @@
+package psidev.psi.mi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
