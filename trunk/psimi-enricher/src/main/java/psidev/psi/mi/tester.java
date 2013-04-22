76605cd9eb0f0a3dcc21fb57561e612ba80313d8
galdamebi@gmail.com <galdamebi@gmail.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-04-22 13:54:30 +0000

    Extended the uniprot query

--- trunk/psimi-enricher/src/main/java/psidev/psi/mi/tester.java
+++ trunk/psimi-enricher/src/main/java/psidev/psi/mi/tester.java
@@ -1,6 +1,7 @@
 package psidev.psi.mi;
 
 import psidev.psi.mi.exception.BridgeFailedException;
+import psidev.psi.mi.exception.UnrecognizedCriteriaException;
 import psidev.psi.mi.exception.UnrecognizedDatabaseException;
 import psidev.psi.mi.exception.UnrecognizedTermException;
 import psidev.psi.mi.query.*;
@@ -18,12 +19,25 @@ public class tester {
         o.setDatabase(DB);
         o.setSearchTerm(term);
 
+        newQuery(o);
+    }
+
+    public void newQuery(String DB,String term, Criteria criteria){
+        QueryObject o = new DefaultQueryObject();
+        o.setDatabase(DB);
+        o.setSearchTerm(term);
+        o.setSearchCriteria(criteria);
+
+        newQuery(o);
+    }
+
+    public void newQuery(QueryObject o){
+        System.out.println("SEARCH: " + o.getSearchTerm()+", IN: "+o.getDatabase()+", ON: "+o.getSearchCriteria());
+
         try{
-
-
             DefaultQueryInterface q = new DefaultQueryInterface();
             o = q.passQuery(o);
-            System.out.println(o.getResult()) ;
+            System.out.println("RESULT: "+o.getResult()) ;
 
         }catch(UnrecognizedTermException e) {
             System.out.println("oops - couldn't find " + o.getSearchTerm());
@@ -34,9 +48,8 @@ public class tester {
         }catch (UnrecognizedDatabaseException e){
             System.out.println("oops - couldn't find a database "+o.getDatabase()+" for " + o.getSearchTerm());
             //e.printStackTrace();
+        } catch (UnrecognizedCriteriaException e){
+            System.out.println("oops - couldn't search on "+o.getSearchCriteria()+" in " + o.getDatabase());
         }
     }
-
-
-
 }
