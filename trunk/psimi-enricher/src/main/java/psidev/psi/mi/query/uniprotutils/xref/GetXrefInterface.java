aa510e958f4f49e97a42335389ff6544a650cad3
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-05-03 15:01:41 +0000

    Beginning the restructure of the uniprot bridge. Introducing the logging with log4j using the SLF4j

---
+++ trunk/psimi-enricher/src/main/java/psidev/psi/mi/query/uniprotutils/xref/GetXrefInterface.java
@@ -0,0 +1,17 @@
+package psidev.psi.mi.query.uniprot.xref;
+
+import psidev.psi.mi.jami.model.Xref;
+import uk.ac.ebi.kraken.interfaces.uniprot.DatabaseCrossReference;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: Gabriel Aldam (galdam@ebi.ac.uk)
+ * Date: 24/04/13
+ * Time: 13:42
+ */
+public interface GetXrefInterface {
+    public ArrayList<Xref> getXref(DatabaseCrossReference dbxref);
+}
