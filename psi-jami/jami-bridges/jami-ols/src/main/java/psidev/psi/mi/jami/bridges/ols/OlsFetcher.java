74337e1b51ba51ea922497ae782f8f2b1a8b995b
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-05-21 14:52:57 +0000

    Changes to the CvTermFetcher where database is now passed as a CvTerm. This change is reflected in the OlsFetcher which now takes a CvTerm or database string (which will be converted into a CvTerm). Also, some form of database is now mandatory even if it can not be matched to an ols identity

--- psi-jami/jami-bridges/jami-ols/src/main/java/psidev/psi/mi/jami/bridges/ols/OlsFetcher.java
+++ psi-jami/jami-bridges/jami-ols/src/main/java/psidev/psi/mi/jami/bridges/ols/OlsFetcher.java
@@ -2,7 +2,6 @@ package psidev.psi.mi.jami.bridges.ols;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import psidev.psi.mi.jami.bridges.exception.*;
 import psidev.psi.mi.jami.bridges.fetcher.CvTermFetcher;
 import psidev.psi.mi.jami.model.Alias;
@@ -14,6 +13,7 @@ import psidev.psi.mi.jami.utils.CvTermUtils;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -26,72 +26,120 @@ public class OlsFetcher implements CvTermFetcher {
 
     private final Logger log = LoggerFactory.getLogger(OlsFetcher.class.getName());
 
-    OlsBridge bridge;
+    private OlsBridge bridge;
 
     public OlsFetcher() throws FetcherException {
         bridge = new OlsBridge();
     }
 
+    private Map<String,String> dbMap = null;
+    protected void initialiseDbMap(){
+        dbMap = new HashMap<String, String>();
+        dbMap.put("psi-mi", "MI");
+        dbMap.put("psi-mod", "MOD");
+        dbMap.put("psi-par", "PAR");
+        dbMap.put("go","GO");
+    }
 
-    public CvTerm getCvTermByID(String identifier, String database)
+    /**
+     * Maps the database name to the database identifier
+     *
+     * @param database
+     * @return
+     */
+    protected String databaseIdentifierGetter(String database){
+        if(dbMap == null) initialiseDbMap();
+        if(database == null)return null;
+        if(dbMap.containsKey(database)) return dbMap.get(database);
+        return null;
+    }
+
+    public CvTerm getCvTermByID(String identifier, String databaseName)
+            throws FetcherException{
+
+        if(databaseName == null) throw new NullSearchException("The provided database was null.");
+        CvTerm database = new DefaultCvTerm(databaseName);
+        return getCvTermByID(identifier, database);
+    }
+
+    public CvTerm getCvTermByID(String identifier, CvTerm database)
             throws FetcherException{
 
         if(identifier == null){
             throw new NullSearchException("The provided identifier was null.");
+        } else if (database == null) {
+            throw new NullSearchException("The provided database was null. " +
+                    "Identifier was ["+identifier+"].");
         }
-        //TODO Change the way ontology moves through this section. Capture database name
 
-        String termName = bridge.fetchFullNameByIdentifier(identifier, ontology);
+        String databaseIdentifier = databaseIdentifierGetter(database.getShortName());
+
+        String termName = bridge.fetchFullNameByIdentifier(identifier, databaseIdentifier);
         if(termName == null){
-            throw new EntryNotFoundException("Identifier "+identifier+" returned no termName.");
+            throw new EntryNotFoundException("Identifier "+identifier+" returned no termName " +
+                    "in database ["+databaseIdentifier+"].");
         }
 
-        if(ontology == null){
-            ontology = OlsUtil.ontologyGetter(identifier);
-            if(ontology == null) return null;
-        }
+        //Todo Check these fields are used correctly
+        Xref dbxref = new DefaultXref(database,identifier, CvTermUtils.getIdentity());
+        CvTerm cvTermEnriched = new DefaultCvTerm(termName,termName,dbxref);
 
-        //Todo Ideally this should be a single, simple factory method
-        Xref dbxref = new DefaultXref(new DefaultCvTerm(ontology),identifier, CvTermUtils.getIdentity());
-        CvTerm cvTerm = new DefaultCvTerm(name,name,dbxref);
-        return completeIdentifiedCvTerm(cvTerm, identifier, ontology);
+        return completeIdentifiedCvTerm(cvTermEnriched, identifier, database.getShortName());
     }
 
 
-    public CvTerm getCvTermByTerm(String searchName, String database)
+    public CvTerm getCvTermByTerm(String searchName, String databaseName)
             throws FetcherException {
+        return getCvTermByTerm(searchName, databaseName, false);
+    }
 
+    public CvTerm getCvTermByTerm(String searchName, CvTerm database)
+            throws FetcherException {
         return getCvTermByTerm(searchName, database, false);
     }
 
+    public CvTerm getCvTermByTerm(String searchName, String databaseName, boolean useFuzzySearch)
+            throws FetcherException {
+        if(databaseName == null) throw new NullSearchException("The provided database was null.");
+        CvTerm database = new DefaultCvTerm(databaseName);
+        return getCvTermByTerm(searchName, database, useFuzzySearch);
+    }
 
-    public CvTerm getCvTermByTerm(String searchName, String database, boolean useFuzzySearch)
+
+    public CvTerm getCvTermByTerm(String searchName, CvTerm database, boolean useFuzzySearch)
             throws FetcherException{
 
         if(searchName == null){
             throw new NullSearchException("The provided searchName was null.");
+        } else if (database == null) {
+            throw new NullSearchException("The provided database was null. " +
+                    "SearchName was ["+searchName+"].");
         }
 
         HashMap<String,String> identifierMap = null;
+        String databaseIdentifier = databaseIdentifierGetter(database.getShortName());
         if(useFuzzySearch){
-            identifierMap = bridge.fetchIDByBestGuessTerm(searchName, ontology);
+            identifierMap = bridge.fetchIDByBestGuessTerm(searchName, databaseIdentifier);
         }else{
-            identifierMap = bridge.fetchIDByExactTerm(searchName, ontology);
+            identifierMap = bridge.fetchIDByExactTerm(searchName, databaseIdentifier);
         }
 
         if(identifierMap == null || identifierMap.size() < 1) {
             throw new EntryNotFoundException(
-                    "The searchName ["+searchName+"] gave 0 IDs.");
+                    "The searchName ["+searchName+"] gave 0 IDs. Search was fuzzy: "+useFuzzySearch);
 
         }else if(identifierMap.size() > 1){
             if(log.isDebugEnabled()){
+                log.debug("The searchName ["+searchName+"] gave "+identifierMap.size()+" IDs. " +
+                        "Search was fuzzy: "+useFuzzySearch);
                 for(Object key : identifierMap.keySet()){
                     log.debug("Term ["+searchName+"] got the ID: "+key.toString()+" with name "+identifierMap.get(key));
                 }
             }
 
             throw new EntryNotFoundException(
-                    "The searchName ["+searchName+"] gave "+identifierMap.size()+" IDs.");
+                    "The searchName ["+searchName+"] gave "+identifierMap.size()+" IDs. " +
+                            "Search was fuzzy: "+useFuzzySearch);
         }
 
         String resultIdentifier = null;
@@ -104,44 +152,36 @@ public class OlsFetcher implements CvTermFetcher {
 
         if(resultIdentifier == null || resultTerm == null){
             throw new BadResultException(
-                    "The searchName ["+searchName+"] gave UNEXPECTED null results. "
+                    "The searchName ["+searchName+"] gave unexpected null results. "
                     +"ID is ["+resultIdentifier+"] and term is ["+resultTerm+"].");
         }
 
+        //Todo Check these fields are used correctly
+        Xref dbxref = new DefaultXref(database, resultIdentifier, CvTermUtils.getIdentity());
+        CvTerm cvTermEnriched = new DefaultCvTerm(resultTerm,resultTerm,dbxref);
 
-        String identifierOntology =  OlsUtil.ontologyGetter(resultIdentifier);
-        //TODO THROW EXCEPTION
-        if(identifierOntology == null) return null;
-
-        //Todo Ideally this should be a single, simple factory method
-        Xref dbxref = new DefaultXref(new DefaultCvTerm(identifierOntology),identifier, CvTermUtils.getIdentity());
-        CvTerm cvTerm = new DefaultCvTerm(identifierTerm,identifierTerm,dbxref);
-
-        return completeIdentifiedCvTerm(cvTerm, identifier, identifierOntology);
+        return completeIdentifiedCvTerm(cvTermEnriched, resultIdentifier, database.getShortName());
     }
-
-
 
     /**
      * Adds additional information after the identifier and full name has been resolved.
      * This adds the short name (if it can be found) and the synonyms (if there are any).
      * The short name and synonyms can only be found for MI and MOD identifiers.
-     * @param cvTerm
+     * @param cvTermEnriched
      * @param identifier
-     * @param ontology
+     * @param databaseName
      * @return
      * @throws BridgeFailedException
      */
-    private CvTerm completeIdentifiedCvTerm(CvTerm cvTerm, String identifier, String ontology)
+    private CvTerm completeIdentifiedCvTerm(CvTerm cvTermEnriched, String identifier, String databaseName)
             throws FetcherException{
 
         HashMap metaDataMap = bridge.fetchMetaDataByID(identifier);
-        String shortName = bridge.extractShortNameFromMetaData(metaDataMap, ontology);
-        if(shortName != null) cvTerm.setShortName(shortName);
-        Collection<Alias> synonyms = bridge.extractSynonymsFromMetaData(metaDataMap, ontology);
-        if(synonyms != null) cvTerm.getSynonyms().addAll(synonyms);
+        String shortName = bridge.extractShortNameFromMetaData(metaDataMap, databaseName);
+        if(shortName != null) cvTermEnriched.setShortName(shortName);
+        Collection<Alias> synonyms = bridge.extractSynonymsFromMetaData(metaDataMap, databaseName);
+        if(synonyms != null) cvTermEnriched.getSynonyms().addAll(synonyms);
 
-        return cvTerm;
+        return cvTermEnriched;
     }
-
 }
