a56201c643c675a4411878a1019b93da15674b97
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-05-22 10:12:43 +0000

    Added new sub exceptions with a super fetcher exception for generalisation. Updated the Fetcher interfaces with new exceptions. Refined the Fetcher methods to add more fields, exceptions and clearer flow.

---
+++ trunk/psi-jami/jami-bridges/bridges-core/src/main/java/psidev/psi/mi/jami/bridges/fetcher/OrganismFetcher.java
@@ -0,0 +1,16 @@
+package psidev.psi.mi.jami.bridges.fetcher;
+
+import psidev.psi.mi.jami.bridges.exception.FetcherException;
+import psidev.psi.mi.jami.model.Organism;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: Gabriel Aldam (galdam@ebi.ac.uk)
+ * Date: 22/05/13
+ * Time: 10:20
+ */
+public interface OrganismFetcher {
+    public Organism getOrganismByTaxID(int taxID)
+            throws FetcherException;
+}
