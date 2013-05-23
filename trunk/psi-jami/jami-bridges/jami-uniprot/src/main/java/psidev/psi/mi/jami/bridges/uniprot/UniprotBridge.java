e70805fc945cd2cca1e0bc8b85c48fc3311b0f40
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-05-23 09:08:01 +0000

    Added dependency for uniprot bridge

--- trunk/psi-jami/jami-bridges/jami-uniprot/src/main/java/psidev/psi/mi/jami/bridges/uniprot/UniprotBridge.java
+++ trunk/psi-jami/jami-bridges/jami-uniprot/src/main/java/psidev/psi/mi/jami/bridges/uniprot/UniprotBridge.java
@@ -1,10 +1,11 @@
-package psidev.psi.mi.jami.bridges.uniprot.uniprot;
+package psidev.psi.mi.jami.bridges.uniprot;
 
 import psidev.psi.mi.jami.bridges.exception.BridgeFailedException;
 import uk.ac.ebi.kraken.interfaces.uniprot.UniProtEntry;
-import uk.ac.ebi.kraken.uuw.services.remoting.EntryRetrievalService;
-import uk.ac.ebi.kraken.uuw.services.remoting.RemoteDataAccessException;
 import uk.ac.ebi.kraken.uuw.services.remoting.UniProtJAPI;
+import uk.ac.ebi.kraken.uuw.services.remoting.RemoteDataAccessException;
+import uk.ac.ebi.kraken.uuw.services.remoting.*;
+import uk.ac.ebi.kraken.uuw.services.remoting.EntryRetrievalService;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,24 +20,24 @@ public class UniprotBridge {
     public UniprotBridge() throws BridgeFailedException {
         try{
             entryRetrievalService = UniProtJAPI.factory.getEntryRetrievalService();
-        }catch (RemoteDataAccessException e){
-            throw new BridgeFailedException(e);
+        }catch(RemoteDataAccessException e) {
+            throw new BridgeFailedException();
         }
     }
 
     public UniProtEntry fetchEntryByID(String ID)
             throws BridgeFailedException {
 
-        try{
+        //try{
             UniProtEntry entry = (UniProtEntry) entryRetrievalService.getUniProtEntry(ID);
 
             if(entry == null) {
                 return null;
             }
             return entry;
-        }catch (RemoteDataAccessException e){
+        /*}catch (RemoteDataAccessException e){
             throw new BridgeFailedException(e);
-        }
+        } */
     }
 
     public UniProtEntry fetchEntriesByIDs(String[] IDs)
