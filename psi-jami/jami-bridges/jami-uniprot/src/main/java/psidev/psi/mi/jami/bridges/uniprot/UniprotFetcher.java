4e87818a071d7e33405c94175ad6ebacb6a174bf
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-05-23 09:08:01 +0000

    Added dependency for uniprot bridge

--- psi-jami/jami-bridges/jami-uniprot/src/main/java/psidev/psi/mi/jami/bridges/uniprot/UniprotFetcher.java
+++ psi-jami/jami-bridges/jami-uniprot/src/main/java/psidev/psi/mi/jami/bridges/uniprot/UniprotFetcher.java
@@ -1,15 +1,10 @@
-package psidev.psi.mi.jami.bridges.uniprot.uniprot;
+package psidev.psi.mi.jami.bridges.uniprot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
-import psidev.psi.mi.jami.bridges.exception.BridgeFailedException;
 import psidev.psi.mi.jami.bridges.exception.EntryNotFoundException;
 import psidev.psi.mi.jami.bridges.exception.FetcherException;
 import psidev.psi.mi.jami.bridges.exception.NullSearchException;
 import psidev.psi.mi.jami.bridges.fetcher.ProteinFetcher;
-import psidev.psi.mi.jami.bridges.uniprot.uniprot.uniprotutil.UniprotToJAMI;
+import psidev.psi.mi.jami.bridges.uniprot.uniprotutil.UniprotToJAMI;
 import psidev.psi.mi.jami.model.Protein;
 import uk.ac.ebi.kraken.interfaces.uniprot.UniProtEntry;
 
@@ -23,12 +18,12 @@ import uk.ac.ebi.kraken.interfaces.uniprot.UniProtEntry;
 public class UniprotFetcher
         implements ProteinFetcher {
 
-    private final Logger log = LoggerFactory.getLogger(UniprotFetcher.class.getName());
+    //private final Logger log = LoggerFactory.getLogger(UniprotFetcher.class.getName());
 
     UniprotBridge bridge;
 
     public UniprotFetcher() throws FetcherException {
-        log.trace("Starting uniprot bridge");
+        //log.debug("Starting uniprot bridge");
         bridge = new UniprotBridge();
     }
 
@@ -47,11 +42,4 @@ public class UniprotFetcher
 
         return p;
     }
-
-    /*
-    public Organism getOrganismByID(String ID) throws BridgeFailedException {
-        UniProtEntry e = bridge.fetchEntryByID(ID);
-        Organism o = UniprotToJAMI.getOrganismFromEntry(e);
-        return o;
-    }  */
 }
