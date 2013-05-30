d98dfa4b40fdb59bdac2a4843dccc07c8d8e5edb
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-05-30 16:00:21 +0000

    Added unit test for DefaultFeatureModificationEffector and the comparators

--- psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/model/impl/DefaultFeatureModificationEffector.java
+++ psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/model/impl/DefaultFeatureModificationEffector.java
@@ -3,6 +3,9 @@ package psidev.psi.mi.jami.model.impl;
 import psidev.psi.mi.jami.model.AllostericEffectorType;
 import psidev.psi.mi.jami.model.FeatureModificationEffector;
 import psidev.psi.mi.jami.model.ModelledFeature;
+import psidev.psi.mi.jami.model.MoleculeEffector;
+import psidev.psi.mi.jami.utils.comparator.cooperativity.UnambiguousExactMoleculeEffectorComparator;
+import psidev.psi.mi.jami.utils.comparator.cooperativity.UnambiguousFeatureModificationEffectorComparator;
 
 /**
  * Default implementation for FeatureModificationEffector
@@ -29,5 +32,28 @@ public class DefaultFeatureModificationEffector implements FeatureModificationEf
 
     public AllostericEffectorType getEffectorType() {
         return AllostericEffectorType.feature_modification;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }
+
+        if (!(o instanceof FeatureModificationEffector)){
+            return false;
+        }
+
+        return UnambiguousFeatureModificationEffectorComparator.areEquals(this, (FeatureModificationEffector) o);
+    }
+
+    @Override
+    public int hashCode() {
+        return UnambiguousFeatureModificationEffectorComparator.hashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return "feature modification effector: " + feature.toString();
     }
 }
