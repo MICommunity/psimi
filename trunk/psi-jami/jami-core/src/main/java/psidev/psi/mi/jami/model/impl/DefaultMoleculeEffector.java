5819e102e44e91ffa820f2702ffc1aa48a650c5d
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-05-30 15:40:58 +0000

    Added unit test forDefaultMoleculeEffector and the comparators

--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/model/impl/DefaultMoleculeEffector.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/model/impl/DefaultMoleculeEffector.java
@@ -3,6 +3,7 @@ package psidev.psi.mi.jami.model.impl;
 import psidev.psi.mi.jami.model.AllostericEffectorType;
 import psidev.psi.mi.jami.model.ModelledParticipant;
 import psidev.psi.mi.jami.model.MoleculeEffector;
+import psidev.psi.mi.jami.utils.comparator.cooperativity.UnambiguousExactMoleculeEffectorComparator;
 
 /**
  * Default implementation for MoleculeEffector
@@ -29,5 +30,28 @@ public class DefaultMoleculeEffector implements MoleculeEffector{
 
     public AllostericEffectorType getEffectorType() {
         return AllostericEffectorType.molecule;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }
+
+        if (!(o instanceof MoleculeEffector)){
+            return false;
+        }
+
+        return UnambiguousExactMoleculeEffectorComparator.areEquals(this, (MoleculeEffector) o);
+    }
+
+    @Override
+    public int hashCode() {
+        return UnambiguousExactMoleculeEffectorComparator.hashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return "molecule effector: " + participant.toString();
     }
 }
