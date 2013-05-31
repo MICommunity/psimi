9bc5dce30e056df3f967ac5570a49d1a2a09819c
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-05-31 10:01:28 +0000

    Added unit tests for CooperativeEffects and comparators for cooperative effects

---
+++ psi-jami/jami-core/src/test/java/psidev/psi/mi/jami/utils/comparator/cooperativity/UnambiguousCooperativeEffectBaseComparatorTest.java
@@ -0,0 +1,147 @@
+package psidev.psi.mi.jami.utils.comparator.cooperativity;
+
+import org.junit.Assert;
+import org.junit.Test;
+import psidev.psi.mi.jami.model.CooperativeEffect;
+import psidev.psi.mi.jami.model.impl.*;
+
+/**
+ * Unit tester for
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>31/05/13</pre>
+ */
+
+public class UnambiguousCooperativeEffectBaseComparatorTest {
+
+    private UnambiguousCooperativeEffectBaseComparator comparator = new UnambiguousCooperativeEffectBaseComparator();
+
+    @Test
+    public void test_cooperative_effect_null_after() throws Exception {
+        CooperativeEffect effect1 = null;
+        CooperativeEffect effect2 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"));
+
+        Assert.assertTrue(comparator.compare(effect1, effect2) > 0);
+        Assert.assertTrue(comparator.compare(effect2, effect1) < 0);
+
+        Assert.assertFalse(UnambiguousCooperativeEffectBaseComparator.areEquals(effect1, effect2));
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.hashCode(effect1) != UnambiguousCooperativeEffectBaseComparator.hashCode(effect2));
+    }
+
+    @Test
+    public void test_compare_different_outcome() throws Exception {
+        CooperativeEffect effect1 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome 2"));
+        CooperativeEffect effect2 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"));
+
+        Assert.assertTrue(comparator.compare(effect1, effect2) > 0);
+        Assert.assertTrue(comparator.compare(effect2, effect1) < 0);
+
+        Assert.assertFalse(UnambiguousCooperativeEffectBaseComparator.areEquals(effect1, effect2));
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.hashCode(effect1) != UnambiguousCooperativeEffectBaseComparator.hashCode(effect2));
+    }
+
+    @Test
+    public void test_compare_same_outcome() throws Exception {
+        CooperativeEffect effect1 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"));
+        CooperativeEffect effect2 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"));
+
+        Assert.assertTrue(comparator.compare(effect1, effect2) == 0);
+        Assert.assertTrue(comparator.compare(effect2, effect1) == 0);
+
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.areEquals(effect1, effect2));
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.hashCode(effect1) == UnambiguousCooperativeEffectBaseComparator.hashCode(effect2));
+    }
+
+    @Test
+    public void test_compare_same_outcome_different_response() throws Exception {
+        CooperativeEffect effect1 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response 2"));
+        CooperativeEffect effect2 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response"));
+
+        Assert.assertTrue(comparator.compare(effect1, effect2) > 0);
+        Assert.assertTrue(comparator.compare(effect2, effect1) < 0);
+
+        Assert.assertFalse(UnambiguousCooperativeEffectBaseComparator.areEquals(effect1, effect2));
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.hashCode(effect1) != UnambiguousCooperativeEffectBaseComparator.hashCode(effect2));
+    }
+
+    @Test
+    public void test_compare_same_response() throws Exception {
+        CooperativeEffect effect1 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response"));
+        CooperativeEffect effect2 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response"));
+
+        Assert.assertTrue(comparator.compare(effect1, effect2) == 0);
+        Assert.assertTrue(comparator.compare(effect2, effect1) == 0);
+
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.areEquals(effect1, effect2));
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.hashCode(effect1) == UnambiguousCooperativeEffectBaseComparator.hashCode(effect2));
+    }
+
+    @Test
+    public void test_compare_same_response_different_evidences() throws Exception {
+        CooperativeEffect effect1 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response"));
+        effect1.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12345")));
+        effect1.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12346")));
+        CooperativeEffect effect2 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response"));
+        effect2.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12345")));
+
+        Assert.assertTrue(comparator.compare(effect1, effect2) > 0);
+        Assert.assertTrue(comparator.compare(effect2, effect1) < 0);
+
+        Assert.assertFalse(UnambiguousCooperativeEffectBaseComparator.areEquals(effect1, effect2));
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.hashCode(effect1) != UnambiguousCooperativeEffectBaseComparator.hashCode(effect2));
+    }
+
+    @Test
+    public void test_compare_same_evidences() throws Exception {
+        CooperativeEffect effect1 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response"));
+        effect1.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12345")));
+        effect1.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12346")));
+        CooperativeEffect effect2 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response"));
+        effect2.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12346")));
+        effect2.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12345")));
+        Assert.assertTrue(comparator.compare(effect1, effect2) == 0);
+        Assert.assertTrue(comparator.compare(effect2, effect1) == 0);
+
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.areEquals(effect1, effect2));
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.hashCode(effect1) == UnambiguousCooperativeEffectBaseComparator.hashCode(effect2));
+    }
+
+    @Test
+    public void test_compare_same_evidences_different_affected_interactions() throws Exception {
+        CooperativeEffect effect1 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response 2"));
+        effect1.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12345")));
+        effect1.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12346")));
+        effect1.getAffectedInteractions().add(new DefaultModelledInteraction("test 1"));
+        effect1.getAffectedInteractions().add(new DefaultModelledInteraction("test 2"));
+        CooperativeEffect effect2 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response"));
+        effect2.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12346")));
+        effect2.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12345")));
+        effect2.getAffectedInteractions().add(new DefaultModelledInteraction("test 1"));
+
+        Assert.assertTrue(comparator.compare(effect1, effect2) > 0);
+        Assert.assertTrue(comparator.compare(effect2, effect1) < 0);
+
+        Assert.assertFalse(UnambiguousCooperativeEffectBaseComparator.areEquals(effect1, effect2));
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.hashCode(effect1) != UnambiguousCooperativeEffectBaseComparator.hashCode(effect2));
+    }
+
+    @Test
+    public void test_compare_same_affected_interactions() throws Exception {
+        CooperativeEffect effect1 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response"));
+        effect1.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12345")));
+        effect1.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12346")));
+        effect1.getAffectedInteractions().add(new DefaultModelledInteraction("test 1"));
+        effect1.getAffectedInteractions().add(new DefaultModelledInteraction("test 2"));
+        CooperativeEffect effect2 = new DefaultCooperativeEffect(new DefaultCvTerm("test outcome"), new DefaultCvTerm("test response"));
+        effect2.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12346")));
+        effect2.getCooperativityEvidences().add(new DefaultCooperativityEvidence(new DefaultPublication("12345")));
+        effect2.getAffectedInteractions().add(new DefaultModelledInteraction("test 2"));
+        effect2.getAffectedInteractions().add(new DefaultModelledInteraction("test 1"));
+        Assert.assertTrue(comparator.compare(effect1, effect2) == 0);
+        Assert.assertTrue(comparator.compare(effect2, effect1) == 0);
+
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.areEquals(effect1, effect2));
+        Assert.assertTrue(UnambiguousCooperativeEffectBaseComparator.hashCode(effect1) == UnambiguousCooperativeEffectBaseComparator.hashCode(effect2));
+    }
+}
