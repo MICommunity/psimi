ba568a9088a12ec9e37cc280edfd06f4fcdd2cad
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-06-04 12:40:16 +0000

    added unit tests for MIDataSourceFactory

---
+++ psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/datasource/MIDataSourceFactory.java
@@ -0,0 +1,147 @@
+package psidev.psi.mi.jami.datasource;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * A factory to create data sources.
+ *
+ * The datasource have to be registered first and then the factory can create the proper datasource depending on the options
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>01/03/13</pre>
+ */
+
+public class MIDataSourceFactory {
+
+    private static final MIDataSourceFactory instance = new MIDataSourceFactory();
+
+    private Map<Class<? extends MIDataSource>, Map<String, Object>> registeredDataSources;
+
+    public static final String SOURCE_OPTION_KEY = "file_data_source_key";
+
+    private MIDataSourceFactory(){
+        registeredDataSources = new ConcurrentHashMap<Class<? extends MIDataSource>, Map<String, Object>>();
+    }
+
+    public static MIDataSourceFactory getInstance() {
+        return instance;
+    }
+
+    public MIDataSource getMIDataSourceWith(Map<String,Object> requiredOptions) throws InstantiationException, IllegalAccessException {
+
+        for (Map.Entry<Class<? extends MIDataSource>, Map<String, Object>> entry : registeredDataSources.entrySet()){
+            // we check for a DataSource that can be used with the given options
+            if (areSupportedOptions(entry.getValue(), requiredOptions)){
+                return instantiateNewDataSource(entry.getKey(), entry.getValue());
+            }
+        }
+
+        return null;
+    }
+
+    public MIFileDataSource getMIFileDataSourceFrom(File file, Map<String,Object> requiredOptions) throws InstantiationException, IllegalAccessException {
+
+        if (requiredOptions == null){
+            requiredOptions = new HashMap<String, Object>();
+            requiredOptions.put(SOURCE_OPTION_KEY, file);
+        }
+
+        for (Map.Entry<Class<? extends MIDataSource>, Map<String, Object>> entry : registeredDataSources.entrySet()){
+            // we check for a DataSource that can be used with the given options
+            if (entry.getKey().isAssignableFrom(MIFileDataSource.class) &&
+                    areSupportedOptions(entry.getValue(), requiredOptions)){
+                return (MIFileDataSource) instantiateNewDataSource(entry.getKey(), entry.getValue());
+            }
+        }
+
+        return null;
+    }
+
+    public MIFileDataSource getMIFileDataSourceFrom(InputStream stream, Map<String,Object> requiredOptions) throws InstantiationException, IllegalAccessException {
+
+        if (requiredOptions == null){
+            requiredOptions = new HashMap<String, Object>();
+            requiredOptions.put(SOURCE_OPTION_KEY, stream);
+        }
+
+        for (Map.Entry<Class<? extends MIDataSource>, Map<String, Object>> entry : registeredDataSources.entrySet()){
+            // we check for a DataSource that can be used with the given options
+            if (entry.getKey().isAssignableFrom(MIFileDataSource.class) &&
+                    areSupportedOptions(entry.getValue(), requiredOptions)){
+                return (MIFileDataSource) instantiateNewDataSource(entry.getKey(), entry.getValue());
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Register a datasource with options in this factory
+     * @param dataSourceClass
+     * @param supportedOptions
+     * @return
+     */
+    public void registerDataSource(Class<? extends MIFileDataSource> dataSourceClass, Map<String,Object> supportedOptions){
+        if (dataSourceClass == null){
+            throw new IllegalArgumentException("Cannot register a MIDataSource without a dataSourceClass");
+        }
+
+        registeredDataSources.put(dataSourceClass, supportedOptions != null ? supportedOptions : new ConcurrentHashMap<String, Object>());
+    }
+
+    /**
+     * Remove the dataSource from this factory
+     * @param dataSourceClass
+     */
+    public void removeDataSource(Class<? extends MIFileDataSource> dataSourceClass){
+        registeredDataSources.remove(dataSourceClass);
+    }
+
+    /**
+     * Clear all the registered datasources from this factory
+     */
+    public void clearRegisteredDataSources(){
+        registeredDataSources.clear();
+    }
+
+    /**
+     * By default, all the options in the given options should be in the supportedOptions of this
+     * registeredDataSource.
+     * @param supportedOptions options supported
+     * @param options that are required
+     * @return
+     */
+    private boolean areSupportedOptions(Map<String, Object> supportedOptions, Map<String, Object> options) {
+        // no required options
+        if (options == null || options.isEmpty()){
+            return true;
+        }
+
+        // check if required options are supported
+        for (Map.Entry<String, Object> entry : options.entrySet()){
+            if (supportedOptions.containsKey(entry.getKey())){
+                if (!supportedOptions.get(entry.getKey()).equals(entry.getValue())){
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private MIDataSource instantiateNewDataSource(Class<? extends MIDataSource> dataSourceClass, Map<String, Object> options) throws IllegalAccessException, InstantiationException {
+
+        MIDataSource dataSource = dataSourceClass.newInstance();
+        dataSource.initialiseContext(options);
+
+        return dataSource;
+    }
+}
---
+++ psi-jami/jami-core/src/test/java/psidev/psi/mi/jami/datasource/MiDataSourceFactoryTest.java
@@ -0,0 +1,103 @@
+package psidev.psi.mi.jami.datasource;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Unit tester for MiDataSourceFactory
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>04/06/13</pre>
+ */
+
+public class MiDataSourceFactoryTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void register_data_source_no_give_class(){
+
+        MIDataSourceFactory.getInstance().registerDataSource(null, Collections.EMPTY_MAP);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_missing_file_data_source_options() throws IllegalAccessException, InstantiationException {
+
+        MIDataSourceFactory.getInstance().registerDataSource(MockExperimentDataSource.class, Collections.EMPTY_MAP);
+
+        Map<String, Object> requiredOptions = new HashMap<String, Object>();
+
+        MIDataSourceFactory.getInstance().getMIDataSourceWith(requiredOptions);
+    }
+
+    public void test_existing_file_data_source_options() throws IllegalAccessException, InstantiationException {
+
+        MIDataSourceFactory.getInstance().registerDataSource(MockExperimentDataSource.class, Collections.EMPTY_MAP);
+
+        Map<String, Object> requiredOptions = new HashMap<String, Object>();
+
+        Assert.assertNotNull(MIDataSourceFactory.getInstance().getMIFileDataSourceFrom(new ByteArrayInputStream("".getBytes()), requiredOptions));
+        Assert.assertTrue(MIDataSourceFactory.getInstance().getMIFileDataSourceFrom(new ByteArrayInputStream("".getBytes()), requiredOptions) instanceof MockExperimentDataSource);
+    }
+
+    @Test
+    public void test_no_supported_options() throws IllegalAccessException, InstantiationException {
+        MIDataSourceFactory.getInstance().registerDataSource(MockInteractionDataSource.class, Collections.EMPTY_MAP);
+
+        Map<String, Object> requiredOptions = new HashMap<String, Object>();
+        requiredOptions.put("file_source", "xml");
+
+        Assert.assertNull(MIDataSourceFactory.getInstance().getMIDataSourceWith(requiredOptions));
+    }
+
+    @Test
+    public void test_supports_options() throws IllegalAccessException, InstantiationException {
+        Map<String, Object> possibleOptions = new HashMap<String, Object>();
+        possibleOptions.put("file_source", "xml");
+
+        MIDataSourceFactory.getInstance().registerDataSource(MockInteractionDataSource.class, possibleOptions);
+
+        Map<String, Object> requiredOptions = new HashMap<String, Object>();
+        requiredOptions.put("file_source", "xml");
+
+        Assert.assertNotNull(MIDataSourceFactory.getInstance().getMIDataSourceWith(requiredOptions));
+    }
+
+    @Test
+    public void test_supports_options_more_options_than_required() throws IllegalAccessException, InstantiationException {
+        Map<String, Object> possibleOptions = new HashMap<String, Object>();
+        possibleOptions.put("file_source", "xml");
+        possibleOptions.put("streaming", "true");
+
+        MIDataSourceFactory.getInstance().registerDataSource(MockInteractionDataSource.class, possibleOptions);
+
+        Map<String, Object> requiredOptions = new HashMap<String, Object>();
+        requiredOptions.put("file_source", "xml");
+
+        Assert.assertNotNull(MIDataSourceFactory.getInstance().getMIDataSourceWith(requiredOptions));
+    }
+
+    @Test
+    public void test_does_not_support_one_required_option() throws IllegalAccessException, InstantiationException {
+        Map<String, Object> possibleOptions = new HashMap<String, Object>();
+        possibleOptions.put("file_source", "xml");
+        possibleOptions.put("streaming", "false");
+
+        MIDataSourceFactory.getInstance().registerDataSource(MockInteractionDataSource.class, possibleOptions);
+
+        Map<String, Object> requiredOptions = new HashMap<String, Object>();
+        requiredOptions.put("file_source", "xml");
+        requiredOptions.put("streaming", "true");
+
+        Assert.assertNull(MIDataSourceFactory.getInstance().getMIDataSourceWith(requiredOptions));
+
+        Map<String, Object> requiredOptions2 = new HashMap<String, Object>();
+        requiredOptions2.put("file_source", "xml");
+        requiredOptions2.put("experiment", "true");
+
+        Assert.assertNull(MIDataSourceFactory.getInstance().getMIDataSourceWith(requiredOptions));
+    }
+}
