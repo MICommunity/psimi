5c42da20fbd494701ea297a9ad016cc09c0491f6
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-06-10 14:37:37 +0000

    Added a utility method in InteractorUtils

--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/InteractorUtils.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/InteractorUtils.java
@@ -1,5 +1,6 @@
 package psidev.psi.mi.jami.utils;
 
+import psidev.psi.mi.jami.model.CvTerm;
 import psidev.psi.mi.jami.model.Interactor;
 import psidev.psi.mi.jami.model.impl.DefaultInteractor;
 
@@ -15,5 +16,36 @@ public class InteractorUtils {
 
     public static Interactor createUnknownBasicInteractor(){
         return new DefaultInteractor("unknown", CvTermUtils.createMICvTerm(Interactor.UNKNOWN_INTERACTOR, Interactor.UNKNOWN_INTERACTOR_MI));
+    }
+
+    /**
+     * To know if an interactor have a specific interactor type.
+     * @param interactor
+     * @param typeId
+     * @param typeName
+     * @return true if the interactor has the type with given name/identifier
+     */
+    public static boolean doesInteractorHaveType(Interactor interactor, String typeId, String typeName){
+
+        if (interactor == null || (typeName == null && typeId == null)){
+            return false;
+        }
+
+        CvTerm type = interactor.getInteractorType();
+        if (type == null){
+            return false;
+        }
+
+        // we can compare identifiers
+        if (typeId != null && type.getMIIdentifier() != null){
+            // we have the same type id
+            return type.getMIIdentifier().equals(typeId);
+        }
+        // we need to compare type names
+        else if (typeName != null) {
+            return typeName.toLowerCase().trim().equals(type.getShortName().toLowerCase().trim());
+        }
+
+        return false;
     }
 }
