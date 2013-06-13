f77d9147c0779f8cf16821de907293084a362243
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-06-13 17:03:50 +0000

    Added skeleton for new enrichers, updaters, fetcher and listeners in readiness for full implementation.

---
+++ trunk/psi-jami/jami-bridges/bridges-core/src/main/java/psidev/psi/mi/jami/bridges/fetcher/ParticipantFetcher.java
@@ -0,0 +1,11 @@
+package psidev.psi.mi.jami.bridges.fetcher;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: Gabriel Aldam (galdam@ebi.ac.uk)
+ * Date: 13/06/13
+ * Time: 17:17
+ */
+public interface ParticipantFetcher {
+}
