e09d6af14a0a490a6e23c9b1910f921f307f79b3
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-06-14 13:29:07 +0000

    Added psimitab.jj with some starting point for JavaCC parser

---
+++ psi-jami/jami-mitab/src/main/java/psidev/psi/mi/jami/mitab/listener/MitabParserListener.java
@@ -0,0 +1,22 @@
+package psidev.psi.mi.jami.mitab.listener;
+
+/**
+ * A listener listening to events when parsing a mitab file
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>14/06/13</pre>
+ */
+
+public interface MitabParserListener {
+
+    public void onSeveralUniqueIdentifiers(int line, int column, int mitabColumn);
+
+    public void onTextFoundInIdentifier(int line, int column, int mitabColumn);
+
+    public void onMissingCvTermName(int line, int column, int mitabColumn);
+
+    public void onSeveralCvTermFound(int line, int column, int mitabColumn);
+
+    public void onSeveralFirstAuthorFound(int line, int column, int mitabColumn);
+}
