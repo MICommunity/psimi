76501546304a6461316214f872c11dcc8fe8b5d1
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-06-18 12:42:31 +0000

    Updated MIDataSourceFactory : updated option names

--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/datasource/MIDataSourceFactory.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/datasource/MIDataSourceFactory.java
@@ -26,6 +26,8 @@ public class MIDataSourceFactory {
     public static final String INPUT_FILE_OPTION_KEY = "input_file_key";
     public static final String INPUT_STREAM_OPTION_KEY = "input_stream_key";
     public static final String READER_OPTION_KEY = "input_reader_key";
+    public static final String STREAMING_OPTION_KEY = "streaming_key";
+    public static final String OBJECT_OPTION_KEY = "object_key";
 
     private MIDataSourceFactory(){
         registeredDataSources = new ConcurrentHashMap<Class<? extends MIDataSource>, Map<String, Object>>();
