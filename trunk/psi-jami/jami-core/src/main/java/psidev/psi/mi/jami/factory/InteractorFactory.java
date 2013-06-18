4e4f672b985f2eb358bdb96e4418e693961d0a36
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-06-18 15:29:31 +0000

    Added two property files

---
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/factory/InteractorFactory.java
@@ -0,0 +1,13 @@
+package psidev.psi.mi.jami.factory;
+
+/**
+ * This factory allows to create a proper interactor depending on the type
+ * and xrefs
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>18/06/13</pre>
+ */
+
+public class InteractorFactory {
+}
