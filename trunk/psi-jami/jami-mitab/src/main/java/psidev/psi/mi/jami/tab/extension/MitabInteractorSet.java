e65ec3f47b67cfc15780377d44a56de9847b5fa8
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-06-18 16:28:30 +0000

    Added a MitabInteractorFactory

---
+++ trunk/psi-jami/jami-mitab/src/main/java/psidev/psi/mi/jami/tab/extension/MitabInteractorSet.java
@@ -0,0 +1,92 @@
+package psidev.psi.mi.jami.tab.extension;
+
+import psidev.psi.mi.jami.datasource.FileSourceContext;
+import psidev.psi.mi.jami.model.CvTerm;
+import psidev.psi.mi.jami.model.Organism;
+import psidev.psi.mi.jami.model.Xref;
+import psidev.psi.mi.jami.model.impl.DefaultInteractorSet;
+
+/**
+ * The mitab extension for InteractorSet
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>18/06/13</pre>
+ */
+
+public class MitabInteractorSet extends DefaultInteractorSet implements FileSourceContext{
+
+    private MitabSourceLocator sourceLocator;
+
+    public MitabInteractorSet(String name, CvTerm type) {
+        super(name, type);
+    }
+
+    public MitabInteractorSet(String name, String fullName, CvTerm type) {
+        super(name, fullName, type);
+    }
+
+    public MitabInteractorSet(String name, CvTerm type, Organism organism) {
+        super(name, type, organism);
+    }
+
+    public MitabInteractorSet(String name, String fullName, CvTerm type, Organism organism) {
+        super(name, fullName, type, organism);
+    }
+
+    public MitabInteractorSet(String name, CvTerm type, Xref uniqueId) {
+        super(name, type, uniqueId);
+    }
+
+    public MitabInteractorSet(String name, String fullName, CvTerm type, Xref uniqueId) {
+        super(name, fullName, type, uniqueId);
+    }
+
+    public MitabInteractorSet(String name, CvTerm type, Organism organism, Xref uniqueId) {
+        super(name, type, organism, uniqueId);
+    }
+
+    public MitabInteractorSet(String name, String fullName, CvTerm type, Organism organism, Xref uniqueId) {
+        super(name, fullName, type, organism, uniqueId);
+    }
+
+    public MitabInteractorSet(String name) {
+        super(name);
+    }
+
+    public MitabInteractorSet(String name, String fullName) {
+        super(name, fullName);
+    }
+
+    public MitabInteractorSet(String name, Organism organism) {
+        super(name, organism);
+    }
+
+    public MitabInteractorSet(String name, String fullName, Organism organism) {
+        super(name, fullName, organism);
+    }
+
+    public MitabInteractorSet(String name, Xref uniqueId) {
+        super(name, uniqueId);
+    }
+
+    public MitabInteractorSet(String name, String fullName, Xref uniqueId) {
+        super(name, fullName, uniqueId);
+    }
+
+    public MitabInteractorSet(String name, Organism organism, Xref uniqueId) {
+        super(name, organism, uniqueId);
+    }
+
+    public MitabInteractorSet(String name, String fullName, Organism organism, Xref uniqueId) {
+        super(name, fullName, organism, uniqueId);
+    }
+
+    public MitabSourceLocator getSourceLocator() {
+        return sourceLocator;
+    }
+
+    public void setSourceLocator(MitabSourceLocator sourceLocator) {
+        this.sourceLocator = sourceLocator;
+    }
+}
