95a58f3cc0ed1a2ba77d02bfb0cc361738caafb5
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-06-21 10:31:48 +0000

    Updated interfaces for writer and MiDataSource : added fatal for general exceptions

--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/datasource/FileParsingErrorType.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/datasource/FileParsingErrorType.java
@@ -10,11 +10,6 @@ package psidev.psi.mi.jami.datasource;
 
 public enum FileParsingErrorType {
 
-    invalid_syntax, multiple_experimental_roles, multiple_host_organisms, multiple_experiments, multiple_expressed_in,
-    multiple_participant_identification_methods, multiple_interaction_types, missing_biological_role, missing_experimental_role, missing_cv, clustered_content,
-    missing_database, missing_database_accession, invalid_feature_range, missing_publication, missing_interaction_detection_method,
-    missing_interactor_type, missing_alias_name, missing_annotation_topic,
-    missing_confidence_type, missing_confidence_value, interaction_evidence_without_experiment, missing_parameter_type, missing_parameter_factor,
-    participant_without_interactor, missing_range_status, missing_range_position, fatal
+    invalid_syntax, fatal, syntax_warning
 
 }
