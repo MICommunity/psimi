7c039eccb84eb3d4ebe7314381fb8e3ef73a77d1
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-06-26 08:44:57 +0000

    Added unit tester for Mitab26 extended writers

--- trunk/psi-jami/jami-mitab/src/test/java/psidev/psi/mi/jami/mitab/io/writer/extended/Mitab26BinaryWriterTest.java
+++ trunk/psi-jami/jami-mitab/src/test/java/psidev/psi/mi/jami/mitab/io/writer/extended/Mitab26BinaryWriterTest.java
@@ -5,14 +5,18 @@ import org.junit.Test;
 import psidev.psi.mi.jami.binary.BinaryInteraction;
 import psidev.psi.mi.jami.binary.BinaryInteractionEvidence;
 import psidev.psi.mi.jami.binary.ModelledBinaryInteraction;
+import psidev.psi.mi.jami.binary.expansion.ComplexExpansionMethod;
 import psidev.psi.mi.jami.exception.DataSourceWriterException;
+import psidev.psi.mi.jami.exception.IllegalParameterException;
 import psidev.psi.mi.jami.factory.InteractionWriterFactory;
 import psidev.psi.mi.jami.model.ModelledParticipant;
+import psidev.psi.mi.jami.model.Participant;
 import psidev.psi.mi.jami.model.ParticipantEvidence;
 import psidev.psi.mi.jami.tab.MitabVersion;
 import psidev.psi.mi.jami.tab.extension.*;
-import psidev.psi.mi.jami.tab.io.writer.extended.Mitab25BinaryWriter;
+import psidev.psi.mi.jami.tab.io.writer.extended.Mitab26BinaryWriter;
 import psidev.psi.mi.jami.tab.utils.MitabUtils;
+import psidev.psi.mi.jami.utils.ChecksumUtils;
 import psidev.psi.mi.jami.utils.CvTermUtils;
 import psidev.psi.mi.jami.utils.XrefUtils;
 
@@ -23,37 +27,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Unit tester for Mitab25BinaryWriter
+ * Unit tester for Mitab26BinaryWriter
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
- * @since <pre>25/06/13</pre>
+ * @since <pre>26/06/13</pre>
  */
 
-public class Mitab25BinaryWriterTest {
+public class Mitab26BinaryWriterTest {
+
     @Test
     public void test_mitab_version_and_header(){
-        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter();
-        Assert.assertEquals(MitabVersion.v2_5, binaryWriter.getVersion());
+        Mitab26BinaryWriter binaryWriter = new Mitab26BinaryWriter();
+        Assert.assertEquals(MitabVersion.v2_6, binaryWriter.getVersion());
         Assert.assertTrue(binaryWriter.isWriteHeader());
     }
 
     @Test(expected = IllegalStateException.class)
     public void test_not_initialised_writer() throws DataSourceWriterException {
-        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter();
+        Mitab26BinaryWriter binaryWriter = new Mitab26BinaryWriter();
         binaryWriter.write(new MitabModelledBinaryInteraction());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void test_not_initialised_no_options() throws DataSourceWriterException {
-        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter();
+        Mitab26BinaryWriter binaryWriter = new Mitab26BinaryWriter();
         binaryWriter.initialiseContext(null);
     }
 
     @Test
-    public void test_write_binary() throws DataSourceWriterException {
+    public void test_write_binary() throws DataSourceWriterException, IllegalParameterException, ParseException {
         StringWriter writer = new StringWriter();
-        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter(writer);
+        Mitab26BinaryWriter binaryWriter = new Mitab26BinaryWriter(writer);
         binaryWriter.setWriteHeader(false);
 
         ModelledBinaryInteraction binary = createModelledBinaryInteraction();
@@ -65,16 +70,16 @@ public class Mitab25BinaryWriterTest {
     }
 
     @Test
-    public void test_write_binary_list() throws DataSourceWriterException, ParseException {
+    public void test_write_binary_list() throws DataSourceWriterException, ParseException, IllegalParameterException {
         StringWriter writer = new StringWriter();
-        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter(writer);
+        Mitab26BinaryWriter binaryWriter = new Mitab26BinaryWriter(writer);
         binaryWriter.setWriteHeader(false);
 
         ModelledBinaryInteraction binary = createModelledBinaryInteraction();
 
         BinaryInteractionEvidence binary2 = createBinaryInteractionEvidence();
 
-        binaryWriter.write(Arrays.asList((BinaryInteraction)binary, (BinaryInteraction)binary2));
+        binaryWriter.write(Arrays.asList((BinaryInteraction) binary, (BinaryInteraction) binary2));
 
         String expected_line = getExpectedMitabLine1();
         String expected_line2 = getExpectedMitabLine2();
@@ -82,9 +87,9 @@ public class Mitab25BinaryWriterTest {
     }
 
     @Test
-    public void test_write_binary2() throws DataSourceWriterException, ParseException {
+    public void test_write_binary2() throws DataSourceWriterException, ParseException, IllegalParameterException {
         StringWriter writer = new StringWriter();
-        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter();
+        Mitab26BinaryWriter binaryWriter = new Mitab26BinaryWriter();
         Map<String, Object> options = new HashMap<String, Object>();
         options.put(MitabUtils.MITAB_HEADER_OPTION, false);
         options.put(InteractionWriterFactory.WRITER_OPTION_KEY, writer);
@@ -114,28 +119,70 @@ public class Mitab25BinaryWriterTest {
                 "\tpsi-mi:\"MI:xxxx\"(association)" +
                 "\tpsi-mi:\"MI:xxx1\"(intact)" +
                 "\tintact:EBI-xxxx" +
-                "\tauthor-score:high(text)";
+                "\tauthor-score:high(text)" +
+                "\tpsi-mi:\"MI:1060\"(spoke expansion)" +
+                "\tpsi-mi:\"MI:0501\"(enzyme)" +
+                "\tpsi-mi:\"MI:xxx5\"(enzyme target)" +
+                "\t-" +
+                "\t-" +
+                "\tpsi-mi:\"MI:0326\"(protein)" +
+                "\tpsi-mi:\"MI:0326\"(protein)" +
+                "\tgo:\"GO:xxxxx\"(component)" +
+                "\tinterpro:\"INTERPRO:xxxxx\"" +
+                "\tgo:\"GO:xxxx2\"(process)" +
+                "\tcomment:\"test comment (to be reviewed)\"" +
+                "\tcaution:sequence withdrawn from uniprot" +
+                "\tfigure-legend:Fig 1." +
+                "\t-" +
+                "\tic50:\"5x10^(-1)\"(molar)" +
+                "\t2006/06/06" +
+                "\t2007/01/01" +
+                "\trogid:xxxx1" +
+                "\trogid:xxxx2" +
+                "\trigid:xxxx3" +
+                "\t-";
     }
 
     private String getExpectedMitabLine2() {
         return "uniprotkb:P12345" +
-                    "\tuniprotkb:P12347" +
-                    "\tuniprotkb:P12346|intact:EBI-12345" +
-                    "\tuniprotkb:P12348|intact:EBI-12346" +
-                    "\tpsi-mi:protein1(display_short)|psi-mi:full name protein1(display_long)|mint:brca2(gene name)|intact:brca2 synonym(gene name synonym)|intact:\\\"bla\\\" author assigned name(author assigned name)" +
-                    "\tpsi-mi:protein2(display_short)|psi-mi:full name protein2(display_long)" +
-                    "\tpsi-mi:\"MI:xxx2\"(pull down)" +
-                    "\tauthor1 et al.(2006)" +
-                    "\tpubmed:12345|imex:IM-1" +
-                    "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
-                    "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
-                    "\tpsi-mi:\"MI:xxxx\"(association)" +
-                    "\tpsi-mi:\"MI:xxx1\"(intact)" +
-                    "\tintact:EBI-xxxx|imex:IM-1-1" +
-                    "\tauthor-score:high(text)";
+                "\tuniprotkb:P12347" +
+                "\tuniprotkb:P12346|intact:EBI-12345" +
+                "\tuniprotkb:P12348|intact:EBI-12346" +
+                "\tpsi-mi:protein1(display_short)|psi-mi:full name protein1(display_long)|mint:brca2(gene name)|intact:brca2 synonym(gene name synonym)|intact:\\\"bla\\\" author assigned name(author assigned name)" +
+                "\tpsi-mi:protein2(display_short)|psi-mi:full name protein2(display_long)" +
+                "\tpsi-mi:\"MI:xxx2\"(pull down)" +
+                "\tauthor1 et al.(2006)" +
+                "\tpubmed:12345|imex:IM-1" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\tpsi-mi:\"MI:xxxx\"(association)" +
+                "\tpsi-mi:\"MI:xxx1\"(intact)" +
+                "\tintact:EBI-xxxx|imex:IM-1-1" +
+                "\tauthor-score:high(text)"+
+                "\tpsi-mi:\"MI:1060\"(spoke expansion)" +
+                "\tpsi-mi:\"MI:0501\"(enzyme)" +
+                "\tpsi-mi:\"MI:xxx5\"(enzyme target)" +
+                "\tpsi-mi:\"MI:0496\"(bait)" +
+                "\tpsi-mi:\"MI:xxx6\"(prey)" +
+                "\tpsi-mi:\"MI:0326\"(protein)" +
+                "\tpsi-mi:\"MI:0326\"(protein)" +
+                "\tgo:\"GO:xxxxx\"(component)" +
+                "\tinterpro:\"INTERPRO:xxxxx\"" +
+                "\tgo:\"GO:xxxx2\"(process)" +
+                "\tcomment:\"test comment (to be reviewed)\"" +
+                "\tcaution:sequence withdrawn from uniprot" +
+                "\tfigure-legend:Fig 1.|imex curation" +
+                "\ttaxid:-1(in vitro)" +
+                "\tic50:\"5x10^(-1)\"(molar)" +
+                "\t2006/06/06" +
+                "\t2007/01/01" +
+                "\trogid:xxxx1" +
+                "\trogid:xxxx2" +
+                "\trigid:xxxx3" +
+                "\ttrue";
     }
 
-    private BinaryInteractionEvidence createBinaryInteractionEvidence() throws ParseException {
+    private BinaryInteractionEvidence createBinaryInteractionEvidence() throws ParseException, IllegalParameterException {
         ParticipantEvidence participantA = new MitabParticipantEvidence(new MitabProtein("protein1", "full name protein1"));
         // add identifiers
         participantA.getInteractor().getIdentifiers().add(XrefUtils.createUniprotIdentity("P12345"));
@@ -155,6 +202,21 @@ public class Mitab25BinaryWriterTest {
         participantB.getInteractor().getIdentifiers().add(XrefUtils.createXref("intact", "EBI-12346"));
         // species
         participantB.getInteractor().setOrganism(new MitabOrganism(9606, "human", "Homo Sapiens"));
+        // biological roles
+        participantA.setBiologicalRole(CvTermUtils.createMICvTerm(Participant.ENZYME_ROLE, Participant.ENZYME_ROLE_MI));
+        participantB.setBiologicalRole(CvTermUtils.createMICvTerm("enzyme target", "MI:xxx5"));
+        // experimental roles
+        participantA.setExperimentalRole(CvTermUtils.createMICvTerm(Participant.BAIT_ROLE, Participant.BAIT_ROLE_MI));
+        participantB.setExperimentalRole(CvTermUtils.createMICvTerm("prey", "MI:xxx6"));
+        // xrefs
+        participantA.getXrefs().add(new MitabXref("go", "GO:xxxxx", "component"));
+        participantB.getInteractor().getXrefs().add(new MitabXref("interpro", "INTERPRO:xxxxx"));
+        // annotations
+        participantA.getAnnotations().add(new MitabAnnotation("comment", "test comment (to be reviewed)"));
+        participantB.getInteractor().getAnnotations().add(new MitabAnnotation("caution", "sequence withdrawn from uniprot"));
+        // checksum
+        participantA.getInteractor().getChecksums().add(ChecksumUtils.createRogid("xxxx1"));
+        participantB.getInteractor().getChecksums().add(ChecksumUtils.createRogid("xxxx2"));
 
         BinaryInteractionEvidence binary = new MitabBinaryInteractionEvidence(participantA, participantB);
         participantA.setInteractionEvidence(binary);
@@ -181,10 +243,28 @@ public class Mitab25BinaryWriterTest {
         binary.getIdentifiers().add(XrefUtils.createXrefWithQualifier("imex", "IM-1-1", "imex-primary"));
         // confidences
         binary.getConfidences().add(new MitabConfidence("author-score", "high", "text"));
+        // expansion
+        binary.setComplexExpansion(CvTermUtils.createMICvTerm(ComplexExpansionMethod.SPOKE_EXPANSION, ComplexExpansionMethod.SPOKE_EXPANSION_MI));
+        // xrefs
+        binary.getXrefs().add(new MitabXref("go", "GO:xxxx2", "process"));
+        // annotations
+        binary.getAnnotations().add(new MitabAnnotation("figure-legend", "Fig 1."));
+        // parameters
+        binary.getParameters().add(new MitabParameter("ic50", "5x10^(-1)", "molar"));
+        // creation date
+        binary.setCreatedDate(MitabUtils.DATE_FORMAT.parse("2006/06/06"));
+        // update date
+        binary.setUpdatedDate(MitabUtils.DATE_FORMAT.parse("2007/01/01"));
+        // checksum
+        binary.getChecksums().add(ChecksumUtils.createRigid("xxxx3"));
+        // host organism
+        binary.getExperiment().setHostOrganism(new MitabOrganism(-1, "in vitro"));
+        // negative
+        binary.setNegative(true);
         return binary;
     }
 
-    private ModelledBinaryInteraction createModelledBinaryInteraction() {
+    private ModelledBinaryInteraction createModelledBinaryInteraction() throws IllegalParameterException, ParseException {
         ModelledParticipant participantA = new MitabModelledParticipant(new MitabProtein("protein1", "full name protein1"));
         // add identifiers
         participantA.getInteractor().getIdentifiers().add(XrefUtils.createUniprotIdentity("P12345"));
@@ -204,6 +284,18 @@ public class Mitab25BinaryWriterTest {
         participantB.getInteractor().getIdentifiers().add(XrefUtils.createXref("intact", "EBI-12346"));
         // species
         participantB.getInteractor().setOrganism(new MitabOrganism(9606, "human", "Homo Sapiens"));
+        // biological roles
+        participantA.setBiologicalRole(CvTermUtils.createMICvTerm(Participant.ENZYME_ROLE, Participant.ENZYME_ROLE_MI));
+        participantB.setBiologicalRole(CvTermUtils.createMICvTerm("enzyme target", "MI:xxx5"));
+        // xrefs
+        participantA.getXrefs().add(new MitabXref("go", "GO:xxxxx", "component"));
+        participantB.getInteractor().getXrefs().add(new MitabXref("interpro", "INTERPRO:xxxxx"));
+        // annotations
+        participantA.getAnnotations().add(new MitabAnnotation("comment", "test comment (to be reviewed)"));
+        participantB.getInteractor().getAnnotations().add(new MitabAnnotation("caution", "sequence withdrawn from uniprot"));
+        // checksum
+        participantA.getInteractor().getChecksums().add(ChecksumUtils.createRogid("xxxx1"));
+        participantB.getInteractor().getChecksums().add(ChecksumUtils.createRogid("xxxx2"));
 
         ModelledBinaryInteraction binary = new MitabModelledBinaryInteraction(participantA, participantB);
         participantA.setModelledInteraction(binary);
@@ -219,6 +311,21 @@ public class Mitab25BinaryWriterTest {
         binary.getIdentifiers().add(XrefUtils.createIdentityXref("intact", "EBI-xxxx"));
         // confidences
         binary.getModelledConfidences().add(new MitabConfidence("author-score", "high", "text"));
+        // expansion
+        binary.setComplexExpansion(CvTermUtils.createMICvTerm(ComplexExpansionMethod.SPOKE_EXPANSION, ComplexExpansionMethod.SPOKE_EXPANSION_MI));
+        // xrefs
+        binary.getXrefs().add(new MitabXref("go", "GO:xxxx2", "process"));
+        // annotations
+        binary.getAnnotations().add(new MitabAnnotation("figure-legend", "Fig 1."));
+        // parameters
+        binary.getModelledParameters().add(new MitabParameter("ic50", "5x10^(-1)", "molar"));
+        // creation date
+        binary.setCreatedDate(MitabUtils.DATE_FORMAT.parse("2006/06/06"));
+        // update date
+        binary.setUpdatedDate(MitabUtils.DATE_FORMAT.parse("2007/01/01"));
+        // checksum
+        binary.getChecksums().add(ChecksumUtils.createRigid("xxxx3"));
+
         return binary;
     }
 }
--- trunk/psi-jami/jami-mitab/src/test/java/psidev/psi/mi/jami/mitab/io/writer/extended/Mitab26ModelledBinaryWriterTest.java
+++ trunk/psi-jami/jami-mitab/src/test/java/psidev/psi/mi/jami/mitab/io/writer/extended/Mitab26ModelledBinaryWriterTest.java
@@ -3,54 +3,59 @@ package psidev.psi.mi.jami.mitab.io.writer.extended;
 import junit.framework.Assert;
 import org.junit.Test;
 import psidev.psi.mi.jami.binary.ModelledBinaryInteraction;
+import psidev.psi.mi.jami.binary.expansion.ComplexExpansionMethod;
 import psidev.psi.mi.jami.exception.DataSourceWriterException;
+import psidev.psi.mi.jami.exception.IllegalParameterException;
 import psidev.psi.mi.jami.factory.InteractionWriterFactory;
 import psidev.psi.mi.jami.model.ModelledParticipant;
+import psidev.psi.mi.jami.model.Participant;
 import psidev.psi.mi.jami.tab.MitabVersion;
 import psidev.psi.mi.jami.tab.extension.*;
-import psidev.psi.mi.jami.tab.io.writer.extended.Mitab25ModelledBinaryWriter;
+import psidev.psi.mi.jami.tab.io.writer.extended.Mitab26ModelledBinaryWriter;
 import psidev.psi.mi.jami.tab.utils.MitabUtils;
+import psidev.psi.mi.jami.utils.ChecksumUtils;
 import psidev.psi.mi.jami.utils.CvTermUtils;
 import psidev.psi.mi.jami.utils.XrefUtils;
 
 import java.io.StringWriter;
+import java.text.ParseException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Unit tester for Mitab25ModelledBinaryWriter
+ * Unit tester for Mitab26ModelledBinaryWriter
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
- * @since <pre>25/06/13</pre>
+ * @since <pre>26/06/13</pre>
  */
 
-public class Mitab25ModelledBinaryWriterTest {
+public class Mitab26ModelledBinaryWriterTest {
 
     @Test
     public void test_mitab_version_and_header(){
-        Mitab25ModelledBinaryWriter binaryWriter = new Mitab25ModelledBinaryWriter();
-        Assert.assertEquals(MitabVersion.v2_5, binaryWriter.getVersion());
+        Mitab26ModelledBinaryWriter binaryWriter = new Mitab26ModelledBinaryWriter();
+        Assert.assertEquals(MitabVersion.v2_6, binaryWriter.getVersion());
         Assert.assertTrue(binaryWriter.isWriteHeader());
     }
 
     @Test(expected = IllegalStateException.class)
     public void test_not_initialised_writer() throws DataSourceWriterException {
-        Mitab25ModelledBinaryWriter binaryWriter = new Mitab25ModelledBinaryWriter();
+        Mitab26ModelledBinaryWriter binaryWriter = new Mitab26ModelledBinaryWriter();
         binaryWriter.write(new MitabModelledBinaryInteraction());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void test_not_initialised_no_options() throws DataSourceWriterException {
-        Mitab25ModelledBinaryWriter binaryWriter = new Mitab25ModelledBinaryWriter();
+        Mitab26ModelledBinaryWriter binaryWriter = new Mitab26ModelledBinaryWriter();
         binaryWriter.initialiseContext(null);
     }
 
     @Test
-    public void test_write_binary() throws DataSourceWriterException {
+    public void test_write_binary() throws DataSourceWriterException, IllegalParameterException, ParseException {
         StringWriter writer = new StringWriter();
-        Mitab25ModelledBinaryWriter binaryWriter = new Mitab25ModelledBinaryWriter(writer);
+        Mitab26ModelledBinaryWriter binaryWriter = new Mitab26ModelledBinaryWriter(writer);
         binaryWriter.setWriteHeader(false);
 
         ModelledBinaryInteraction binary = createModelledBinaryInteraction();
@@ -62,9 +67,9 @@ public class Mitab25ModelledBinaryWriterTest {
     }
 
     @Test
-    public void test_write_binary_list() throws DataSourceWriterException {
+    public void test_write_binary_list() throws DataSourceWriterException, IllegalParameterException, ParseException {
         StringWriter writer = new StringWriter();
-        Mitab25ModelledBinaryWriter binaryWriter = new Mitab25ModelledBinaryWriter(writer);
+        Mitab26ModelledBinaryWriter binaryWriter = new Mitab26ModelledBinaryWriter(writer);
         binaryWriter.setWriteHeader(false);
 
         ModelledBinaryInteraction binary = createModelledBinaryInteraction();
@@ -76,9 +81,9 @@ public class Mitab25ModelledBinaryWriterTest {
     }
 
     @Test
-    public void test_write_binary2() throws DataSourceWriterException {
+    public void test_write_binary2() throws DataSourceWriterException, IllegalParameterException, ParseException {
         StringWriter writer = new StringWriter();
-        Mitab25ModelledBinaryWriter binaryWriter = new Mitab25ModelledBinaryWriter();
+        Mitab26ModelledBinaryWriter binaryWriter = new Mitab26ModelledBinaryWriter();
         Map<String, Object> options = new HashMap<String, Object>();
         options.put(MitabUtils.MITAB_HEADER_OPTION, false);
         options.put(InteractionWriterFactory.WRITER_OPTION_KEY, writer);
@@ -94,23 +99,44 @@ public class Mitab25ModelledBinaryWriterTest {
 
     private String getExpectedMitabLine() {
         return "uniprotkb:P12345" +
-                    "\tuniprotkb:P12347" +
-                    "\tuniprotkb:P12346|intact:EBI-12345" +
-                    "\tuniprotkb:P12348|intact:EBI-12346" +
-                    "\tpsi-mi:protein1(display_short)|psi-mi:full name protein1(display_long)|mint:brca2(gene name)|intact:brca2 synonym(gene name synonym)|intact:\\\"bla\\\" author assigned name(author assigned name)" +
-                    "\tpsi-mi:protein2(display_short)|psi-mi:full name protein2(display_long)" +
-                    "\t-" +
-                    "\t-" +
-                    "\t-" +
-                    "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
-                    "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
-                    "\tpsi-mi:\"MI:xxxx\"(association)" +
-                    "\tpsi-mi:\"MI:xxx1\"(intact)" +
-                    "\tintact:EBI-xxxx" +
-                    "\tauthor-score:high(text)";
+                "\tuniprotkb:P12347" +
+                "\tuniprotkb:P12346|intact:EBI-12345" +
+                "\tuniprotkb:P12348|intact:EBI-12346" +
+                "\tpsi-mi:protein1(display_short)|psi-mi:full name protein1(display_long)|mint:brca2(gene name)|intact:brca2 synonym(gene name synonym)|intact:\\\"bla\\\" author assigned name(author assigned name)" +
+                "\tpsi-mi:protein2(display_short)|psi-mi:full name protein2(display_long)" +
+                "\t-" +
+                "\t-" +
+                "\t-" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\tpsi-mi:\"MI:xxxx\"(association)" +
+                "\tpsi-mi:\"MI:xxx1\"(intact)" +
+                "\tintact:EBI-xxxx" +
+                "\tauthor-score:high(text)" +
+                "\tpsi-mi:\"MI:1060\"(spoke expansion)" +
+                "\tpsi-mi:\"MI:0501\"(enzyme)" +
+                "\tpsi-mi:\"MI:xxx5\"(enzyme target)" +
+                "\t-" +
+                "\t-" +
+                "\tpsi-mi:\"MI:0326\"(protein)" +
+                "\tpsi-mi:\"MI:0326\"(protein)" +
+                "\tgo:\"GO:xxxxx\"(component)" +
+                "\tinterpro:\"INTERPRO:xxxxx\"" +
+                "\tgo:\"GO:xxxx2\"(process)" +
+                "\tcomment:\"test comment (to be reviewed)\"" +
+                "\tcaution:sequence withdrawn from uniprot" +
+                "\tfigure-legend:Fig 1." +
+                "\t-" +
+                "\tic50:\"5x10^(-1)\"(molar)" +
+                "\t2006/06/06" +
+                "\t2007/01/01" +
+                "\trogid:xxxx1" +
+                "\trogid:xxxx2" +
+                "\trigid:xxxx3" +
+                "\t-";
     }
 
-    private ModelledBinaryInteraction createModelledBinaryInteraction() {
+    private ModelledBinaryInteraction createModelledBinaryInteraction() throws IllegalParameterException, ParseException {
         ModelledParticipant participantA = new MitabModelledParticipant(new MitabProtein("protein1", "full name protein1"));
         // add identifiers
         participantA.getInteractor().getIdentifiers().add(XrefUtils.createUniprotIdentity("P12345"));
@@ -130,6 +156,18 @@ public class Mitab25ModelledBinaryWriterTest {
         participantB.getInteractor().getIdentifiers().add(XrefUtils.createXref("intact", "EBI-12346"));
         // species
         participantB.getInteractor().setOrganism(new MitabOrganism(9606, "human", "Homo Sapiens"));
+        // biological roles
+        participantA.setBiologicalRole(CvTermUtils.createMICvTerm(Participant.ENZYME_ROLE, Participant.ENZYME_ROLE_MI));
+        participantB.setBiologicalRole(CvTermUtils.createMICvTerm("enzyme target", "MI:xxx5"));
+        // xrefs
+        participantA.getXrefs().add(new MitabXref("go", "GO:xxxxx", "component"));
+        participantB.getInteractor().getXrefs().add(new MitabXref("interpro", "INTERPRO:xxxxx"));
+        // annotations
+        participantA.getAnnotations().add(new MitabAnnotation("comment", "test comment (to be reviewed)"));
+        participantB.getInteractor().getAnnotations().add(new MitabAnnotation("caution", "sequence withdrawn from uniprot"));
+        // checksum
+        participantA.getInteractor().getChecksums().add(ChecksumUtils.createRogid("xxxx1"));
+        participantB.getInteractor().getChecksums().add(ChecksumUtils.createRogid("xxxx2"));
 
         ModelledBinaryInteraction binary = new MitabModelledBinaryInteraction(participantA, participantB);
         participantA.setModelledInteraction(binary);
@@ -145,6 +183,21 @@ public class Mitab25ModelledBinaryWriterTest {
         binary.getIdentifiers().add(XrefUtils.createIdentityXref("intact", "EBI-xxxx"));
         // confidences
         binary.getModelledConfidences().add(new MitabConfidence("author-score", "high", "text"));
+        // expansion
+        binary.setComplexExpansion(CvTermUtils.createMICvTerm(ComplexExpansionMethod.SPOKE_EXPANSION, ComplexExpansionMethod.SPOKE_EXPANSION_MI));
+        // xrefs
+        binary.getXrefs().add(new MitabXref("go", "GO:xxxx2", "process"));
+        // annotations
+        binary.getAnnotations().add(new MitabAnnotation("figure-legend", "Fig 1."));
+        // parameters
+        binary.getModelledParameters().add(new MitabParameter("ic50", "5x10^(-1)", "molar"));
+        // creation date
+        binary.setCreatedDate(MitabUtils.DATE_FORMAT.parse("2006/06/06"));
+        // update date
+        binary.setUpdatedDate(MitabUtils.DATE_FORMAT.parse("2007/01/01"));
+        // checksum
+        binary.getChecksums().add(ChecksumUtils.createRigid("xxxx3"));
+
         return binary;
     }
 }
