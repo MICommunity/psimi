70372e5a560c32ff6c7970a7aa6bd99fd2a29e69
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-06-26 07:35:20 +0000

    Added unit test for Mitab25BinaryWriter

---
+++ psi-jami/jami-mitab/src/test/java/psidev/psi/mi/jami/mitab/io/writer/extended/Mitab25BinaryWriterTest.java
@@ -0,0 +1,311 @@
+package psidev.psi.mi.jami.mitab.io.writer.extended;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import psidev.psi.mi.jami.binary.BinaryInteraction;
+import psidev.psi.mi.jami.binary.BinaryInteractionEvidence;
+import psidev.psi.mi.jami.binary.ModelledBinaryInteraction;
+import psidev.psi.mi.jami.exception.DataSourceWriterException;
+import psidev.psi.mi.jami.factory.InteractionWriterFactory;
+import psidev.psi.mi.jami.model.ModelledParticipant;
+import psidev.psi.mi.jami.model.ParticipantEvidence;
+import psidev.psi.mi.jami.tab.MitabVersion;
+import psidev.psi.mi.jami.tab.extension.*;
+import psidev.psi.mi.jami.tab.io.writer.extended.Mitab25BinaryWriter;
+import psidev.psi.mi.jami.tab.utils.MitabUtils;
+import psidev.psi.mi.jami.utils.CvTermUtils;
+import psidev.psi.mi.jami.utils.XrefUtils;
+
+import java.io.StringWriter;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Unit tester for Mitab25BinaryWriter
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>25/06/13</pre>
+ */
+
+public class Mitab25BinaryWriterTest {
+    @Test
+    public void test_mitab_version_and_header(){
+        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter();
+        Assert.assertEquals(MitabVersion.v2_5, binaryWriter.getVersion());
+        Assert.assertTrue(binaryWriter.isWriteHeader());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_not_initialised_writer() throws DataSourceWriterException {
+        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter();
+        binaryWriter.write(new MitabModelledBinaryInteraction());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_not_initialised_no_options() throws DataSourceWriterException {
+        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter();
+        binaryWriter.initialiseContext(null);
+    }
+
+    @Test
+    public void test_write_binary() throws DataSourceWriterException {
+        StringWriter writer = new StringWriter();
+        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter(writer);
+        binaryWriter.setWriteHeader(false);
+
+        ModelledParticipant participantA = new MitabModelledParticipant(new MitabProtein("protein1", "full name protein1"));
+        // add identifiers
+        participantA.getInteractor().getIdentifiers().add(XrefUtils.createUniprotIdentity("P12345"));
+        participantA.getInteractor().getIdentifiers().add(XrefUtils.createUniprotSecondary("P12346"));
+        participantA.getInteractor().getIdentifiers().add(XrefUtils.createXref("intact", "EBI-12345"));
+        // add aliases
+        participantA.getInteractor().getAliases().add(new MitabAlias("mint", "brca2", "gene name"));
+        participantA.getInteractor().getAliases().add(new MitabAlias("intact", "brca2 synonym", "gene name synonym"));
+        participantA.getAliases().add(new MitabAlias("intact", "\"bla\" author assigned name", "author assigned name"));
+        // species
+        participantA.getInteractor().setOrganism(new MitabOrganism(9606, "human", "Homo Sapiens"));
+        //participantA.getAliases()
+        ModelledParticipant participantB = new MitabModelledParticipant(new MitabProtein("protein2", "full name protein2"));
+        // add identifiers
+        participantB.getInteractor().getIdentifiers().add(XrefUtils.createUniprotIdentity("P12347"));
+        participantB.getInteractor().getIdentifiers().add(XrefUtils.createUniprotSecondary("P12348"));
+        participantB.getInteractor().getIdentifiers().add(XrefUtils.createXref("intact", "EBI-12346"));
+        // species
+        participantB.getInteractor().setOrganism(new MitabOrganism(9606, "human", "Homo Sapiens"));
+
+        ModelledBinaryInteraction binary = new MitabModelledBinaryInteraction(participantA, participantB);
+        participantA.setModelledInteraction(binary);
+        participantB.setModelledInteraction(binary);
+
+        // interaction type
+        binary.setInteractionType(CvTermUtils.createMICvTerm("association", "MI:xxxx"));
+        // source database
+        MitabSource source = new MitabSource("intact");
+        source.setMIIdentifier("MI:xxx1");
+        binary.setSource(source);
+        // interaction identifiers
+        binary.getIdentifiers().add(XrefUtils.createIdentityXref("intact", "EBI-xxxx"));
+        // confidences
+        binary.getModelledConfidences().add(new MitabConfidence("author-score", "high", "text"));
+
+        binaryWriter.write(binary);
+
+        String expected_line = "uniprotkb:P12345" +
+                "\tuniprotkb:P12347" +
+                "\tuniprotkb:P12346|intact:EBI-12345" +
+                "\tuniprotkb:P12348|intact:EBI-12346" +
+                "\tpsi-mi:protein1(display_short)|psi-mi:full name protein1(display_long)|mint:brca2(gene name)|intact:brca2 synonym(gene name synonym)|intact:\\\"bla\\\" author assigned name(author assigned name)" +
+                "\tpsi-mi:protein2(display_short)|psi-mi:full name protein2(display_long)" +
+                "\t-" +
+                "\t-" +
+                "\t-" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\tpsi-mi:\"MI:xxxx\"(association)" +
+                "\tpsi-mi:\"MI:xxx1\"(intact)" +
+                "\tintact:EBI-xxxx" +
+                "\tauthor-score:high(text)";
+        Assert.assertEquals(expected_line, writer.toString());
+    }
+
+    @Test
+    public void test_write_binary_list() throws DataSourceWriterException, ParseException {
+        StringWriter writer = new StringWriter();
+        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter(writer);
+        binaryWriter.setWriteHeader(false);
+
+        ModelledParticipant participantA = new MitabModelledParticipant(new MitabProtein("protein1", "full name protein1"));
+        // add identifiers
+        participantA.getInteractor().getIdentifiers().add(XrefUtils.createUniprotIdentity("P12345"));
+        participantA.getInteractor().getIdentifiers().add(XrefUtils.createUniprotSecondary("P12346"));
+        participantA.getInteractor().getIdentifiers().add(XrefUtils.createXref("intact", "EBI-12345"));
+        // add aliases
+        participantA.getInteractor().getAliases().add(new MitabAlias("mint", "brca2", "gene name"));
+        participantA.getInteractor().getAliases().add(new MitabAlias("intact", "brca2 synonym", "gene name synonym"));
+        participantA.getAliases().add(new MitabAlias("intact", "\"bla\" author assigned name", "author assigned name"));
+        // species
+        participantA.getInteractor().setOrganism(new MitabOrganism(9606, "human", "Homo Sapiens"));
+        //participantA.getAliases()
+        ModelledParticipant participantB = new MitabModelledParticipant(new MitabProtein("protein2", "full name protein2"));
+        // add identifiers
+        participantB.getInteractor().getIdentifiers().add(XrefUtils.createUniprotIdentity("P12347"));
+        participantB.getInteractor().getIdentifiers().add(XrefUtils.createUniprotSecondary("P12348"));
+        participantB.getInteractor().getIdentifiers().add(XrefUtils.createXref("intact", "EBI-12346"));
+        // species
+        participantB.getInteractor().setOrganism(new MitabOrganism(9606, "human", "Homo Sapiens"));
+
+        ModelledBinaryInteraction binary = new MitabModelledBinaryInteraction(participantA, participantB);
+        participantA.setModelledInteraction(binary);
+        participantB.setModelledInteraction(binary);
+
+        // interaction type
+        binary.setInteractionType(CvTermUtils.createMICvTerm("association", "MI:xxxx"));
+        // source database
+        MitabSource source = new MitabSource("intact");
+        source.setMIIdentifier("MI:xxx1");
+        binary.setSource(source);
+        // interaction identifiers
+        binary.getIdentifiers().add(XrefUtils.createIdentityXref("intact", "EBI-xxxx"));
+        // confidences
+        binary.getModelledConfidences().add(new MitabConfidence("author-score", "high", "text"));
+
+        ParticipantEvidence participantA2 = new MitabParticipantEvidence(new MitabProtein("protein1", "full name protein1"));
+        // add identifiers
+        participantA2.getInteractor().getIdentifiers().add(XrefUtils.createUniprotIdentity("P12345"));
+        participantA2.getInteractor().getIdentifiers().add(XrefUtils.createUniprotSecondary("P12346"));
+        participantA2.getInteractor().getIdentifiers().add(XrefUtils.createXref("intact", "EBI-12345"));
+        // add aliases
+        participantA2.getInteractor().getAliases().add(new MitabAlias("mint", "brca2", "gene name"));
+        participantA2.getInteractor().getAliases().add(new MitabAlias("intact", "brca2 synonym", "gene name synonym"));
+        participantA2.getAliases().add(new MitabAlias("intact", "\"bla\" author assigned name", "author assigned name"));
+        // species
+        participantA2.getInteractor().setOrganism(new MitabOrganism(9606, "human", "Homo Sapiens"));
+        //participantA.getAliases()
+        ParticipantEvidence participantB2 = new MitabParticipantEvidence(new MitabProtein("protein2", "full name protein2"));
+        // add identifiers
+        participantB2.getInteractor().getIdentifiers().add(XrefUtils.createUniprotIdentity("P12347"));
+        participantB2.getInteractor().getIdentifiers().add(XrefUtils.createUniprotSecondary("P12348"));
+        participantB2.getInteractor().getIdentifiers().add(XrefUtils.createXref("intact", "EBI-12346"));
+        // species
+        participantB2.getInteractor().setOrganism(new MitabOrganism(9606, "human", "Homo Sapiens"));
+
+        BinaryInteractionEvidence binary2 = new MitabBinaryInteractionEvidence(participantA2, participantB2);
+        participantA2.setInteractionEvidence(binary2);
+        participantB2.setInteractionEvidence(binary2);
+
+        // detection method
+        binary2.setExperiment(new MitabExperiment(new MitabPublication()));
+        binary2.getExperiment().setInteractionDetectionMethod(new MitabCvTerm("pull down", "MI:xxx2"));
+        // first author
+        binary2.getExperiment().getPublication().setPublicationDate(MitabUtils.PUBLICATION_YEAR_FORMAT.parse("2006"));
+        binary2.getExperiment().getPublication().getAuthors().add("author1");
+        binary2.getExperiment().getPublication().getAuthors().add("author2");
+        // publication identifiers
+        binary2.getExperiment().getPublication().setPubmedId("12345");
+        binary2.getExperiment().getPublication().assignImexId("IM-1");
+        // interaction type
+        binary2.setInteractionType(CvTermUtils.createMICvTerm("association", "MI:xxxx"));
+        // source database
+        MitabSource source2 = new MitabSource("intact");
+        source2.setMIIdentifier("MI:xxx1");
+        binary2.getExperiment().getPublication().setSource(source2);
+        // interaction identifiers
+        binary2.getIdentifiers().add(XrefUtils.createIdentityXref("intact", "EBI-xxxx"));
+        binary2.getIdentifiers().add(XrefUtils.createXrefWithQualifier("imex", "IM-1-1", "imex-primary"));
+        // confidences
+        binary2.getConfidences().add(new MitabConfidence("author-score", "high", "text"));
+
+        binaryWriter.write(Arrays.asList((BinaryInteraction)binary, (BinaryInteraction)binary2));
+
+        String expected_line = "uniprotkb:P12345" +
+                "\tuniprotkb:P12347" +
+                "\tuniprotkb:P12346|intact:EBI-12345" +
+                "\tuniprotkb:P12348|intact:EBI-12346" +
+                "\tpsi-mi:protein1(display_short)|psi-mi:full name protein1(display_long)|mint:brca2(gene name)|intact:brca2 synonym(gene name synonym)|intact:\\\"bla\\\" author assigned name(author assigned name)" +
+                "\tpsi-mi:protein2(display_short)|psi-mi:full name protein2(display_long)" +
+                "\t-" +
+                "\t-" +
+                "\t-" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\tpsi-mi:\"MI:xxxx\"(association)" +
+                "\tpsi-mi:\"MI:xxx1\"(intact)" +
+                "\tintact:EBI-xxxx" +
+                "\tauthor-score:high(text)";
+        String expected_line2 = "uniprotkb:P12345" +
+                "\tuniprotkb:P12347" +
+                "\tuniprotkb:P12346|intact:EBI-12345" +
+                "\tuniprotkb:P12348|intact:EBI-12346" +
+                "\tpsi-mi:protein1(display_short)|psi-mi:full name protein1(display_long)|mint:brca2(gene name)|intact:brca2 synonym(gene name synonym)|intact:\\\"bla\\\" author assigned name(author assigned name)" +
+                "\tpsi-mi:protein2(display_short)|psi-mi:full name protein2(display_long)" +
+                "\tpsi-mi:\"MI:xxx2\"(pull down)" +
+                "\tauthor1 et al.(2006)" +
+                "\tpubmed:12345|imex:IM-1" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\tpsi-mi:\"MI:xxxx\"(association)" +
+                "\tpsi-mi:\"MI:xxx1\"(intact)" +
+                "\tintact:EBI-xxxx|imex:IM-1-1" +
+                "\tauthor-score:high(text)";
+        Assert.assertEquals(expected_line+ MitabUtils.LINE_BREAK+expected_line2, writer.toString());
+    }
+
+    @Test
+    public void test_write_binary2() throws DataSourceWriterException, ParseException {
+        StringWriter writer = new StringWriter();
+        Mitab25BinaryWriter binaryWriter = new Mitab25BinaryWriter();
+        Map<String, Object> options = new HashMap<String, Object>();
+        options.put(MitabUtils.MITAB_HEADER_OPTION, false);
+        options.put(InteractionWriterFactory.WRITER_OPTION_KEY, writer);
+        binaryWriter.initialiseContext(options);
+
+        ParticipantEvidence participantA = new MitabParticipantEvidence(new MitabProtein("protein1", "full name protein1"));
+        // add identifiers
+        participantA.getInteractor().getIdentifiers().add(XrefUtils.createUniprotIdentity("P12345"));
+        participantA.getInteractor().getIdentifiers().add(XrefUtils.createUniprotSecondary("P12346"));
+        participantA.getInteractor().getIdentifiers().add(XrefUtils.createXref("intact", "EBI-12345"));
+        // add aliases
+        participantA.getInteractor().getAliases().add(new MitabAlias("mint", "brca2", "gene name"));
+        participantA.getInteractor().getAliases().add(new MitabAlias("intact", "brca2 synonym", "gene name synonym"));
+        participantA.getAliases().add(new MitabAlias("intact", "\"bla\" author assigned name", "author assigned name"));
+        // species
+        participantA.getInteractor().setOrganism(new MitabOrganism(9606, "human", "Homo Sapiens"));
+        //participantA.getAliases()
+        ParticipantEvidence participantB = new MitabParticipantEvidence(new MitabProtein("protein2", "full name protein2"));
+        // add identifiers
+        participantB.getInteractor().getIdentifiers().add(XrefUtils.createUniprotIdentity("P12347"));
+        participantB.getInteractor().getIdentifiers().add(XrefUtils.createUniprotSecondary("P12348"));
+        participantB.getInteractor().getIdentifiers().add(XrefUtils.createXref("intact", "EBI-12346"));
+        // species
+        participantB.getInteractor().setOrganism(new MitabOrganism(9606, "human", "Homo Sapiens"));
+
+        BinaryInteractionEvidence binary = new MitabBinaryInteractionEvidence(participantA, participantB);
+        participantA.setInteractionEvidence(binary);
+        participantB.setInteractionEvidence(binary);
+
+        // detection method
+        binary.setExperiment(new MitabExperiment(new MitabPublication()));
+        binary.getExperiment().setInteractionDetectionMethod(new MitabCvTerm("pull down", "MI:xxx2"));
+        // first author
+        binary.getExperiment().getPublication().setPublicationDate(MitabUtils.PUBLICATION_YEAR_FORMAT.parse("2006"));
+        binary.getExperiment().getPublication().getAuthors().add("author1");
+        binary.getExperiment().getPublication().getAuthors().add("author2");
+        // publication identifiers
+        binary.getExperiment().getPublication().setPubmedId("12345");
+        binary.getExperiment().getPublication().assignImexId("IM-1");
+        // interaction type
+        binary.setInteractionType(CvTermUtils.createMICvTerm("association", "MI:xxxx"));
+        // source database
+        MitabSource source = new MitabSource("intact");
+        source.setMIIdentifier("MI:xxx1");
+        binary.getExperiment().getPublication().setSource(source);
+        // interaction identifiers
+        binary.getIdentifiers().add(XrefUtils.createIdentityXref("intact", "EBI-xxxx"));
+        binary.getIdentifiers().add(XrefUtils.createXrefWithQualifier("imex", "IM-1-1", "imex-primary"));
+        // confidences
+        binary.getConfidences().add(new MitabConfidence("author-score", "high", "text"));
+
+        String expected_line = "uniprotkb:P12345" +
+                "\tuniprotkb:P12347" +
+                "\tuniprotkb:P12346|intact:EBI-12345" +
+                "\tuniprotkb:P12348|intact:EBI-12346" +
+                "\tpsi-mi:protein1(display_short)|psi-mi:full name protein1(display_long)|mint:brca2(gene name)|intact:brca2 synonym(gene name synonym)|intact:\\\"bla\\\" author assigned name(author assigned name)" +
+                "\tpsi-mi:protein2(display_short)|psi-mi:full name protein2(display_long)" +
+                "\tpsi-mi:\"MI:xxx2\"(pull down)" +
+                "\tauthor1 et al.(2006)" +
+                "\tpubmed:12345|imex:IM-1" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\ttaxid:9606(human)|taxid:9606(Homo Sapiens)" +
+                "\tpsi-mi:\"MI:xxxx\"(association)" +
+                "\tpsi-mi:\"MI:xxx1\"(intact)" +
+                "\tintact:EBI-xxxx|imex:IM-1-1" +
+                "\tauthor-score:high(text)";
+
+        binaryWriter.write(binary);
+
+        Assert.assertEquals(expected_line, writer.toString());
+    }
+}
