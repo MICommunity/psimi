3fa08449647a4dcf66f761601c75ed4304e09d8c
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-06-28 15:00:31 +0000

    Added interaction enricher

---
+++ psi-jami/jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/impl/interaction/listener/InteractionEnricherListener.java
@@ -0,0 +1,10 @@
+package psidev.psi.mi.jami.enricher.impl.interaction.listener;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author Gabriel Aldam (galdam@ebi.ac.uk)
+ * @since 28/06/13
+ */
+public interface InteractionEnricherListener {
+}
