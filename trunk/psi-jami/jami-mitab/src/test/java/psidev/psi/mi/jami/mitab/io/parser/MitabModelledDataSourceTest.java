601d60c24eabe74829e74cddaad512741735ae7c
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-07-02 10:51:02 +0000

    Added unit test for MitabEvidenceDataSource

---
+++ trunk/psi-jami/jami-mitab/src/test/java/psidev/psi/mi/jami/mitab/io/parser/MitabModelledDataSourceTest.java
@@ -0,0 +1,12 @@
+package psidev.psi.mi.jami.mitab.io.parser;
+
+/**
+ * Unit tester for MitabModelledDataSource
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>02/07/13</pre>
+ */
+
+public class MitabModelledDataSourceTest {
+}
