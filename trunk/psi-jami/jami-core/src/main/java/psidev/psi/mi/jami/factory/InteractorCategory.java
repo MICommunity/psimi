f702f455ac59c9e24cffc80c2c343d7d0d5f8574
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-07-02 18:10:13 +0000

    Updated InteractorFactory

---
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/factory/InteractorCategory.java
@@ -0,0 +1,13 @@
+package psidev.psi.mi.jami.factory;
+
+/**
+ * Enum for interactor category
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>02/07/13</pre>
+ */
+
+public enum InteractorCategory {
+    protein, nucleic_acid, gene, bioactive_entity, polymer, complex, interactor_set, other
+}
