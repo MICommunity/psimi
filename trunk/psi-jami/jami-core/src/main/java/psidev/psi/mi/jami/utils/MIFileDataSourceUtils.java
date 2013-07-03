056803846606d3c1c74d587c0d43f7c94ca13173
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-07-03 09:48:15 +0000

    Added io-commons from apache as a new dependency and update MIFileDataSourceUtils

--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/MIFileDataSourceUtils.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/MIFileDataSourceUtils.java
@@ -1,5 +1,7 @@
 package psidev.psi.mi.jami.utils;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.*;
 
 /**
@@ -14,6 +16,7 @@ public class MIFileDataSourceUtils {
 
     /**
      * Store the content of the given input stream into a temporary file and return its descriptor.
+     * It will not close the given input stream.
      *
      * @param is the input stream to store.
      * @return a File descriptor describing a temporary file storing the content of the given input stream.
@@ -34,28 +37,55 @@ public class MIFileDataSourceUtils {
         }
 
         File tempFile = File.createTempFile( name, extension , tempDirectory );
-
         BufferedReader in = new BufferedReader( new InputStreamReader( is ) );
+        Writer out = null;
         try{
-
-            BufferedWriter out = new BufferedWriter( new FileWriter( tempFile ) );
-
-            try{
-                String line;
-                while ( ( line = in.readLine() ) != null ) {
-                    out.write( line );
-                    out.write( System.getProperty("line.separator"));
-                }
-
-
-                out.flush();
-            }
-            finally {
-                out.close();
-            }
+            out = new BufferedWriter( new FileWriter( tempFile ) );
+            IOUtils.copyLarge(in, out);
         }
         finally {
             in.close();
+            if (out != null){
+                out.close();
+            }
+        }
+
+        return tempFile;
+    }
+
+    /**
+     * Store the content of the given reader into a temporary file and return its descriptor.
+     * It will not close the given reader.
+     *
+     * @param reader the reader to use.
+     * @return a File descriptor describing a temporary file storing the content of the given input stream.
+     * @throws java.io.IOException if an IO error occur.
+     */
+    public static File storeAsTemporaryFile( Reader reader, String name, String extension ) throws IOException {
+
+        if ( reader == null ) {
+            throw new IllegalArgumentException( "You must give a non null reader" );
+        }
+
+        // Create a temp file and write URL content in it.
+        File tempDirectory = new File( System.getProperty( "java.io.tmpdir", "tmp" ) );
+        if ( !tempDirectory.exists() ) {
+            if ( !tempDirectory.mkdirs() ) {
+                throw new IOException( "Cannot create temp directory: " + tempDirectory.getAbsolutePath() );
+            }
+        }
+
+        File tempFile = File.createTempFile( name, extension , tempDirectory );
+
+        Writer out = null;
+        try{
+            out = new BufferedWriter( new FileWriter( tempFile ) );
+            IOUtils.copyLarge(reader, out);
+        }
+        finally {
+            if (out != null){
+                out.close();
+            }
         }
 
         return tempFile;
