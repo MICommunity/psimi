a6059a0acb57b8d727e1360376f96375f2195045
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-07-05 10:34:12 +0000

    Added more fields to jsonBinaryWriter.
    Extracted static properties in JSONUtils

---
+++ psi-jami/jami-json/src/main/java/psidev/psi/mi/jami/json/JsonUtils.java
@@ -0,0 +1,21 @@
+package psidev.psi.mi.jami.json;
+
+/**
+ * Utility class for public properties
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>05/07/13</pre>
+ */
+
+public class JsonUtils {
+    public final static String OPEN = "{";
+    public final static String CLOSE = "}";
+    public final static String OPEN_ARRAY = "[";
+    public final static String CLOSE_ARRAY = "]";
+    public final static String PROPERTY_DELIMITER = "\"";
+    public final static String PROPERTY_VALUE_SEPARATOR = ":";
+    public final static String ELEMENT_SEPARATOR = ",";
+    public final static String LINE_SEPARATOR = System.getProperty("line.separator");
+    public final static String INDENT = "\t";
+}
