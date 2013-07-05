9eca420140620a4e05228aa12af8f474a083d3e2
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-07-05 16:01:03 +0000

    Created a general JSON writer

---
+++ psi-jami/jami-json/src/main/java/psidev/psi/mi/jami/json/JsonWriter.java
@@ -0,0 +1,152 @@
+package psidev.psi.mi.jami.json;
+
+import psidev.psi.mi.jami.binary.BinaryInteractionEvidence;
+import psidev.psi.mi.jami.binary.expansion.ComplexExpansionMethod;
+import psidev.psi.mi.jami.binary.expansion.InteractionEvidenceSpokeExpansion;
+import psidev.psi.mi.jami.bridges.fetcher.OntologyTermFetcher;
+import psidev.psi.mi.jami.datasource.InteractionWriter;
+import psidev.psi.mi.jami.exception.DataSourceWriterException;
+import psidev.psi.mi.jami.factory.InteractionWriterFactory;
+import psidev.psi.mi.jami.model.InteractionEvidence;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * The jsonWriter which writes the interaction evidences
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>05/07/13</pre>
+ */
+
+public class JsonWriter implements InteractionWriter<InteractionEvidence> {
+
+    private JsonBinaryWriter binaryWriter;
+    private ComplexExpansionMethod<InteractionEvidence, BinaryInteractionEvidence> expansionMethod;
+    private int currentExpansionId = 0;
+
+    public JsonWriter(){
+    }
+
+    public JsonWriter(File file, OntologyTermFetcher fetcher) throws IOException {
+        this.binaryWriter = new JsonBinaryWriter(file, fetcher);
+        this.expansionMethod = new InteractionEvidenceSpokeExpansion();
+    }
+
+    public JsonWriter(OutputStream output, OntologyTermFetcher fetcher) {
+        this.binaryWriter = new JsonBinaryWriter(output, fetcher);
+        this.expansionMethod = new InteractionEvidenceSpokeExpansion();
+    }
+
+    public JsonWriter(Writer writer, OntologyTermFetcher fetcher) {
+        this.binaryWriter = new JsonBinaryWriter(writer, fetcher);
+        this.expansionMethod = new InteractionEvidenceSpokeExpansion();
+    }
+
+    public JsonWriter(File file, OntologyTermFetcher fetcher, ComplexExpansionMethod<InteractionEvidence, BinaryInteractionEvidence> expansionMethod) throws IOException {
+        this.binaryWriter = new JsonBinaryWriter(file, fetcher);
+        if(expansionMethod != null){
+            this.expansionMethod = expansionMethod;
+        }
+        else{
+            this.expansionMethod = new InteractionEvidenceSpokeExpansion();
+        }
+    }
+
+    public JsonWriter(OutputStream output, OntologyTermFetcher fetcher, ComplexExpansionMethod<InteractionEvidence, BinaryInteractionEvidence> expansionMethod) {
+        this.binaryWriter = new JsonBinaryWriter(output, fetcher);
+        if(expansionMethod != null){
+            this.expansionMethod = expansionMethod;
+        }
+        else{
+            this.expansionMethod = new InteractionEvidenceSpokeExpansion();
+        }
+    }
+
+    public JsonWriter(Writer writer, OntologyTermFetcher fetcher, ComplexExpansionMethod<InteractionEvidence, BinaryInteractionEvidence> expansionMethod) {
+        this.binaryWriter = new JsonBinaryWriter(writer, fetcher);
+        if(expansionMethod != null){
+            this.expansionMethod = expansionMethod;
+        }
+        else{
+            this.expansionMethod = new InteractionEvidenceSpokeExpansion();
+        }
+    }
+
+    public void initialiseContext(Map<String, Object> options) {
+        if (options == null && binaryWriter != null){
+            throw new IllegalArgumentException("The options for the json writer should contain at least "+ InteractionWriterFactory.OUTPUT_OPTION_KEY + " to know where to write the interactions and "+JsonUtils.ONTOLOGY_FETCHER_OPTION_KEY+" to know which OntologyTermFetcher to use.");
+        }
+        else if (options == null){
+            return;
+        }
+        else if (this.binaryWriter == null){
+            this.binaryWriter = new JsonBinaryWriter();
+            this.binaryWriter.initialiseContext(options);
+        }
+        else {
+            this.binaryWriter.initialiseContext(options);
+        }
+
+        if (options.containsKey(InteractionWriterFactory.COMPLEX_EXPANSION_OPTION_KEY)){
+            Object o = options.get(InteractionWriterFactory.COMPLEX_EXPANSION_OPTION_KEY);
+            if (o == null){
+               this.expansionMethod = new InteractionEvidenceSpokeExpansion();
+            }
+            else {
+                this.expansionMethod = (ComplexExpansionMethod<InteractionEvidence, BinaryInteractionEvidence>)options.get(InteractionWriterFactory.COMPLEX_EXPANSION_OPTION_KEY);
+            }
+        }
+    }
+
+    public void write(InteractionEvidence interaction) throws DataSourceWriterException {
+        if (this.binaryWriter == null){
+            throw new IllegalArgumentException("The json writer has not been initialised. The options for the json writer should contain at least "+ InteractionWriterFactory.OUTPUT_OPTION_KEY + " to know where to write the interactions and "+JsonUtils.ONTOLOGY_FETCHER_OPTION_KEY+" to know which OntologyTermFetcher to use.");
+        }
+
+        // reset expansion id
+        this.binaryWriter.setExpansionId(null);
+        if (interaction != null){
+            Collection<BinaryInteractionEvidence> binaryInteractions = expansionMethod.expand(interaction);
+
+            // we expanded a n-ary interaction
+            if(binaryInteractions.size() > 1){
+                this.binaryWriter.setExpansionId(currentExpansionId);
+                currentExpansionId++;
+            }
+            this.binaryWriter.write(binaryInteractions);
+        }
+    }
+
+    public void write(Collection<InteractionEvidence> interactions) throws DataSourceWriterException {
+        Iterator<InteractionEvidence> iterator = interactions.iterator();
+        write(iterator);
+    }
+
+    public void write(Iterator<InteractionEvidence> interactions) throws DataSourceWriterException {
+        while(interactions.hasNext()){
+            write(interactions.next());
+        }
+    }
+
+    public void flush() throws DataSourceWriterException {
+        if (binaryWriter != null){
+            binaryWriter.flush();
+        }
+    }
+
+    public void close() throws DataSourceWriterException {
+        if (binaryWriter != null){
+            binaryWriter.close();
+        }
+        binaryWriter = null;
+        currentExpansionId = 0;
+        expansionMethod = null;
+    }
+}
