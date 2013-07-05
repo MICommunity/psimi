1bd35e984f00567335ceaa4a024a41c49e2d0f50
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-07-05 15:35:31 +0000

    Updated json writer to recognize features

--- psi-jami/jami-json/src/main/java/psidev/psi/mi/jami/json/JsonUtils.java
+++ psi-jami/jami-json/src/main/java/psidev/psi/mi/jami/json/JsonUtils.java
@@ -19,4 +19,5 @@ public class JsonUtils {
     public final static String LINE_SEPARATOR = System.getProperty("line.separator");
     public final static String INDENT = "\t";
     public static final String ONTOLOGY_FETCHER_OPTION_KEY = "ontology_fetcher_key";
+
 }
