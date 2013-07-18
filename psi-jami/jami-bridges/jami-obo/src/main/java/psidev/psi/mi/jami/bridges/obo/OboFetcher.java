7b7ee2e428ad6dd47ee52e7d1a3976d11a3f2926
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-07-18 09:18:02 +0000

    Updated OboFetcher

--- psi-jami/jami-bridges/jami-obo/src/main/java/psidev/psi/mi/jami/bridges/obo/OboFetcher.java
+++ psi-jami/jami-bridges/jami-obo/src/main/java/psidev/psi/mi/jami/bridges/obo/OboFetcher.java
@@ -1,13 +1,10 @@
 package psidev.psi.mi.jami.bridges.obo;
 
-import psidev.psi.mi.jami.bridges.exception.BridgeFailedException;
 import psidev.psi.mi.jami.bridges.fetcher.CvTermFetcher;
 import psidev.psi.mi.jami.model.CvTerm;
 
-import java.util.Collection;
-
 /**
- * Simple ftecher based on OBO files
+ * Simple fetcher based on OBO files
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
@@ -16,51 +13,11 @@ import java.util.Collection;
 
 public class OboFetcher extends OboFetcherTemplate<CvTerm> implements CvTermFetcher<CvTerm>{
 
-    public OboFetcher(CvTerm database, AbstractOboLoader<CvTerm> oboLoader, String filePath) {
-        super(database, oboLoader, filePath);
+    public OboFetcher(CvTerm database, String filePath) {
+        super(database, new BasicOboLoader(database), filePath);
     }
 
-    public OboFetcher(String databaseName, AbstractOboLoader<CvTerm> oboLoader, String filePath) {
-        super(databaseName, oboLoader, filePath);
-    }
-
-    public CvTerm getCvTermByIdentifier(String termIdentifier, String ontologyDatabaseName) throws BridgeFailedException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    public CvTerm getCvTermByIdentifier(String termIdentifier, CvTerm ontologyDatabase) throws BridgeFailedException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    public CvTerm getCvTermByExactName(String searchName, String ontologyDatabaseName) throws BridgeFailedException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    public CvTerm getCvTermByExactName(String searchName) throws BridgeFailedException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    public Collection<CvTerm> getCvTermByInexactName(String searchName, String databaseName) throws BridgeFailedException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    public Collection<CvTerm> getCvTermByInexactName(String searchName, CvTerm database) throws BridgeFailedException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    public Collection<CvTerm> getCvTermsByIdentifiers(Collection<String> termIdentifiers, String ontologyDatabaseName) throws BridgeFailedException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    public Collection<CvTerm> getCvTermsByIdentifiers(Collection<String> termIdentifiers, CvTerm ontologyDatabase) throws BridgeFailedException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    public Collection<CvTerm> getCvTermsByExactNames(Collection<String> searchNames, String ontologyDatabaseName) throws BridgeFailedException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    public Collection<CvTerm> getCvTermsByExactNames(Collection<String> searchNames) throws BridgeFailedException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public OboFetcher(String databaseName, String filePath) {
+        super(databaseName, new BasicOboLoader(databaseName), filePath);
     }
 }
