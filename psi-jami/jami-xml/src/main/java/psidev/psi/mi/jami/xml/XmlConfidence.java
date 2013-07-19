3964c030c25e4a8ac208e28921731a44d7e83cf3
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-07-19 14:51:16 +0000

    Added other specialised Cvterms

---
+++ psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/XmlConfidence.java
@@ -0,0 +1,12 @@
+package psidev.psi.mi.jami.xml;
+
+/**
+ * Xml implementation of confidence
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>19/07/13</pre>
+ */
+
+public class XmlConfidence {
+}
