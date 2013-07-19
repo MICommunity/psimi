f6f6578f280f71a26cb0daecd2b6e672a2e820e9
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-07-19 14:11:24 +0000

    Added XmlOpenCvTerm

--- psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/XmlOpenCvTerm.java
+++ psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/XmlOpenCvTerm.java
@@ -1,5 +1,11 @@
 package psidev.psi.mi.jami.xml;
 
+import psidev.psi.mi.jami.xml.utils.PsiXmlUtils;
+
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Xml implementation of CvTerm.
  *
@@ -9,6 +15,152 @@ package psidev.psi.mi.jami.xml;
  * @version $Id$
  * @since <pre>18/07/13</pre>
  */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "openCvType", propOrder = {
+        "names",
+        "xref",
+        "annotations"
+})
+public class XmlOpenCvTerm extends AbstractXmlCvTerm{
 
-public class XmlOpenCvTerm {
+    private NamesContainer names;
+    private CvTermXrefAndIdentifierContainer xref;
+    private Collection<XmlAnnotation> annotations;
+
+    /**
+     * Gets the value of the names property.
+     *
+     * @return
+     *     possible object is
+     *     {@link NamesContainer }
+     *
+     */
+    @XmlElement(required = true)
+    public NamesContainer getNames() {
+        if (names == null){
+            names = new NamesContainer();
+            names.setShortLabel(PsiXmlUtils.UNSPECIFIED);
+        }
+        return names;
+    }
+
+    /**
+     * Sets the value of the names property.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link NamesContainer }
+     *
+     */
+    public void setNames(NamesContainer value) {
+        if (value == null){
+            names = new NamesContainer();
+            names.setShortLabel(PsiXmlUtils.UNSPECIFIED);
+        }
+        else {
+            this.names = value;
+            if (this.names.getShortLabel() == null){
+                names.setShortLabel(PsiXmlUtils.UNSPECIFIED);
+            }
+        }
+    }
+
+    /**
+     * Gets the value of the xref property.
+     *
+     * @return
+     *     possible object is
+     *     {@link XrefContainer }
+     *
+     */
+    @XmlElement(required = true)
+    public CvTermXrefAndIdentifierContainer getXref() {
+        if (xref != null){
+            if (xref.isEmpty()){
+                return null;
+            }
+        }
+        return xref;
+    }
+
+    /**
+     * Sets the value of the xref property.
+     *
+     * @param value
+     *     allowed object is
+     *     {@link XrefContainer }
+     *
+     */
+    public void setXref(CvTermXrefAndIdentifierContainer value) {
+        this.xref = value;
+    }
+
+    @XmlTransient
+    public String getShortName() {
+        return getNames().getShortLabel();
+    }
+
+    public void setShortName(String name) {
+        getNames().setShortLabel(name != null ? name : PsiXmlUtils.UNSPECIFIED);
+    }
+
+    @XmlTransient
+    public String getFullName() {
+        return getNames().getFullName();
+    }
+
+    public void setFullName(String name) {
+        getNames().setFullName(name);
+    }
+
+    @XmlTransient
+    public Collection<XmlXref> getIdentifiers() {
+        return getXrefContainer().getAllIdentifiers();
+    }
+
+    @XmlTransient
+    public String getMIIdentifier() {
+        return getXrefContainer().getMIIdentifier();
+    }
+
+    @XmlTransient
+    public String getMODIdentifier() {
+        return getXrefContainer().getMODIdentifier();
+    }
+
+    @XmlTransient
+    public String getPARIdentifier() {
+        return getXrefContainer().getPARIdentifier();
+    }
+
+    public void setMIIdentifier(String mi) {
+        getXrefContainer().setMIIdentifier(mi);
+    }
+
+    public void setMODIdentifier(String mod) {
+        getXrefContainer().setMODIdentifier(mod);
+    }
+
+    public void setPARIdentifier(String par) {
+        getXrefContainer().setPARIdentifier(par);
+    }
+
+    @XmlTransient
+    public Collection<XmlXref> getXrefs() {
+        return getXrefContainer().getAllXrefs();
+    }
+
+    @XmlElementWrapper(name="attributeList")
+    @XmlElement(name="attribute")
+    public Collection<XmlAnnotation> getAnnotations() {
+        if (annotations == null){
+            this.annotations = new ArrayList<XmlAnnotation>();
+        }
+        return this.annotations;
+    }
+
+    @XmlTransient
+    public Collection<XmlAlias> getSynonyms() {
+        return getNames().getAliases();
+    }
 }
