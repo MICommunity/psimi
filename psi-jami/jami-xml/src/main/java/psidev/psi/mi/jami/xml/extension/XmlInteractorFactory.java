e107dce36db0fc8f026c91df1d78086e16236a1e
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-07-24 14:43:17 +0000

    Added XmlInteractorFactory

---
+++ psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/extension/XmlInteractorFactory.java
@@ -0,0 +1,59 @@
+package psidev.psi.mi.jami.xml.extension;
+
+import psidev.psi.mi.jami.factory.InteractorFactory;
+import psidev.psi.mi.jami.model.CvTerm;
+
+/**
+ * Interactor factory for XML interactors
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>24/07/13</pre>
+ */
+
+public class XmlInteractorFactory extends InteractorFactory{
+
+    public XmlInteractorFactory() {
+        super();
+    }
+
+    @Override
+    public XmlProtein createProtein(String name, CvTerm type) {
+        return new XmlProtein(name, type);
+    }
+
+    @Override
+    public XmlNucleciAcid createNucleicAcid(String name, CvTerm type) {
+        return new XmlNucleciAcid(name, type);
+    }
+
+    @Override
+    public XmlGene createGene(String name) {
+        return new XmlGene(name);
+    }
+
+    /*@Override
+    public C createComplex(String name, CvTerm type) {
+        return new MitabComplex(name, type);
+    }*/
+
+    @Override
+    public XmlBioactiveEntity createBioactiveEntity(String name, CvTerm type) {
+        return new XmlBioactiveEntity(name, type);
+    }
+
+    @Override
+    public XmlPolymer createPolymer(String name, CvTerm type) {
+        return new XmlPolymer(name, type);
+    }
+
+    @Override
+    public XmlInteractor createInteractor(String name, CvTerm type) {
+        return new XmlInteractor(name, type);
+    }
+
+    @Override
+    public XmlInteractorSet createInteractorSet(String name, CvTerm type) {
+        return new XmlInteractorSet(name, type);
+    }
+}
