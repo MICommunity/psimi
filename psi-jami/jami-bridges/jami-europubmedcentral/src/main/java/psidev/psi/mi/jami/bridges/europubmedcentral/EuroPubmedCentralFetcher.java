40c10c607eec2e99a87119073030d66a3043a1e2
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-07-31 15:19:54 +0000

    Added Xrefs to the Pubmed fetcher.

--- psi-jami/jami-bridges/jami-europubmedcentral/src/main/java/psidev/psi/mi/jami/bridges/europubmedcentral/EuroPubmedCentralFetcher.java
+++ psi-jami/jami-bridges/jami-europubmedcentral/src/main/java/psidev/psi/mi/jami/bridges/europubmedcentral/EuroPubmedCentralFetcher.java
@@ -1,7 +1,11 @@
 package psidev.psi.mi.jami.bridges.europubmedcentral;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import psidev.psi.mi.jami.bridges.europubmedcentral.util.EuroPubmedCentralTranslationUtil;
 import psidev.psi.mi.jami.bridges.exception.BridgeFailedException;
+
+import psidev.psi.mi.jami.model.impl.DefaultPublication;
 import uk.ac.ebi.cdb.webservice.*;
 
 import psidev.psi.mi.jami.bridges.fetcher.PublicationFetcher;
@@ -10,8 +14,8 @@ import psidev.psi.mi.jami.model.Publication;
 import javax.xml.namespace.QName;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Collections;
 import java.util.List;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -22,17 +26,30 @@ import java.util.List;
 public class EuroPubmedCentralFetcher
         implements PublicationFetcher {
 
+
+    protected static final Logger log = LoggerFactory.getLogger(EuroPubmedCentralTranslationUtil.class.getName());
+
+    // private ResponseWrapper searchResult;
+    private static final String IDENTIFIER_TYPE = "med";
+    private static final String DATA_SET = "metadata";
+    private static final String RESULT_TYPE = "core"; // "lite";
+    private static final String EMAIL = "intact-dev@ebi.ac.uk";
+
+    // int offSet = 0;
+    // boolean synonym = false;
+
+
     private WSCitationImplService service;
 
-    public EuroPubmedCentralFetcher(){
+    public EuroPubmedCentralFetcher() throws BridgeFailedException {
         this("http://www.ebi.ac.uk/webservices/citexplore/v3.0.1/service?wsdl");
     }
 
-    public EuroPubmedCentralFetcher(String wsdlUrl){
+    private EuroPubmedCentralFetcher(String wsdlUrl) throws BridgeFailedException {
         try {
             service = new WSCitationImplService(new URL(wsdlUrl), new QName("http://webservice.cdb.ebi.ac.uk/", "WSCitationImplService"));
         } catch (MalformedURLException e) {
-            throw new RuntimeException( e );
+            throw new BridgeFailedException("Unable to initiate the publication fetcher.", e );
         }
     }
 
@@ -40,14 +57,15 @@ public class EuroPubmedCentralFetcher
         return service.getWSCitationImplPort();
     }
 
-    private ResultList searchCitationsByExternalId(String id) throws BridgeFailedException {
-        final String query = "EXT_ID:" + id + " SRC:med";
+    private ResultList searchForMetaDataByPubmedID(String id) throws BridgeFailedException {
+        final String query = "EXT_ID:" + id + " SRC:"+IDENTIFIER_TYPE;
         try {
             // SRC:med is needed as the external ids are not unique.
             // ex : extId1 coresponds to 2 publication in citexplore one from medline, one from CiteSeer.
             // Putting : core allow to get a lighter object just with the title, authors name...
             // we can choose between metadata for citations or full text for full text searches
-            ResponseWrapper wrapper = getPort().searchPublications(query, "metadata","core", 0, false, "intact-dev@ebi.ac.uk");
+            ResponseWrapper wrapper = getPort().searchPublications(query, DATA_SET , RESULT_TYPE, 0, false, EMAIL);
+            //ResponseWrapper wrapper = getPort().searchPublications(query, "metadata", "lite" , 0, false, "intact-dev@ebi.ac.uk");
 
             return  wrapper.getResultList();//"core"
         } catch (QueryException_Exception e) {
@@ -55,30 +73,111 @@ public class EuroPubmedCentralFetcher
         }
     }
 
-    /*public static void main(String[] args) throws Exception{
-        CitexploreClient client = new CitexploreClient();
-        Result c = client.getCitationById("1");
+    private ResultList searchForCrossReferencesByPubmedID(String id) throws BridgeFailedException {
+        try {
+            ResponseWrapper wrapper = getPort().getDatabaseLinks(id , IDENTIFIER_TYPE , null , 0 , EMAIL);
 
-        System.out.println(c.getJournalInfo());
-        System.out.println(c.getJournalInfo().getYearOfPublication());
+            return  wrapper.getResultList();
+        } catch (QueryException_Exception e) {
+            throw new BridgeFailedException("Problem fetching query", e);
+        }
+    }
 
-        System.out.println(client.getCitationById("1234567").getTitle());
-    }  */
-
-
-
-    public Publication getPublicationByPubmedID(String pubmedID) throws BridgeFailedException{
-        if(pubmedID == null || pubmedID.length() < 1)
+    public Publication getPublicationByPubmedID(String id) throws BridgeFailedException{
+        if(id == null || id.length() < 1)
             throw new IllegalArgumentException("Can not fetch on an empty identifier");
 
-        List<Result> results = searchCitationsByExternalId(pubmedID).getResult();
+        List<Result> results = searchForMetaDataByPubmedID(id).getResult();
+
+        Publication publication = new DefaultPublication();
 
         if (!results.isEmpty()) {
-            return EuroPubmedCentralTranslationUtil.convertResultToPublication(results.iterator().next());
+            EuroPubmedCentralTranslationUtil.
+                    convertDataResultToPublication(publication, results.iterator().next());
+
+            if(results.iterator().next().getHasDbCrossReferences().equals("Y")){
+                try {
+                    ResponseWrapper xrefResults = getPort().getDatabaseLinks(id, IDENTIFIER_TYPE, null, 0, EMAIL) ;
+                    EuroPubmedCentralTranslationUtil.
+                            convertXrefResultToPublication(publication, xrefResults);
+                } catch (QueryException_Exception e) {
+                    throw new BridgeFailedException(e);
+                }
+            }
         }
 
-        return null;
+
+        /*ResultList resultsList = searchForCrossReferencesByPubmedID(pubmedID);
+        //log.info("checking test "+resultsList.getResult().toString());
+        if (resultsList != null && !resultsList.getResult().isEmpty()) {
+            log.info("checking test "+resultsList.getResult().toString());
+            EuroPubmedCentralTranslationUtil.convertXrefResultToPublication(publication, results.iterator().next());
+        }*/
+
+        return publication;
+    }
+
+    /*
+
+    protected void runAllMethodsSingle(String id) throws QueryException_Exception {
+
+        //String query = "HAS_UNIPROT:y HAS_FREE_FULLTEXT:y HAS_REFLIST:y HAS_XREFS:y sort_cited:y";
+        final String query = "EXT_ID:" + id + " SRC:med";
+
+        ResponseWrapper resultsBean = getPort().searchPublications(query, DATA_SET , RESULT_TYPE, offSet, synonym, EMAIL);
+
+        List<Result> beanCollection = resultsBean.getResultList().getResult();
+
+        for (Result bean : beanCollection) {
+            printResultBean(bean);
+                                     //DATABASE
+            searchResult = getPort().getDatabaseLinks(bean.getPmid(), bean.getSource(), null , 0, EMAIL);
+            printFirstDbCrossReference(searchResult);
+        }
     }
 
 
+    private void printResultBean(Result citation) {
+        log.info("Title: " + citation.getTitle());
+        DataHandler dataHandler = citation.getFullText();
+        try {
+            if (dataHandler != null) {
+                log.info("");
+                dataHandler.getInputStream().close();
+                log.info("");
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    private void printFirstDbCrossReference(ResponseWrapper searchResult) {
+        if(searchResult == null || searchResult.getDbCrossReferenceList() == null) return;
+
+        for(DbCrossReference dbXref : searchResult.getDbCrossReferenceList().getDbCrossReference()){
+            for(DbCrossReferenceInfo dbXrefInfo : dbXref.getDbCrossReferenceInfo()){
+                // log.info(dbXref.getDbName()+": "+dbXrefInfo.getInfo1()+", "+dbXrefInfo.getInfo2()+", "+ dbXrefInfo.getInfo3()+", "+dbXrefInfo.getInfo4());
+                Xref xref = null;
+                if( dbXref.getDbName().equals("UNIPROT") ){
+                    xref = XrefUtils.createUniprotIdentity(dbXrefInfo.getInfo1());
+                } else if( dbXref.getDbName().equals("EMBL")){
+                    xref = XrefUtils.createIdentityXref(Xref.DDBJ_EMBL_GENBANK , Xref.DDBJ_EMBL_GENBANK_MI , dbXrefInfo.getInfo1());
+                } else if( dbXref.getDbName().equals("PDB") ){
+
+                } else if( dbXref.getDbName().equals("INTERPRO") ){
+                    xref = XrefUtils.createIdentityXref(Xref.INTERPRO , Xref.INTERPRO_MI , dbXrefInfo.getInfo1());
+
+                } else if( dbXref.getDbName().equals("OMIN") ){
+
+                } else if( dbXref.getDbName().equals("CHEBI") ){
+                    xref = XrefUtils.createChebiIdentity(dbXrefInfo.getInfo1());
+                } else if( dbXref.getDbName().equals("CHEMBL") ){
+
+                } else if( dbXref.getDbName().equals("INTACT")){
+
+                } else if( dbXref.getDbName().equals("ARXPR")){
+
+                }
+            }
+        }
+    } */
 }
--- psi-jami/jami-bridges/jami-europubmedcentral/src/main/java/psidev/psi/mi/jami/bridges/europubmedcentral/util/EuroPubmedCentralTranslationUtil.java
+++ psi-jami/jami-bridges/jami-europubmedcentral/src/main/java/psidev/psi/mi/jami/bridges/europubmedcentral/util/EuroPubmedCentralTranslationUtil.java
@@ -3,10 +3,10 @@ package psidev.psi.mi.jami.bridges.europubmedcentral.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import psidev.psi.mi.jami.model.Publication;
-import psidev.psi.mi.jami.model.impl.DefaultPublication;
+import psidev.psi.mi.jami.model.Xref;
 import psidev.psi.mi.jami.model.impl.DefaultSource;
-import uk.ac.ebi.cdb.webservice.Authors;
-import uk.ac.ebi.cdb.webservice.Result;
+import psidev.psi.mi.jami.utils.XrefUtils;
+import uk.ac.ebi.cdb.webservice.*;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -21,10 +21,8 @@ public class EuroPubmedCentralTranslationUtil {
 
     protected static final Logger log = LoggerFactory.getLogger(EuroPubmedCentralTranslationUtil.class.getName());
 
-    public static Publication convertResultToPublication(Result result){
+    public static void convertDataResultToPublication(Publication publication , Result result){
         if(result == null) throw new IllegalArgumentException("Can not translate null publication result");
-
-        Publication publication = new DefaultPublication();
 
         // PubMed ID
         publication.setPubmedId(result.getPmid());
@@ -34,8 +32,7 @@ public class EuroPubmedCentralTranslationUtil {
         publication.setDoi(result.getDOI());
         log.info("publication.setDoi: "+result.getDOI());
 
-
-
+        /*
         log.info("DB xrefs "+result.getHasDbCrossReferences());
         if(result.getHasDbCrossReferences().equals("Y"))
             //publication.getIdentifiers
@@ -45,8 +42,6 @@ public class EuroPubmedCentralTranslationUtil {
                     log.info("Xref types"+ac);
                 }
 
-
-
         log.info("Ac Numbers "+result.getHasTMAccessionNumbers());
         if(result.getHasTMAccessionNumbers().equals("Y"))
             //publication.getIdentifiers
@@ -54,11 +49,7 @@ public class EuroPubmedCentralTranslationUtil {
                     && result.getTmAccessionTypeList().getAccessionType()  != null )
                 for( String ac : result.getTmAccessionTypeList().getAccessionType() ){
                     log.info("AC types"+ac);
-                }
-
-
-
-
+                } */
 
         //assignImexId
 
@@ -67,15 +58,19 @@ public class EuroPubmedCentralTranslationUtil {
         log.info("publication.setTitle: "+result.getTitle());
 
         // Journal Name
-        publication.setJournal(result.getJournalInfo().getJournal().getTitle());
-        log.info("publication.setJournal: "+result.getJournalInfo().getJournal().getTitle());
+        if(result.getJournalInfo() != null ){
+            if(result.getJournalInfo().getJournal() != null){
+                publication.setJournal(result.getJournalInfo().getJournal().getTitle());
+                log.info("publication.setJournal: "+result.getJournalInfo().getJournal().getTitle());
+            }
 
-        // Publication Date
-        Calendar date = new GregorianCalendar(result.getJournalInfo().getYearOfPublication() ,
-                result.getJournalInfo().getMonthOfPublication()-1 , 1); // Dates begin from month 0; Days start from 1
-        publication.setPublicationDate(date.getTime());
-        log.info(" - month "+result.getJournalInfo().getMonthOfPublication().toString());
-        log.info(" - year "+result.getJournalInfo().getYearOfPublication().toString());
+            // Publication Date
+            Calendar date = new GregorianCalendar(result.getJournalInfo().getYearOfPublication() ,
+                    result.getJournalInfo().getMonthOfPublication()-1 , 1); // Dates begin from month 0; Days start from 1
+            publication.setPublicationDate(date.getTime());
+            log.info(" - month "+result.getJournalInfo().getMonthOfPublication().toString());
+            log.info(" - year "+result.getJournalInfo().getYearOfPublication().toString());
+        }
 
         // Publication Authors
         for(Authors authors : result.getAuthorList().getAuthor()){
@@ -97,22 +92,33 @@ public class EuroPubmedCentralTranslationUtil {
         if(result.getAffiliation() != null && result.getAffiliation().length() > 0)
             publication.setSource(new DefaultSource(result.getAffiliation())); // TODO
         log.info("publication.setSource: " + result.getAffiliation());
-
-
-
-        /*
-        log.info(result.getJournalInfo().getDateOfPublication());
-        // log.info(result.getJournalInfo().getIssue());
-        // log.info(result.getJournalInfo().getVolume());
-        log.info(result.getJournalInfo().getJournal().getTitle());
-        log.info(result.getJournalInfo().getJournal().getISOAbbreviation());
-        log.info(result.getJournalInfo().getJournal().getMedlineAbbreviation());
-        log.info(result.getJournalInfo().getJournal().getNLMid());
-        log.info(result.getJournalInfo().getJournal().getISSN());
-        log.info(result.getJournalInfo().getJournalIssueId().toString());
-         */
-
-        return publication;
     }
 
+    public static void convertXrefResultToPublication(Publication publication, ResponseWrapper result) {
+        if(result == null || result.getDbCrossReferenceList() == null) return;
+
+        for(DbCrossReference dbXref : result.getDbCrossReferenceList().getDbCrossReference()){
+            for(DbCrossReferenceInfo dbXrefInfo : dbXref.getDbCrossReferenceInfo()){
+                log.info("publication.addXref: "+dbXref.getDbName()+": "+dbXrefInfo.getInfo1());
+                if( dbXref.getDbName().equals("UNIPROT") ){
+                    publication.getXrefs().add(
+                            XrefUtils.createUniprotIdentity(dbXrefInfo.getInfo1()));
+                } else if( dbXref.getDbName().equals("EMBL")){
+                    publication.getXrefs().add(
+                            XrefUtils.createIdentityXref(Xref.DDBJ_EMBL_GENBANK , Xref.DDBJ_EMBL_GENBANK_MI , dbXrefInfo.getInfo1()));
+                } else if( dbXref.getDbName().equals("PDB") ){
+                } else if( dbXref.getDbName().equals("INTERPRO") ){
+                    publication.getXrefs().add(
+                            XrefUtils.createIdentityXref(Xref.INTERPRO , Xref.INTERPRO_MI , dbXrefInfo.getInfo1()));
+                } else if( dbXref.getDbName().equals("OMIN") ){
+                } else if( dbXref.getDbName().equals("CHEBI") ){
+                    publication.getXrefs().add(
+                            XrefUtils.createChebiIdentity(dbXrefInfo.getInfo1()));
+                } else if( dbXref.getDbName().equals("CHEMBL") ){
+                } else if( dbXref.getDbName().equals("INTACT")){
+                } else if( dbXref.getDbName().equals("ARXPR")){
+                }
+            }
+        }
+    }
 }
