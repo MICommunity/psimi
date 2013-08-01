d147c7b77e92672d6d8778fcec81084777ef1452
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-08-01 15:22:21 +0000

    Added processing for publications. All processing is assumed as minimum for now, with some parts to be moved to maximum later.

--- trunk/psi-jami/jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/impl/publication/MinimumPublicationEnricher.java
+++ trunk/psi-jami/jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/impl/publication/MinimumPublicationEnricher.java
@@ -17,7 +17,78 @@ public class MinimumPublicationEnricher extends AbstractPublicationEnricher {
 
     @Override
     protected void processPublication(Publication publicationToEnrich) {
-        //To change body of implemented methods use File | Settings | File Templates.
+
+        // PUBMED ID
+        if(publicationToEnrich.getPubmedId() == null
+                && publicationFetched.getPubmedId() != null) {
+            publicationToEnrich.setPubmedId(publicationFetched.getPubmedId());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onPubmedIdUpdate(publicationToEnrich , null);
+        }
+
+        // DOI
+        if(publicationToEnrich.getDoi() == null
+                && publicationFetched.getDoi() != null) {
+            publicationToEnrich.setPubmedId(publicationFetched.getDoi());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onDoiUpdate(publicationToEnrich , null);
+        }
+
+        //IDS
+        //IMEX
+
+        // TITLE
+        if(publicationToEnrich.getTitle() == null
+                && publicationFetched.getTitle() != null) {
+            publicationToEnrich.setTitle(publicationFetched.getTitle());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onTitleUpdated(publicationToEnrich , null);
+        }
+
+        // JOURNAL
+        if(publicationToEnrich.getJournal() == null
+                && publicationFetched.getJournal() != null) {
+            publicationToEnrich.setJournal(publicationFetched.getJournal());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onTitleUpdated(publicationToEnrich , null);
+        }
+
+        // PUBLICATION DATE
+        if(publicationToEnrich.getPublicationDate() == null
+                && publicationFetched.getPublicationDate() != null) {
+            publicationToEnrich.setPublicationDate(publicationFetched.getPublicationDate());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onPublicationDateUpdated(publicationToEnrich , null);
+        }
+
+        // AUTHORS  // TODO - cases sensitive and alternative ways to display represent a problem
+        if(!publicationFetched.getAuthors().isEmpty()){
+            for(String author : publicationFetched.getAuthors()){
+                if(publicationToEnrich.getAuthors().add(author))
+                    if(getPublicationEnricherListener() != null)
+                        getPublicationEnricherListener().onAuthorAdded(
+                                publicationToEnrich , author);
+            }
+        }
+
+        // XREFS
+
+        // ANNOTATIONS
+
+        // EXPERIMENTS
+
+        // CURATION DEPTH
+
+        // RELEASE DATE
+        if(publicationToEnrich.getReleasedDate() == null
+                && publicationFetched.getReleasedDate() != null) {
+            publicationToEnrich.setReleasedDate(publicationFetched.getReleasedDate());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onReleaseDateUpdated(publicationToEnrich , null);
+        }
+
+        // SOURCE
+
     }
 
 
--- trunk/psi-jami/jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/impl/publication/MinimumPublicationUpdater.java
+++ trunk/psi-jami/jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/impl/publication/MinimumPublicationUpdater.java
@@ -3,6 +3,10 @@ package psidev.psi.mi.jami.enricher.impl.publication;
 import psidev.psi.mi.jami.bridges.fetcher.PublicationFetcher;
 import psidev.psi.mi.jami.model.Publication;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
 /**
  * Created with IntelliJ IDEA.
  *
@@ -19,6 +23,91 @@ public class MinimumPublicationUpdater
 
     @Override
     protected void processPublication(Publication publicationToEnrich) {
-        //To change body of implemented methods use File | Settings | File Templates.
+
+        // PUBMED ID
+        if(! publicationToEnrich.getPubmedId().equals(publicationFetched.getPubmedId())
+                && publicationFetched.getPubmedId() != null) {
+            String oldValue = publicationToEnrich.getPubmedId();
+            publicationToEnrich.setPubmedId(publicationFetched.getPubmedId());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onPubmedIdUpdate(publicationToEnrich , oldValue);
+        }
+
+        // DOI
+        if(publicationToEnrich.getDoi().equals(publicationFetched.getDoi())
+                && publicationFetched.getDoi() != null) {
+            String oldValue = publicationToEnrich.getDoi();
+            publicationToEnrich.setPubmedId(publicationFetched.getDoi());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onDoiUpdate(publicationToEnrich , oldValue);
+        }
+
+        //IDS
+        //IMEX
+
+        // TITLE
+        if(publicationToEnrich.getTitle().equals(publicationFetched.getTitle())
+                && publicationFetched.getTitle() != null) {
+            String oldValue = publicationToEnrich.getTitle();
+            publicationToEnrich.setTitle(publicationFetched.getTitle());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onTitleUpdated(publicationToEnrich , oldValue);
+        }
+
+        // JOURNAL
+        if(publicationToEnrich.getJournal().equals( publicationFetched.getJournal() )
+                && publicationFetched.getJournal() != null) {
+            String oldValue = publicationToEnrich.getJournal();
+            publicationToEnrich.setJournal(publicationFetched.getJournal());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onTitleUpdated(publicationToEnrich , oldValue);
+        }
+
+        // PUBLICATION DATE
+        if(publicationToEnrich.getPublicationDate().equals( publicationFetched.getPublicationDate() )
+                && publicationFetched.getPublicationDate() != null) {
+            Date oldValue = publicationToEnrich.getPublicationDate();
+            publicationToEnrich.setPublicationDate(publicationFetched.getPublicationDate());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onPublicationDateUpdated(publicationToEnrich , oldValue);
+        }
+
+        // AUTHORS  // TODO - cases sensitive and alternative ways to display represent a problem
+        if(!publicationFetched.getAuthors().isEmpty()){
+            Collection<String> authorsToRemove = new ArrayList<String>();
+            authorsToRemove.addAll(publicationToEnrich.getAuthors());
+            authorsToRemove.removeAll(publicationFetched.getAuthors());
+            for(String author : authorsToRemove){
+                publicationToEnrich.getAuthors().remove(author);
+                if(getPublicationEnricherListener() != null)
+                    getPublicationEnricherListener().onAuthorRemoved(publicationToEnrich, author);
+            }
+
+            for(String author : publicationFetched.getAuthors()){
+                if(publicationToEnrich.getAuthors().add(author))
+                    if(getPublicationEnricherListener() != null)
+                        getPublicationEnricherListener().onAuthorAdded(
+                                publicationToEnrich , author);
+            }
+        }
+
+        // XREFS
+
+        // ANNOTATIONS
+
+        // EXPERIMENTS
+
+        // CURATION DEPTH
+
+        // RELEASE DATE
+        if(publicationToEnrich.getReleasedDate().equals(publicationFetched.getReleasedDate())
+                && publicationFetched.getReleasedDate() != null) {
+            Date oldValue = publicationToEnrich.getReleasedDate() ;
+            publicationToEnrich.setReleasedDate(publicationFetched.getReleasedDate());
+            if(getPublicationEnricherListener() != null)
+                getPublicationEnricherListener().onReleaseDateUpdated(publicationToEnrich , oldValue);
+        }
+
+        // SOURCE
     }
 }
