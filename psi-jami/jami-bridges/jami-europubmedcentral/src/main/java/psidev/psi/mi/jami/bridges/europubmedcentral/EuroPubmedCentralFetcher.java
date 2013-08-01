ef9576c7dafc2cca9f834bfb44f9a359d7cbcf3d
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-08-01 10:48:53 +0000

    Restructured the abstract levels.

--- psi-jami/jami-bridges/jami-europubmedcentral/src/main/java/psidev/psi/mi/jami/bridges/europubmedcentral/EuroPubmedCentralFetcher.java
+++ psi-jami/jami-bridges/jami-europubmedcentral/src/main/java/psidev/psi/mi/jami/bridges/europubmedcentral/EuroPubmedCentralFetcher.java
@@ -14,11 +14,13 @@ import psidev.psi.mi.jami.model.Publication;
 import javax.xml.namespace.QName;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 
 /**
- * Created with IntelliJ IDEA.
+ * Uses the EuroPubmedCentral WSDL service to fetch publication entries.
  *
  * @author Gabriel Aldam (galdam@ebi.ac.uk)
  * @since 30/07/13
@@ -26,20 +28,15 @@ import java.util.List;
 public class EuroPubmedCentralFetcher
         implements PublicationFetcher {
 
+    protected static final Logger log = LoggerFactory.getLogger(EuroPubmedCentralFetcher.class.getName());
 
-    protected static final Logger log = LoggerFactory.getLogger(EuroPubmedCentralTranslationUtil.class.getName());
-
-    // private ResponseWrapper searchResult;
     private static final String IDENTIFIER_TYPE = "med";
     private static final String DATA_SET = "metadata";
     private static final String RESULT_TYPE = "core"; // "lite";
     private static final String EMAIL = "intact-dev@ebi.ac.uk";
 
-    // int offSet = 0;
-    // boolean synonym = false;
-
-
     private WSCitationImplService service;
+
 
     public EuroPubmedCentralFetcher() throws BridgeFailedException {
         this("http://www.ebi.ac.uk/webservices/citexplore/v3.0.1/service?wsdl");
@@ -57,45 +54,40 @@ public class EuroPubmedCentralFetcher
         return service.getWSCitationImplPort();
     }
 
-    private ResultList searchForMetaDataByPubmedID(String id) throws BridgeFailedException {
-        final String query = "EXT_ID:" + id + " SRC:"+IDENTIFIER_TYPE;
-        try {
-            // SRC:med is needed as the external ids are not unique.
-            // ex : extId1 coresponds to 2 publication in citexplore one from medline, one from CiteSeer.
-            // Putting : core allow to get a lighter object just with the title, authors name...
-            // we can choose between metadata for citations or full text for full text searches
-            ResponseWrapper wrapper = getPort().searchPublications(query, DATA_SET , RESULT_TYPE, 0, false, EMAIL);
-            //ResponseWrapper wrapper = getPort().searchPublications(query, "metadata", "lite" , 0, false, "intact-dev@ebi.ac.uk");
 
-            return  wrapper.getResultList();//"core"
-        } catch (QueryException_Exception e) {
-            throw new BridgeFailedException("Problem fetching query: "+query, e);
-        }
-    }
-
-    private ResultList searchForCrossReferencesByPubmedID(String id) throws BridgeFailedException {
-        try {
-            ResponseWrapper wrapper = getPort().getDatabaseLinks(id , IDENTIFIER_TYPE , null , 0 , EMAIL);
-
-            return  wrapper.getResultList();
-        } catch (QueryException_Exception e) {
-            throw new BridgeFailedException("Problem fetching query", e);
-        }
-    }
-
+    /**
+     * Queries the EuroPubmedCentral WSDL service for the meta data.
+     * A second query is made to gather Xrefs if the meta data shows they exist.
+     * @param id    the pubmedID of the publication
+     * @return      a completed publication record.
+     * @throws BridgeFailedException
+     */
     public Publication getPublicationByPubmedID(String id) throws BridgeFailedException{
         if(id == null || id.length() < 1)
             throw new IllegalArgumentException("Can not fetch on an empty identifier");
 
-        List<Result> results = searchForMetaDataByPubmedID(id).getResult();
+
+        Collection<Result> results = Collections.EMPTY_LIST;
+
+        final String query = "EXT_ID:" + id + " SRC:"+IDENTIFIER_TYPE;
+        try {
+            ResponseWrapper wrapper = getPort().searchPublications(query, DATA_SET , RESULT_TYPE, 0, false, EMAIL);
+
+            if(wrapper.getResultList() != null)
+                results = wrapper.getResultList().getResult();
+        } catch (QueryException_Exception e) {
+            throw new BridgeFailedException("Problem fetching query: "+query, e);
+        }
 
         Publication publication = new DefaultPublication();
 
         if (!results.isEmpty()) {
-            EuroPubmedCentralTranslationUtil.
-                    convertDataResultToPublication(publication, results.iterator().next());
+            Result entry = results.iterator().next();
 
-            if(results.iterator().next().getHasDbCrossReferences().equals("Y")){
+            EuroPubmedCentralTranslationUtil.
+                    convertDataResultToPublication(publication, entry);
+
+            if( entry.getHasDbCrossReferences().equals("Y") ){
                 try {
                     ResponseWrapper xrefResults = getPort().getDatabaseLinks(id, IDENTIFIER_TYPE, null, 0, EMAIL) ;
                     EuroPubmedCentralTranslationUtil.
@@ -106,78 +98,6 @@ public class EuroPubmedCentralFetcher
             }
         }
 
-
-        /*ResultList resultsList = searchForCrossReferencesByPubmedID(pubmedID);
-        //log.info("checking test "+resultsList.getResult().toString());
-        if (resultsList != null && !resultsList.getResult().isEmpty()) {
-            log.info("checking test "+resultsList.getResult().toString());
-            EuroPubmedCentralTranslationUtil.convertXrefResultToPublication(publication, results.iterator().next());
-        }*/
-
         return publication;
     }
-
-    /*
-
-    protected void runAllMethodsSingle(String id) throws QueryException_Exception {
-
-        //String query = "HAS_UNIPROT:y HAS_FREE_FULLTEXT:y HAS_REFLIST:y HAS_XREFS:y sort_cited:y";
-        final String query = "EXT_ID:" + id + " SRC:med";
-
-        ResponseWrapper resultsBean = getPort().searchPublications(query, DATA_SET , RESULT_TYPE, offSet, synonym, EMAIL);
-
-        List<Result> beanCollection = resultsBean.getResultList().getResult();
-
-        for (Result bean : beanCollection) {
-            printResultBean(bean);
-                                     //DATABASE
-            searchResult = getPort().getDatabaseLinks(bean.getPmid(), bean.getSource(), null , 0, EMAIL);
-            printFirstDbCrossReference(searchResult);
-        }
-    }
-
-
-    private void printResultBean(Result citation) {
-        log.info("Title: " + citation.getTitle());
-        DataHandler dataHandler = citation.getFullText();
-        try {
-            if (dataHandler != null) {
-                log.info("");
-                dataHandler.getInputStream().close();
-                log.info("");
-            }
-        } catch (Exception e) {
-        }
-    }
-
-    private void printFirstDbCrossReference(ResponseWrapper searchResult) {
-        if(searchResult == null || searchResult.getDbCrossReferenceList() == null) return;
-
-        for(DbCrossReference dbXref : searchResult.getDbCrossReferenceList().getDbCrossReference()){
-            for(DbCrossReferenceInfo dbXrefInfo : dbXref.getDbCrossReferenceInfo()){
-                // log.info(dbXref.getDbName()+": "+dbXrefInfo.getInfo1()+", "+dbXrefInfo.getInfo2()+", "+ dbXrefInfo.getInfo3()+", "+dbXrefInfo.getInfo4());
-                Xref xref = null;
-                if( dbXref.getDbName().equals("UNIPROT") ){
-                    xref = XrefUtils.createUniprotIdentity(dbXrefInfo.getInfo1());
-                } else if( dbXref.getDbName().equals("EMBL")){
-                    xref = XrefUtils.createIdentityXref(Xref.DDBJ_EMBL_GENBANK , Xref.DDBJ_EMBL_GENBANK_MI , dbXrefInfo.getInfo1());
-                } else if( dbXref.getDbName().equals("PDB") ){
-
-                } else if( dbXref.getDbName().equals("INTERPRO") ){
-                    xref = XrefUtils.createIdentityXref(Xref.INTERPRO , Xref.INTERPRO_MI , dbXrefInfo.getInfo1());
-
-                } else if( dbXref.getDbName().equals("OMIN") ){
-
-                } else if( dbXref.getDbName().equals("CHEBI") ){
-                    xref = XrefUtils.createChebiIdentity(dbXrefInfo.getInfo1());
-                } else if( dbXref.getDbName().equals("CHEMBL") ){
-
-                } else if( dbXref.getDbName().equals("INTACT")){
-
-                } else if( dbXref.getDbName().equals("ARXPR")){
-
-                }
-            }
-        }
-    } */
 }
--- psi-jami/jami-bridges/jami-europubmedcentral/src/test/java/psidev/psi/mi/jami/bridges/europubmedcentral/EuroPubmedCentralFetcherTest.java
+++ psi-jami/jami-bridges/jami-europubmedcentral/src/test/java/psidev/psi/mi/jami/bridges/europubmedcentral/EuroPubmedCentralFetcherTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import psidev.psi.mi.jami.bridges.exception.BridgeFailedException;
+import psidev.psi.mi.jami.model.Publication;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created with IntelliJ IDEA.
@@ -27,11 +29,62 @@ public class EuroPubmedCentralFetcherTest {
 
     @Test
     public void test() throws BridgeFailedException {
-        fetcher.getPublicationByPubmedID("23671334");
         log.info("------c-----");
         fetcher.getPublicationByPubmedID("13054692");
         log.info("----c-------");
         fetcher.getPublicationByPubmedID("10831611");
     }
 
+    @Test
+    public void test_publicationC() throws BridgeFailedException{
+
+        Publication publication = fetcher.getPublicationByPubmedID("13054692");
+        assertEquals( publication.getPubmedId() , "13054692");
+        assertEquals( publication.getDoi() , "10.1038/171737a0");
+        assertEquals( publication.getTitle() , "Molecular structure of nucleic acids; a structure for deoxyribose nucleic acid.");
+        assertEquals( publication.getJournal() , "Nature");
+
+        //month 4
+        //year 1953
+        //publication.addAuthor: WATSON JD
+        //publication.addAuthor: CRICK FH
+        //publication.setSource: null
+    }
+
+    @Test
+    public void test_publicationB() throws  BridgeFailedException{
+        Publication publication = fetcher.getPublicationByPubmedID("23671334");
+        assertEquals("23671334" , publication.getPubmedId());
+        assertEquals("" , publication.getTitle());
+    }
+
+    @Test
+    public void test_publicationA() throws BridgeFailedException{
+
+        Publication publication = fetcher.getPublicationByPubmedID("10831611");
+        assertEquals( publication.getPubmedId() , "10831611");
+        assertEquals( publication.getDoi() , "10.1083/jcb.149.5.1073");
+        assertEquals( publication.getTitle() , "Zyxin, a regulator of actin filament assembly, targets the mitotic apparatus by interacting with h-warts/LATS1 tumor suppressor.");
+        assertEquals( publication.getJournal() , "The Journal of cell biology");
+        // month 5 - year 2000
+        //publication.addAuthor: Hirota T
+        //publication.addAuthor: Morisaki T
+        //publication.addAuthor: Nishiyama Y
+        //publication.addAuthor: Marumoto T
+        //publication.addAuthor: Tada K
+        //publication.addAuthor: Hara T
+        //publication.addAuthor: Masuko N
+        //publication.addAuthor: Inagaki M
+        //publication.addAuthor: Hatakeyama K
+        //publication.addAuthor: Saya H
+        //publication.setSource: Department of Tumor Genetics and Biology, Kumamoto University School of Medicine, 2-2-1 Honjo, Kumamoto 860-0811, Japan.
+        ///publication.addXref: INTACT: EBI-444276
+        //publication.addXref: INTACT: EBI-444205
+        //publication.addXref: INTACT: EBI-444263
+        //publication.addXref: INTACT: EBI-444239
+        //publication.addXref: INTACT: EBI-444292
+        //publication.addXref: INTERPRO: IPR001781
+        //publication.addXref: OMIM: 602002
+        //publication.addXref: UNIPROT: O95835
+    }
 }
