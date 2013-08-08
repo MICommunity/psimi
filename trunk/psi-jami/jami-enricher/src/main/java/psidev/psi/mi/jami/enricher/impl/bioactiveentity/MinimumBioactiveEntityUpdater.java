a947b9011d2fdf25a92fad753a4a5508d396d343
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-08-08 12:04:45 +0000

    Added behaviour to the bioactiveEntity enrichment

--- trunk/psi-jami/jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/impl/bioactiveentity/MinimumBioactiveEntityUpdater.java
+++ trunk/psi-jami/jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/impl/bioactiveentity/MinimumBioactiveEntityUpdater.java
@@ -19,5 +19,52 @@ public class MinimumBioactiveEntityUpdater
     @Override
     protected void processBioactiveEntity(BioactiveEntity bioactiveEntityToEnrich) {
 
+
+        // bioactiveEntityToEnrich.getShortName();
+        //bioactiveEntityToEnrich.getInteractorType();
+        //bioactiveEntityToEnrich.getOrganism();
+        //bioactiveEntityToEnrich.getAliases();
+        //bioactiveEntityToEnrich.getXrefs();
+
+        // FULL NAME
+        if(bioactiveEntityFetched.getFullName() != null
+                && ! bioactiveEntityFetched.getFullName().equalsIgnoreCase(bioactiveEntityToEnrich.getFullName())){
+            bioactiveEntityToEnrich.setFullName(bioactiveEntityFetched.getFullName());
+            if(getBioactiveEntityEnricherListener() != null)
+                getBioactiveEntityEnricherListener().onFullNameUpdate(bioactiveEntityToEnrich , null);
+        }
+
+        // CHEBI IDENTIFIER
+        if(bioactiveEntityFetched.getChebi() != null
+                && ! bioactiveEntityFetched.getChebi().equalsIgnoreCase(bioactiveEntityToEnrich.getChebi())){
+            bioactiveEntityToEnrich.setChebi(bioactiveEntityFetched.getChebi());
+            if(getBioactiveEntityEnricherListener() != null)
+                getBioactiveEntityEnricherListener().onChebiUpdate(bioactiveEntityToEnrich , null);
+        }
+
+        // INCHI Code
+        if( bioactiveEntityFetched.getStandardInchi() != null
+                && bioactiveEntityFetched.getStandardInchi().equalsIgnoreCase(bioactiveEntityToEnrich.getStandardInchi())){
+
+            bioactiveEntityToEnrich.setStandardInchi(bioactiveEntityFetched.getStandardInchi());
+            if(getBioactiveEntityEnricherListener() != null)
+                getBioactiveEntityEnricherListener().onStandardInchiUpdate(bioactiveEntityToEnrich , null);
+        }
+
+        // INCHI KEY
+        if(bioactiveEntityFetched.getStandardInchiKey() != null
+                && ! bioactiveEntityFetched.getStandardInchiKey().equalsIgnoreCase(bioactiveEntityToEnrich.getStandardInchiKey())){
+            bioactiveEntityToEnrich.setStandardInchiKey(bioactiveEntityFetched.getStandardInchiKey());
+            if(getBioactiveEntityEnricherListener() != null)
+                getBioactiveEntityEnricherListener().onStandardInchiKeyUpdate(bioactiveEntityToEnrich , null);
+        }
+
+        // SMILE
+        if(bioactiveEntityFetched.getSmile() != null
+                && bioactiveEntityFetched.getSmile().equalsIgnoreCase(bioactiveEntityToEnrich.getSmile())){
+            bioactiveEntityToEnrich.setSmile(bioactiveEntityFetched.getSmile());
+            if(getBioactiveEntityEnricherListener() != null)
+                getBioactiveEntityEnricherListener().onSmileUpdate(bioactiveEntityToEnrich , null);
+        }
     }
 }
