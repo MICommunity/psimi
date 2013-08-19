03bac2afb5df3dd5f8e0f152ec7f32aa9d3d5379
galdamebi <galdamebi@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-08-19 13:37:55 +0000

    Minor spelling fix.

--- psimi-enricher/enrich-mitab.sh
+++ psimi-enricher/enrich-mitab.sh
@@ -6,13 +6,13 @@ then
       OUTPUT_FILE=$2;
       echo "Input file: ${INPUT_FILE}"
       echo "Output file: ${OUTPUT_FILE}"
-      mvn clean install -Penrich-file -DmiFile=${INPUT_FILE} -DmiOutput=${OUPUT_FILE} -Dspring.config=classpath:META-INF/mitab-enricher-spring.xml -Dmaven.test.skip
+      mvn clean install -Penrich-file -DmiFile=${INPUT_FILE} -DmiOutput=${OUTPUT_FILE} -Dspring.config=classpath:META-INF/mitab-enricher-spring.xml -Dmaven.test.skip
 else
       echo ""
       echo "ERROR: wrong number of parameters ($#)."
       echo "usage: INPUT_FILE OUTPUT_FILE"
       echo "usage: INPUT_FILE: the name of the file to enrich."
-      echo "usage: OUTUT_FILE: the name of the file where to write the enriched interactions"
+      echo "usage: OUTPUT_FILE: the name of the file where to write the enriched interactions"
       echo ""
       exit 1
 fi
