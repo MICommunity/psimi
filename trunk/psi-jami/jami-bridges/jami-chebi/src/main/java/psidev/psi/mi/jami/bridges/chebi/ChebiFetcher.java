630fe0ec5097310ac5f9bafc0e59c15992b58e8a
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-09-09 10:42:09 +0000

    Updated CHEBI service to use the Java client and have a proper batch query

--- trunk/psi-jami/jami-bridges/jami-chebi/src/main/java/psidev/psi/mi/jami/bridges/chebi/ChebiFetcher.java
+++ trunk/psi-jami/jami-bridges/jami-chebi/src/main/java/psidev/psi/mi/jami/bridges/chebi/ChebiFetcher.java
@@ -1,20 +1,25 @@
 package psidev.psi.mi.jami.bridges.chebi;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import psidev.psi.mi.jami.bridges.exception.BridgeFailedException;
 import psidev.psi.mi.jami.bridges.fetcher.BioactiveEntityFetcher;
 import psidev.psi.mi.jami.model.Alias;
 import psidev.psi.mi.jami.model.BioactiveEntity;
-import psidev.psi.mi.jami.model.impl.DefaultAlias;
+import psidev.psi.mi.jami.model.CvTerm;
 import psidev.psi.mi.jami.model.impl.DefaultBioactiveEntity;
 import psidev.psi.mi.jami.utils.AliasUtils;
+import psidev.psi.mi.jami.utils.CvTermUtils;
 import psidev.psi.mi.jami.utils.XrefUtils;
-import uk.ac.ebi.webservices.chebi.*;
+import psidev.psi.mi.jami.utils.collection.ListUtils;
+import uk.ac.ebi.chebi.webapps.chebiWS.client.ChebiWebServiceClient;
+import uk.ac.ebi.chebi.webapps.chebiWS.model.ChebiWebServiceFault_Exception;
+import uk.ac.ebi.chebi.webapps.chebiWS.model.DataItem;
+import uk.ac.ebi.chebi.webapps.chebiWS.model.Entity;
+import uk.ac.ebi.chebi.webapps.chebiWS.model.OntologyDataItem;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Accesses Chebi entries using the WSDL SOAP service.
@@ -25,15 +30,12 @@ import java.util.Collection;
 public class ChebiFetcher
         implements BioactiveEntityFetcher {
 
-    public static final String IUPAC_MI = "MI:2007";
-    public static final String IUPAC = "iupac name";
-
-    protected static final Logger log = LoggerFactory.getLogger(ChebiFetcher.class.getName());
-
-    ChebiWebServiceService client;
+    private ChebiWebServiceClient client;
+    private static final int MAX_SIZE_CHEBI_IDS = 50;
+    public final static String CHEBI_POLYSACCHARYDE_PARENT="CHEBI:18154";
 
     public ChebiFetcher(){
-        client = new ChebiWebServiceService();
+        client = new ChebiWebServiceClient();
     }
 
     /**
@@ -49,60 +51,106 @@ public class ChebiFetcher
     public BioactiveEntity fetchBioactiveEntityByIdentifier (String identifier) throws BridgeFailedException {
         if(identifier == null) throw new IllegalArgumentException("Can not fetch on null identifier");
 
-        BioactiveEntity bioactiveEntity;
+        BioactiveEntity bioactiveEntity = null;
         try {
-            Entity entity = client.getChebiWebServicePort().getCompleteEntity(identifier);
+            Entity entity = client.getCompleteEntity(identifier);
             if(entity == null) return null;
-
-            // == Short name / Full name
-            bioactiveEntity = new DefaultBioactiveEntity(entity.getChebiAsciiName());
-            // bioactiveEntity.setFullName( entity.getChebiAsciiName() );
-
-            // == Chebi ID
-            bioactiveEntity.setChebi( entity.getChebiId() );
-
-            // == Secondary CHEBI IDs
-            for(String secondaryId : entity.getSecondaryChEBIIds()){
-                bioactiveEntity.getIdentifiers().add(XrefUtils.createChebiSecondary(secondaryId));
-            }
-
-            // == Smile
-            bioactiveEntity.setSmile( entity.getSmiles() );
-
-            // == Inchi / Inchi Key
-            bioactiveEntity.setStandardInchi( entity.getInchi() );
-            bioactiveEntity.setStandardInchiKey( entity.getInchiKey() );
-
-            // == SYNONYMS
-            for(DataItem syn : entity.getSynonyms()){
-                bioactiveEntity.getAliases().add(AliasUtils.createAlias(
-                        Alias.SYNONYM , Alias.SYNONYM_MI , syn.getData()));
-            }
-
-            // == IUPAC names
-            for(DataItem syn : entity.getIupacNames()){
-                bioactiveEntity.getAliases().add(AliasUtils.createAlias(
-                        IUPAC , IUPAC_MI , syn.getData()));
-            }
-
-            //UNUSED FIELDS
-            /*for(DataItem syn : entity.getDatabaseLinks()){
-                log.info("LIN: ["+syn.getData()+"] ty ["+syn.getType()+"]");
-            } */
-
+            bioactiveEntity = createNewBioactiveEntity(entity);
 
         } catch ( ChebiWebServiceFault_Exception e ) {
-            throw new BridgeFailedException(e);
+            throw new BridgeFailedException("Cannot fetch the bioactive entity from CHEBI",e);
         }
         return bioactiveEntity;
     }
 
-    @Override
-    public Collection<BioactiveEntity> fetchBioactiveEntitiesByIdentifiers(Collection<String> identifiers) throws BridgeFailedException {
-        Collection<BioactiveEntity> results = new ArrayList<BioactiveEntity>();
-        for(String id : identifiers){
-            results.add(fetchBioactiveEntityByIdentifier(id));
+    private BioactiveEntity createNewBioactiveEntity(Entity entity) {
+
+        CvTerm entityType = retrieveMoleculeTypeFrom(entity);
+
+        BioactiveEntity bioactiveEntity = new DefaultBioactiveEntity(entity.getChebiAsciiName(), entity.getChebiAsciiName(), entityType);
+
+        // == Chebi ID
+        bioactiveEntity.setChebi( entity.getChebiId() );
+
+        // == Secondary CHEBI IDs
+        for(String secondaryId : entity.getSecondaryChEBIIds()){
+            bioactiveEntity.getIdentifiers().add(XrefUtils.createChebiSecondary(secondaryId));
         }
+
+        // == Smile
+        bioactiveEntity.setSmile( entity.getSmiles() );
+
+        // == Inchi / Inchi Key
+        bioactiveEntity.setStandardInchi( entity.getInchi() );
+        bioactiveEntity.setStandardInchiKey( entity.getInchiKey() );
+
+        // == SYNONYMS
+        for(DataItem syn : entity.getSynonyms()){
+            bioactiveEntity.getAliases().add(AliasUtils.createAlias(
+                    Alias.SYNONYM, Alias.SYNONYM_MI, syn.getData()));
+        }
+
+        // == IUPAC names
+        for(DataItem syn : entity.getIupacNames()){
+            bioactiveEntity.getAliases().add(AliasUtils.createAlias(
+                    Alias.IUPAC ,  Alias.IUPAC_MI , syn.getData()));
+        }
+        return bioactiveEntity;
+    }
+
+    private CvTerm retrieveMoleculeTypeFrom(Entity entity) {
+        CvTerm entityType = null;
+        if (entity.getChebiId().equals(CHEBI_POLYSACCHARYDE_PARENT)){
+            entityType = CvTermUtils.createMICvTerm(BioactiveEntity.POLYSACCHARIDE, BioactiveEntity.POLYSACCHARIDE_MI);
+        }
+        else{
+            List<OntologyDataItem> parents = entity.getOntologyParents();
+            if (parents != null && !parents.isEmpty()){
+                for (OntologyDataItem item : parents){
+                    if (item.getChebiId().equals(CHEBI_POLYSACCHARYDE_PARENT)){
+                        entityType = CvTermUtils.createMICvTerm(BioactiveEntity.POLYSACCHARIDE, BioactiveEntity.POLYSACCHARIDE_MI);
+                        break;
+                    }
+                }
+
+                if (entityType == null){
+                    entityType = CvTermUtils.createMICvTerm(BioactiveEntity.SMALL_MOLECULE, BioactiveEntity.SMALL_MOLECULE_MI);
+                }
+            }
+            else{
+                entityType = CvTermUtils.createMICvTerm(BioactiveEntity.SMALL_MOLECULE, BioactiveEntity.SMALL_MOLECULE_MI);
+            }
+        }
+        return entityType;
+    }
+
+    public Collection<BioactiveEntity> fetchBioactiveEntitiesByIdentifiers(Collection<String> identifiers) throws BridgeFailedException {
+
+        if (identifiers == null) {
+            throw new IllegalArgumentException("The collection of identifiers cannot be null");
+        }
+        Collection<BioactiveEntity> results = new ArrayList<BioactiveEntity>();
+        List<Entity> entities = null;
+
+        List<List<String>> parts = ListUtils.splitter(new ArrayList<String>(identifiers), MAX_SIZE_CHEBI_IDS);
+
+        for (List<String> part : parts) {
+            try {
+                //If we have the same Id in the list, we will have only one Entity
+                entities = client.getCompleteEntityByList(part);
+
+                if (entities != null && !entities.isEmpty()) {
+                    for (Entity entity : entities) {
+                        results.add(createNewBioactiveEntity(entity));
+                    }
+                }
+
+            } catch (ChebiWebServiceFault_Exception e) {
+                throw new BridgeFailedException("Cannot fetch the bioactive entity from CHEBI",e);
+            }
+            entities.clear();
+        }
+
         return results;
     }
 }
