d16c4e443310308cab6c7a6bcd104ab19a77c9e6
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-09-30 15:58:58 +0000

    Added an interface for fetching sequence from a sequence version and primary id

--- psi-jami/jami-bridges/bridges-core/src/main/java/psidev/psi/mi/jami/bridges/fetcher/SequenceVersionFetcher.java
+++ psi-jami/jami-bridges/bridges-core/src/main/java/psidev/psi/mi/jami/bridges/fetcher/SequenceVersionFetcher.java
@@ -1,5 +1,7 @@
 package psidev.psi.mi.jami.bridges.fetcher;
 
+import psidev.psi.mi.jami.bridges.exception.BridgeFailedException;
+
 /**
  * A fetcher that fetches a sequence from a specific version and/or a version from a specific sequence
  *
@@ -10,7 +12,7 @@ package psidev.psi.mi.jami.bridges.fetcher;
 
 public interface SequenceVersionFetcher {
 
-    public String fetchSequenceFromVersion(String id, int version);
+    public String fetchSequenceFromVersion(String id, int version) throws BridgeFailedException;
 
-    public int fetchVersionFromSequence(String id, String sequence);
+    public int fetchVersionFromSequence(String id, String sequence) throws BridgeFailedException;
 }
