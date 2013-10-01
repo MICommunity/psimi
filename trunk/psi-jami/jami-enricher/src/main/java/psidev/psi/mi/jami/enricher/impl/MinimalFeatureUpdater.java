f9dd03bdd54f0deb77d780eb08a693a3e3ee18d9
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-10-01 10:15:31 +0000

    Refactored and fixed mistakes in feature enricher

--- trunk/psi-jami/jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/impl/MinimalFeatureUpdater.java
+++ trunk/psi-jami/jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/impl/MinimalFeatureUpdater.java
@@ -1,6 +1,10 @@
 package psidev.psi.mi.jami.enricher.impl;
 
 import psidev.psi.mi.jami.model.Feature;
+import psidev.psi.mi.jami.model.Range;
+import psidev.psi.mi.jami.utils.PositionUtils;
+
+import java.util.Collection;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,5 +17,27 @@ public class MinimalFeatureUpdater<F extends  Feature> extends MinimalFeatureEnr
     @Override
     protected boolean updateRangePositions() {
         return true;
+    }
+
+    @Override
+    protected void onInvalidRange(F feature, Range range, Collection<String> errorMessages) {
+        if (getFeatureEnricherListener() != null){
+            getFeatureEnricherListener().onRemovedRange(feature, range);
+        }
+        range.setPositions(PositionUtils.createUndeterminedPosition(), PositionUtils.createUndeterminedPosition());
+        if (getFeatureEnricherListener() != null){
+            getFeatureEnricherListener().onAddedRange(feature, range);
+        }
+    }
+
+    @Override
+    protected void onOutOfDateRange(F feature, Range range, Collection<String> errorMessages, String oldSequence) {
+        if (getFeatureEnricherListener() != null){
+            getFeatureEnricherListener().onRemovedRange(feature, range);
+        }
+        range.setPositions(PositionUtils.createUndeterminedPosition(), PositionUtils.createUndeterminedPosition());
+        if (getFeatureEnricherListener() != null){
+            getFeatureEnricherListener().onAddedRange(feature, range);
+        }
     }
 }
