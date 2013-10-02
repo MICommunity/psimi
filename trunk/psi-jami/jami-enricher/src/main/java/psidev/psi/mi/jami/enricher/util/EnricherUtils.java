fb7a99ba0ddcdc45561fc0143d34679f3c7ccc9b
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-10-02 12:21:22 +0000

    deleted old protein package and refactored protein listener

--- trunk/psi-jami/jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/util/EnricherUtils.java
+++ trunk/psi-jami/jami-enricher/src/main/java/psidev/psi/mi/jami/enricher/util/EnricherUtils.java
@@ -1,13 +1,12 @@
 package psidev.psi.mi.jami.enricher.util;
 
-import psidev.psi.mi.jami.enricher.*;
-import psidev.psi.mi.jami.enricher.listener.protein.ProteinEnricherListener;
-import psidev.psi.mi.jami.enricher.listener.protein.ProteinEnricherListenerManager;
 import psidev.psi.mi.jami.listener.AliasesChangeListener;
 import psidev.psi.mi.jami.listener.ChecksumsChangeListener;
 import psidev.psi.mi.jami.listener.IdentifiersChangeListener;
 import psidev.psi.mi.jami.listener.XrefsChangeListener;
-import psidev.psi.mi.jami.model.*;
+import psidev.psi.mi.jami.model.Alias;
+import psidev.psi.mi.jami.model.Checksum;
+import psidev.psi.mi.jami.model.Xref;
 import psidev.psi.mi.jami.utils.comparator.alias.DefaultAliasComparator;
 import psidev.psi.mi.jami.utils.comparator.checksum.DefaultChecksumComparator;
 import psidev.psi.mi.jami.utils.comparator.xref.DefaultExternalIdentifierComparator;
@@ -28,51 +27,6 @@ public class EnricherUtils {
     public static final String NEW_LINE = "\n";
     public static final String NEW_EVENT = "\t";
     public static final String BLANK_SPACE = "-";
-
-    /**
-     * Adds the FeatureEnricher as a listener of the ProteinEnricher.
-     *
-     * If the ProteinEnricher already has a listener, it is checked to see if it is the featureEnricher.
-     * If so, no changes need to be made.
-     * If it is not the featureEnricher, it is checked to see if it is a listenerManager,
-     * if so, the feature enricher is added to the manager.
-     * If not, a new manager is created and both listeners are added.
-     *
-     * @param featureEnricher   The feature enricher to be added to the protein enricher
-     * @param proteinEnricher   The protein enricher to have a feature enricher
-     */
-    public static void linkFeatureEnricherToProteinEnricher(
-            FeatureEnricher featureEnricher , ProteinEnricher proteinEnricher ){
-        // Do nothing if either is null
-        if(featureEnricher == null || proteinEnricher == null ) return;
-
-        // Do nothing if the feature enricher is already the listener
-        if(featureEnricher == proteinEnricher.getProteinEnricherListener()) return;
-
-        // Check the feature enricher listens
-        if(featureEnricher instanceof ProteinListeningFeatureEnricher){
-            // Cast the feature enricher
-            ProteinListeningFeatureEnricher listeningFeatureEnricher = (ProteinListeningFeatureEnricher) featureEnricher;
-
-            ProteinEnricherListener listener = proteinEnricher.getProteinEnricherListener();
-            if(listener != null ) {
-                // If the listener is a manger, add it to that
-                if(listener instanceof ProteinEnricherListenerManager){
-                    ProteinEnricherListenerManager manager = (ProteinEnricherListenerManager) listener;
-                    manager.addEnricherListener(listeningFeatureEnricher);
-                }
-                // Else create a new manager and add the current listener
-                else {
-                    proteinEnricher.setProteinEnricherListener(
-                            new ProteinEnricherListenerManager(
-                                    listener,
-                                    listeningFeatureEnricher ));
-                }
-            } else {
-                proteinEnricher.setProteinEnricherListener(listeningFeatureEnricher);
-            }
-        }
-    }
 
     /**
      * Takes two lists of Xrefs and produces a list of those to add and those to remove.
