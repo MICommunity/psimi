26a0f424866da12549a379e55ca706a34f8df118
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-10-02 12:51:00 +0000

    Updated listeners

--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/listener/ProteinChangeListener.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/listener/ProteinChangeListener.java
@@ -13,42 +13,6 @@ import psidev.psi.mi.jami.model.Protein;
 public interface ProteinChangeListener extends InteractorChangeListener<Protein>{
 
     /**
-     * Listen to the event where the uniprotkb of a protein has been changed.
-     * If oldUniprot is null, it means that a uniprot has been added to the protein.
-     * If the uniprotkb of the protein is null, it means that the uniprot of the protein has been removed
-     * @param protein
-     * @param oldUniprot
-     */
-    public void onUniprotKbUpdate(Protein protein, String oldUniprot);
-
-    /**
-     * Listen to the event where the refseq of a protein has been changed.
-     * If oldRefseq is null, it means that a refseq has been added to the protein.
-     * If the refseq of the protein is null, it means that the refseq of the protein has been removed
-     * @param protein
-     * @param oldRefseq
-     */
-    public void onRefseqUpdate(Protein protein, String oldRefseq);
-
-    /**
-     * Listen to the event where the gene name of a protein has been changed.
-     * If oldGeneName is null, it means that a gene name has been added to the protein.
-     * If the gene name of the protein is null, it means that the gene name of the protein has been removed
-     * @param protein
-     * @param oldGeneName
-     */
-    public void onGeneNameUpdate(Protein protein, String oldGeneName);
-
-    /**
-     * Listen to the event where the rogid of a protein has been changed.
-     * If oldRogid is null, it means that a rogid has been added to the protein.
-     * If the rogid of the protein is null, it means that the rogid of the protein has been removed
-     * @param protein
-     * @param oldRogid
-     */
-    public void onRogidUpdate(Protein protein, String oldRogid);
-
-    /**
      * Listen to the event where the sequence of a protein has been changed.
      * If oldSequence is null, it means that the sequence has been initialised.
      * If the sequence of the protein is null, it means that the sequence of the protein has been reset
--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/listener/impl/ProteinChangeLogger.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/listener/impl/ProteinChangeLogger.java
@@ -18,55 +18,6 @@ public class ProteinChangeLogger implements ProteinChangeListener {
 
     private static final Logger proteinChangeLogger = Logger.getLogger("ProteinChangeLogger");
 
-    public void onUniprotKbUpdate(Protein protein, String oldUniprot) {
-
-        if (oldUniprot == null){
-            proteinChangeLogger.log(Level.INFO, "The uniprotkb " + protein.getUniprotkb() + " has been added to the protein " + protein.toString());
-        }
-        else if (protein.getUniprotkb() == null){
-            proteinChangeLogger.log(Level.INFO, "The uniprotkb "+ oldUniprot+ " has been removed from the protein " + protein.toString());
-        }
-        else {
-            proteinChangeLogger.log(Level.INFO, "The uniprotkb "+oldUniprot+" has been updated with " + protein.getUniprotkb() + " in the protein " + protein.toString());
-        }
-    }
-
-    public void onRefseqUpdate(Protein protein, String oldRefseq) {
-        if (oldRefseq == null){
-            proteinChangeLogger.log(Level.INFO, "The refseq id " + protein.getRefseq() + " has been added to the protein " + protein.toString());
-        }
-        else if (protein.getRefseq() == null){
-            proteinChangeLogger.log(Level.INFO, "The refseq id "+oldRefseq+" has been removed from the protein " + protein.toString());
-        }
-        else {
-            proteinChangeLogger.log(Level.INFO, "The refseq id "+oldRefseq+" has been updated with " + protein.getRefseq() + " in the protein " + protein.toString());
-        }
-    }
-
-    public void onGeneNameUpdate(Protein protein, String oldGeneName) {
-        if (oldGeneName == null){
-            proteinChangeLogger.log(Level.INFO, "The gene name " + protein.getGeneName() + " has been added to the protein " + protein.toString());
-        }
-        else if (protein.getGeneName() == null){
-            proteinChangeLogger.log(Level.INFO, "The gene name "+oldGeneName+" has been removed from the protein " + protein.toString());
-        }
-        else {
-            proteinChangeLogger.log(Level.INFO, "The gene name "+oldGeneName+" has been updated with " + protein.getGeneName() + " in the protein " + protein.toString());
-        }
-    }
-
-    public void onRogidUpdate(Protein protein, String oldRogid) {
-        if (oldRogid == null){
-            proteinChangeLogger.log(Level.INFO, "The rogid " + protein.getRogid() + " has been added to the protein " + protein.toString());
-        }
-        else if (protein.getRogid() == null){
-            proteinChangeLogger.log(Level.INFO, "The rogid "+oldRogid+" has been removed from the protein " + protein.toString());
-        }
-        else {
-            proteinChangeLogger.log(Level.INFO, "The rogid "+oldRogid+" has been updated with " + protein.getRogid() + " in the protein " + protein.toString());
-        }
-    }
-
     public void onSequenceUpdate(Protein protein, String oldSequence) {
         if (oldSequence == null){
             proteinChangeLogger.log(Level.INFO, "The sequence has been initialised for the protein " + protein.toString());
