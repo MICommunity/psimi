37cd0b98f7e29ca557069f42ddf0176db2aa7e36
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-10-03 12:16:20 +0000

    updated different enrichers

--- trunk/psimi-enricher/src/main/java/psidev/psi/mi/enricher/batch/processor/InteractionEvidenceEnricherProcessor.java
+++ trunk/psimi-enricher/src/main/java/psidev/psi/mi/enricher/batch/processor/InteractionEvidenceEnricherProcessor.java
@@ -1,10 +1,22 @@
 package psidev.psi.mi.enricher.batch.processor;
 
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemStream;
+import org.springframework.batch.item.ItemStreamException;
+import org.springframework.core.io.Resource;
+import org.springframework.util.Assert;
+import psidev.psi.mi.jami.datasource.FileSourceContext;
 import psidev.psi.mi.jami.enricher.InteractionEnricher;
+import psidev.psi.mi.jami.enricher.exception.EnricherException;
 import psidev.psi.mi.jami.model.FeatureEvidence;
 import psidev.psi.mi.jami.model.InteractionEvidence;
 import psidev.psi.mi.jami.model.ParticipantEvidence;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
 /**
  * Spring batch processor that enriches the interaction evidence
@@ -14,8 +26,10 @@ import psidev.psi.mi.jami.model.ParticipantEvidence;
  * @since <pre>23/07/13</pre>
  */
 
-public class InteractionEvidenceEnricherProcessor implements ItemProcessor<InteractionEvidence, InteractionEvidence>{
+public class InteractionEvidenceEnricherProcessor implements ItemProcessor<InteractionEvidence, InteractionEvidence>, ItemStream{
 
+    private Resource errorResource;
+    private Writer errorWriter;
     private InteractionEnricher<InteractionEvidence, ParticipantEvidence, FeatureEvidence> interactionEnricher;
 
     public InteractionEvidence process(InteractionEvidence item) throws Exception {
@@ -27,13 +41,70 @@ public class InteractionEvidenceEnricherProcessor implements ItemProcessor<Inter
         }
 
         // enrich interaction
-        interactionEnricher.enrichInteraction(item);
-        // TODO - test try catch
+        try{
+            interactionEnricher.enrich(item);
+        }
+        catch (EnricherException e){
+            String source = item.toString();
+            if (item instanceof FileSourceContext){
+               FileSourceContext context = (FileSourceContext) item;
+                if (context.getSourceLocator() != null){
+                    source = context.getSourceLocator().toString();
+                }
+            }
+            errorWriter.write("Cannot enrich interaction " + source);
+        }
 
         return item;
     }
 
     public void setInteractionEnricher(InteractionEnricher<InteractionEvidence, ParticipantEvidence, FeatureEvidence> interactionEnricher) {
         this.interactionEnricher = interactionEnricher;
+    }
+
+    public Resource getErrorResource() {
+        return errorResource;
+    }
+
+    public void setErrorResource(Resource errorResource) {
+        this.errorResource = errorResource;
+    }
+
+    public void open(ExecutionContext executionContext) throws ItemStreamException {
+        Assert.notNull(executionContext, "ExecutionContext must not be null");
+
+        if (errorResource == null){
+            throw new IllegalStateException("Error resource must be provided. ");
+        }
+
+        File fileToRead = null;
+        try {
+            fileToRead = errorResource.getFile();
+
+            this.errorWriter = new FileWriter(fileToRead);
+        } catch (IOException e) {
+            throw new ItemStreamException("Error resource must be writable: "
+                    + errorResource, e);
+        }
+    }
+
+    public void update(ExecutionContext executionContext) throws ItemStreamException {
+        try {
+            errorWriter.flush();
+        } catch (IOException e) {
+            throw new ItemStreamException("Cannot flush Error resource: "
+                    + errorResource, e);
+        }
+    }
+
+    public void close() throws ItemStreamException {
+        if (errorWriter != null){
+            try {
+                errorWriter.close();
+            } catch (IOException e) {
+                throw new ItemStreamException("Error resource cannot be closed: "
+                        + errorResource, e);
+            }
+        }
     }
 }
