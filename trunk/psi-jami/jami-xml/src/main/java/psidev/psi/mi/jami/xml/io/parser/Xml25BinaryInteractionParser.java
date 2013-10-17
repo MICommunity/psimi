1f2ef8d8e0c4c28b6860da9614a29c1f83e69ecb
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-10-17 09:45:50 +0000

    Added binary parsers

--- trunk/psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/io/parser/Xml25BinaryInteractionParser.java
+++ trunk/psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/io/parser/Xml25BinaryInteractionParser.java
@@ -1,12 +1,48 @@
 package psidev.psi.mi.jami.xml.io.parser;
 
+import psidev.psi.mi.jami.binary.BinaryInteraction;
+import psidev.psi.mi.jami.binary.expansion.ComplexExpansionMethod;
+import psidev.psi.mi.jami.binary.expansion.SpokeExpansion;
+import psidev.psi.mi.jami.model.Interaction;
+import psidev.psi.mi.jami.model.Participant;
+import psidev.psi.mi.jami.xml.extension.*;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLStreamException;
+import java.io.*;
+import java.net.URL;
+
 /**
- * TODO comment this
+ * Xml 2.5 parser creating binary interactions
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
  * @since <pre>16/10/13</pre>
  */
 
-public class Xml25BinaryInteractionParser {
+public class Xml25BinaryInteractionParser extends AbstractPsixml25BinaryParser<Interaction<? extends Participant>,BinaryInteraction>{
+    public Xml25BinaryInteractionParser(File file, ComplexExpansionMethod<Interaction<? extends Participant>, BinaryInteraction> expansionMethod) throws FileNotFoundException, XMLStreamException, JAXBException {
+        super(file, expansionMethod != null ? expansionMethod : new SpokeExpansion());
+    }
+
+    public Xml25BinaryInteractionParser(InputStream inputStream, ComplexExpansionMethod<Interaction<? extends Participant>, BinaryInteraction> expansionMethod) throws XMLStreamException, JAXBException {
+        super(inputStream, expansionMethod != null ? expansionMethod : new SpokeExpansion());
+    }
+
+    public Xml25BinaryInteractionParser(URL url, ComplexExpansionMethod<Interaction<? extends Participant>, BinaryInteraction> expansionMethod) throws IOException, XMLStreamException, JAXBException {
+        super(url, expansionMethod != null ? expansionMethod : new SpokeExpansion());
+    }
+
+    public Xml25BinaryInteractionParser(Reader reader, ComplexExpansionMethod<Interaction<? extends Participant>, BinaryInteraction> expansionMethod) throws XMLStreamException, JAXBException {
+        super(reader, expansionMethod != null ? expansionMethod : new SpokeExpansion());
+    }
+
+    @Override
+    protected Unmarshaller createJAXBUnmarshaller() throws JAXBException {
+        JAXBContext ctx = JAXBContext.newInstance(XmlBasicInteraction.class, XmlExperiment.class, XmlInteractor.class,
+                Availability.class, XmlSource.class, XmlAnnotation.class);
+        return ctx.createUnmarshaller();
+    }
 }
--- trunk/psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/io/parser/Xml25ModelledBinaryInteractionParser.java
+++ trunk/psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/io/parser/Xml25ModelledBinaryInteractionParser.java
@@ -1,12 +1,47 @@
 package psidev.psi.mi.jami.xml.io.parser;
 
+import psidev.psi.mi.jami.binary.ModelledBinaryInteraction;
+import psidev.psi.mi.jami.binary.expansion.ComplexExpansionMethod;
+import psidev.psi.mi.jami.binary.expansion.ModelledInteractionSpokeExpansion;
+import psidev.psi.mi.jami.model.ModelledInteraction;
+import psidev.psi.mi.jami.xml.extension.*;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLStreamException;
+import java.io.*;
+import java.net.URL;
+
 /**
- * TODO comment this
+ * Xml 2.5 parser creating modelled binary interaction
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
  * @since <pre>16/10/13</pre>
  */
 
-public class Xml25ModelledBinaryInteractionParser {
+public class Xml25ModelledBinaryInteractionParser extends AbstractPsixml25BinaryParser<ModelledInteraction, ModelledBinaryInteraction>{
+    public Xml25ModelledBinaryInteractionParser(File file, ComplexExpansionMethod<ModelledInteraction, ModelledBinaryInteraction> expansionMethod) throws FileNotFoundException, XMLStreamException, JAXBException {
+        super(file, expansionMethod != null ? expansionMethod : new ModelledInteractionSpokeExpansion());
+    }
+
+    public Xml25ModelledBinaryInteractionParser(InputStream inputStream, ComplexExpansionMethod<ModelledInteraction, ModelledBinaryInteraction> expansionMethod) throws XMLStreamException, JAXBException {
+        super(inputStream, expansionMethod != null ? expansionMethod : new ModelledInteractionSpokeExpansion());
+    }
+
+    public Xml25ModelledBinaryInteractionParser(URL url, ComplexExpansionMethod<ModelledInteraction, ModelledBinaryInteraction> expansionMethod) throws IOException, XMLStreamException, JAXBException {
+        super(url, expansionMethod != null ? expansionMethod : new ModelledInteractionSpokeExpansion());
+    }
+
+    public Xml25ModelledBinaryInteractionParser(Reader reader, ComplexExpansionMethod<ModelledInteraction, ModelledBinaryInteraction> expansionMethod) throws XMLStreamException, JAXBException {
+        super(reader, expansionMethod != null ? expansionMethod : new ModelledInteractionSpokeExpansion());
+    }
+
+    @Override
+    protected Unmarshaller createJAXBUnmarshaller() throws JAXBException {
+        JAXBContext ctx = JAXBContext.newInstance(XmlModelledInteraction.class, XmlExperiment.class, XmlInteractor.class,
+                Availability.class, XmlSource.class, XmlAnnotation.class);
+        return ctx.createUnmarshaller();
+    }
 }
