53292846aaa8076fbf26a45333df168d8fd2775b
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-10-29 09:28:24 +0000

    Updated source. There were no reasons why postal address was treated differently from url

--- psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/model/impl/DefaultSource.java
+++ psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/model/impl/DefaultSource.java
@@ -5,8 +5,6 @@ import psidev.psi.mi.jami.utils.AnnotationUtils;
 import psidev.psi.mi.jami.utils.CvTermUtils;
 import psidev.psi.mi.jami.utils.collection.AbstractListHavingProperties;
 
-import java.util.Collection;
-
 /**
  * Default implementation for Source
  *
@@ -20,7 +18,7 @@ import java.util.Collection;
 public class DefaultSource extends DefaultCvTerm implements Source {
 
     private Annotation url;
-    private String postalAddress;
+    private Annotation postalAddress;
     private Publication bibRef;
 
     public DefaultSource(String shortName) {
@@ -38,21 +36,21 @@ public class DefaultSource extends DefaultCvTerm implements Source {
     public DefaultSource(String shortName, String url, String address, Publication bibRef) {
         super(shortName);
         setUrl(url);
-        this.postalAddress = address;
+        setPostalAddress(address);
         this.bibRef = bibRef;
     }
 
     public DefaultSource(String shortName, Xref ontologyId, String url, String address, Publication bibRef) {
         super(shortName, ontologyId);
         setUrl(url);
-        this.postalAddress = address;
+        setPostalAddress(address);
         this.bibRef = bibRef;
     }
 
     public DefaultSource(String shortName, String fullName, Xref ontologyId, String url, String address, Publication bibRef) {
         super(shortName, fullName, ontologyId);
         setUrl(url);
-        this.postalAddress = address;
+        setPostalAddress(address);
         this.bibRef = bibRef;
     }
 
@@ -67,14 +65,14 @@ public class DefaultSource extends DefaultCvTerm implements Source {
     public DefaultSource(String shortName, String miId, String url, String address, Publication bibRef) {
         super(shortName, miId);
         setUrl(url);
-        this.postalAddress = address;
+        setPostalAddress(address);
         this.bibRef = bibRef;
     }
 
     public DefaultSource(String shortName, String fullName, String miId, String url, String address, Publication bibRef) {
         super(shortName, fullName, miId);
         setUrl(url);
-        this.postalAddress = address;
+        setPostalAddress(address);
         this.bibRef = bibRef;
     }
 
@@ -88,17 +86,17 @@ public class DefaultSource extends DefaultCvTerm implements Source {
     }
 
     public void setUrl(String url) {
-        Collection<Annotation> sourceAnnotationList = getAnnotations();
+        SourceAnnotationList sourceAnnotationList = (SourceAnnotationList)getAnnotations();
 
         // add new url if not null
         if (url != null){
-            CvTerm complexPhysicalProperties = CvTermUtils.createMICvTerm(Annotation.URL, Annotation.URL_MI);
+            CvTerm urlTopic = CvTermUtils.createMICvTerm(Annotation.URL, Annotation.URL_MI);
             // first remove old url if not null
             if (this.url != null){
-                sourceAnnotationList.remove(this.url);
+                sourceAnnotationList.removeOnly(this.url);
             }
-            this.url = new DefaultAnnotation(complexPhysicalProperties, url);
-            sourceAnnotationList.add(this.url);
+            this.url = new DefaultAnnotation(urlTopic, url);
+            sourceAnnotationList.addOnly(this.url);
         }
         // remove all url if the collection is not empty
         else if (!sourceAnnotationList.isEmpty()) {
@@ -108,11 +106,27 @@ public class DefaultSource extends DefaultCvTerm implements Source {
     }
 
     public String getPostalAddress() {
-        return this.postalAddress;
+        return this.postalAddress != null ? this.postalAddress.getValue() : null;
     }
 
     public void setPostalAddress(String address) {
-        this.postalAddress = address;
+        SourceAnnotationList sourceAnnotationList = (SourceAnnotationList)getAnnotations();
+
+        // add new url if not null
+        if (address != null){
+            CvTerm addressTopic = new DefaultCvTerm(Annotation.POSTAL_ADDRESS);
+            // first remove old url if not null
+            if (this.postalAddress != null){
+                sourceAnnotationList.removeOnly(this.postalAddress);
+            }
+            this.postalAddress = new DefaultAnnotation(addressTopic, address);
+            sourceAnnotationList.addOnly(this.postalAddress);
+        }
+        // remove all url if the collection is not empty
+        else if (!sourceAnnotationList.isEmpty()) {
+            AnnotationUtils.removeAllAnnotationsWithTopic(sourceAnnotationList, null, Annotation.POSTAL_ADDRESS);
+            this.postalAddress = null;
+        }
     }
 
     public Publication getPublication() {
@@ -127,16 +141,23 @@ public class DefaultSource extends DefaultCvTerm implements Source {
         if (url == null && AnnotationUtils.doesAnnotationHaveTopic(added, Annotation.URL_MI, Annotation.URL)){
             url = added;
         }
+        else if (postalAddress == null && AnnotationUtils.doesAnnotationHaveTopic(added, null, Annotation.POSTAL_ADDRESS)){
+            postalAddress = added;
+        }
     }
 
     protected void processRemovedAnnotationEvent(Annotation removed) {
         if (url != null && url.equals(removed)){
             url = null;
         }
+        else if (postalAddress != null && postalAddress.equals(removed)){
+            postalAddress = null;
+        }
     }
 
     protected void clearPropertiesLinkedToAnnotations() {
         url = null;
+        postalAddress = null;
     }
 
     private class SourceAnnotationList extends AbstractListHavingProperties<Annotation> {
