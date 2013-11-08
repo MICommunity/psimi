7f44606c823318c4b437159de0f6126f054b15bd
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-11-08 11:12:30 +0000

    Added more unit tests

--- trunk/psi-jami/jami-xml/src/test/java/psidev/psi/mi/jami/xml/io/parser/Xml25InteractionEvidenceParserTest.java
+++ trunk/psi-jami/jami-xml/src/test/java/psidev/psi/mi/jami/xml/io/parser/Xml25InteractionEvidenceParserTest.java
@@ -453,6 +453,129 @@ public class Xml25InteractionEvidenceParserTest {
     }
 
     @Test
+    public void test_read_valid_xml25_several_entries() throws PsiXmlParserException, JAXBException, XMLStreamException {
+        InputStream stream = Xml25InteractionEvidenceParserTest.class.getResourceAsStream("/samples/10049915-several-entries.xml");
+
+        PsiXml25Parser<InteractionEvidence> parser = new Xml25InteractionEvidenceParser(stream);
+
+        InteractionEvidence interaction = parser.parseNextInteraction();
+
+        Assert.assertNotNull(interaction);
+        Assert.assertEquals("rad53-dbf4", interaction.getShortName());
+
+        // experiment
+        Assert.assertNotNull(interaction.getExperiment());
+        ExtendedPsi25Experiment exp = (ExtendedPsi25Experiment)interaction.getExperiment();
+        Assert.assertEquals("dohrmann-1999-1", exp.getShortLabel());
+        // publication
+        Assert.assertNotNull(exp.getPublication());
+        Publication pub = exp.getPublication();
+        Assert.assertEquals("10049915", pub.getPubmedId());
+        // source
+        Assert.assertNotNull(pub.getSource());
+        Source source = pub.getSource();
+        Assert.assertEquals("MINT", source.getShortName());
+        Assert.assertEquals("MINT, Dpt of Biology, University of Rome Tor Vergata", source.getFullName());
+        Assert.assertEquals(0, source.getSynonyms().size());
+        Assert.assertEquals("MI:0471", source.getMIIdentifier());
+        Assert.assertEquals(2, source.getIdentifiers().size());
+        Assert.assertEquals(0, source.getXrefs().size());
+        Assert.assertEquals(2, source.getAnnotations().size());
+        Assert.assertEquals("http://mint.bio.uniroma2.it/mint", source.getUrl());
+
+        // participants
+        Assert.assertEquals(2, interaction.getParticipants().size());
+        Iterator<ParticipantEvidence> partIterator = interaction.getParticipants().iterator();
+        ParticipantEvidence p1 = partIterator.next();
+        Assert.assertEquals("rad53_yeast", ((ExtendedPsi25ParticipantEvidence) p1).getShortLabel());
+        // features
+        Assert.assertEquals(1, p1.getFeatures().size());
+        FeatureEvidence f = p1.getFeatures().iterator().next();
+        Assert.assertEquals("tagged molecule", f.getShortName());
+        // interactor
+        Assert.assertNotNull(p1.getInteractor());
+        Assert.assertEquals("rad53_yeast", p1.getInteractor().getShortName());
+        Assert.assertEquals("Serine/threonine-protein kinase RAD53", p1.getInteractor().getFullName());
+        Assert.assertEquals(8, p1.getInteractor().getAliases().size());
+        Assert.assertTrue(p1.getInteractor() instanceof Protein);
+        Protein prot = (Protein)p1.getInteractor();
+        Assert.assertEquals("RAD53", prot.getGeneName());
+        Assert.assertEquals(44, prot.getXrefs().size());
+        Assert.assertEquals(4, prot.getIdentifiers().size());
+        Assert.assertEquals("P22216", prot.getUniprotkb());
+        Assert.assertEquals("MENITQPTQQSTQATQRFLIEKFSQEQIGENIVCRVICTTGQIPIRDLSADISQVLKEKRSIKKVWTFGRNPACDYHLGNISRLSNKHFQILLGEDGNLLLNDISTNGTWLNGQKVEKNSNQLLSQGDEITVGVGVESDILSLVIFINDKFKQCLEQNKVDRIRSNLKNTSKIASPGLTSSTASSMVANKTGIFKDFSIIDEVVGQGAFATVKKAIERTTGKTFAVKIISKRKVIGNMDGVTRELEVLQKLNHPRIVRLKGFYEDTESYYMVMEFVSGGDLMDFVAAHGAVGEDAGREISRQILTAIKYIHSMGISHRDLKPDNILIEQDDPVLVKITDFGLAKVQGNGSFMKTFCGTLAYVAPEVIRGKDTSVSPDEYEERNEYSSLVDMWSMGCLVYVILTGHLPFSGSTQDQLYKQIGRGSYHEGPLKDFRISEEARDFIDSLLQVDPNNRSTAAKALNHPWIKMSPLGSQSYGDFSQISLSQSLSQQKLLENMDDAQYEFVKAQRKLQMEQQLQEQDQEDQDGKIQGFKIPAHAPIRYTQPKSIEAETREQKLLHSNNTENVKSSKKKGNGRFLTLKPLPDSIIQESLEIQQGVNPFFIGRSEDCNCKIEDNRLSRVHCFIFKKRHAVGKSMYESPAQGLDDIWYCHTGTNVSYLNNNRMIQGTKFLLQDGDEIKIIWDKNNKFVIGFKVEINDTTGLFNEGLGMLQEQRVVLKQTAEEKDLVKKLTQMMAAQRANQPSASSSSMSAKKPPVSDTNNNGNNSVLNDLVESPINANTGNILKRIHSVSLSQSQIDPSKKVKRAKLDQTSKGPENLQFS", prot.getSequence());
+        Assert.assertNotNull(prot.getOrganism());
+        Assert.assertEquals(559292, prot.getOrganism().getTaxId());
+
+        Assert.assertFalse(parser.hasFinished());
+
+        interaction = parser.parseNextInteraction();
+
+        Assert.assertNotNull(interaction);
+        Assert.assertEquals("trp-inad-2", interaction.getShortName());
+        Assert.assertEquals("IM-16552-12", interaction.getImexId());
+
+        // experiment
+        Assert.assertNotNull(interaction.getExperiment());
+        exp = (ExtendedPsi25Experiment)interaction.getExperiment();
+        Assert.assertEquals("liu-2011d-10", exp.getShortLabel());
+        // publication
+        Assert.assertNotNull(exp.getPublication());
+        pub = exp.getPublication();
+        Assert.assertEquals("21703451", pub.getPubmedId());
+        // source
+        Assert.assertNotNull(pub.getSource());
+        source = pub.getSource();
+        Assert.assertEquals("IntAct", source.getShortName());
+        Assert.assertEquals("European Bioinformatics Institute", source.getFullName());
+        Assert.assertEquals(0, source.getSynonyms().size());
+        Assert.assertEquals("MI:0469", source.getMIIdentifier());
+        Assert.assertEquals(2, source.getIdentifiers().size());
+        Assert.assertEquals(1, source.getXrefs().size());
+        Assert.assertEquals(3, source.getAnnotations().size());
+        Assert.assertEquals("http://www.ebi.ac.uk/", source.getUrl());
+        Assert.assertEquals("European Bioinformatics Institute; Wellcome Trust Genome Campus; Hinxton, Cambridge; CB10 1SD; United Kingdom", source.getPostalAddress());
+
+        // participants
+        Assert.assertEquals(2, interaction.getParticipants().size());
+        partIterator = interaction.getParticipants().iterator();
+         p1 = partIterator.next();
+        Assert.assertEquals("n/a", ((ExtendedPsi25ParticipantEvidence) p1).getShortLabel());
+        // features
+        Assert.assertEquals(2, p1.getFeatures().size());
+        f = p1.getFeatures().iterator().next();
+        Assert.assertEquals("gb1 tag region", f.getShortName());
+        // interactor
+        Assert.assertNotNull(p1.getInteractor());
+        Assert.assertEquals("trp_drome", p1.getInteractor().getShortName());
+        Assert.assertEquals("Transient receptor potential protein", p1.getInteractor().getFullName());
+        Assert.assertEquals(2, p1.getInteractor().getAliases().size());
+        Assert.assertTrue(p1.getInteractor() instanceof Protein);
+        prot = (Protein)p1.getInteractor();
+        Assert.assertEquals("trp", prot.getGeneName());
+        Assert.assertEquals(23, prot.getXrefs().size());
+        Assert.assertEquals(4, prot.getIdentifiers().size());
+        Assert.assertEquals("P19334", prot.getUniprotkb());
+        Assert.assertEquals("MGSNTESDAEKALGSRLDYDLMMAEEYILSDVEKNFILSCERGDLPGVKKILEEYQGTDKFNINCTDPMNRSALISAIENENFDLMVILLEHNIEVGDALLHAISEEYVEAVEELLQWEETNHKEGQPYSWEAVDRSKSTFTVDITPLILAAHRNNYEILKILLDRGATLPMPHDVKCGCDECVTSQMTDSLRHSQSRINAYRALSASSLIALSSRDPVLTAFQLSWELKRLQAMESEFRAEYTEMRQMVQDFGTSLLDHARTSMELEVMLNFNHEPSHDIWCLGQRQTLERLKLAIRYKQKTFVAHPNVQQLLAAIWYDGLPGFRRKQASQQLMDVVKLGCSFPIYSLKYILAPDSEGAKFMRKPFVKFITHSCSYMFFLMLLGAASLRVVQITFELLAFPWMLTMLEDWRKHERGSLPGPIELAIITYIMALIFEELKSLYSDGLFEYIMDLWNIVDYISNMFYVTWILCRATAWVIVHRDLWFRGIDPYFPREHWHPFDPMLLSEGAFAAGMVFSYLKLVHIFSINPHLGPLQVSLGRMIIDIIKFFFIYTLVLFAFGCGLNQLLWYYAELEKNKCYHLHPDVADFDDQEKACTIWRRFSNLFETSQSLFWASFGLVDLVSFDLAGIKSFTRFWALLMFGSYSVINIIVLLNMLIAMMSNSYQIISERADTEWKFARSQLWMSYFEDGGTIPPPFNLCPNMKMLRKTLGRKRPSRTKSFMRKSMERAQTLHDKVMKLLVRRYITAEQRRRDDYGITEDDIIEVRQDISSLRFELLEIFTNNNWDVPDIEKKSQGVARTTKGKVMERRILKDFQIGFVENLKQEMSESESGRDIFSSLAKVIGRKKTQKGDKDWNAIARKNTFASDPIGSKRSSMQRHSQRSLRRKIIEQANEGLQMNQTQLIEFNPNLGDVTRATRVAYVKFMRKKMAADEVSLADDEGAPNGEGEKKPLDASGSKKSITSGGTGGGASMLAAAALRASVKNVDEKSGADGKPGTMGKPTDDKKAGDDKDKQQPPKDSKPSAGGPKPGDQKPTPGAGAPKPQAAGTISKPGESQKKDAPAPPTKPGDTKPAAPKPGESAKPEAAAKKEESSKTEASKPAATNGAAKSAAPSAPSDAKPDSKLKPGAAGAPEATKATNGASKPDEKKSGPEEPKKAAGDSKPGDDAKDKDKKPGDDKDKKPGDDKDKKPADNNDKKPADDKDKKPGDDKDKKPGDDKDKKPSDDKDKKPADDKDKKPAAAPLKPAIKVGQSSAAAGGERGKSTVTGRMISGWL", prot.getSequence());
+        Assert.assertNotNull(prot.getOrganism());
+        Assert.assertEquals(7227, prot.getOrganism().getTaxId());
+
+        parser.close();
+    }
+
+    @Test
+    public void test_empty_file() throws JAXBException, XMLStreamException, PsiXmlParserException {
+        InputStream stream = Xml25InteractionEvidenceParserTest.class.getResourceAsStream("/samples/empty.xml");
+        PsiXml25Parser<InteractionEvidence> parser = new Xml25InteractionEvidenceParser(stream);
+
+        InteractionEvidence interaction = parser.parseNextInteraction();
+
+        // read first interaction
+        Assert.assertNull(interaction);
+        Assert.assertTrue(parser.hasFinished());
+    }
+
+    @Test
     public void test_read_valid_xml25_inferred() throws PsiXmlParserException, JAXBException, XMLStreamException {
         InputStream stream = Xml25InteractionEvidenceParserTest.class.getResourceAsStream("/samples/21703451.xml");
 
