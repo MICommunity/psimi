56eed62f681d6f44c4b0184a103cf2f21cab36cb
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-11-08 14:00:58 +0000

    Fixed a bug with toString methods

---
+++ trunk/psi-jami/jami-xml/src/test/java/psidev/psi/mi/jami/xml/io/iterator/Xml25ModelledBinaryInteractionIteratorTest.java
@@ -0,0 +1,82 @@
+package psidev.psi.mi.jami.xml.io.iterator;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import psidev.psi.mi.jami.binary.ModelledBinaryInteraction;
+import psidev.psi.mi.jami.binary.expansion.ComplexExpansionMethod;
+import psidev.psi.mi.jami.datasource.FileSourceContext;
+import psidev.psi.mi.jami.xml.exception.PsiXmlParserException;
+import psidev.psi.mi.jami.xml.io.parser.Xml25InteractionEvidenceParserTest;
+import psidev.psi.mi.jami.xml.io.parser.Xml25ModelledBinaryInteractionParser;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Iterator;
+
+/**
+ * Unit tester for Xml25ModelledBiaryInteractionIterator
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>08/11/13</pre>
+ */
+
+public class Xml25ModelledBinaryInteractionIteratorTest {
+
+    @Test
+    public void test_read_valid_xml25_inferred() throws PsiXmlParserException, JAXBException, XMLStreamException {
+        InputStream stream = Xml25InteractionEvidenceParserTest.class.getResourceAsStream("/samples/21703451.xml");
+
+        Iterator<ModelledBinaryInteraction> iterator = new Xml25ModelledBinaryInteractionIterator(new Xml25ModelledBinaryInteractionParser(stream));
+        int index = 0;
+        while(iterator.hasNext()){
+            ModelledBinaryInteraction interaction = iterator.next();
+            Assert.assertNotNull(interaction);
+            index++;
+        }
+
+        Assert.assertEquals(26, index);
+    }
+
+    @Test
+    public void test_read_valid_xml25_2() throws PsiXmlParserException, JAXBException, XMLStreamException, IOException {
+        InputStream stream = new URL("ftp://ftp.ebi.ac.uk/pub/databases/intact/current/psi25/pmid/2011/19536198_gong-2009-1_01.xml").openStream();
+
+        System.out.println("Start"+System.currentTimeMillis());
+        Iterator<ModelledBinaryInteraction> iterator = new Xml25ModelledBinaryInteractionIterator(new Xml25ModelledBinaryInteractionParser(stream));
+        int index = 0;
+        while(iterator.hasNext()){
+            ModelledBinaryInteraction interaction = iterator.next();
+            Assert.assertNotNull(interaction);
+            Assert.assertNotNull(((FileSourceContext)interaction).getSourceLocator());
+            index++;
+        }
+        System.out.println("End"+System.currentTimeMillis());
+
+        Assert.assertEquals(2000,index);
+    }
+
+    @Test
+    public void test_read_valid_xml25_nary() throws PsiXmlParserException, JAXBException, XMLStreamException {
+        InputStream stream = Xml25InteractionEvidenceParserTest.class.getResourceAsStream("/samples/15144954.xml");
+
+        Iterator<ModelledBinaryInteraction> iterator = new Xml25ModelledBinaryInteractionIterator(new Xml25ModelledBinaryInteractionParser(stream));
+        int index = 0;
+        int numberOfExpanded=0;
+        while(iterator.hasNext()){
+            ModelledBinaryInteraction interaction = iterator.next();
+            Assert.assertNotNull(interaction);
+            Assert.assertNotNull(((FileSourceContext)interaction).getSourceLocator());
+            if (interaction.getComplexExpansion() != null && ComplexExpansionMethod.SPOKE_EXPANSION_MI.equals(interaction.getComplexExpansion().getMIIdentifier())){
+                numberOfExpanded++;
+            }
+            index++;
+        }
+
+        Assert.assertEquals(15, index);
+        Assert.assertEquals(5, numberOfExpanded);
+    }
+}
