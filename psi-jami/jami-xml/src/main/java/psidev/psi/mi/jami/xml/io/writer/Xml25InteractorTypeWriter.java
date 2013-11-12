9aa7751c527d6f616a724f64a8dda8e227afa600
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-11-12 15:51:45 +0000

    Added interactor writer

---
+++ psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/io/writer/Xml25InteractorTypeWriter.java
@@ -0,0 +1,35 @@
+package psidev.psi.mi.jami.xml.io.writer;
+
+import org.codehaus.stax2.XMLStreamWriter2;
+import psidev.psi.mi.jami.model.Alias;
+import psidev.psi.mi.jami.model.CvTerm;
+
+import javax.xml.stream.XMLStreamException;
+
+/**
+ * Xml25 writer for interactor type
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>12/11/13</pre>
+ */
+
+public class Xml25InteractorTypeWriter extends AbstractXml25CvTermWriter{
+    public Xml25InteractorTypeWriter(XMLStreamWriter2 writer) {
+        super(writer);
+    }
+
+    public Xml25InteractorTypeWriter(XMLStreamWriter2 writer, PsiXml25ElementWriter<Alias> aliasWriter, PsiXml25XrefWriter primaryRefWriter, PsiXml25XrefWriter secondaryRefWriter) {
+        super(writer, aliasWriter, primaryRefWriter, secondaryRefWriter);
+    }
+
+    @Override
+    protected void writeStartCvTerm() throws XMLStreamException {
+        getStreamWriter().writeStartElement("interactorType");
+    }
+
+    @Override
+    protected void writeOtherProperties(CvTerm term) throws XMLStreamException {
+        // nothing to do
+    }
+}
