78ad6f429f211080082ee8f603bfe8e3faf2ceac
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-11-14 16:03:50 +0000

    Added some algorithm to retrieve the clique graph of binding sites using Bron-Kerbosch clique detection algorithm

--- psi-jami/jami-core/src/test/java/psidev/psi/mi/jami/analysis/graph/BindingSiteCliqueFinderTest.java
+++ psi-jami/jami-core/src/test/java/psidev/psi/mi/jami/analysis/graph/BindingSiteCliqueFinderTest.java
@@ -67,4 +67,32 @@ public class BindingSiteCliqueFinderTest {
         Assert.assertTrue(set1.contains(f1) && set1.contains(f2) && set1.contains(f3));
         Assert.assertTrue(set2.contains(f4) && set1.contains(f3));
     }
+
+    @Test
+    public void test_empty(){
+        // create small interaction with 4 participants and 4 different features
+        // no inferred interactions because no linked features
+        Feature f1 = new DefaultFeature();
+        Feature f2 = new DefaultFeature();
+        Feature f3 = new DefaultFeature();
+        Feature f4 = new DefaultFeature();
+        Interaction interaction = new DefaultInteraction();
+        Participant p1 = new DefaultParticipant(new DefaultProtein("p1"));
+        Participant p2 = new DefaultParticipant(new DefaultProtein("p2"));
+        Participant p3 = new DefaultParticipant(new DefaultProtein("p3"));
+        Participant p4 = new DefaultParticipant(new DefaultProtein("p4"));
+        p1.addFeature(f1);
+        p2.addFeature(f2);
+        p3.addFeature(f3);
+        p4.addFeature(f4);
+        interaction.addParticipant(p1);
+        interaction.addParticipant(p2);
+        interaction.addParticipant(p3);
+        interaction.addParticipant(p4);
+
+        BindingSiteCliqueFinder<Interaction,Feature> cliqueFinder = new BindingSiteCliqueFinder<Interaction, Feature>(interaction);
+        Collection<Set<Feature>> inferredInteractions = cliqueFinder.getAllMaximalCliques();
+
+        Assert.assertEquals(0, inferredInteractions.size());
+    }
 }
