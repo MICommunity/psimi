009b7d781275fac42088c10b1952913fa1a97387
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-11-18 09:50:10 +0000

    Added extended basic interaction writers

---
+++ trunk/psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/io/writer/elements/impl/extended/CompactXml25BasicInteractionWriter.java
@@ -0,0 +1,73 @@
+package psidev.psi.mi.jami.xml.io.writer.elements.impl.extended;
+
+import org.codehaus.stax2.XMLStreamWriter2;
+import psidev.psi.mi.jami.model.*;
+import psidev.psi.mi.jami.xml.PsiXml25ObjectIndex;
+import psidev.psi.mi.jami.xml.extension.InferredInteraction;
+import psidev.psi.mi.jami.xml.io.writer.elements.CompactPsiXml25ElementWriter;
+import psidev.psi.mi.jami.xml.io.writer.elements.PsiXml25ElementWriter;
+import psidev.psi.mi.jami.xml.io.writer.elements.PsiXml25XrefWriter;
+import psidev.psi.mi.jami.xml.io.writer.elements.impl.CompactXml25ParticipantWriter;
+
+import javax.xml.stream.XMLStreamException;
+
+/**
+ * Compact XML 2.5 writer for a basic interaction (ignore experimental details) which has a fullname
+ * and aliases in addition to a shortname
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>15/11/13</pre>
+ */
+
+public class CompactXml25BasicInteractionWriter extends AbstractXml25InteractionWriter<Interaction,Participant> implements CompactPsiXml25ElementWriter<Interaction> {
+
+    public CompactXml25BasicInteractionWriter(XMLStreamWriter2 writer, PsiXml25ObjectIndex objectIndex) {
+        super(writer, objectIndex, new CompactXml25ParticipantWriter(writer, objectIndex));
+    }
+
+    public CompactXml25BasicInteractionWriter(XMLStreamWriter2 writer, PsiXml25ObjectIndex objectIndex,
+                                              PsiXml25XrefWriter primaryRefWriter, PsiXml25XrefWriter secondaryRefWriter,
+                                              CompactPsiXml25ElementWriter<Participant> participantWriter, PsiXml25ElementWriter<CvTerm> interactionTypeWriter,
+                                              PsiXml25ElementWriter<Annotation> attributeWriter, PsiXml25ElementWriter<Alias> aliasWriter,
+                                              PsiXml25ElementWriter<InferredInteraction> inferredInteractionWriter) {
+        super(writer, objectIndex, primaryRefWriter, secondaryRefWriter,
+                participantWriter != null ? participantWriter : new CompactXml25ParticipantWriter(writer, objectIndex), interactionTypeWriter, attributeWriter, aliasWriter,
+                inferredInteractionWriter);
+    }
+
+    @Override
+    protected void writeAvailability(Interaction object) {
+        // nothing to do
+    }
+
+    @Override
+    protected void writeExperiments(Interaction object) throws XMLStreamException {
+        writeExperimentRef();
+    }
+
+    @Override
+    protected void writeOtherAttributes(Interaction object) {
+        // nothing to do
+    }
+
+    @Override
+    protected void writeModelled(Interaction object) {
+        // do nothing
+    }
+
+    @Override
+    protected void writeParameters(Interaction object) {
+        // nothing to do
+    }
+
+    @Override
+    protected void writeConfidences(Interaction object) {
+        // nothing to do
+    }
+
+    @Override
+    protected void writeNegative(Interaction object) {
+        // nothing to do
+    }
+}
