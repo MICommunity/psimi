3824321ba548e7ccee865b72b9b197550583edf9
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-11-18 15:14:56 +0000

    Added interaction evidence writers

---
+++ trunk/psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/PsiXml25Type.java
@@ -0,0 +1,14 @@
+package psidev.psi.mi.jami.xml;
+
+/**
+ * The type of PSI-XML 2.5.
+ * It can be compact or expanded
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>18/11/13</pre>
+ */
+
+public enum PsiXml25Type {
+    compact, expanded
+}
