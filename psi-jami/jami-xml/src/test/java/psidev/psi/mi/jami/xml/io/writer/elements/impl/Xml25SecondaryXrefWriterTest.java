3a42ee72b796c1b0e73d2e48edde490c612e2f95
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-11-21 09:11:39 +0000

    Added unit tests for primaryRef and secondaryRef

--- psi-jami/jami-xml/src/test/java/psidev/psi/mi/jami/xml/io/writer/elements/impl/Xml25SecondaryXrefWriterTest.java
+++ psi-jami/jami-xml/src/test/java/psidev/psi/mi/jami/xml/io/writer/elements/impl/Xml25SecondaryXrefWriterTest.java
@@ -15,26 +15,26 @@ import java.io.IOException;
 import java.io.StringWriter;
 
 /**
- * Unit tester for Xml25PrimaryRefWriter
+ * Unit tester for Xml25SecondaryXrefWriter
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
  * @since <pre>20/11/13</pre>
  */
 
-public class Xml25PrimaryXrefWriterTest {
+public class Xml25SecondaryXrefWriterTest {
 
-    private String xref_no_database_ac ="<primaryRef db=\"uniprotkb\" id=\"P12345\" refType=\"identity\" refTypeAc=\"MI:0356\"/>";
-    private String xref_no_reftype ="<primaryRef db=\"uniprotkb\" dbAc=\"MI:0486\" id=\"P12345\"/>";
-    private String xref_no_reftype_ac ="<primaryRef db=\"uniprotkb\" dbAc=\"MI:0486\" id=\"P12345\" refType=\"identity\"/>";
-    private String xref ="<primaryRef db=\"uniprotkb\" dbAc=\"MI:0486\" id=\"P12345\" refType=\"identity\" refTypeAc=\"MI:0356\"/>";
-    private String xref_version ="<primaryRef db=\"uniprotkb\" dbAc=\"MI:0486\" id=\"P12345\" version=\"2.4\" refType=\"identity\" refTypeAc=\"MI:0356\"/>";
+    private String xref_no_database_ac ="<secondaryRef db=\"uniprotkb\" id=\"P12345\" refType=\"identity\" refTypeAc=\"MI:0356\"/>";
+    private String xref_no_reftype ="<secondaryRef db=\"uniprotkb\" dbAc=\"MI:0486\" id=\"P12345\"/>";
+    private String xref_no_reftype_ac ="<secondaryRef db=\"uniprotkb\" dbAc=\"MI:0486\" id=\"P12345\" refType=\"identity\"/>";
+    private String xref ="<secondaryRef db=\"uniprotkb\" dbAc=\"MI:0486\" id=\"P12345\" refType=\"identity\" refTypeAc=\"MI:0356\"/>";
+    private String xref_version ="<secondaryRef db=\"uniprotkb\" dbAc=\"MI:0486\" id=\"P12345\" version=\"2.4\" refType=\"identity\" refTypeAc=\"MI:0356\"/>";
     private StringWriter output;
     private XMLStreamWriter2 streamWriter;
     @Test
     public void test_write_xref_null() throws XMLStreamException, IOException {
 
-        Xml25PrimaryXrefWriter writer = new Xml25PrimaryXrefWriter(createStreamWriter());
+        Xml25SecondaryXrefWriter writer = new Xml25SecondaryXrefWriter(createStreamWriter());
         writer.write(null);
         streamWriter.flush();
 
@@ -45,7 +45,7 @@ public class Xml25PrimaryXrefWriterTest {
     public void test_write_xref_do_database_ac() throws XMLStreamException, IOException {
         Xref ref = new DefaultXref(new DefaultCvTerm(Xref.UNIPROTKB), "P12345", CvTermUtils.createIdentityQualifier());
 
-        Xml25PrimaryXrefWriter writer = new Xml25PrimaryXrefWriter(createStreamWriter());
+        Xml25SecondaryXrefWriter writer = new Xml25SecondaryXrefWriter(createStreamWriter());
         writer.write(ref);
         streamWriter.flush();
 
@@ -56,7 +56,7 @@ public class Xml25PrimaryXrefWriterTest {
     public void test_write_xref_no_reftype() throws XMLStreamException, IOException {
         Xref ref = new DefaultXref(new DefaultCvTerm(Xref.UNIPROTKB, Xref.UNIPROTKB_MI), "P12345");
 
-        Xml25PrimaryXrefWriter writer = new Xml25PrimaryXrefWriter(createStreamWriter());
+        Xml25SecondaryXrefWriter writer = new Xml25SecondaryXrefWriter(createStreamWriter());
         writer.write(ref);
         streamWriter.flush();
 
@@ -67,7 +67,7 @@ public class Xml25PrimaryXrefWriterTest {
     public void test_write_xref_no_reftype_ac() throws XMLStreamException, IOException {
         Xref ref = new DefaultXref(new DefaultCvTerm(Xref.UNIPROTKB, Xref.UNIPROTKB_MI), "P12345", new DefaultCvTerm(Xref.IDENTITY));
 
-        Xml25PrimaryXrefWriter writer = new Xml25PrimaryXrefWriter(createStreamWriter());
+        Xml25SecondaryXrefWriter writer = new Xml25SecondaryXrefWriter(createStreamWriter());
         writer.write(ref);
         streamWriter.flush();
 
@@ -78,7 +78,7 @@ public class Xml25PrimaryXrefWriterTest {
     public void test_write_xref() throws XMLStreamException, IOException {
         Xref ref = new DefaultXref(new DefaultCvTerm(Xref.UNIPROTKB, Xref.UNIPROTKB_MI), "P12345", CvTermUtils.createIdentityQualifier());
 
-        Xml25PrimaryXrefWriter writer = new Xml25PrimaryXrefWriter(createStreamWriter());
+        Xml25SecondaryXrefWriter writer = new Xml25SecondaryXrefWriter(createStreamWriter());
         writer.write(ref);
         streamWriter.flush();
 
@@ -89,7 +89,7 @@ public class Xml25PrimaryXrefWriterTest {
     public void test_write_xref_version() throws XMLStreamException, IOException {
         Xref ref = new DefaultXref(new DefaultCvTerm(Xref.UNIPROTKB, Xref.UNIPROTKB_MI), "P12345", "2.4", CvTermUtils.createIdentityQualifier());
 
-        Xml25PrimaryXrefWriter writer = new Xml25PrimaryXrefWriter(createStreamWriter());
+        Xml25SecondaryXrefWriter writer = new Xml25SecondaryXrefWriter(createStreamWriter());
         writer.write(ref);
         streamWriter.flush();
 
---
+++ psi-jami/jami-xml/src/test/java/psidev/psi/mi/jami/xml/io/writer/elements/impl/extended/Xml25SecondaryXrefWriterTest.java
@@ -0,0 +1,87 @@
+package psidev.psi.mi.jami.xml.io.writer.elements.impl.extended;
+
+import junit.framework.Assert;
+import org.codehaus.stax2.XMLOutputFactory2;
+import org.codehaus.stax2.XMLStreamWriter2;
+import org.junit.Test;
+import psidev.psi.mi.jami.model.Xref;
+import psidev.psi.mi.jami.model.impl.DefaultAnnotation;
+import psidev.psi.mi.jami.model.impl.DefaultCvTerm;
+import psidev.psi.mi.jami.utils.CvTermUtils;
+import psidev.psi.mi.jami.xml.extension.XmlXref;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+import java.io.StringWriter;
+
+/**
+ * Unit tester for Xml25SecondaryXrefWriter
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>20/11/13</pre>
+ */
+
+public class Xml25SecondaryXrefWriterTest {
+
+    private String xref ="<secondaryRef db=\"uniprotkb\" dbAc=\"MI:0486\" id=\"P12345\" refType=\"identity\" refTypeAc=\"MI:0356\"/>";
+    private String xref_secondary ="<secondaryRef db=\"uniprotkb\" dbAc=\"MI:0486\" id=\"P12345\" refType=\"identity\" refTypeAc=\"MI:0356\" secondary=\"P12346\"/>";
+    private String xref_attributes ="<secondaryRef db=\"uniprotkb\" dbAc=\"MI:0486\" id=\"P12345\" refType=\"identity\" refTypeAc=\"MI:0356\" secondary=\"P12346\">\n" +
+            "<attributeList>\n" +
+            "<attribute name=\"caution\">test caution</attribute>\n" +
+            "</attributeList>\n" +
+            "</secondaryRef>";
+    private StringWriter output;
+    private XMLStreamWriter2 streamWriter;
+    @Test
+    public void test_write_xref_null() throws XMLStreamException, IOException {
+
+        Xml25SecondaryXrefWriter writer = new Xml25SecondaryXrefWriter(createStreamWriter());
+        writer.write(null);
+        streamWriter.flush();
+
+        Assert.assertEquals("", output.toString());
+    }
+
+    @Test
+    public void test_write_xref() throws XMLStreamException, IOException {
+        XmlXref ref = new XmlXref(new DefaultCvTerm(Xref.UNIPROTKB, Xref.UNIPROTKB_MI), "P12345", CvTermUtils.createIdentityQualifier());
+
+        Xml25SecondaryXrefWriter writer = new Xml25SecondaryXrefWriter(createStreamWriter());
+        writer.write(ref);
+        streamWriter.flush();
+
+        Assert.assertEquals(xref, output.toString());
+    }
+
+    @Test
+    public void test_write_xref_secondary() throws XMLStreamException, IOException {
+        XmlXref ref = new XmlXref(new DefaultCvTerm(Xref.UNIPROTKB, Xref.UNIPROTKB_MI), "P12345", CvTermUtils.createIdentityQualifier());
+        ref.setSecondary("P12346");
+        Xml25SecondaryXrefWriter writer = new Xml25SecondaryXrefWriter(createStreamWriter());
+        writer.write(ref);
+        streamWriter.flush();
+
+        Assert.assertEquals(xref_secondary, output.toString());
+    }
+
+    @Test
+    public void test_write_xref_attributes() throws XMLStreamException, IOException {
+        XmlXref ref = new XmlXref(new DefaultCvTerm(Xref.UNIPROTKB, Xref.UNIPROTKB_MI), "P12345", CvTermUtils.createIdentityQualifier());
+        ref.setSecondary("P12346");
+        ref.getAnnotations().add(new DefaultAnnotation(new DefaultCvTerm("caution"), "test caution"));
+        Xml25SecondaryXrefWriter writer = new Xml25SecondaryXrefWriter(createStreamWriter());
+        writer.write(ref);
+        streamWriter.flush();
+
+        Assert.assertEquals(xref_attributes, output.toString());
+    }
+
+    private XMLStreamWriter2 createStreamWriter() throws XMLStreamException {
+        XMLOutputFactory outputFactory = XMLOutputFactory2.newInstance();
+        this.output = new StringWriter();
+        this.streamWriter = (XMLStreamWriter2)outputFactory.createXMLStreamWriter(this.output);
+        return this.streamWriter;
+    }
+}
