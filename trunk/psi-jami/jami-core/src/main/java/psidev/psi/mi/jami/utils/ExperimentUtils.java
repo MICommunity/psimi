41160617262b74091f604075bccba9ae2fa2d867
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-12-04 12:50:42 +0000

    Added utility methods for experiment and publication

--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/ExperimentUtils.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/ExperimentUtils.java
@@ -1,7 +1,10 @@
 package psidev.psi.mi.jami.utils;
 
-import psidev.psi.mi.jami.model.Experiment;
+import psidev.psi.mi.jami.model.*;
 import psidev.psi.mi.jami.model.impl.DefaultExperiment;
+import psidev.psi.mi.jami.model.impl.DefaultNamedExperiment;
+import psidev.psi.mi.jami.model.impl.DefaultOrganism;
+import psidev.psi.mi.jami.model.impl.DefaultPublication;
 
 /**
  * Factory for experiment
@@ -19,5 +22,84 @@ public class ExperimentUtils {
 
     public static Experiment createExperimentWithoutPublication(){
         return new DefaultExperiment(null, CvTermUtils.createUnspecifiedMethod());
+    }
+
+    public static Experiment createBasicExperimentForModelledInteractions(){
+        return new DefaultExperiment(PublicationUtils.createBasicPublicationForModelledInteractions(), CvTermUtils.createUnspecifiedMethod());
+    }
+
+    public static Experiment createBasicExperimentForComplexes(){
+        return new DefaultExperiment(PublicationUtils.createBasicPublicationForModelledInteractions(), CvTermUtils.createMICvTerm(Experiment.INFERRED_BY_CURATOR, Experiment.INFERRED_BY_CURATOR_MI));
+    }
+
+    public static Experiment createBasicExperimentForComplexes(int taxid){
+        Experiment experiment = new DefaultExperiment(PublicationUtils.createBasicPublicationForModelledInteractions(), CvTermUtils.createMICvTerm(Experiment.INFERRED_BY_CURATOR,Experiment.INFERRED_BY_CURATOR_MI));
+        experiment.setHostOrganism(new DefaultOrganism(taxid));
+        return experiment;
+    }
+
+    public static Experiment createBasicExperimentForComplexes(int taxid, String name){
+        Experiment experiment = new DefaultExperiment(PublicationUtils.createBasicPublicationForModelledInteractions(), CvTermUtils.createMICvTerm(Experiment.INFERRED_BY_CURATOR,Experiment.INFERRED_BY_CURATOR_MI));
+        experiment.setHostOrganism(new DefaultOrganism(taxid, name));
+        return experiment;
+    }
+    
+    public static String getPubmedId(Experiment exp){
+       if (exp == null){
+          return null; 
+       }
+        else if (exp.getPublication() == null){
+           return null; 
+       }
+        else{
+           return exp.getPublication().getPubmedId();
+       }
+    }
+
+    public static String getDoiId(Experiment exp){
+        if (exp == null){
+            return null;
+        }
+        else if (exp.getPublication() == null){
+            return null;
+        }
+        else{
+            return exp.getPublication().getDoi();
+        }
+    }
+
+    public static Xref getPubmedReference(Experiment exp){
+        if (exp == null){
+            return null;
+        }
+        else{
+            return PublicationUtils.getPubmedReference(exp.getPublication());
+        }
+    }
+
+    public static Xref getDoiReference(Experiment exp){
+        if (exp == null){
+            return null;
+        }
+        else{
+            return PublicationUtils.getDoiReference(exp.getPublication());
+        }
+    }
+
+    public static Experiment createExperiment(String pubmedId, CvTerm interactionDetectionMethod,
+                                              Organism hostOrganism) {
+        Experiment experiment = new DefaultExperiment(new DefaultPublication(pubmedId), interactionDetectionMethod);
+        experiment.setHostOrganism(hostOrganism);
+
+        return experiment;
+    }
+
+    public static NamedExperiment createExperiment(String name, String pubmedId, CvTerm interactionDetectionMethod,
+                                              Organism hostOrganism) {
+        NamedExperiment experiment = new DefaultNamedExperiment(new DefaultPublication(pubmedId), interactionDetectionMethod);
+        experiment.setHostOrganism(hostOrganism);
+        experiment.setShortName(name);
+
+        return experiment;
     }
 }
--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/PublicationUtils.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/PublicationUtils.java
@@ -1,6 +1,7 @@
 package psidev.psi.mi.jami.utils;
 
 import psidev.psi.mi.jami.model.Publication;
+import psidev.psi.mi.jami.model.Xref;
 import psidev.psi.mi.jami.model.impl.DefaultPublication;
 
 import java.util.Date;
@@ -16,7 +17,42 @@ import java.util.Date;
 public class PublicationUtils {
     
     public static Publication createUnknownBasicPublication(){
+        return new DefaultPublication("Mock publication for experiments that do not have a publication reference", (String)null, (Date)null);
+    }
 
-        return new DefaultPublication("unknown", "unknown", (Date)null);
+    public static Publication createBasicPublicationForModelledInteractions(){
+        return new DefaultPublication("Mock publication and experiment for modelled interactions that are not interaction evidences.", (String)null, (Date)null);
+    }
+
+    public static Publication createPublicationPubmed(String pubmed){
+        return new DefaultPublication(pubmed);
+    }
+
+    public static Publication createPublicationDoi(String doi){
+        return new DefaultPublication(XrefUtils.createDoiIdentity(doi));
+    }
+
+    public static Xref getPubmedReference(Publication pub){
+        if (pub == null){
+           return null;
+        }
+        else if (pub.getPubmedId() == null){
+            return null;
+        }
+        else{
+            return XrefUtils.collectFirstIdentifierWithDatabaseAndId(pub.getIdentifiers(), Xref.PUBMED_MI, Xref.PUBMED, pub.getPubmedId());
+        }
+    }
+
+    public static Xref getDoiReference(Publication pub){
+        if (pub == null){
+            return null;
+        }
+        else if (pub.getDoi() == null){
+            return null;
+        }
+        else{
+            return XrefUtils.collectFirstIdentifierWithDatabaseAndId(pub.getIdentifiers(), Xref.DOI_MI, Xref.DOI, pub.getDoi());
+        }
     }
 }
