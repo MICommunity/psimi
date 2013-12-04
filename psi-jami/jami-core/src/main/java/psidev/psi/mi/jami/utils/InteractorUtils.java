78159f7c638e4bdc9ff2bad270103ef815ae3006
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-12-04 13:27:11 +0000

    Added utility methods for interactors

--- psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/InteractorUtils.java
+++ psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/InteractorUtils.java
@@ -1,8 +1,7 @@
 package psidev.psi.mi.jami.utils;
 
-import psidev.psi.mi.jami.model.CvTerm;
-import psidev.psi.mi.jami.model.Interactor;
-import psidev.psi.mi.jami.model.impl.DefaultInteractor;
+import psidev.psi.mi.jami.model.*;
+import psidev.psi.mi.jami.model.impl.*;
 
 /**
  * Factory for interactors
@@ -27,25 +26,79 @@ public class InteractorUtils {
      */
     public static boolean doesInteractorHaveType(Interactor interactor, String typeId, String typeName){
 
-        if (interactor == null || (typeName == null && typeId == null)){
+        if (interactor == null){
             return false;
         }
+        return CvTermUtils.isCvTerm(interactor.getInteractorType(), typeId, typeName);
+    }
 
-        CvTerm type = interactor.getInteractorType();
-        if (type == null){
-            return false;
-        }
+    public static Protein createProteinUniprot(String name, String uniprot){
+        return new DefaultProtein(name, XrefUtils.createUniprotIdentity(uniprot));
+    }
 
-        // we can compare identifiers
-        if (typeId != null && type.getMIIdentifier() != null){
-            // we have the same type id
-            return type.getMIIdentifier().equals(typeId);
-        }
-        // we need to compare type names
-        else if (typeName != null) {
-            return typeName.equalsIgnoreCase(type.getShortName());
-        }
+    public static Protein createProteinRefseq(String name, String refseq){
+        return new DefaultProtein(name, XrefUtils.createRefseqIdentity(refseq));
+    }
 
-        return false;
+    public static Protein createProteinGeneName(String name, String geneName){
+        Protein protein = new DefaultProtein(name);
+        protein.setGeneName(geneName);
+        return protein;
+    }
+
+    public static Protein createProteinRogid(String name, String rogid){
+        Protein protein = new DefaultProtein(name);
+        protein.setRogid(rogid);
+        return protein;
+    }
+
+    public static BioactiveEntity createBioactiveEntityChebi(String name, String chebi){
+        return new DefaultBioactiveEntity(name, XrefUtils.createChebiIdentity(chebi));
+    }
+
+    public static BioactiveEntity createBioactiveEntitySmile(String name, String smile){
+        BioactiveEntity entity = new DefaultBioactiveEntity(name);
+        entity.setSmile(smile);
+        return entity;
+    }
+
+    public static BioactiveEntity createBioactiveEntityStandardInchi(String name, String standard){
+        BioactiveEntity entity = new DefaultBioactiveEntity(name);
+        entity.setStandardInchi(standard);
+        return entity;
+    }
+
+    public static BioactiveEntity createBioactiveEntityStandardInchiKey(String name, String key){
+        BioactiveEntity entity = new DefaultBioactiveEntity(name);
+        entity.setStandardInchiKey(key);
+        return entity;
+    }
+
+    public static NucleicAcid createNucleicAcidDdbjEmblGenbank(String name, String identity){
+        return new DefaultNucleicAcid(name, XrefUtils.createDdbjEmblGenbankIdentity(identity));
+    }
+
+    public static NucleicAcid createNucleicAcidRefseq(String name, String identity){
+        return new DefaultNucleicAcid(name, XrefUtils.createRefseqIdentity(identity));
+    }
+
+    public static Gene createGeneRefseq(String name, String identity){
+        return new DefaultGene(name, XrefUtils.createRefseqIdentity(identity));
+    }
+
+    public static Gene createGeneEnsembl(String name, String identity){
+        return new DefaultGene(name, XrefUtils.createEnsemblIdentity(identity));
+    }
+    public static Gene createGeneEnsemblGenomes(String name, String identity){
+        return new DefaultGene(name, XrefUtils.createEnsemblGenomesIdentity(identity));
+    }
+    public static Gene createGeneEntrezGene(String name, String identity){
+        return new DefaultGene(name, XrefUtils.createEntrezGeneIdIdentity(identity));
+    }
+
+    public static Polymer createPolymer(String name, String sequence){
+        Polymer polymer = new DefaultPolymer(name);
+        polymer.setSequence(sequence);
+        return polymer;
     }
 }
