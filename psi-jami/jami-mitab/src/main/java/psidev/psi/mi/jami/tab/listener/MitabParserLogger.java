7a0c5dbdd6b8e2a6f3c33d4ec23f206609e653ab
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2013-12-13 09:05:47 +0000

    Updated MITAB listeners

--- psi-jami/jami-mitab/src/main/java/psidev/psi/mi/jami/tab/listener/MitabParserLogger.java
+++ psi-jami/jami-mitab/src/main/java/psidev/psi/mi/jami/tab/listener/MitabParserLogger.java
@@ -1,7 +1,7 @@
 package psidev.psi.mi.jami.tab.listener;
 
 import psidev.psi.mi.jami.datasource.FileSourceContext;
-import psidev.psi.mi.jami.model.*;
+import psidev.psi.mi.jami.listener.impl.MIFileParserLogger;
 import psidev.psi.mi.jami.tab.extension.*;
 
 import java.util.Collection;
@@ -16,15 +16,11 @@ import java.util.logging.Logger;
  * @since <pre>24/06/13</pre>
  */
 
-public class MitabParserLogger implements MitabParserListener{
+public class MitabParserLogger extends MIFileParserLogger implements MitabParserListener{
     private static final Logger logger = Logger.getLogger("MitabParserLogger");
 
     public void onTextFoundInIdentifier(MitabXref xref) {
         logger.log(Level.WARNING, "Some text has been found in one of the identifier (" + xref.toString() + "). In unique identifiers and alternative identifiers columns, we expect db:id and not db:id(text).");
-    }
-
-    public void onMissingCvTermName(CvTerm term, FileSourceContext context, String message) {
-        logger.log(Level.WARNING, message + "(" + context.toString() + ")");
     }
 
     public void onTextFoundInConfidence(MitabConfidence conf) {
@@ -35,24 +31,12 @@ public class MitabParserLogger implements MitabParserListener{
         logger.log(Level.WARNING, "The complex expansion does not have a MI identifier. (" + expansion.toString() + "). In the complex expansion column, we expect to find db:id(name) and not just a name. It is however taken into account for backward compatibility");
     }
 
-    public void onInvalidSyntax(FileSourceContext context, Exception e) {
-        logger.log(Level.SEVERE, "Invalid syntax (" + context.toString() + "): ", e);
-    }
-
-    public void onSyntaxWarning(FileSourceContext context, String message) {
-        logger.log(Level.WARNING, message + "(" + context.toString() + ")");
-    }
-
     public void onSeveralUniqueIdentifiers(Collection<MitabXref> ids) {
         logger.log(Level.WARNING, ids.size() + " unique identifiers have been found in (" + ids.iterator().next().toString() + "). Only one unique identifier is expected.");
     }
 
     public void onEmptyUniqueIdentifiers(int line, int column, int mitabColumn) {
         logger.log(Level.WARNING, "No unique identifier have been found in (line: " + line + ", column: " + column + ", mitab column: " + mitabColumn + "). An unique identifier is expected.");
-    }
-
-    public void onMissingInteractorName(Interactor interactor, FileSourceContext context) {
-        logger.log(Level.WARNING, "No aliases have been found in (" + context.toString() + "). At least one alias is expected for each interactor.");
     }
 
     public void onSeveralCvTermsFound(Collection<MitabCvTerm> terms, FileSourceContext context, String message) {
@@ -67,72 +51,8 @@ public class MitabParserLogger implements MitabParserListener{
         logger.log(Level.WARNING, organisms.size() + " organisms have been found in (" + organisms.iterator().next().toString() + "). Only one organism is expected per interactor so only the first organism will be loaded.");
     }
 
-    public void onParticipantWithoutInteractor(Participant participant, FileSourceContext context) {
-        logger.log(Level.SEVERE, "No interactor details have been found in participant (" + context.toString() + "). At least one unique identifier and one alias is expected to describe the interactor. The interactor will be loaded as unknown interactor.");
-    }
-
     public void onSeveralStoichiometryFound(Collection<MitabStoichiometry> stoichiometry) {
         logger.log(Level.WARNING, stoichiometry.size() + " stoichiometry values have been found in (" + stoichiometry.iterator().next().toString() + "). Only one stoichiometry is expected per participant so only the first stoichiometry will be loaded.");
-    }
-
-    public void onInteractionWithoutParticipants(Interaction interaction, FileSourceContext context) {
-        logger.log(Level.SEVERE, "No participant and interactor details have been found in interaction (" + context.toString() + "). At least one participant is expected per interaction. The interaction will be empty.");
-    }
-
-    public void onInvalidOrganismTaxid(String taxid, FileSourceContext context) {
-        logger.log(Level.WARNING, "The organism taxid "+taxid+" ("+context.toString()+") is not valid. Only positive integers, -1, -2, -3, -4 and -5 are recognized. The organism will be loaded as -3(unknown)");
-    }
-
-    public void onMissingParameterValue(FileSourceContext context) {
-        logger.log(Level.WARNING, "The parameter is not valid as it does not have a value ("+context.toString()+"). The parameter will be loaded with a value of 0.");
-    }
-
-    public void onMissingParameterType(FileSourceContext context) {
-        logger.log(Level.WARNING, "The parameter is not valid as it does not have a parameter type ("+context.toString()+"). The parameter will be loaded with a type 'unknown'.");
-    }
-
-    public void onMissingConfidenceValue(FileSourceContext context) {
-        logger.log(Level.WARNING, "The confidence is not valid as it does not have a value ("+context.toString()+"). The confidence will be loaded with a value 'unknown'.");
-    }
-
-    public void onMissingConfidenceType(FileSourceContext context) {
-        logger.log(Level.WARNING, "The confidence is not valid as it does not have a confidence type ("+context.toString()+"). The confidence will be loaded with a type 'unknown'.");
-    }
-
-    public void onMissingChecksumValue(FileSourceContext context) {
-        logger.log(Level.WARNING, "The checksum is not valid as it does not have a value ("+context.toString()+"). The checksum will be loaded with a value 'unknown'.");
-    }
-
-    public void onMissingChecksumMethod(FileSourceContext context) {
-        logger.log(Level.WARNING, "The checksum is not valid as it does not have a checksum method ("+context.toString()+"). The checksum will be loaded with a method 'unknown'.");
-    }
-
-    public void onInvalidPosition(String message, FileSourceContext context) {
-        logger.log(Level.WARNING, "The position is not valid ("+context.toString()+"): "+message+". The position will be loaded as undetermined position.");
-    }
-
-    public void onInvalidRange(String message, FileSourceContext context) {
-        logger.log(Level.WARNING, "The range is not valid ("+context.toString()+"): "+ message+". The range will be loaded as undetermined range.");
-    }
-
-    public void onInvalidStoichiometry(String message, FileSourceContext context) {
-        logger.log(Level.WARNING, "The stoichiometry is not valid ("+context.toString()+"): "+message+". The stoichiometry will be loaded with a value of 0.");
-    }
-
-    public void onXrefWithoutDatabase(FileSourceContext context) {
-        logger.log(Level.WARNING, "The xref is not valid as it does not have a database ("+context.toString()+"). The xref will be loaded with a database 'unknown'.");
-    }
-
-    public void onXrefWithoutId(FileSourceContext context) {
-        logger.log(Level.WARNING, "The xref is not valid as it does not have an id ("+context.toString()+"). The xref will be loaded with an id 'unknown'.");
-    }
-
-    public void onAnnotationWithoutTopic(FileSourceContext context) {
-        logger.log(Level.WARNING, "The annotation is not valid as it does not have a topic ("+context.toString()+"). The annotation will be loaded with a topic 'unknown'.");
-    }
-
-    public void onAliasWithoutName(FileSourceContext context) {
-        logger.log(Level.WARNING, "The alias is not valid as it does not have a name ("+context.toString()+"). The alias will be loaded with a name 'unspecified'.");
     }
 
     public void onSeveralFirstAuthorFound(Collection<MitabAuthor> authors) {
