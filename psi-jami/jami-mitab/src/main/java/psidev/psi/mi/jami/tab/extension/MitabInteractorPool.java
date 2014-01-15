e651c739777f4667c764db0f38c7e5b422fcaf79
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-01-15 15:41:23 +0000

    Updated interactor pool and entity pool

--- psi-jami/jami-mitab/src/main/java/psidev/psi/mi/jami/tab/extension/MitabInteractorPool.java
+++ psi-jami/jami-mitab/src/main/java/psidev/psi/mi/jami/tab/extension/MitabInteractorPool.java
@@ -5,7 +5,7 @@ import psidev.psi.mi.jami.datasource.FileSourceLocator;
 import psidev.psi.mi.jami.model.CvTerm;
 import psidev.psi.mi.jami.model.Organism;
 import psidev.psi.mi.jami.model.Xref;
-import psidev.psi.mi.jami.model.impl.DefaultInteractorSet;
+import psidev.psi.mi.jami.model.impl.DefaultInteractorPool;
 
 /**
  * The mitab extension for InteractorSet
@@ -15,71 +15,71 @@ import psidev.psi.mi.jami.model.impl.DefaultInteractorSet;
  * @since <pre>18/06/13</pre>
  */
 
-public class MitabInteractorSet extends DefaultInteractorSet implements FileSourceContext{
+public class MitabInteractorPool extends DefaultInteractorPool implements FileSourceContext{
 
     private FileSourceLocator sourceLocator;
 
-    public MitabInteractorSet(String name, CvTerm type) {
+    public MitabInteractorPool(String name, CvTerm type) {
         super(name, type);
     }
 
-    public MitabInteractorSet(String name, String fullName, CvTerm type) {
+    public MitabInteractorPool(String name, String fullName, CvTerm type) {
         super(name, fullName, type);
     }
 
-    public MitabInteractorSet(String name, CvTerm type, Organism organism) {
+    public MitabInteractorPool(String name, CvTerm type, Organism organism) {
         super(name, type, organism);
     }
 
-    public MitabInteractorSet(String name, String fullName, CvTerm type, Organism organism) {
+    public MitabInteractorPool(String name, String fullName, CvTerm type, Organism organism) {
         super(name, fullName, type, organism);
     }
 
-    public MitabInteractorSet(String name, CvTerm type, Xref uniqueId) {
+    public MitabInteractorPool(String name, CvTerm type, Xref uniqueId) {
         super(name, type, uniqueId);
     }
 
-    public MitabInteractorSet(String name, String fullName, CvTerm type, Xref uniqueId) {
+    public MitabInteractorPool(String name, String fullName, CvTerm type, Xref uniqueId) {
         super(name, fullName, type, uniqueId);
     }
 
-    public MitabInteractorSet(String name, CvTerm type, Organism organism, Xref uniqueId) {
+    public MitabInteractorPool(String name, CvTerm type, Organism organism, Xref uniqueId) {
         super(name, type, organism, uniqueId);
     }
 
-    public MitabInteractorSet(String name, String fullName, CvTerm type, Organism organism, Xref uniqueId) {
+    public MitabInteractorPool(String name, String fullName, CvTerm type, Organism organism, Xref uniqueId) {
         super(name, fullName, type, organism, uniqueId);
     }
 
-    public MitabInteractorSet(String name) {
+    public MitabInteractorPool(String name) {
         super(name);
     }
 
-    public MitabInteractorSet(String name, String fullName) {
+    public MitabInteractorPool(String name, String fullName) {
         super(name, fullName);
     }
 
-    public MitabInteractorSet(String name, Organism organism) {
+    public MitabInteractorPool(String name, Organism organism) {
         super(name, organism);
     }
 
-    public MitabInteractorSet(String name, String fullName, Organism organism) {
+    public MitabInteractorPool(String name, String fullName, Organism organism) {
         super(name, fullName, organism);
     }
 
-    public MitabInteractorSet(String name, Xref uniqueId) {
+    public MitabInteractorPool(String name, Xref uniqueId) {
         super(name, uniqueId);
     }
 
-    public MitabInteractorSet(String name, String fullName, Xref uniqueId) {
+    public MitabInteractorPool(String name, String fullName, Xref uniqueId) {
         super(name, fullName, uniqueId);
     }
 
-    public MitabInteractorSet(String name, Organism organism, Xref uniqueId) {
+    public MitabInteractorPool(String name, Organism organism, Xref uniqueId) {
         super(name, organism, uniqueId);
     }
 
-    public MitabInteractorSet(String name, String fullName, Organism organism, Xref uniqueId) {
+    public MitabInteractorPool(String name, String fullName, Organism organism, Xref uniqueId) {
         super(name, fullName, organism, uniqueId);
     }
 
