151e6b015b8236bc6d3bfd47c4919cdcec04cf38
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-01-29 13:46:37 +0000

    Added SourceChangeListener

--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/listener/impl/OntologyTermChangeLogger.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/listener/impl/OntologyTermChangeLogger.java
@@ -19,18 +19,30 @@ public class OntologyTermChangeLogger extends CvTermChangeLogger implements Onto
     private static final Logger ontologyTermChangeLogger = Logger.getLogger("OntologyTermChangeLogger");
 
     public void onAddedParent(OntologyTerm cv, OntologyTerm added) {
-        ontologyTermChangeLogger.log(Level.INFO, "The parent " + added.toString() + " has been added to the cv term " + cv.toString());
+        ontologyTermChangeLogger.log(Level.INFO, "The parent " + added.toString() + " has been added to the ontology term " + cv.toString());
     }
 
     public void onRemovedParent(OntologyTerm cv, OntologyTerm removed) {
-        ontologyTermChangeLogger.log(Level.INFO, "The parent " + removed.toString() + " has been removed from the cv term " + cv.toString());
+        ontologyTermChangeLogger.log(Level.INFO, "The parent " + removed.toString() + " has been removed from the ontology term " + cv.toString());
     }
 
     public void onAddedChild(OntologyTerm cv, OntologyTerm added) {
-        ontologyTermChangeLogger.log(Level.INFO, "The child " + added.toString() + " has been added to the cv term " + cv.toString());
+        ontologyTermChangeLogger.log(Level.INFO, "The child " + added.toString() + " has been added to the ontology term " + cv.toString());
     }
 
     public void onRemovedChild(OntologyTerm cv, OntologyTerm removed) {
-        ontologyTermChangeLogger.log(Level.INFO, "The child " + removed.toString() + " has been removed from the cv term " + cv.toString());
+        ontologyTermChangeLogger.log(Level.INFO, "The child " + removed.toString() + " has been removed from the ontology term " + cv.toString());
+    }
+
+    public void onDefinitionUpdate(OntologyTerm cv, String oldDef) {
+        if (oldDef == null){
+            ontologyTermChangeLogger.log(Level.INFO, "The definition " + cv.getDefinition() + " has been added to the ontology term " + cv.toString());
+        }
+        else if (cv.getDefinition() == null){
+            ontologyTermChangeLogger.log(Level.INFO, "The definition "+ oldDef+ " has been removed from the ontology term " + cv.toString());
+        }
+        else {
+            ontologyTermChangeLogger.log(Level.INFO, "The definition "+oldDef+" has been updated with " + cv.getDefinition() + " in the ontology term " + cv.toString());
+        }
     }
 }
