4c8ff3b2940b947c8c104b4c52372332cd357340
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-02-04 16:11:39 +0000

    Updated publication listener

--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/listener/PublicationChangeListener.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/listener/PublicationChangeListener.java
@@ -1,6 +1,7 @@
 package psidev.psi.mi.jami.listener;
 
 import psidev.psi.mi.jami.model.Publication;
+import psidev.psi.mi.jami.model.Source;
 import psidev.psi.mi.jami.model.Xref;
 
 import java.util.Date;
@@ -17,7 +18,7 @@ public interface PublicationChangeListener extends IdentifiersChangeListener<Pub
 
     public void onDoiUpdate(Publication publication , String oldDoi);
 
-    public void onImexIdentifierAdded(Publication publication, Xref addedXref);
+    public void onImexIdentifierUpdate(Publication publication, Xref addedXref);
 
     public void onTitleUpdated(Publication publication , String oldTitle);
 
@@ -30,4 +31,5 @@ public interface PublicationChangeListener extends IdentifiersChangeListener<Pub
 
     public void onReleaseDateUpdated(Publication publication , Date oldDate);
 
+    public void onSourceUpdated(Publication publication , Source oldSource);
 }
