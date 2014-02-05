be896db0a05a5820cd6337c3dbeada5398aec397
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-02-05 15:04:56 +0000

    updated cloner for experiments

--- psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/clone/ExperimentCloner.java
+++ psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/clone/ExperimentCloner.java
@@ -1,10 +1,6 @@
 package psidev.psi.mi.jami.utils.clone;
 
 import psidev.psi.mi.jami.model.Experiment;
-import psidev.psi.mi.jami.model.VariableParameter;
-import psidev.psi.mi.jami.model.VariableParameterValue;
-import psidev.psi.mi.jami.model.impl.DefaultVariableParameter;
-import psidev.psi.mi.jami.model.impl.DefaultVariableParameterValue;
 
 /**
  * Utility class for cloning experiments
@@ -38,15 +34,35 @@ public class ExperimentCloner {
             target.getConfidences().clear();
             target.getConfidences().addAll(source.getConfidences());
             target.getVariableParameters().clear();
+            target.addAllVariableParameters(source.getVariableParameters());
+        }
+    }
 
-            // for variableParameters, need to create new ones
-            for (VariableParameter p : source.getVariableParameters()){
-                VariableParameter newParam = new DefaultVariableParameter(p.getDescription(), target, p.getUnit());
-                for (VariableParameterValue v : p.getVariableValues()){
-                    newParam.getVariableValues().add(new DefaultVariableParameterValue(v.getValue(), newParam, v.getOrder()));
-                }
-                target.getVariableParameters().add(newParam);
-            }
+    /***
+     * This method will copy properties of experiment source in experiment target and will override all the other properties of Target experiment.
+     * This method will also move interaction evidences from source to target
+     * It will set the publication but will not add this experiment to the list of experiments of the publication reported in the source experiment
+     * It will fully clone all the VariableParameters and set their experiment to the current experiment target
+     * @param source
+     * @param target
+     */
+    public static void copyAndOverrideExperimentPropertiesAndInteractionEvidences(Experiment source, Experiment target){
+        if (source != null && target != null){
+            target.setHostOrganism(source.getHostOrganism());
+            target.setInteractionDetectionMethod(source.getInteractionDetectionMethod());
+            target.setPublication(source.getPublication());
+
+            // copy collections
+            target.getAnnotations().clear();
+            target.getAnnotations().addAll(source.getAnnotations());
+            target.getXrefs().clear();
+            target.getXrefs().addAll(source.getXrefs());
+            target.getConfidences().clear();
+            target.getConfidences().addAll(source.getConfidences());
+            target.getVariableParameters().clear();
+            target.addAllVariableParameters(source.getVariableParameters());
+            target.getInteractionEvidences().clear();
+            target.addAllInteractionEvidences(source.getInteractionEvidences());
         }
     }
 }
