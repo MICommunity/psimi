37aba8c92301238c99c67bf8569743d9322bfd1e
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-02-14 14:40:39 +0000

    Added source fetcher

--- trunk/psi-jami/jami-bridges/jami-ols/src/main/java/psidev/psi/mi/jami/bridges/ols/OlsSourceFetcher.java
+++ trunk/psi-jami/jami-bridges/jami-ols/src/main/java/psidev/psi/mi/jami/bridges/ols/OlsSourceFetcher.java
@@ -2,6 +2,7 @@ package psidev.psi.mi.jami.bridges.ols;
 
 
 import psidev.psi.mi.jami.bridges.exception.BridgeFailedException;
+import psidev.psi.mi.jami.bridges.fetcher.SourceFetcher;
 import psidev.psi.mi.jami.model.Source;
 import psidev.psi.mi.jami.model.Xref;
 
@@ -11,7 +12,7 @@ import psidev.psi.mi.jami.model.Xref;
  * @author Gabriel Aldam (galdam@ebi.ac.uk)
  * @since 08/05/13
  */
-public class OlsSourceFetcher extends AbstractOlsFetcher<Source>{
+public class OlsSourceFetcher extends AbstractOlsFetcher<Source> implements SourceFetcher{
 
 
     public OlsSourceFetcher() throws BridgeFailedException {
