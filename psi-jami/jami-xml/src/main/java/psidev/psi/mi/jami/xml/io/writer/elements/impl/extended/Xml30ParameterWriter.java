c1c5c799c31526f13fe53d9d6cf23379749b5639
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-04-25 10:16:10 +0000

    Added unit tests for paramater writer

---
+++ psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/io/writer/elements/impl/extended/Xml30ParameterWriter.java
@@ -0,0 +1,36 @@
+package psidev.psi.mi.jami.xml.io.writer.elements.impl.extended;
+
+import psidev.psi.mi.jami.model.Parameter;
+import psidev.psi.mi.jami.xml.cache.PsiXmlObjectCache;
+import psidev.psi.mi.jami.xml.model.extension.XmlParameter;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+/**
+ * XML 3.0 writer of a parameter
+ *
+ * @author Marine Dumousseau (marine@ebi.ac.uk)
+ * @version $Id$
+ * @since <pre>14/11/13</pre>
+ */
+
+public class Xml30ParameterWriter extends psidev.psi.mi.jami.xml.io.writer.elements.impl.Xml30ParameterWriter {
+
+    public Xml30ParameterWriter(XMLStreamWriter writer, PsiXmlObjectCache objectIndex){
+        super(writer, objectIndex);
+    }
+
+    @Override
+    protected void writeOtherProperties(Parameter object) throws XMLStreamException {
+        // write experiment Ref
+        if (object instanceof XmlParameter){
+            XmlParameter xmlParameter = (XmlParameter)object;
+            if (xmlParameter.getExperiment() != null){
+                getStreamWriter().writeStartElement("experimentRef");
+                getStreamWriter().writeCharacters(Integer.toString(getObjectIndex().extractIdForExperiment(xmlParameter.getExperiment())));
+                getStreamWriter().writeEndElement();
+            }
+        }
+    }
+}
