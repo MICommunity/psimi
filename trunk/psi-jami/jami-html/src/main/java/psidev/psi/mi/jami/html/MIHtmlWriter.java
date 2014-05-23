f6cc1b7dde9c57a0f2f3d77267d8f0173eefc3f6
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-05-23 15:52:53 +0000

    Remapped options to new jami core options

--- trunk/psi-jami/jami-html/src/main/java/psidev/psi/mi/jami/html/MIHtmlWriter.java
+++ trunk/psi-jami/jami-html/src/main/java/psidev/psi/mi/jami/html/MIHtmlWriter.java
@@ -1,6 +1,6 @@
 package psidev.psi.mi.jami.html;
 
-import psidev.psi.mi.jami.factory.InteractionWriterOptions;
+import psidev.psi.mi.jami.factory.options.InteractionWriterOptions;
 import psidev.psi.mi.jami.exception.MIIOException;
 import psidev.psi.mi.jami.model.*;
 
