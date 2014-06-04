4a9825fb2c71bc04d35156dfa626e9f8c3d124bf
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-06-04 10:52:23 +0000

    Added unit tests for cooperative effects

--- trunk/psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/io/writer/elements/impl/compact/xml30/XmlModelledBinaryInteractionWriter.java
+++ trunk/psi-jami/jami-xml/src/main/java/psidev/psi/mi/jami/xml/io/writer/elements/impl/compact/xml30/XmlModelledBinaryInteractionWriter.java
@@ -58,10 +58,6 @@ public class XmlModelledBinaryInteractionWriter extends psidev.psi.mi.jami.xml.i
             for (Object c : object.getChecksums()){
                 getChecksumWriter().write((Checksum)c);
             }
-            // can only write the FIRST cooperative effect
-            if (!object.getCooperativeEffects().isEmpty()){
-                writeCooperativeEffect(object, false);
-            }
             // write complex expansion if any
             if (object.getComplexExpansion() != null){
                 super.writeAttribute(object.getComplexExpansion().getShortName(), object.getComplexExpansion().getMIIdentifier());
@@ -76,30 +72,9 @@ public class XmlModelledBinaryInteractionWriter extends psidev.psi.mi.jami.xml.i
             for (Object c : object.getChecksums()){
                 getChecksumWriter().write((Checksum)c);
             }
-            // can only write the FIRST cooperative effect
-            if (!object.getCooperativeEffects().isEmpty()){
-                writeCooperativeEffect(object, false);
-            }
             // write complex expansion if any
             if (object.getComplexExpansion() != null){
                 super.writeAttribute(object.getComplexExpansion().getShortName(), object.getComplexExpansion().getMIIdentifier());
-            }
-            // write end attributeList
-            getStreamWriter().writeEndElement();
-        }
-        // can only write the FIRST cooperative effect
-        else if (!object.getCooperativeEffects().isEmpty()){
-            // write start attribute list
-            getStreamWriter().writeStartElement("attributeList");
-            writeCooperativeEffect(object, false);
-            // write complex expansion if any
-
-            if (object.getComplexExpansion() != null){
-                // write start attribute list
-                getStreamWriter().writeStartElement("attributeList");
-                super.writeAttribute(object.getComplexExpansion().getShortName(), object.getComplexExpansion().getMIIdentifier());
-                // write end attributeList
-                getStreamWriter().writeEndElement();
             }
             // write end attributeList
             getStreamWriter().writeEndElement();
