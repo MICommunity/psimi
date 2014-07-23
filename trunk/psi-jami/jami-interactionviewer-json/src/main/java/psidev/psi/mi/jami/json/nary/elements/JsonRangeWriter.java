8e297f1585ade67e1eca045062bd40a35e5fc565
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-07-23 12:39:46 +0000

    Added json element writer for ranges

--- trunk/psi-jami/jami-interactionviewer-json/src/main/java/psidev/psi/mi/jami/json/nary/elements/JsonRangeWriter.java
+++ trunk/psi-jami/jami-interactionviewer-json/src/main/java/psidev/psi/mi/jami/json/nary/elements/JsonRangeWriter.java
@@ -1,9 +1,12 @@
 package psidev.psi.mi.jami.json.nary.elements;
 
+import psidev.psi.mi.jami.json.MIJsonUtils;
 import psidev.psi.mi.jami.model.Range;
+import psidev.psi.mi.jami.utils.RangeUtils;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Map;
 
 /**
  * Json writer for ranges
@@ -16,15 +19,37 @@ import java.io.Writer;
 public class JsonRangeWriter implements JsonElementWriter<Range>{
 
     private Writer writer;
+    private Map<String, Integer> processedInteractors;
 
-    public JsonRangeWriter(Writer writer){
+    public JsonRangeWriter(Writer writer, Map<String, Integer> processedInteractors){
         if (writer == null){
             throw new IllegalArgumentException("The json range writer needs a non null Writer");
         }
         this.writer = writer;
+        if (processedInteractors == null){
+            throw new IllegalArgumentException("The json range writer needs a non null map of processed interactors");
+        }
+        this.processedInteractors = processedInteractors;
     }
 
     public void write(Range object) throws IOException {
+        if (object.getParticipant() != null){
+            String[] extractedInteractorId =  MIJsonUtils.extractInteractorId(object.getParticipant().getInteractor().getPreferredIdentifier(), object.getParticipant().getInteractor());
+            String key = extractedInteractorId[0]+"_"+extractedInteractorId[1];
 
+            if (this.processedInteractors.containsKey(key)){
+                MIJsonUtils.writeStartObject(writer);
+                MIJsonUtils.writeProperty("pos", RangeUtils.convertRangeToString(object), writer);
+                MIJsonUtils.writeSeparator(writer);
+                MIJsonUtils.writeProperty("interactorRef", key, writer);
+                MIJsonUtils.writeEndObject(writer);
+            }
+            else{
+                MIJsonUtils.writePropertyValue(RangeUtils.convertRangeToString(object), writer);
+            }
+        }
+        else{
+            MIJsonUtils.writePropertyValue(RangeUtils.convertRangeToString(object), writer);
+        }
     }
 }
