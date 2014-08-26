9d93d3b79373d1883f3ba3f7bc2445135b7c6b3c
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-08-26 14:55:04 +0000

    Finished sources

--- trunk/psi-jami/jami-crosslink-csv/src/main/java/psidev/psi/mi/jami/crosslink/extension/datasource/CsvBinaryEvidenceStreamSource.java
+++ trunk/psi-jami/jami-crosslink-csv/src/main/java/psidev/psi/mi/jami/crosslink/extension/datasource/CsvBinaryEvidenceStreamSource.java
@@ -5,14 +5,14 @@ import psidev.psi.mi.jami.crosslink.io.parser.AbstractCsvInteractionEvidencePars
 import psidev.psi.mi.jami.crosslink.io.parser.CsvBinaryInteractionEvidenceParser;
 
 /**
- * CrossLink CVS source of binary interaction evidences
+ * CrossLink CVS stream source of binary interaction evidences
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
  * @since <pre>26/08/14</pre>
  */
 
-public class CvsBinaryEvidenceSource extends AbstractCsvSource<BinaryInteractionEvidence>{
+public class CsvBinaryEvidenceStreamSource extends AbstractCsvStreamSource<BinaryInteractionEvidence>{
     @Override
     protected AbstractCsvInteractionEvidenceParser<BinaryInteractionEvidence> instantiateLineParser() {
         return new CsvBinaryInteractionEvidenceParser();
--- trunk/psi-jami/jami-crosslink-csv/src/main/java/psidev/psi/mi/jami/crosslink/extension/datasource/CsvEvidenceStreamSource.java
+++ trunk/psi-jami/jami-crosslink-csv/src/main/java/psidev/psi/mi/jami/crosslink/extension/datasource/CsvEvidenceStreamSource.java
@@ -5,14 +5,15 @@ import psidev.psi.mi.jami.crosslink.io.parser.CsvInteractionEvidenceParser;
 import psidev.psi.mi.jami.model.InteractionEvidence;
 
 /**
- * CrossLink CVS source of interaction evidences
+ * CrossLink CVS stream source of interaction evidences
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
  * @since <pre>26/08/14</pre>
  */
 
-public class CvsEvidenceSource extends AbstractCsvSource<InteractionEvidence>{
+public class CsvEvidenceStreamSource extends AbstractCsvStreamSource<InteractionEvidence>{
+
     @Override
     protected AbstractCsvInteractionEvidenceParser<InteractionEvidence> instantiateLineParser() {
         return new CsvInteractionEvidenceParser();
