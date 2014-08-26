aa4c8a1668a1e4ee9407305e3ab61337b2b93c9e
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-08-26 14:55:04 +0000

    Finished sources

--- psi-jami/jami-crosslink-csv/src/main/java/psidev/psi/mi/jami/crosslink/extension/datasource/CsvBinaryEvidenceSource.java
+++ psi-jami/jami-crosslink-csv/src/main/java/psidev/psi/mi/jami/crosslink/extension/datasource/CsvBinaryEvidenceSource.java
@@ -5,14 +5,14 @@ import psidev.psi.mi.jami.crosslink.io.parser.AbstractCsvInteractionEvidencePars
 import psidev.psi.mi.jami.crosslink.io.parser.CsvBinaryInteractionEvidenceParser;
 
 /**
- * CrossLink CVS stream source of binary interaction evidences
+ * CrossLink CVS source of binary interaction evidences
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
  * @since <pre>26/08/14</pre>
  */
 
-public class CvsBinaryEvidenceStreamSource extends AbstractCsvStreamSource<BinaryInteractionEvidence>{
+public class CsvBinaryEvidenceSource extends AbstractCsvSource<BinaryInteractionEvidence>{
     @Override
     protected AbstractCsvInteractionEvidenceParser<BinaryInteractionEvidence> instantiateLineParser() {
         return new CsvBinaryInteractionEvidenceParser();
--- psi-jami/jami-crosslink-csv/src/main/java/psidev/psi/mi/jami/crosslink/extension/datasource/CsvEvidenceSource.java
+++ psi-jami/jami-crosslink-csv/src/main/java/psidev/psi/mi/jami/crosslink/extension/datasource/CsvEvidenceSource.java
@@ -5,15 +5,14 @@ import psidev.psi.mi.jami.crosslink.io.parser.CsvInteractionEvidenceParser;
 import psidev.psi.mi.jami.model.InteractionEvidence;
 
 /**
- * CrossLink CVS stream source of interaction evidences
+ * CrossLink CVS source of interaction evidences
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
  * @since <pre>26/08/14</pre>
  */
 
-public class CvsEvidenceStreamSource extends AbstractCsvStreamSource<InteractionEvidence>{
-
+public class CsvEvidenceSource extends AbstractCsvSource<InteractionEvidence>{
     @Override
     protected AbstractCsvInteractionEvidenceParser<InteractionEvidence> instantiateLineParser() {
         return new CsvInteractionEvidenceParser();
