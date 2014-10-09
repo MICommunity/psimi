cfe6c6cc913c8f2290c80fee4c5542d7b9e86121
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-10-09 18:03:57 +0000

    Updated fetcher to return list of root terms

--- trunk/psi-jami/jami-bridges/jami-obo/src/main/java/psidev/psi/mi/jami/bridges/obo/OboOntologyTermFetcher.java
+++ trunk/psi-jami/jami-bridges/jami-obo/src/main/java/psidev/psi/mi/jami/bridges/obo/OboOntologyTermFetcher.java
@@ -4,6 +4,11 @@ import psidev.psi.mi.jami.bridges.fetcher.OntologyTermFetcher;
 import psidev.psi.mi.jami.model.CvTerm;
 import psidev.psi.mi.jami.model.OntologyTerm;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+
 /**
  * The ontology fetcher based on OBO file
  *
@@ -13,11 +18,37 @@ import psidev.psi.mi.jami.model.OntologyTerm;
  */
 
 public class OboOntologyTermFetcher extends OboFetcherTemplate<OntologyTerm> implements OntologyTermFetcher{
+    Collection<OntologyTerm> rootTerms=null;
+
     public OboOntologyTermFetcher(CvTerm database, String filePath) {
         super(database, new OntologyOboLoader(database), filePath);
     }
 
     public OboOntologyTermFetcher(String databaseName, String filePath) {
         super(databaseName, new OntologyOboLoader(databaseName), filePath);
+    }
+
+    public Collection<OntologyTerm> getRoots() {
+
+        if ( rootTerms != null ) {
+            return rootTerms;
+        }
+
+        // it wasn't precalculated, then do it here...
+        rootTerms = new HashSet<OntologyTerm>();
+
+        for ( Iterator<OntologyTerm> iterator = getId2Term().values().iterator(); iterator.hasNext(); ) {
+            OntologyTerm ontologyTerm = iterator.next();
+
+            if ( ontologyTerm.getParents().isEmpty() ) {
+                rootTerms.add( ontologyTerm );
+            }
+        }
+
+        if ( rootTerms.isEmpty() ) {
+            return Collections.EMPTY_LIST;
+        }
+
+        return rootTerms;
     }
 }
