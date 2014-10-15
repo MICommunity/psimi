199c7ec6b4f631bb7cd236fd0abdf6e9202d1877
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-10-15 09:15:24 +0000

    Updated fetcher to return list of root terms

--- trunk/psi-jami/jami-bridges/jami-obo/src/main/java/psidev/psi/mi/jami/bridges/obo/OboOntologyTermFetcher.java
+++ trunk/psi-jami/jami-bridges/jami-obo/src/main/java/psidev/psi/mi/jami/bridges/obo/OboOntologyTermFetcher.java
@@ -1,5 +1,6 @@
 package psidev.psi.mi.jami.bridges.obo;
 
+import psidev.psi.mi.jami.bridges.exception.BridgeFailedException;
 import psidev.psi.mi.jami.bridges.fetcher.OntologyTermFetcher;
 import psidev.psi.mi.jami.model.CvTerm;
 import psidev.psi.mi.jami.model.OntologyTerm;
@@ -25,7 +26,7 @@ public class OboOntologyTermFetcher extends OboFetcherTemplate<OntologyTerm> imp
         super(databaseName, new OntologyOboLoader(databaseName), filePath);
     }
 
-    public Set<OntologyTerm> fetchRootTerms(String database) {
+    public Set<OntologyTerm> fetchRootTerms(String database) throws BridgeFailedException {
 
         if (database != null && !getOntologyDatabase().getShortName().equalsIgnoreCase(database)){
             return Collections.EMPTY_SET;
@@ -47,7 +48,7 @@ public class OboOntologyTermFetcher extends OboFetcherTemplate<OntologyTerm> imp
         return terms;
     }
 
-    public Set<OntologyTerm> fetchRootTerms(CvTerm database) {
+    public Set<OntologyTerm> fetchRootTerms(CvTerm database) throws BridgeFailedException{
 
         if (database != null && !DefaultCvTermComparator.areEquals(getOntologyDatabase(), database)){
             return Collections.EMPTY_SET;
