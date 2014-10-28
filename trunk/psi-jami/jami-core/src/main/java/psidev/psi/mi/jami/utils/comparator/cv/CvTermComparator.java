44e4f23d1daab638cd8aabc77c5f5c41d7904c98
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-10-28 10:32:34 +0000

    Updated cv comparator

--- trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/comparator/cv/CvTermComparator.java
+++ trunk/psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/comparator/cv/CvTermComparator.java
@@ -3,28 +3,26 @@ package psidev.psi.mi.jami.utils.comparator.cv;
 import psidev.psi.mi.jami.model.CvTerm;
 import psidev.psi.mi.jami.model.Xref;
 import psidev.psi.mi.jami.utils.comparator.CollectionComparator;
-import psidev.psi.mi.jami.utils.comparator.xref.UnambiguousExternalIdentifierComparator;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
 /**
- * Cv term comparator that only takes into consideration identifiers.
+ * comparator for CvTerms :
+ * If one CvTerm does not have any identifiers, it will only compare the short names (case insensitive).
+ * If both CvTerm objects have identifiers, it will look for exact same collection of identifiers using UnambiguousExternalIdentifierComparator and ignores all the other properties.
  *
- * Will first look at MI identifiers, then MOD identifiers, then PAR identifiers and finally the full collection of identifiers .
- *
- * If both cv terms do not have any identifiers, it will consider them as identical
- *
+ * - Two CvTerms which are null are equals
+ * - The CvTerm which is not null is before null.
+ * - If the two external identifiers are set, use UnambiguousExternalIdentifier comparator
+ * - The CvTerm without an external identifier is after the CvTerm with an identifier (do not compare short names if we have one external identifier)
+ * - When both CvTerms do not have an external identifier, it compares the short names (case insensitive) which cannot be null
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
  * @since <pre>18/12/12</pre>
  */
 
-public class CvTermIdentifierComparator implements Comparator<CvTerm> {
+public class CvTermComparator implements Comparator<CvTerm> {
 
-    private static CvTermIdentifierComparator unambiguousCvTermComparator;
     private Comparator<Xref> identifierComparator;
     private CollectionComparator<Xref> identifierCollectionComparator;
 
@@ -32,23 +30,28 @@ public class CvTermIdentifierComparator implements Comparator<CvTerm> {
      * Creates a new CvTermComparator with UnambiguousExternalIdentifierComparator
      *
      */
-    public CvTermIdentifierComparator() {
-        this.identifierComparator = new UnambiguousExternalIdentifierComparator();
-        this.identifierCollectionComparator = new CollectionComparator<Xref>(this.identifierComparator);
+    public CvTermComparator(Comparator<Xref> identifierComparator) {
+        if (identifierComparator == null){
+           throw new IllegalArgumentException("The identifier comparator cannot be null");
+        }
+        this.identifierComparator = identifierComparator;
+        this.identifierCollectionComparator = new CollectionComparator<Xref>(identifierComparator);
     }
 
-    public CvTermIdentifierComparator(Comparator<Xref> identifierComparator) {
-        this.identifierComparator = identifierComparator != null ? identifierComparator : new UnambiguousExternalIdentifierComparator();
-        this.identifierCollectionComparator = new CollectionComparator<Xref>(this.identifierComparator);
-    }
-
-    public CvTermIdentifierComparator(CollectionComparator<Xref> identifierComparator) {
-        this.identifierComparator = identifierComparator != null ? identifierComparator.getObjectComparator() : new UnambiguousExternalIdentifierComparator();
-        this.identifierCollectionComparator = identifierComparator != null ? identifierComparator : new CollectionComparator<Xref>(this.identifierComparator);
+    public CvTermComparator(CollectionComparator<Xref> identifierComparator) {
+        if (identifierComparator == null){
+            throw new IllegalArgumentException("The identifier comparator cannot be null");
+        }
+        this.identifierComparator = identifierComparator.getObjectComparator();
+        this.identifierCollectionComparator = identifierComparator;
     }
 
     public Comparator<Xref> getIdentifierComparator() {
         return identifierComparator;
+    }
+
+    public CollectionComparator<Xref> getIdentifierCollectionComparator() {
+        return identifierCollectionComparator;
     }
 
     /**
@@ -116,7 +119,7 @@ public class CvTermIdentifierComparator implements Comparator<CvTerm> {
             }
             else if (!cvTerm1.getIdentifiers().isEmpty() && !cvTerm2.getIdentifiers().isEmpty()){
                 // the identifiers must be the same
-                return this.identifierCollectionComparator.compare(cvTerm1.getIdentifiers(), cvTerm2.getIdentifiers());
+                return getIdentifierCollectionComparator().compare(cvTerm1.getIdentifiers(), cvTerm2.getIdentifiers());
             }
             else if (!cvTerm1.getIdentifiers().isEmpty()){
                 return BEFORE;
@@ -125,59 +128,12 @@ public class CvTermIdentifierComparator implements Comparator<CvTerm> {
                 return AFTER;
             }
             else {
+                // check names which cannot be null because we could not compare the identifiers
+                String label1 = cvTerm1.getShortName();
+                String label2 = cvTerm2.getShortName();
 
-                return 0;
+                return label1.toLowerCase().trim().compareTo(label2.toLowerCase().trim());
             }
         }
-    }
-
-    /**
-     * Use UnambiguousCvTermComparator to know if two CvTerms are equals.
-     * @param cv1
-     * @param cv2
-     * @return true if the two CvTerms are equal
-     */
-    public static boolean areEquals(CvTerm cv1, CvTerm cv2){
-        if (unambiguousCvTermComparator == null){
-            unambiguousCvTermComparator = new CvTermIdentifierComparator();
-        }
-
-        return unambiguousCvTermComparator.compare(cv1, cv2) == 0;
-    }
-
-    /**
-     *
-     * @param cv1
-     * @return the hashcode consistent with the equals method for this comparator
-     */
-    public static int hashCode(CvTerm cv1){
-        if (unambiguousCvTermComparator == null){
-            unambiguousCvTermComparator = new CvTermIdentifierComparator();
-        }
-
-        if (cv1 == null){
-            return 0;
-        }
-
-        int hashcode = 31;
-
-        if (cv1.getMIIdentifier() != null){
-            hashcode = 31*hashcode + cv1.getMIIdentifier().hashCode();
-        }
-        else if (cv1.getMODIdentifier() != null){
-            hashcode = 31*hashcode + cv1.getMODIdentifier().hashCode();
-        }
-        else if (cv1.getPARIdentifier() != null){
-            hashcode = 31*hashcode + cv1.getPARIdentifier().hashCode();
-        }
-        else if (!cv1.getIdentifiers().isEmpty()){
-            List<Xref> list1 = new ArrayList<Xref>(cv1.getIdentifiers());
-            Collections.sort(list1, unambiguousCvTermComparator.getIdentifierComparator());
-            for (Xref ref : list1){
-                hashcode = 31*hashcode + UnambiguousExternalIdentifierComparator.hashCode(ref);
-            }
-        }
-
-        return hashcode;
     }
 }
