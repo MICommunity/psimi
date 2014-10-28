010cfd46c7ab684f9e4ee6f9eff01be2c6d189d9
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-10-28 11:27:30 +0000

    Updated other comparators comparator

--- psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/comparator/interaction/UnambiguousInteractionBaseComparator.java
+++ psi-jami/jami-core/src/main/java/psidev/psi/mi/jami/utils/comparator/interaction/UnambiguousInteractionBaseComparator.java
@@ -1,13 +1,13 @@
 package psidev.psi.mi.jami.utils.comparator.interaction;
 
-import psidev.psi.mi.jami.model.CvTerm;
 import psidev.psi.mi.jami.model.Interaction;
 import psidev.psi.mi.jami.model.Xref;
 import psidev.psi.mi.jami.utils.comparator.cv.UnambiguousCvTermComparator;
 import psidev.psi.mi.jami.utils.comparator.xref.UnambiguousExternalIdentifierComparator;
-import psidev.psi.mi.jami.utils.comparator.xref.XrefsCollectionComparator;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Unambiguous Interaction comparator.
@@ -23,109 +23,24 @@ import java.util.*;
  * @since <pre>18/01/13</pre>
  */
 
-public class UnambiguousInteractionBaseComparator implements Comparator<Interaction> {
+public class UnambiguousInteractionBaseComparator extends InteractionBaseComparator {
 
     private static UnambiguousInteractionBaseComparator unambiguousInteractionComparator;
-
-    private XrefsCollectionComparator identifierCollectionComparator;
-    private UnambiguousCvTermComparator cvTermComparator;
-    private UnambiguousExternalIdentifierComparator identifierComparator;
 
     /**
      * Creates a new UnambiguousInteractionBaseComparator. It will use a UnambiguousParticipantBaseComparator to
      * compare participants and UnambiguousCvTermcomparator to compare interaction types
      */
     public UnambiguousInteractionBaseComparator() {
-        this.identifierComparator = new UnambiguousExternalIdentifierComparator();
-        this.cvTermComparator = new UnambiguousCvTermComparator();
-        this.identifierCollectionComparator = new XrefsCollectionComparator(getIdentifierComparator());
+        super( new UnambiguousExternalIdentifierComparator(),new UnambiguousCvTermComparator());
     }
 
     public UnambiguousCvTermComparator getCvTermComparator() {
-        return cvTermComparator;
-    }
-
-    public XrefsCollectionComparator getIdentifierCollectionComparator() {
-        return identifierCollectionComparator;
+        return (UnambiguousCvTermComparator) super.getCvTermComparator();
     }
 
     public UnambiguousExternalIdentifierComparator getIdentifierComparator() {
-        return this.identifierComparator;
-    }
-
-    /**
-     * It will first compare the interaction types using UnambiguousCvTermComparator.
-     * Then it will compare the rigids (case sensitive, null rigids always come after).
-     * Then it will compare the identifiers using UnambiguousExternalIdentifierComparator.
-     * If the interactions do not have any identifiers, it will compare the shortnames (case sensitive, shortname null comes always after)
-     *
-     *
-     */
-    public int compare(Interaction interaction1, Interaction interaction2) {
-        int EQUAL = 0;
-        int BEFORE = -1;
-        int AFTER = 1;
-
-        if (interaction1 == interaction2){
-            return 0;
-        }
-        else if (interaction1 == null){
-            return AFTER;
-        }
-        else if (interaction2 == null){
-            return BEFORE;
-        }
-        else {
-
-            // First compares interaction type
-            CvTerm type1 = interaction1.getInteractionType();
-            CvTerm type2 = interaction2.getInteractionType();
-
-            int comp = cvTermComparator.compare(type1, type2);
-            if (comp != 0){
-                return comp;
-            }
-
-            // then compares rigid if both are set
-            String rigid1 = interaction1.getRigid();
-            String rigid2 = interaction2.getRigid();
-            if (rigid1 != null && rigid2 != null){
-                comp = rigid1.compareTo(rigid2);
-                if (comp != 0){
-                    return comp;
-                }
-            }
-            else if (rigid1 != null){
-                return BEFORE;
-            }
-            else if (rigid2 != null){
-                return AFTER;
-            }
-
-            // then compares identifiers if both are set
-            if (!interaction1.getIdentifiers().isEmpty() || !interaction2.getIdentifiers().isEmpty()){
-               Collection<Xref> identifiers1 = interaction1.getIdentifiers();
-               Collection<Xref> identifiers2 = interaction2.getIdentifiers();
-
-                return identifierCollectionComparator.compare(identifiers1, identifiers2);
-            }
-            else {
-                // then compares shortnames if both are set
-                String shortname1 = interaction1.getShortName();
-                String shortname2 = interaction2.getShortName();
-                if (shortname1 != null && shortname2 != null){
-                    return shortname1.compareTo(shortname2);
-                }
-                else if (shortname1 != null) {
-                    return BEFORE;
-                }
-                else if (shortname2 != null){
-                    return AFTER;
-                }
-            }
-
-            return comp;
-        }
+        return (UnambiguousExternalIdentifierComparator)super.getIdentifierComparator();
     }
 
     /**
