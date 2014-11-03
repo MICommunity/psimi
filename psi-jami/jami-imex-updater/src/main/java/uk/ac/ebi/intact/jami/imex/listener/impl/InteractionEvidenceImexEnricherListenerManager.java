4dad7875c0518fd8942a8af6192770a72f1b272d
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-11-03 11:52:18 +0000

    Added listeners

---
+++ psi-jami/jami-imex-updater/src/main/java/uk/ac/ebi/intact/jami/imex/listener/impl/InteractionEvidenceImexEnricherListenerManager.java
@@ -0,0 +1,53 @@
+package uk.ac.ebi.intact.jami.imex.listener.impl;
+
+import psidev.psi.mi.jami.enricher.listener.InteractionEnricherListener;
+import psidev.psi.mi.jami.enricher.listener.impl.InteractionEvidenceEnricherListenerManager;
+import psidev.psi.mi.jami.model.InteractionEvidence;
+import psidev.psi.mi.jami.model.Xref;
+import uk.ac.ebi.intact.jami.imex.listener.InteractionImexEnricherListener;
+
+import java.util.Collection;
+
+/**
+ * A manager for listeners which holds a list of listeners.
+ * Listener manager allows enrichers to send events to multiple listeners.
+ * A listener itself, it implements all methods
+ * which will then fire the corresponding method in each entry of the listener list.
+ * No promise can be given to the order in which the listeners are fired.
+ *
+ */
+public class InteractionEvidenceImexEnricherListenerManager
+        extends InteractionEvidenceEnricherListenerManager
+        implements InteractionImexEnricherListener {
+
+    /**
+     * A constructor to create a listener manager with no listeners.
+     */
+    public InteractionEvidenceImexEnricherListenerManager(){ }
+
+    /**
+     * A constructor to initiate a listener manager with as many listeners as required.
+     * @param listeners     The listeners to add.
+     */
+    public InteractionEvidenceImexEnricherListenerManager(InteractionImexEnricherListener... listeners){
+        super(listeners);
+    }
+
+    public void onImexIdConflicts(InteractionEvidence originalInteraction, Collection<Xref> conflictingXrefs) {
+        for (InteractionEnricherListener listener : getListenersList()){
+            if (listener instanceof InteractionImexEnricherListener){
+                ((InteractionImexEnricherListener) listener).onImexIdConflicts(originalInteraction, conflictingXrefs);
+            }
+        }
+    }
+
+    public void onImexIdAssigned(InteractionEvidence interaction, String imex) {
+        for (InteractionEnricherListener listener : getListenersList()){
+            if (listener instanceof InteractionImexEnricherListener){
+                ((InteractionImexEnricherListener) listener).onImexIdAssigned(interaction, imex);
+            }
+        }
+    }
+
+    //============================================================================================
+}
---
+++ psi-jami/jami-imex-updater/src/main/java/uk/ac/ebi/intact/jami/imex/listener/impl/PublicationImexEnricherStatisticsWriter.java
@@ -0,0 +1,114 @@
+package uk.ac.ebi.intact.jami.imex.listener.impl;
+
+import psidev.psi.mi.jami.enricher.listener.impl.writer.PublicationEnricherStatisticsWriter;
+import psidev.psi.mi.jami.model.CurationDepth;
+import psidev.psi.mi.jami.model.Publication;
+import psidev.psi.mi.jami.model.Source;
+import psidev.psi.mi.jami.model.Xref;
+import uk.ac.ebi.intact.jami.bridges.imex.PublicationStatus;
+import uk.ac.ebi.intact.jami.imex.listener.PublicationImexEnricherListener;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+/**
+ * A statistics logger which records changes made by the enricher.
+ * Each addition, removal or update is counted and, upon the completion of the enrichment of the object,
+ * is logged in either a file of successes or failures depending on the enrichmentStatus.
+ *
+ */
+public class PublicationImexEnricherStatisticsWriter
+        extends PublicationEnricherStatisticsWriter implements PublicationImexEnricherListener{
+
+    /**
+     * Uses the known name of the JamiObject type as the seed to generate names for the success an failure log files.
+     * @throws java.io.IOException      Thrown if a problem is encountered with file location.
+     */
+    public PublicationImexEnricherStatisticsWriter() throws IOException {
+        super();
+    }
+
+    /**
+     * Creates the files from the provided seed file name with 'success' and 'failure' appended.
+     * @param fileName          The seed to base the names of the files on.
+     * @throws java.io.IOException      Thrown if a problem is encountered with file location.
+     */
+    public PublicationImexEnricherStatisticsWriter(String fileName) throws IOException {
+        super(fileName);
+    }
+
+    /**
+     * Uses the provided names to create the files for successful and failed enrichment logging.
+     * @param successFileName   The exact name for the file to log successful enrichments in
+     * @param failureFileName   The exact name for the file to log failed enrichments in
+     * @throws java.io.IOException      Thrown if a problem is encountered with file location.
+     */
+    public PublicationImexEnricherStatisticsWriter(String successFileName, String failureFileName) throws IOException {
+        super(successFileName, failureFileName);
+    }
+
+    /**
+     * Uses the exact files provided to log successful and failed enrichments.
+     * @param successFile       The file to log successful enrichments in
+     * @param failureFile       The file to log failed enrichments in.
+     * @throws java.io.IOException      Thrown if a problem is encountered with file location.
+     */
+    public PublicationImexEnricherStatisticsWriter(File successFile, File failureFile) throws IOException {
+        super(successFile, failureFile);
+    }
+
+    public void onImexIdConflicts(Publication originalPublication, Collection<Xref> conflictingXrefs) {
+        super.onEnrichmentError(originalPublication, "The publication "+originalPublication+" has "+conflictingXrefs.size()
+                +" IMEx primary references and only one is allowed.", null);
+    }
+
+    public void onMissingImexId(Publication publication) {
+        super.onEnrichmentError(publication, "The publication "+publication+" does not have any IMEx primary reference and " +
+                "cannot be updated.", null);
+    }
+
+    public void onCurationDepthUpdated(Publication publication, CurationDepth oldDepth) {
+        checkObject(publication);
+        incrementUpdateCount();
+    }
+
+    public void onImexAdminGroupUpdated(Publication publication, Source oldSource) {
+        checkObject(publication);
+        incrementUpdateCount();
+    }
+
+    public void onImexStatusUpdated(Publication publication, PublicationStatus oldStatus) {
+        checkObject(publication);
+        incrementUpdateCount();
+    }
+
+    public void onImexPublicationIdentifierSynchronized(Publication publication) {
+        checkObject(publication);
+        incrementUpdateCount();
+    }
+
+    public void onPublicationAlreadyRegisteredInImexCentral(Publication publication, String imex) {
+        super.onEnrichmentError(publication, "The publication "+publication+" is already registered in IMEx central with IMEx "+imex, null);
+    }
+
+    public void onPublicationRegisteredInImexCentral(Publication publication) {
+        checkObject(publication);
+        incrementUpdateCount();
+    }
+
+    public void onPublicationWhichCannotBeRegistered(Publication publication) {
+        super.onEnrichmentError(publication, "The publication "+publication+" cannot be registered in IMEx central. It does not have a valid pubmed id or " +
+                "it is not eligible for IMEx", null);
+    }
+
+    public void onPublicationNotEligibleForImex(Publication publication) {
+        super.onEnrichmentError(publication, "The publication "+publication+" cannot be registered in IMEx central. It does not have a valid pubmed id or " +
+                "it is not eligible for IMEx", null);
+    }
+
+    public void onImexIdAssigned(Publication publication, String imex) {
+        checkObject(publication);
+        incrementUpdateCount();
+    }
+}
