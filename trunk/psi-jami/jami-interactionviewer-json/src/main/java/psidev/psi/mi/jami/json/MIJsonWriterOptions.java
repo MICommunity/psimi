2b50dff519e673ef1fc5f3c49c72dccbeeaf263a
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-11-19 11:26:06 +0000

    Added method to register existing writers to writer factory

--- trunk/psi-jami/jami-interactionviewer-json/src/main/java/psidev/psi/mi/jami/json/MIJsonWriterOptions.java
+++ trunk/psi-jami/jami-interactionviewer-json/src/main/java/psidev/psi/mi/jami/json/MIJsonWriterOptions.java
@@ -3,8 +3,8 @@ package psidev.psi.mi.jami.json;
 import psidev.psi.mi.jami.factory.options.InteractionWriterOptions;
 
 /**
- * Class that lists all possible options for MIJson writers.
- * All options listed in InteractionWriterOptions are also valid for MIJson writers.
+ * Class that lists all possible options for InteractionViewerJson writers.
+ * All options listed in InteractionWriterOptions are also valid for InteractionViewerJson writers.
  *
  * @author Marine Dumousseau (marine@ebi.ac.uk)
  * @version $Id$
@@ -18,5 +18,15 @@ public class MIJsonWriterOptions extends InteractionWriterOptions {
      * This object can fetch ontology terms,
      */
     public static final String ONTOLOGY_FETCHER_OPTION_KEY = "ontology_fetcher_key";
+
+    /**
+     * The json type. It should be an enum of type MIJsonType. If it is not provided, it will be MIJsonType.n_ary_only
+     */
+    public static final String MI_JSON_TYPE = "interaction_viewer_json";
+
+    /**
+     * The json format value to use with MIJsonWriterOptions.OUTPUT_FORMAT_OPTION_KEY
+     */
+    public static final String MI_JSON_FORMAT = "interaction_viewer_json";
 
 }
