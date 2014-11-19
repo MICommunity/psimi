e26d342349f9d2b028c038a3eef64b78e168a27b
mdumousseau@yahoo.com <mdumousseau@yahoo.com@73e66818-4ebf-11de-a6a3-df26d2c71dbe>
2014-11-19 10:55:08 +0000

    Added option factory for html writers

--- trunk/psi-jami/jami-html/src/main/java/psidev/psi/mi/jami/html/utils/HtmlWriterUtils.java
+++ trunk/psi-jami/jami-html/src/main/java/psidev/psi/mi/jami/html/utils/HtmlWriterUtils.java
@@ -2,17 +2,6 @@ package psidev.psi.mi.jami.html.utils;
 
 import psidev.psi.mi.jami.datasource.FileSourceContext;
 import psidev.psi.mi.jami.datasource.FileSourceLocator;
-import psidev.psi.mi.jami.factory.InteractionWriterFactory;
-import psidev.psi.mi.jami.factory.options.InteractionWriterOptions;
-import psidev.psi.mi.jami.html.LightMIHtmlWriter;
-import psidev.psi.mi.jami.html.MIEvidenceHtmlWriter;
-import psidev.psi.mi.jami.html.MIHtmlWriter;
-import psidev.psi.mi.jami.html.MIModelledHtmlWriter;
-import psidev.psi.mi.jami.model.ComplexType;
-import psidev.psi.mi.jami.model.InteractionCategory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Utility class for writing molecular interactions in html
@@ -43,29 +32,5 @@ public class HtmlWriterUtils {
             return Integer.toString(object.hashCode());
         }
         return null;
-    }
-
-    public static void initialiseWriterFactoryWithMIHtmlWriters(){
-        InteractionWriterFactory writerFactory = InteractionWriterFactory.getInstance();
-
-        Map<String, Object> supportedOptions1 = createMIHtmlWriterOptions(InteractionCategory.evidence, ComplexType.n_ary);
-        writerFactory.registerDataSourceWriter(MIEvidenceHtmlWriter.class, supportedOptions1);
-        Map<String, Object> supportedOptions2 = createMIHtmlWriterOptions(InteractionCategory.modelled, ComplexType.n_ary);
-        writerFactory.registerDataSourceWriter(MIModelledHtmlWriter.class, supportedOptions2);
-        Map<String, Object> supportedOptions3 = createMIHtmlWriterOptions(InteractionCategory.mixed, ComplexType.n_ary);
-        writerFactory.registerDataSourceWriter(MIHtmlWriter.class, supportedOptions3);
-        Map<String, Object> supportedOptions4 = createMIHtmlWriterOptions(InteractionCategory.basic, ComplexType.n_ary);
-        writerFactory.registerDataSourceWriter(LightMIHtmlWriter.class, supportedOptions4);
-    }
-
-    private static Map<String, Object> createMIHtmlWriterOptions(InteractionCategory interactionCategory, ComplexType complexType) {
-        Map<String, Object> supportedOptions4 = new HashMap<String, Object>(9);
-        supportedOptions4.put(InteractionWriterOptions.OUTPUT_FORMAT_OPTION_KEY, "miHtml");
-        supportedOptions4.put(InteractionWriterOptions.INTERACTION_CATEGORY_OPTION_KEY, interactionCategory);
-        supportedOptions4.put(InteractionWriterOptions.COMPLEX_TYPE_OPTION_KEY, complexType);
-        supportedOptions4.put(InteractionWriterOptions.COMPLEX_EXPANSION_OPTION_KEY, null);
-        supportedOptions4.put(HtmlWriterOptions.WRITE_HTML_HEADER_BODY_OPTION, null);
-        supportedOptions4.put(InteractionWriterOptions.OUTPUT_OPTION_KEY, null);
-        return supportedOptions4;
     }
 }
